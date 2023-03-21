bdde9473939a459f245cc8cbff541c19ee641c7a
bbbii <hyeongjin1326@gmail.com>
2023-03-21 09:48:18 +0900

    add: [김형진] 한 번 더 출력 [JAVA-2]

--- Java/HyeongJinKim/java/java/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    // psvm
-    public static void main(String[] args) {
-        // sout
-        System.out.println("안녕 자바!");
-        // 2023/03/21 추가
-        System.out.println("변경 내역 추가");
-    }
-}
