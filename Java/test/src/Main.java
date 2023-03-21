4275ce5ad512310a2bafe1430d6141969d98f671
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-03-21 09:35:19 +0900

    add: [윤주아] 한 번 더 출력[JAVA-2]

--- Java/test/src/Main.java
+++ Java/test/src/Main.java
@@ -2,7 +2,9 @@ public class Main {
 
     // psvm
     public static void main(String[] args) {
-        // sout
+        // sout --2023/03/20
         System.out.println("자바 안녕 !");
+        //2023/03/21 추가
+        System.out.println("변경 내용 추가");
     }
 }
