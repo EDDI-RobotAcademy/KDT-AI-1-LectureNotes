652f53d52e51156d13d4d0c30833a7b4c950f4ab
Jinsun Kim <unchlo27@gmail.com>
2023-03-22 08:33:05 +0900

    feat: [김진선] 주사위게임 주석 추가 [HOMEWORKx]

--- Java/JinsunKim/Java/java/src/dice/dice.java
+++ Java/JinsunKim/Java/java/src/dice/dice.java
@@ -3,24 +3,23 @@ package dice;
 public class dice {
     public static void main(String[] args) {
 
+        //주사위 개수
         final int NUM = 2;
-
+        //주사위 합
         int sum = 0;
 
+        //랜덤뽑기 , 총 합
         for (int i = 0; i < NUM; i++) {
             int randomNumber = (int)(Math.random() * 6) +1;
             sum += randomNumber;
         }
-        System.out.println(sum);
+        System.out.println("합계 = " + sum);
 
-
+        //결과
         if (sum % 4 == 0) {
             System.out.println("승리");
         } else {
             System.out.println("패배");
         }
-
     }
-
-
 }
