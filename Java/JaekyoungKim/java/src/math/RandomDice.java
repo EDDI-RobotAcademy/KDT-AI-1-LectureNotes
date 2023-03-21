6d3f97c4603533bea816516f7cea1f0e14f89d24
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-03-21 15:18:28 +0900

    feat [김재경] 주사위 게임 [RANDOM]

---
+++ Java/JaekyoungKim/java/src/math/RandomDice.java
@@ -0,0 +1,40 @@
+package math;
+
+public class RandomDice {
+    public static void main(String[] args) {
+        final int START=0;
+        final int END=10;
+
+        final int MAX =6;
+        final int MIN =1;
+        int randomDice1 =0;
+        int randomDice2 =0;
+
+
+
+       for (int i=START;i<END;i++){
+            randomDice1= (int)(Math.random() * (MAX - MIN + 1)) + MIN;
+
+        }
+        for (int i=START;i<END;i++){
+            randomDice2= (int)(Math.random() * (MAX - MIN + 1)) + MIN;
+
+        }
+        int diceSum=(randomDice1+randomDice2);
+        int diceDivison=(randomDice1+randomDice2)%4;
+
+        switch (diceDivison){
+            case 0:
+                System.out.println("Dice 1의 값은 "+randomDice1+" Dice 2의 값은 "+randomDice2+"로 합한 숫자 "+diceSum+"는 4의 배수이므로 승리입니다.");
+                break;
+            default:
+                System.out.println("Dice 1의 값은 "+randomDice1+" Dice 2의 값은 "+randomDice2+"로 합한 숫자 "+diceSum+"는 4의 배수가 아니므로 패배입니다.");
+                break;
+        }
+
+
+    }
+
+
+
+}
