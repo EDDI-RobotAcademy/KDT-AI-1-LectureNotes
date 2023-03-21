71968407213616e45da4f48085f7c4e581f5c39d
유형래 <hyunglae1214@gmail.com>
2023-03-21 10:38:19 +0900

    add: [유형래] 한 번 더 출력[JAVA-2]

--- Java/hyunglaeyoo/Java/src/Main.java
+++ Java/hyunglaeyoo/Java/src/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
         System.out.println("안녕 자바!");
         //2023/03/21 추가
-        System.out.println("변경 내역 추가")
+        System.out.println("변경 내역 추가");
+        System.out.println("테스트");
     }
 
 
