48e6b0d363bfc107b49d77a4eaa2f074f7204843
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-03-21 12:43:47 +0900

    feat: [김진호] switch test [SWITCHx]

---
+++ Java/JinHoKim/java/untitled/src/flowControl/SwitchTest.java
@@ -0,0 +1,39 @@
+package flowControl;
+
+import java.util.Scanner;
+
+public class SwitchTest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        boolean isLoop = true;
+
+        while (isLoop) {
+            System.out.println("숫자를 입력하세요: ");
+            int inputNumber = scan.nextInt();
+            switch (inputNumber) {
+                case 0:
+                    System.out.println("종료!");
+                    isLoop = false;
+                    break;
+
+                case 1:
+                    System.out.println("입금!");
+                    break;
+
+                case 2:
+                    System.out.println("출금!");
+                    break;
+
+                case 3:
+                    System.out.println("조회");
+                    break;
+
+                default:
+                    System.out.println("그런 명령은 존재하지 않습니다!");
+                    break;
+            }
+        }
+    }
+}
