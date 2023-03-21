8d87811e532efce43e74ccd3e533e341f19565f3
zisoo <zisoo_3@naver.com>
2023-03-21 09:35:15 +0900

    add: [최지수] 한 번 더 출력 [JAVA-2]

--- Java/JisooChoi/java/test/src/Main.java
+++ Java/JisooChoi/java/test/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     // psvm
     public static void main(String[] args) {
-        // sout
+        // sout --- 2023/03/20
         System.out.println("안녕 자바!");
+        // 2023/03/20 추가
+        System.out.println("변경 내역 추가");
     }
 }
