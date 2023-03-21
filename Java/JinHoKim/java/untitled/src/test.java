27a360f7da6a770671ba830bd5f47cc3d6475196
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-03-21 09:35:16 +0900

    add: [r김진호] 한 번 더 출력 [JAVA-2]]

--- Java/JinHoKim/java/untitled/src/test.java
+++ Java/JinHoKim/java/untitled/src/test.java
@@ -1,5 +1,9 @@
 public class test {
     public static void main(String[] args) {
+        // sout --- 2023/03/20
         System.out.println("Hello world");
+        // 2023/03/21 추가
+        System.out.println("변경 내역 추가");
+
     }
 }
