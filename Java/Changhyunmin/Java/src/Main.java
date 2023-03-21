6aa4a59d7f7f897d93abc0614b754fbb836c6018
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-03-21 09:35:39 +0900

    add: [장현민]한 번 더 출력 [JAVA-2]

--- Java/Changhyunmin/Java/src/Main.java
+++ Java/Changhyunmin/Java/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("안녕자바!"); // sout
+    //psvm
+    public static void main(String[]    args) {
+        // sout
+        System.out.println("안녕자바!");
+        //2023/03/21 추가
+        System.out.println("변경 내역 추가");
     }
 }
