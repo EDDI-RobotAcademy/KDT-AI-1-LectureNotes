a97028779a7b98529738c4302d73d1046f1b5758
hwansik <hwansik08@gmail.com>
2023-03-21 09:50:45 +0900

    feat: [민환식] 저장소 초기화 [JAVA-2]

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+    // psvm
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // sout --- 2023/03/20
+        System.out.println("안녕 자바");\
+        // 2023/03/21 추가
+        System.out.println("변경 내역 추가")
     }
-}
+}   
