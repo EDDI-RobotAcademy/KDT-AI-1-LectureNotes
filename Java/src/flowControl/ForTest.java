5687fc3cbc163c8890a674dec3fb093c2acbcaf9
hwansik <hwansik08@gmail.com>
2023-03-21 14:25:29 +0900

    feat: [민환식] for 테스트 [FORx]

--- Java/src/flowControl/ForTest.java
+++ Java/src/flowControl/ForTest.java
@@ -15,23 +15,25 @@ public class ForTest {
     //      조건을 만족하는 동안 for문이 반복됩니다.
     //    - 증감의 경우에도 없어도 됩니다.
     //      표현을 조금 더 예쁘게 만들어주기 위해 증감파트가 존재한다 봐도 무방합니다.
+    // 3. 중괄호 내에는 for문을 반복하며 작업할 내용을 적어줍니다.
 
     //  중요한 것은 어찌되었든 for문은
     //  조건 파트가 참인 동안은 언제든지 반복된다는 것입니다.
     //  초기화나 증감파트는 곁다리란 것이죠.
     for (int idx = START; idx < END; idx++){
-        System.out.println("idx =" + idx);
+        System.out.println("idx = " + idx);
     }
 
     /*
     for (int idx = START; idx < END; idx++){
-        System.out.println("idx =" + idx);
+        System.out.println("idx = " + idx);
      */
 
 
     //   루프를 돌면서 덧셈을 하려면 반드시 아래와 같이
     //   외부에 0으로 초기화된 변수를 가지고 누산해야 합니다.
     int sum = 0;
+    int count = 0;
 
     for (int idx = START; idx <= END; idx++){
         // sum += idx;
@@ -52,7 +54,7 @@ public class ForTest {
             // '%' 연산자는 나머지 연산자입니다.
             // (mod 2)와 동의어이며 이건 몰라도 됩니다.
 
-            //현재 i 값을 2로 나눈 나머지가 0이냐를 묻는 것이고
+            // 현재 i 값을 2로 나눈 나머지가 0이냐를 묻는 것이고
             // 2로 나눈 나머지가 0이란 것은 짝수임을 의미합니다.
             // 고로 이 로직은 3 ~ 10 사이의 홀수만 출력합니다.
             if (i % 2 == 0) {continue;}     // continue는 skip과 동일합니다.
