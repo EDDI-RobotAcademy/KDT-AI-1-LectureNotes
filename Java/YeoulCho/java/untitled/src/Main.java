a6d1c34ae4fc3135273fc4c98e08cd22921720d9
YeoulCho <cyu5733@gmail.com>
2023-03-21 10:40:00 +0900

    add: [조여울] 새로운 내용 추가 [JAVA-3]

--- Java/YeoulCho/java/untitled/src/Main.java
+++ Java/YeoulCho/java/untitled/src/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("안녕자바!");
         System.out.println("변경 내역 추가");
-
-    }
+        System.out.println("내맘대로 추가");}
 }
