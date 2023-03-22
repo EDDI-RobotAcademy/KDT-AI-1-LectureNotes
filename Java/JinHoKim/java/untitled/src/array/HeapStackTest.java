a9fcb3ae80fbbfbe40839c3f88b6f837d15a2e11
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-03-22 11:47:00 +0900

    add: [김진호] Heap에 배열 할당하기 [HEAP-ARRAYx]

--- Java/JinHoKim/java/untitled/src/array/HeapStackTest.java
+++ Java/JinHoKim/java/untitled/src/array/HeapStackTest.java
@@ -7,9 +7,16 @@ public class HeapStackTest {
 
         /*
         heap에 할당된 메모리 변경 금지를 요청한 것이고 내부에 배치하는 것에는 영향을 받지 않기 때문임
+
          */
 
         final int[] numberArray = new int[ALLOC_ARRAY_NUMBER];
+        /*
+        아래와 같이 새로운 메모리를 할당해서 전달하는 것을 막고 있습니다
+        조금 풀어보자면 객체를 상수화 하느냐 개체 내부의 값을 상수화 하느냐의 관점으로 봐야합니다
+        현재 관점은 객체를 상수화하였기 때문에 다른 객체 대입이 막히는 모습입니다.
+        numberArray = new int[8];
+         */
 
         for (int i = START; i < ALLOC_ARRAY_NUMBER; i++) {
             numberArray[i] = i + 1;
@@ -26,14 +33,10 @@ public class HeapStackTest {
             두 번째는 ', numberArray[i]' 가 대응해서 numberArray[i]에 해당하는 배열값이 %d를 대체합니다
              */
             System.out.printf("numberArray[%d] = %d\n", i, numberArray[i]);
-            System.out.println("numberArray[" + i + "] = " + numberArray[i]);
+            //System.out.println("numberArray[" + i + "] = " + numberArray[i]);
         }
-        /*
-        아래와 같이 새로운 메모리를 할당해서 전달하는 것을 막고 있습니다
-        조금 풀어보자면 객체를 상수화 하느냐 개체 내부의 값을 상수화 하느냐의 관점으로 봐야합니다
-        현재 관점은 객체를 상수화하였기 때문에 다른 객체 대입이 막히는 모습입니다.
-        numberArray = new int[8];
-         */
+        System.out.println();
+
         for (final int num : numberArray) {  // foreach에서는 final이 가능하지만 for(;;)에서는 불가능하다
             System.out.println("numberArray elem: " + num);
         }
