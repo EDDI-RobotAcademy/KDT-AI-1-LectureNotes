2ccd5fc326be1bfd4a5368f2ad78886ee83e8f95
bbbii <hyeongjin1326@gmail.com>
2023-03-22 10:31:03 +0900

    update: [김형진] (수정)주사위 문제 [JAVA-4]

--- Java/HyeongJinKim/java/src/math/Dice.java
+++ Java/HyeongJinKim/java/src/math/Dice.java
@@ -2,16 +2,21 @@ package math;
 
 public class Dice {
     public static void main(String[] args) {
-        int round = 2;
+        final int MIN = 1;
+        final int MAX = 6;
+        final int FOR_WIN = 4;
+
+        final int ROUND = 2;
         final int DICE = 2;
-        for (int i = 0; i < round; i++){
+
+        for (int i = 0; i < ROUND; i++){
             int result = 0;
             for (int j = 0; j < DICE; j++) {
-                int diceNumber = (int) (Math.random() * 6) + 1;
-                result += diceNumber;
+                final int DICE_NUMBER = (int) (Math.random() * (MAX - MIN + 1)) + MIN;
+                result += DICE_NUMBER;
             }
             System.out.println(i + 1 + "라운드 결과: 주사위 " + DICE + "개의 합 = " + result);
-            if (result % 4 == 0) {
+            if (result % FOR_WIN == 0) {
                 System.out.println("승리");
             }
             else {
