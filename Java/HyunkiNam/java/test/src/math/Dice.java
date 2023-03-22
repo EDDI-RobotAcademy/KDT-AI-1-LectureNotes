6814880b201bac2fa0f9d0779d8138025851b8c4
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-03-22 10:27:27 +0900

    update: [남현기] 주사위 문제 수정 [RANDOMx]

--- Java/HyunkiNam/java/test/src/math/Dice.java
+++ Java/HyunkiNam/java/test/src/math/Dice.java
@@ -8,11 +8,13 @@ public class Dice {
         final int randomValue1 = (int)(Math.random() * (MAX-MIN+1)) + MIN;
         final int randomValue2 = (int)(Math.random() * (MAX-MIN+1)) + MIN;
 
-        int sum = randomValue1 + randomValue2;
+        final int sum = randomValue1 + randomValue2; // final 추가
+
+        final int winner_condition = 4; // 승리 조건 상수화
 
         System.out.println("주사위 1의 값 : " + randomValue1
                 + ", 주사위 2의 값 : "+randomValue2+", 합 : "+sum);
-        if(sum % 4 == 0)
+        if(sum % winner_condition == 0) // 4 -> winner_condition으로 변경
             System.out.println(sum+"은(는) 4의 배수 이므로 승리!");
         else
             System.out.println(sum+"은(는) 4의 배수가 아니므로 패배..");
