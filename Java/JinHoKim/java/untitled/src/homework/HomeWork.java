d4d68b9075cea1a6d79a18dd6aed029d21f08d8a
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-03-22 09:48:42 +0900

    add: [김진호] 랜덤 주사위 [HomeWork]

--- Java/JinHoKim/java/untitled/src/homework/HomeWork.java
+++ Java/JinHoKim/java/untitled/src/homework/HomeWork.java
@@ -4,11 +4,12 @@ public class HomeWork {
     public static void main(String[] args) {
         final int Dice1 = (int)(Math.random() * 6) + 1;
         final int Dice2 = (int)(Math.random() * 6) + 1;
-        int randomValue = 0;
+
 
         System.out.println("Dice1 + Dice2 = " + (Dice1 + Dice2));
 
         int SUM = Dice1 + Dice2;
+
         if (SUM % 4 == 0) {
             System.out.println("승리");
         } else {
