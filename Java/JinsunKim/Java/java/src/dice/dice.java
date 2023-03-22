57a2432a62e0cb92f092ea9b7ac45526c84c02a2
Jinsun Kim <unchlo27@gmail.com>
2023-03-22 10:29:04 +0900

    update: [김진선] 주사위게임 문제풀이 후 수정 [HOMEWORK-1]

--- Java/JinsunKim/Java/java/src/dice/dice.java
+++ Java/JinsunKim/Java/java/src/dice/dice.java
@@ -3,20 +3,24 @@ package dice;
 public class dice {
     public static void main(String[] args) {
 
-        //주사위 개수
-        final int NUM = 2;
-        //주사위 합
-        int sum = 0;
+        final int MAX = 6;
+        final int MIN = 1;
+        final int DICE_NUMBER = 2;
 
-        //랜덤뽑기 , 총 합
-        for (int i = 0; i < NUM; i++) {
-            int randomNumber = (int)(Math.random() * 6) +1;
-            sum += randomNumber;
+        int diceNumberSum = 0;
+
+        for (int idx = 0; idx < DICE_NUMBER; idx++) {
+            final int diceNumber = (int)(Math.random() * (MAX - MIN +1)) +MIN;
+            diceNumberSum += diceNumber;
+            System.out.println("idx: " + idx + ", diceNumber: " + diceNumber);
         }
-        System.out.println("합계 = " + sum);
 
-        //결과
-        if (sum % 4 == 0) {
+
+        System.out.println("주사위 굴려서 합친값: " + diceNumberSum);
+
+        final int WINNER_DECISION_NUMBER = 4;
+
+        if (diceNumberSum % WINNER_DECISION_NUMBER == 0) {
             System.out.println("승리");
         } else {
             System.out.println("패배");
