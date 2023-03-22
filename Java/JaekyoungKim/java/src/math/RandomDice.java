0071114c2823067f736c4bd9668c964b1c1a3c28
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-03-22 10:25:19 +0900

    feat: [김재경] 주사위 게임[HOMEWORKx]

--- Java/JaekyoungKim/java/src/math/RandomDice.java
+++ Java/JaekyoungKim/java/src/math/RandomDice.java
@@ -5,20 +5,20 @@ public class RandomDice {
 
         final int MAX =6;
         final int MIN =1;
-        final int randomDice1 =(int)(Math.random() * (MAX - MIN + 1)) + MIN;
-        final int randomDice2 =(int)(Math.random() * (MAX - MIN + 1)) + MIN;
-        final int diceSum=(randomDice1+randomDice2);
+        final int RANDOMDICE1 =(int)(Math.random() * (MAX - MIN + 1)) + MIN;
+        final int RANDOMDICE2 =(int)(Math.random() * (MAX - MIN + 1)) + MIN;
+        final int diceSum=(RANDOMDICE1+RANDOMDICE2);
         final int diceDivison=(diceSum)%4;
 
-        System.out.println("주사위 1의 값은 "+randomDice1);
-        System.out.println("주사위 2의 값은 "+randomDice2);
+        System.out.println("주사위 1의 값은 "+RANDOMDICE1);
+        System.out.println("주사위 2의 값은 "+RANDOMDICE2);
 
         switch (diceDivison){
             case 0:
-                System.out.println("Dice 1의 값은 "+randomDice1+" Dice 2의 값은 "+randomDice2+"로 합한 숫자 "+diceSum+"는 4의 배수이므로 승리입니다.");
+                System.out.println("Dice 1의 값은 "+RANDOMDICE1+" Dice 2의 값은 "+RANDOMDICE2+"로 합한 숫자 "+diceSum+"는 4의 배수이므로 승리입니다.");
                 break;
             default:
-                System.out.println("주사위 1의 값은 "+randomDice1+" 주사위 2의 값은 "+randomDice2+"로 합한 숫자 "+diceSum+"는 4의 배수가 아니므로 패배입니다.");
+                System.out.println("주사위 1의 값은 "+RANDOMDICE1+" 주사위 2의 값은 "+RANDOMDICE2+"로 합한 숫자 "+diceSum+"는 4의 배수가 아니므로 패배입니다.");
                 break;
         }
 
