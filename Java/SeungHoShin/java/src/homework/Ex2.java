a13537fd07694a40615aa9a54cb36e849e5b21ee
신승호 <ssh970502@gmail.com>
2023-03-22 11:14:17 +0900

    feat: [신승호] 저장소초기화 [JAVA-1]

--- Java/SeungHoShin/java/src/homework/Ex2.java
+++ Java/SeungHoShin/java/src/homework/Ex2.java
@@ -1,26 +1,40 @@
 package homework;
 
+import java.util.Scanner;
+
 public class Ex2 {
     public static void main(String[] args) {
-
-        int n = 2;
-
+        int n = 5;
         int sum = 0;
+
         for (int i = 0; i < n; i++) {
-            int dice = (int) (Math.random() * 6) + 1;
-            System.out.println("주사위 " + (i+1) + ": " + dice);
-            sum += dice;
+            final int dice_Number = (int) (Math.random() * 6) + 1;
+
+            System.out.println("주사위" + (i + 1) + ":" + dice_Number);
+            sum += dice_Number;
+
+
         }
 
-        System.out.println("주사위 " + n + "개의 합은 " + sum + "입니다.");
+        System.out.println("주사위" + n + "의 합은" + sum);
 
-        if (sum % 4 == 0) {
-            System.out.println("승리하셨습니다.");
+        final int WInner_decision_number=4;
+
+        if (sum % WInner_decision_number == 0) {
+            System.out.println("승리하셨습니다");
         } else {
             System.out.println("패배하셨습니다.");
+
+
         }
-
-
     }
 }
+
+
+
+
+
+
+
+
 
