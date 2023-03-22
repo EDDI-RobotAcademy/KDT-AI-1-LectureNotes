bbc1fb6ce165a89ccc08e886811ec8d8e075d876
zisoo <zisoo_3@naver.com>
2023-03-22 15:18:43 +0900

    feat: [최지수] 주사위 class 과제 만들기 틀

---
+++ Java/JisooChoi/java/test/src/homework/DiceHomwork_class.java
@@ -0,0 +1,22 @@
+package homework;
+
+class Dice{
+    //필드
+    private final int num;
+
+    //생성자
+    public Dice(int num){
+        this.num = num;
+    }
+
+    //메서드
+    public int DiceNum(){
+
+    }
+}
+public class DiceHomwork_class {
+    // 1. 주사위 객체를 만든다.
+    // 2. 주사위는 면 6개를 가지고 있다.
+    // 3. 주사위를 굴려서 합이 3의 배수 혹은 4의 배수이면 승리 !
+    // 4. 외에는 패배 !
+}
