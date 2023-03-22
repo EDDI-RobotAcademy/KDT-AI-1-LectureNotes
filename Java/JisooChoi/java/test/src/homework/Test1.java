44ed8b3e20f368c68c3c50c4ac7c2bd4ddd9c9db
zisoo <zisoo_3@naver.com>
2023-03-22 10:26:31 +0900

    update: [최지수] 주사위 문제 [HOMEWORK]

--- Java/JisooChoi/java/test/src/homework/Test1.java
+++ Java/JisooChoi/java/test/src/homework/Test1.java
@@ -6,18 +6,18 @@ public class Test1 {
         final int MIN = 1;
         final int DICE_DECIDE_VICTORY = 4;
 
-        final int DiceNumber1 = (int)(Math.random() * MAX) +MIN;
-        final int DiceNumber2 = (int)(Math.random() * MAX) +MIN;
+        final int diceNumber1 = (int)(Math.random() * MAX) +MIN;
+        final int diceNumber2 = (int)(Math.random() * MAX) +MIN;
 
-        final int randomDiceSum = DiceNumber1 + DiceNumber2;
+        final int randomDiceSum = diceNumber1 + diceNumber2;
 
         if((randomDiceSum % DICE_DECIDE_VICTORY)== 0){
             System.out.println("결과 : "+randomDiceSum);
-            System.out.println("첫 번째 주사위 값: "+DiceNumber1+", 두 번째 주사위 값: "+DiceNumber2+" => 성공 !");
+            System.out.println("첫 번째 주사위 값: "+diceNumber1+", 두 번째 주사위 값: "+diceNumber2+" => 성공 !");
         }
         else{
             System.out.println("결과 : "+randomDiceSum);
-            System.out.println("첫 번째 주사위 값: "+DiceNumber1+", 두 번째 주사위 값: "+DiceNumber2+" => 실패 !");
+            System.out.println("첫 번째 주사위 값: "+diceNumber1+", 두 번째 주사위 값: "+diceNumber2+" => 실패 !");
         }
     }
 }
