ab5d8a091997ece879a8c71530eff9c20d8b3099
hwansik <hwansik08@gmail.com>
2023-03-22 21:54:38 +0900

    feat: [민환식] 주사위 문제 2 [HOMEWORKx]

---
+++ Java/HwansikMin/src/Assignment/RandomDIce2.java
@@ -0,0 +1,19 @@
+package Assignment;;
+
+public class Game {
+    final int MAX = 6;
+    final int MIN = 1;
+    final int diceNumber = (int)(Math.random() * (MAX - MIN + 1)) + 1;
+}
+
+public class diceNumber {
+    public static void main(String[] args) {
+    final
+    }
+
+}
+
+class Score {
+}
+
+public class
