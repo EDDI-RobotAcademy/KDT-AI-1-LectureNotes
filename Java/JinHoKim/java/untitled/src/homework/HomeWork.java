94420e00d58c7dcc7d2b1d5d9e3fdfb5b0ec9668
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-03-22 15:11:08 +0900

    Update: [김진호] 랜덤 주사위 수정 [RANDOMx]

--- Java/JinHoKim/java/untitled/src/homework/HomeWork.java
+++ Java/JinHoKim/java/untitled/src/homework/HomeWork.java
@@ -2,15 +2,15 @@ package homework;
 
 public class HomeWork {
     public static void main(String[] args) {
-        final int Dice1 = (int)(Math.random() * 6) + 1;
-        final int Dice2 = (int)(Math.random() * 6) + 1;
+        final int diceA = (int)(Math.random() * 6) + 1;
+        final int diceB = (int)(Math.random() * 6) + 1;
 
 
-        System.out.println("Dice1 + Dice2 = " + (Dice1 + Dice2));
+        System.out.println("diceA + diceB = " + (diceA + diceB));
 
-        int SUM = Dice1 + Dice2;
+        int diceSum = diceA + diceB;
 
-        if (SUM % 4 == 0) {
+        if (diceSum % 4 == 0) {
             System.out.println("승리");
         } else {
             System.out.println("패배");
