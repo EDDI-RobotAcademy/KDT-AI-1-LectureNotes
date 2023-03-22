1de0ce8fb9f75d0ac1a512f49b7b8e558b1519d9
yukyungwoo <128335501+yukyungwoo@users.noreply.github.com>
2023-03-22 11:12:43 +0900

    feat: [유경우] Stack에 배열만들기 [STACK-ARRAYx]

--- Java/yukyungwoo/JAVA/src/math/Dice.java
+++ Java/yukyungwoo/JAVA/src/math/Dice.java
@@ -8,16 +8,23 @@ public class Dice {
         final int randomDice2 = (int) (Math.random() * 6 ) + 1;
         System.out.println("randomDice2 = " + randomDice2);
 
-        int sumDice = (randomDice1 + randomDice2);
+        final int sumDice = (randomDice1 + randomDice2);
         System.out.println("sumDice = " + sumDice);
 
-        if (sumDice % 4 == 0) {
+        final int WINNER_DECISION_NUMBER = 4;
+
+        if (sumDice % WINNER_DECISION_NUMBER == 0) {
             System.out.println("승리");
 
-        }else {
-                System.out.println("패배");
+        } else {System.out.println("패배");
             }
 
+        if (sumDice % WINNER_DECISION_NUMBER == 0) {
+            System.out.println("승리");
+            }
+        if (sumDice % WINNER_DECISION_NUMBER != 0) {
+            System.out.println("패배");
+            }
 
 
 
