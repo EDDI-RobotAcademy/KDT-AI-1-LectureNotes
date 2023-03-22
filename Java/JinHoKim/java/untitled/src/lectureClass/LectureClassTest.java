f7c6f5231ff79583dec9e42311c2e3aaf7740e61
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-03-22 13:49:21 +0900

    update: [김진호] 내용추가 [CLASS-BASICx]

--- Java/JinHoKim/java/untitled/src/lectureClass/LectureClassTest.java
+++ Java/JinHoKim/java/untitled/src/lectureClass/LectureClassTest.java
@@ -95,11 +95,16 @@ public class LectureClassTest {
          */
         //생성자 호출하는 부분[ new Led() ]
         System.out.println("생성자 호출 전");
-        final Led led = new Led();
+        /*
+        왜 데이터 타입을 적는 곳에 class 이름이 오는 것이지?
+        A: 클래스라는 것 자체가 커스텀 데이터 타입이기 때문입니다
+        내가 직접 커스텀 할 수 있는 데이터 타입이 클래스라 보면 됩니다
+         */
+        final Led led = new Led();      // 초기 생성(꺼짐 - isTurnOn: false)
         System.out.println("생성자 호출 후");
 
         System.out.println("현재 전구 상태: " + (led.getTurnOn() ? "켜짐" : "꺼짐"));
-        led.setTurnOn(true);
+        led.setTurnOn(true);            // 상태 변경(켜짐 - isTurnOn: true)
         System.out.println("현재 전구 상태: " + (led.getTurnOn() ? "켜짐" : "꺼짐"));
     }
 }
