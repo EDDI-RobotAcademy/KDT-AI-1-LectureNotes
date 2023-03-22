753940db8c9ef0faa11487b58f84c6228abdf21a
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-03-22 14:31:33 +0900

    feat: [유경우] 클래스 기본 문제 1 [PERSONx]

---
+++ Java/yukyungwoo/JAVA/src/lectureClass/PersonClass.java
@@ -0,0 +1,29 @@
+package lectureClass;
+
+class Person {
+    private int isAge
+    public Person() {
+        this.isAge = 100;
+    }
+
+    public int getAge () {
+        return isAge;
+
+    }
+
+    private String isName
+    public Person () {
+        this.isName = 홍길동;
+
+    }
+    public String getName () {
+        return .isName;
+    }
+}
+
+public class PersonClass {
+    public static void main(String[] args) {
+
+    }
+}
+
