d90a173ed307dfc09adfd5cc77cb2b38cf89f038
jeongdawun <jeongdawun33@gmail.com>
2023-03-22 09:03:19 +0900

    update: [정다운] 주사위 게임 [RANDOM-2]

--- Java/dawunjeong/java/src/dice/Dice.java
+++ Java/dawunjeong/java/src/dice/Dice.java
@@ -2,18 +2,34 @@ package dice;
 
 public class Dice {
     public static void main(String[] args) {
-        final int START = 1;
-        final int END = 6;
-        final int dice1 =  (int)(Math.random() * 6) + 1;
-        final int dice2 =  (int)(Math.random() * 6) + 1;
 
-        int sum = dice1 + dice2;
+        // 1차 코딩
+//        final int START = 1;
+//        final int END = 6;
+//        final int dice1 =  (int)(Math.random() * 6) + 1;
+//        final int dice2 =  (int)(Math.random() * 6) + 1;
+//
+//        int sum = dice1 + dice2;
+//
+//        System.out.println("dice1 = " + dice1);
+//        System.out.println("dice2 = " + dice2);
+//        System.out.println("두 주사위의 합은 " + sum + "입니다.");
 
-        System.out.println("dice1 = " + dice1);
-        System.out.println("dice2 = " + dice2);
-        System.out.println("두 주사위의 합은 " + sum + "입니다.");
+        final int MAX = 6;
+        final int MIN = 1;
+        final int ROLL_NUM = 2;
+        final int multipleNumber = 4;
 
-        if(sum % 4 == 0) {
+        int sum = 0;
+        for(int i =1; i <= ROLL_NUM; i++){
+            int dice =  (int)(Math.random() * MAX) + MIN;
+            sum += dice;
+            System.out.println(i + "번째 주사위의 수 : " + dice);
+        }
+
+        System.out.println("주사위의 합 : " + sum);
+
+        if(sum % multipleNumber == 0) {
         System.out.println("승리입니다!");
         } else {
             System.out.println("패배입니다!");
