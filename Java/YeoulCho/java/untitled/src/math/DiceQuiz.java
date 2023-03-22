c937e7e034f4085298900a54810d254a3f79123f
YeoulCho <cyu5733@gmail.com>
2023-03-22 09:28:58 +0900

    update: [조여울] 주사위게임 수정[RANDOMx]

--- Java/YeoulCho/java/untitled/src/math/DiceQuiz.java
+++ Java/YeoulCho/java/untitled/src/math/DiceQuiz.java
@@ -2,29 +2,21 @@ package math;
 
 public class DiceQuiz {
     public static void main(String[] args) {
-        final int START = 0;
-        final int END = 10;
         final int MAX = 6;
         final int MIN = 1;
 
-        final int randomNumber = (int)(Math.random() * 10) + 1;
+        int dice1 = (int)(Math.random() * 6) + 1;
+        int dice2 = (int)(Math.random() * 6) + 1;
 
-        int randomValue = 0;
-        int dice1=0;
-        int dice2=0;
         int sum_dice = dice1 + dice2;
 
-        for (int i = START; i < END; i++) {
-            dice1 = (int) (Math.random() * (MAX - MIN + 1)) + MIN; //1~6
-            dice2 = (int) (Math.random() * (MAX - MIN + 1)) + MIN;
-            System.out.println("i=" + i + ", dice1 = " + dice1 + ", dice2 = " +dice2);
-        }
-            System.out.println(sum_dice);
+
+        System.out.println(sum_dice);
 
         if(sum_dice %4 == 0) {
-             System.out.println( "승리");
+             System.out.println("4의 배수라면 승리");
         }else {
-             System.out.println("패배");
+             System.out.println("4의 배수가 아니라면 패배");
         }
     }
 }
