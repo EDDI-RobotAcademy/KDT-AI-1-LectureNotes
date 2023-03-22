90974b69bcc2ad4c9b575eb6a4edf810c6876c09
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-03-22 15:20:10 +0900

    feat: [김진호] 주사위 4개 과제 [HOMEWORKx]

--- Java/JinHoKim/java/untitled/src/homework/HomeWork.java
+++ Java/JinHoKim/java/untitled/src/homework/HomeWork.java
@@ -1,19 +1,28 @@
 package homework;
 
 public class HomeWork {
+
     public static void main(String[] args) {
         final int diceA = (int)(Math.random() * 6) + 1;
         final int diceB = (int)(Math.random() * 6) + 1;
+        final int diceC = (int)(Math.random() * 6) + 1;
+        final int diceD = (int)(Math.random() * 6) + 1;
 
 
-        System.out.println("diceA + diceB = " + (diceA + diceB));
 
-        int diceSum = diceA + diceB;
+        System.out.println("A주사위 + B주사위 + C주사위 + D주사위 = " + (diceA + diceB + diceC + diceD));
 
-        if (diceSum % 4 == 0) {
+        int diceSum = diceA + diceB + diceC + diceD;
+
+        if (diceSum % 4 == 0 || diceSum % 3 == 0) {
             System.out.println("승리");
         } else {
             System.out.println("패배");
         }
     }
 }
+
+
+/*
+
+ */
