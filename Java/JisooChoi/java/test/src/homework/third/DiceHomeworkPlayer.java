bd47138f980c51e8c19921cc6a4e51196f72dfc1
zisoo <zisoo_3@naver.com>
2023-03-24 08:58:03 +0900

    update: [최지수] 주사위 문제3 [HOMEWORK-3]

--- Java/JisooChoi/java/test/src/homework/third/DiceHomeworkPlayer.java
+++ Java/JisooChoi/java/test/src/homework/third/DiceHomeworkPlayer.java
@@ -3,6 +3,9 @@ package homework.third;
 class Game{
     final private Dice[] diceArray;
     final private int DICE_NUM = 3;
+    Player playerMe = new Player();
+    Player playerYou = new Player();
+    int thirdDiceArray = 0; // 세번째 주사위 수가 들어갈 곳
 
     public Game(){
         this.diceArray = new Dice[DICE_NUM];
@@ -22,8 +25,29 @@ class Game{
             }
         }
 
-        // 세 번쨰 주사위 특수 기능
-        diceArray[DICE_NUM-1].setDiceNumber(diceArray[DICE_NUM-1].diceNumber);
+        // 세번째 주사위 특수기능
+        thirdDiceArray = diceArray[DICE_NUM-1].diceNumber;
+        setDiceNumber(thirdDiceArray);
+    }
+
+    public void setDiceNumber(int thirdDiceArray) {
+        this.thirdDiceArray = thirdDiceArray;
+
+        if (thirdDiceArray == 1) {
+            playerMe.case1TakeScoreMe();
+            playerYou.case1TakeScoreYou();
+            System.out.println("친구에게 점수 3점 뺏음 [내 현재 점수 : " + playerMe.score + "]");
+            System.out.println("친구에게 점수 3점 뺏김 [친구 현재 점수 : " + playerYou.score + "]");
+        }
+        if (thirdDiceArray == 3) {
+            playerMe.case2AddScoreMe();
+            playerYou.case2AddScoreYou();
+            System.out.println("자신에게 2점 가산 [내 현재 점수: " + playerMe.score + "]");
+            System.out.println("친구에게 2점 가산 [친구 현재 점수 : " + playerYou.score + "]");
+        }
+        if (thirdDiceArray == 4) {
+            System.out.println("무조건 패배");
+        }
     }
 }
 
@@ -35,24 +59,6 @@ class Dice{
     public Dice(){
         this.diceNumber = (int)(Math.random() * MAX) + MIN;
     }
-
-    public void setDiceNumber(int diceNumber) {
-        this.diceNumber = diceNumber;
-        Player playerMe = new Player();
-        //Player playerYou = new Player();
-
-        if(diceNumber == 1){
-            playerMe.case1TakeScore();
-            System.out.println("친구에게 점수 3점 뺏음 [현재 점수 : "+playerMe.score+"]");
-        }
-        if(diceNumber == 3){
-            playerMe.case2AddScore();
-            System.out.println("자신에게 2점 가산 [현재 점수: "+ playerMe.score +"]");
-        }
-        if(diceNumber == 4){
-            System.out.println("무조건 패배");
-        }
-    }
 }
 
 class Player{
@@ -61,18 +67,23 @@ class Player{
     public Player(){
     }
 
-    public int case1TakeScore(){
+    public int case1TakeScoreMe(){
         return  score += 3;
     }
-
-    public int case2AddScore(){
+    public int case1TakeScoreYou(){
+        return  score -= 3;
+    }
+    public int case2AddScoreMe(){
         return  score += 2;
+    }
+    public int case2AddScoreYou(){
+        return  score -= 2;
     }
 }
 
 public class DiceHomeworkPlayer {
     public static void main(String[] args) {
-        System.out.println("주사위 첫 번째 게임");
+        System.out.println("--- 주사위 첫 번째 게임 ---");
         Game diceGame = new Game();
     }
 }
