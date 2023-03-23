19474707e588745ad831cbb218b187e395c6d27e
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-03-23 10:19:02 +0900

    add: [장현민]주사위 문제 변경[HOMEWORKx]

--- Java/Changhyunmin/Java/src/homework/HomeWorkDice.java
+++ Java/Changhyunmin/Java/src/homework/HomeWorkDice.java
@@ -1,13 +1,23 @@
 package homework;
-class Dice {
-    final int MIN = 1;
-    final int MAX = 6;
+final class Dice {
+    final private int MIN = 1;
+    final private int MAX = 6;
+    final private int WINNER1 = 3;
+    final private int WINNER2 = 4;
+
     final int diceNum1 = (int)(Math.random() * (MAX - MIN +1)) + MIN;
     final int diceNum2 = (int)(Math.random() * (MAX - MIN +1)) + MIN;
     final int diceNum3 = (int)(Math.random() * (MAX - MIN +1)) + MIN;
     final int diceNum4 = (int)(Math.random() * (MAX - MIN +1)) + MIN;
 
     final int diceSum = (diceNum1 + diceNum2 + diceNum3 + diceNum4);
+
+    public boolean Win(){
+        if (diceSum % WINNER1 == 0 || diceSum % WINNER2 == 0){
+            return true;
+        }
+        return false;
+    }
 
     @Override
     public String toString() {
@@ -17,23 +27,13 @@ class Dice {
                 "네번째 주사위: " + diceNum4 ;
     }
 }
-class Score {
-    final int WINNER1 = 3;
-    final int WINNER2 = 4;
-}
 public class HomeWorkDice {
     public static void main(String[] args) {
 
         Dice dice = new Dice();
-        Score score = new Score();
 
         System.out.println(dice);
         System.out.println("총 주사위의 값: " + dice.diceSum);
-
-        if (dice.diceSum % score.WINNER1 == 0||dice.diceSum % score.WINNER2 == 0){
-            System.out.println("승리하셨습니다!");
-        }else {
-            System.out.println("패배하셨습니다...");
-        }
+        System.out.println(dice.Win()? "승리" : "패배");
     }
 }
