5fd88ed6324bcf693abeba7979e6397d02019eb4
JiwonChoi <128335544+cjw0824@users.noreply.github.com>
2023-03-23 20:38:59 +0900

    update: [최지원] 주사위 문제 1 수정 [HOMEWORKx]

--- Java/JiwonChoi/java_first/src/homeWork/diceClass.java
+++ Java/JiwonChoi/java_first/src/homeWork/diceClass.java
@@ -1,10 +1,13 @@
 package homeWork;
 
+import homeWork.refacktor.randomNumber;
+import homeWork.refacktor.diceGame;
+import homeWork.refacktor.rollDice;
 class Dice {
-    public Dice(int COUNT_DICE, int MAX_DICE, int totalScore) {
+    public Dice(int COUNT_DICE, int MAX_DICE, int startScore) {
         this.COUNT_DICE = COUNT_DICE;
         this.MAX_DICE = MAX_DICE;
-        this.totalScore = totalScore;
+        this.startScore = startScore;
     }
 
     //주사위 개수
@@ -26,52 +29,38 @@ class Dice {
     }
 
     //주사위의 총합
-    private int totalScore;
-    public int getTotalScore() {
-        return totalScore;
+    private int startScore;
+    public int getstartScore() {
+        return startScore;
     }
-    public void setTotalScore(int totalScore) {
-        this.totalScore = totalScore;
+    public void setstartScore(int startScore) {
+        this.startScore = startScore;
     }
 }
 
 
 public class diceClass {
     public static void main(String[] args) {
-        final int countDice = 4;
-        final int maxDice = 6;
-        final int startScore = 0;
+        final int countDice = 4; //주사위 개수
+        final int maxDice = 6; //주사위 최고 숫자
+        final int minDice = 1; //주사위 최소숫자
+        int startScore = 0; //시작 점수
 
+        //주사위 설정
         Dice game1 = new Dice(countDice, maxDice, startScore);
         System.out.println(
                 "주사위 개수 :" + game1.getCOUNT_DICE() + "," +
                 "주사위 최대값 :" + game1.getMAX_DICE() + ",");
-
-        int sum = 0;
-        for(int i = 0; i < game1.getCOUNT_DICE(); i++){
-            int randomDice = (int)(Math.random() * game1.getMAX_DICE()+ 1);
-            System.out.println((i+1) + "번째 주사위를 굴려 " + randomDice + "가 나왔습니다.");
-            sum = sum + randomDice;
-            game1.setTotalScore(sum);
-        }
-        System.out.println("주사위의 합은 " + game1.getTotalScore() + "입니다.");
         System.out.println();
 
-        if(game1.getTotalScore() % 12 == 0){
-            System.out.println("3과 4의 배수이며 승리입니다.");
-        }  else if (game1.getTotalScore() % 3 == 0) {
-            System.out.println("3의 배수이며 승리입니다.");
-        } else if (game1.getTotalScore() % 4 == 0) {
-            System.out.println("4의 배수이며 승리입니다.");
-        } else {
-            System.out.println("패배입니다.");
-        }
+        //주사위 굴리기
+        rollDice.rollDice(countDice, maxDice, minDice, startScore);
+        System.out.println(startScore);
 
-
-
-
-
-
-
+        // 승리판정
+        final int winNumber1 = 3;
+        final int winNumber2 = 4;
+        System.out.println(diceGame.diceGame(startScore, winNumber1, winNumber2 ));
     }
+
 }
