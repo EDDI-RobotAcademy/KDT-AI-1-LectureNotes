da495e55e61a57e9c45e61174c3799f9192571f1
dusdn943 <dusdn943@gmail.com>
2023-03-24 08:57:33 +0900

    feat: [최연우] 클래스 문제 2 [HOMEWORKx]

--- Java/yeonwoochoi/JAVA/src/homework/DiceGame3.java
+++ Java/yeonwoochoi/JAVA/src/homework/DiceGame3.java
@@ -2,13 +2,10 @@ package homework;
 
 class Game3 {
 
-    // 주사위가 짝수라면
-    if (dice % 2 == 0)
 
-}
+    public class DiceGame3 {
+        public static void main(String[] args) {
 
-public class DiceGame3 {
-    public static void main(String[] args) {
-
+        }
     }
 }
