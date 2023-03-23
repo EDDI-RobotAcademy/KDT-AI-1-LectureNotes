2f7242efd43b31412823fa5536df630a39747e7a
hanhwack <hanhwack93@gmail.com>
2023-03-23 09:05:46 +0900

    fear [한확] 랜덤다이스 [HOMEWORK2]

---
+++ Java/hanhwack/java/java2/src/homework/ranDice2.java
@@ -0,0 +1,39 @@
+package homework;
+
+class Dice2 {
+    final int MAX_DICE = 4;
+    final int[] diceNumberArr = new int[MAX_DICE];
+    int totalScore;
+
+    public Dice2() {
+        for (int i = 0; i < 4; i++) {
+            diceNumberArr[i] = (int)(Math.random()*6)+1;
+            totalScore += diceNumberArr[i];
+        }
+    }
+    public Integer getTotalScore() {
+
+        return totalScore;
+    }
+
+    public void getTotalScore(Integer totalScore) {
+        this.totalScore = totalScore;
+    }
+
+
+}
+
+public class ranDice2 {
+    public static void main(String[] args) {
+
+        final Dice2 dice = new Dice2();
+
+        if (dice.getTotalScore() % 3 == 0 || dice.getTotalScore() % 4 == 0) {
+            System.out.println("승리입니다");
+        } else {
+            System.out.println("패배하였습니다");
+        }
+
+
+    }
+}
