69bc25545cd75817e66d63f5cbb4fe59acff37a8
Uri <128335705+aldrwk@users.noreply.github.com>
2023-03-24 15:07:10 +0900

    1

---
+++ Java/Kwanyeong_Jin/Java/src/TEST/Wiederholung/main/Wurfelspiel.java
@@ -0,0 +1,20 @@
+package TEST.Wiederholung.main;
+
+import TEST.Wiederholung.faktors.Spieler;
+import TEST.Wiederholung.faktors.Wurfel;
+
+class Spiel {
+    public Spieler spieler;
+
+
+
+}
+
+
+
+public class Wurfelspiel {
+    public static void main(String[] args) {
+//        Spiel spiel = new Spiel();
+        Wurfel test = new Wurfel();
+    }
+}
