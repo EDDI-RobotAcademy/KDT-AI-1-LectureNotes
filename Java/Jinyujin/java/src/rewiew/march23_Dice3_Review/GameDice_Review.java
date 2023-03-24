b6ad8ddcb4541f64b0961d6560bb9d0012efec72
wlsdbwls <wlsdbwls614@gmail.com>
2023-03-24 14:50:08 +0900

    중단

--- Java/Jinyujin/java/src/rewiew/march23_Dice3_Review/GameDice_Review.java
+++ Java/Jinyujin/java/src/rewiew/march23_Dice3_Review/GameDice_Review.java
@@ -8,20 +8,15 @@ import utility.random.CustomRandom;
 public class GameDice_Review {
 
     final int MAX_DICE_NUMBER = 3;
-    final int diceNumber;
+
     final int MIN = 1;
     final int MAX = 6;
 
     public GameDice_Review(int diceNumber,) {
         for (int i = 0; i < MAX_DICE_NUMBER; i++) {
-            this.diceNumber = CustomRandom.generateNumber(1,6);
 
         }
 
-        public int getDiceNumber() {
-            return diceNumber;
+
         }
     }
-
-
-}
