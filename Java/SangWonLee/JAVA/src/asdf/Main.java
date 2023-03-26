00e9e933e6d514b8bdd36bd3558a3845f44e11d2
qnfthslr <ojs2354@naver.com>
2023-03-26 12:40:34 +0900

    Update: [이상원] Basic_grammar 정리 및 Class 공부 [HOMEWORKx]

--- Java/SangWonLee/JAVA/src/asdf/Main.java
+++ Java/SangWonLee/JAVA/src/asdf/Main.java
@@ -4,27 +4,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Yass duckso = new Yass("덕소고등학교 1학년 1반");
+        Student.yass duckso = new Student.yass("덕소고등학교 1학년 1반");
 
-        duckso.addStudent(new Student("asdf", 5 ,69));
-        duckso.addStudent(new Student("aerwewr", 4 ,27));
+        duckso.addStudent(new Student.student("asdf", 5 ,69));
+        duckso.addStudent(new Student.student("aerwewr", 4 ,27));
 
-//        Student choice = null;
-        Student choice = duckso.getStudents().get(0);
+        Student.student choice1 = null;
+        Student.student choice2 = null;
+//        Student choice = duckso.getStudents().get(0); // 이렇게 해서 맨 처음(0)에 있는 얘 result 가져올 수도 있다.
 
-
-        for (Student st : duckso.getStudents()) {
-            if (st.getName().equals("asdf")) {
-                choice = st;
+        for (Student.student st1 : duckso.getStudents()) {
+            if (st1.getName().equals("asdf")) {
+                choice1 = st1;
                 break;
             }
         }
-
-        System.out.println(choice.getResult());
-
-
-
-
+        for (Student.student st2 : duckso.getStudents()) {
+            if (st2.getName().equals("aerwewr")) {
+                choice2 = st2;
+                break;
+            }
+        }
+        System.out.println(choice1.getName() + choice1.getResult() + " " + choice1.getNumber());
+        System.out.println(choice2.getName() + choice2.getResult() + " " + choice2.getNumber());
 
     }
 
--- Java/SangWonLee/JAVA/src/asdf/Yass.java
+++ Java/SangWonLee/JAVA/src/asdf/Yass.java
@@ -1,9 +1,46 @@
-package asdf;
+//package asdf;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class Yass {
+//
+//    private String name;
+//    private List<Student> students;
+//
+//    public Yass(String name) {
+//        this.name = name;
+//        this.students = new ArrayList<>();
+//
+//
+//    }
+//    public void addStudent(Student st){
+//        this.students.add(st);
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public List<Student> getStudents() {
+//        return students;
+//    }
+//
+//    public void setStudents(List<Student> students) {
+//        this.students = students;
+//    }
+//
+//}
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Yass {
+
 
     private String name;
     private List<Student> students;
@@ -11,11 +48,6 @@ public class Yass {
     public Yass(String name) {
         this.name = name;
         this.students = new ArrayList<>();
-
-
-    }
-    public void addStudent(Student st){
-        this.students.add(st);
     }
 
     public String getName() {
@@ -34,4 +66,43 @@ public class Yass {
         this.students = students;
     }
 
+    public void addStudent(Student st) {
+        this.students.add(st);
+    }
+
+    public static class Student {
+        private String name;
+        private int number;
+        private int result;
+
+        public String getName() {
+            return name;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public int getResult() {
+            return result;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setNumber(int number) {
+            this.number = number;
+        }
+
+        public void setResult(int result) {
+            this.result = result;
+        }
+
+        public Student(String name, int number, int result) {
+            this.name = name;
+            this.number = number;
+            this.result = result;
+        }
+    }
 }
