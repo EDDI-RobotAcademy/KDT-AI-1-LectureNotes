b66de29238a84811a804ceb36f92d2812e1c5784
Sanghoon Lee <gcccompil3r@gmail.com>
2023-03-27 12:21:55 +0900

    feat: [이상훈] 상품 정보 회원이 구매하기 [THEREARE-PROBLEMx]

--- Java/SanghoonLee/java/test/src/variousClassUsage/extension/ExtensionVariousClassUsageTest.java
+++ Java/SanghoonLee/java/test/src/variousClassUsage/extension/ExtensionVariousClassUsageTest.java
@@ -15,5 +15,11 @@ public class ExtensionVariousClassUsageTest {
         Manager manager = new Manager();
         manager.addRandomMember();
         manager.printMemberList();
+
+        manager.addRandomProduct();
+        manager.printProductList();
+
+        manager.buyRandomProduct();
+        manager.printProductList();
     }
 }
