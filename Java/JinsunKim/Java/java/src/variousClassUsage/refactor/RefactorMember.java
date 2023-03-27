6b0bbbe6331a0a18f3eb5add4b4d84681ed2d122
Jinsun Kim <unchlo27@gmail.com>
2023-03-27 14:46:54 +0900

    feat: [김진선] 여러 사용자가 동일한 상품을 구매하는 경우 리팩토링 [LISTx]

--- Java/JinsunKim/Java/java/src/variousClassUsage/refactor/RefactorMember.java
+++ Java/JinsunKim/Java/java/src/variousClassUsage/refactor/RefactorMember.java
@@ -1,15 +1,11 @@
-package variousClassUsage;
+package variousClassUsage.refactor;
 
-//사용자관리, 누가 이상품을 구매했는지 관리
-
-public class TestMember {
+public class RefactorMember {
     final private int memberId;
-    // 이것을 도입하게 된 계기는..
-    // 이메일보다는 숫자형태로 검색하는게 빠르기 때문에 int 형 정보를 생성한다.
     final private String email;
     final private String password;
 
-    public TestMember(int memberId, String email, String password) {
+    public RefactorMember(int memberId, String email, String password) {
         this.memberId = memberId;
         this.email = email;
         this.password = password;
