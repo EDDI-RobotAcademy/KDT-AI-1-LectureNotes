f88f7188dfadc4f572bd178b253cc3b7e47ce2ba
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-03-27 12:40:54 +0900

    feat: [남현기] 상품 정보 회원이 구매하기 [THEREARE-PROBLEMx]

---
+++ Java/HyunkiNam/java/test/src/variousClassUsage/extension/ExtensionVariousClassUsageTest.java
@@ -0,0 +1,23 @@
+package variousClassUsage.extension;
+
+import variousClassUsage.TestMember;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExtensionVariousClassUsageTest {
+    public static void main(String[] args) {
+        // 일단 여러명의 회원과 여러개의 상품을 배치
+        // 랜덤을 사용해 랜덤한 상품을 랜덤한 회원이 구매
+        // 상품 구매자를 추적해보자.
+        Manager manager = new Manager();
+        manager.addRandomMember();
+        manager.printMemberList();
+
+        manager.addRandomProduct();
+        manager.printProductList();
+
+        manager.buyRandomProduct();
+        manager.printProductList();
+    }
+}
