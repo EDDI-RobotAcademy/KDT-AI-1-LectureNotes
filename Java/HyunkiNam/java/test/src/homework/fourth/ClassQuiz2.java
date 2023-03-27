7ddaa3f571988b7977887caae082c279796ece3c
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-03-27 10:59:38 +0900

    feat: [남현기] 클래스 기본 문제3 수정 [HOMEWORKx]

--- Java/HyunkiNam/java/test/src/homework/fourth/ClassQuiz2.java
+++ Java/HyunkiNam/java/test/src/homework/fourth/ClassQuiz2.java
@@ -8,48 +8,31 @@ class Board{
     String writer;
     String detail;
 
-    final String[] titleArray = new String[MAX_NUMBER];
-    final String[] writerArray = new String[MAX_NUMBER];
-    final String[] detailArray = new String[MAX_NUMBER];
-
-    int count = 0;
     public Board(){
-        for(int i=0; i<MAX_NUMBER; i++){
-            Scanner sc = new Scanner(System.in);
-            System.out.print("제목 입력 : ");
-            this.title = sc.nextLine();
-            System.out.print("작성자 입력 : ");
-            this.writer = sc.nextLine();
-            System.out.print("내용 입력 : ");
-            this.detail = sc.nextLine();
-
-            saveInfo();
-        }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("제목 입력 : ");
+        this.title = sc.nextLine();
+        System.out.print("작성자 입력 : ");
+        this.writer = sc.nextLine();
+        System.out.print("내용 입력 : ");
+        this.detail = sc.nextLine();
     }
-
-    public void saveInfo(){
-        titleArray[count] = title;
-        writerArray[count] = writer;
-        detailArray[count] = detail;
-
-        count++;
-    }
-
-
-
+    
     public void print(){
-        for(int i=0; i<MAX_NUMBER; i++){
-            System.out.println("번호 : " + (i+1) + ", 제목 : " + titleArray[i] + ", 작성자 : " + writerArray[i]
-            + ", 내용 : " + detailArray[i]);
-        }
+            System.out.println("번호 : " + ", 제목 : " + title + ", 작성자 : " + writer
+            + ", 내용 : " + detail);
     }
-
-
 }
+
 public class ClassQuiz2 {
 
     public static void main(String[] args) {
-        Board board = new Board();
-        board.print();
+        Board[] boards = new Board[3];
+        for(int i=0; i< boards.length; i++){
+            boards[i] = new Board();
+        }
+        for(int i=0; i< boards.length;i++){
+            boards[i].print();
+        }
     }
 }
