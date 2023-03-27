a4664d5a8649f61c81d8b55491089ba306d3c17c
zisoo <zisoo_3@naver.com>
2023-03-28 00:18:06 +0900

    update: [최지수] 리스트 기본 문제1 [LISTx]

--- Java/JisooChoi/java/test/src/homework/fifth/CardListTest.java
+++ Java/JisooChoi/java/test/src/homework/fifth/CardListTest.java
@@ -1,9 +1,26 @@
 package homework.fifth;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CardListTest {
     public static void main(String[] args) {
-        Card card = new Card();
-        card.addCardList();
-        System.out.println(card);
+        List<Card> cards = new ArrayList<>();
+
+        cards.add(new Card("A"));
+        cards.add(new Card("1"));
+        cards.add(new Card("2"));
+        cards.add(new Card("3"));
+        cards.add(new Card("4"));
+        cards.add(new Card("5"));
+        cards.add(new Card("6"));
+        cards.add(new Card("7"));
+        cards.add(new Card("8"));
+        cards.add(new Card("9"));
+        cards.add(new Card("J"));
+        cards.add(new Card("Q"));
+        cards.add(new Card("K"));
+
+        System.out.println(cards);
     }
 }
