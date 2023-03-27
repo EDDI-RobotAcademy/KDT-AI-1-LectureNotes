6dd6126c1f5c132fb1011d2add4bbdfdf52640b1
aldrwk <badrow008@gmail.com>
2023-03-27 20:41:05 +0900

    feat: [진관영] 포커카드 출력 [PockerTest_1]

--- Java/Kwanyeong_Jin/Java/src/TEST/card_uebung/Card.java
+++ Java/Kwanyeong_Jin/Java/src/TEST/card_uebung/Card.java
@@ -3,7 +3,6 @@ package TEST.card_uebung;
 public class Card {
     public static void main(String[] args) {
         CardSpiel card = new CardSpiel();
-//        System.out.println(card);
-        card.cardtake();
+        System.out.println(card);
     }
 }
