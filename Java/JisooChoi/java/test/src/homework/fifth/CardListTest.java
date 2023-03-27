1a0d834b0c3f7e5f03fba44340afa51102d621b7
zisoo <zisoo_3@naver.com>
2023-03-27 14:37:23 +0900

    feat: [최지수] 리스트 기본 문제1 [LISTx]

---
+++ Java/JisooChoi/java/test/src/homework/fifth/CardListTest.java
@@ -0,0 +1,9 @@
+package homework.fifth;
+
+public class CardListTest {
+    public static void main(String[] args) {
+        Card card = new Card();
+        card.addCardList();
+        System.out.println(card);
+    }
+}
