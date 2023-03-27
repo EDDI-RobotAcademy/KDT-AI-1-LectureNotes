c9f9fa7df96610e88b2fae38f23dd641891b77a4
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-03-27 15:55:26 +0900

    update: [김재경] 둘이 포커 카드 뽑기[HOMEWORK]

--- Java/JaekyoungKim/java/src/homework/forth/CardDraw.java
+++ Java/JaekyoungKim/java/src/homework/forth/CardDraw.java
@@ -2,9 +2,11 @@ package homework.forth;
 
 public class CardDraw {
     public static void main(String[] args) {
-       Card card=new Card();
+       Card card1=new Card(1);
+        Card card2=new Card(2);
 
-        System.out.println(card);
+        System.out.println(card1);
+        System.out.println(card2);
 
     }
 
