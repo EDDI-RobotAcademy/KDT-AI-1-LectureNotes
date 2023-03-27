2d0e2891becf3a74257eccc7e8f1cb909e9596b3
hanhwack <hanhwack93@gmail.com>
2023-03-27 11:20:03 +0900

    feat: [한확] 클래스 간 데이터 전달 [CLASS-INTERACTIONx]

---
+++ Java/hanhwack/java/java2/src/variousClassUsage/TestProduct.java
@@ -0,0 +1,40 @@
+package variousClassUsage;
+
+public class TestProduct {
+
+    final private String productName;
+    final private String vendor;
+    private int price;
+    //상품을 구매할 때마다 사용자 정보가 매번 쌓임
+    //private TestMember member;
+    final private int memberId;
+
+    public TestProduct(String productName, String vendor, int price, int memberId) {
+        this.productName = productName;
+        this.vendor = vendor;
+        this.price = price;
+        this.memberId = memberId;
+    }
+
+    public void changePrice(int price) {
+        this.price = price;
+    }
+    public void findMember(TestMember member) {
+        System.out.println("상품 구매자를 찾습니다!");
+
+        if(memberId == member.getMemberId()) {
+            System.out.println(member);
+            return;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "TestProduct{" +
+                "productName='" + productName + '\'' +
+                ", vendor='" + vendor + '\'' +
+                ", price=" + price +
+                ", memberId=" + memberId +
+                '}';
+    }
+}
