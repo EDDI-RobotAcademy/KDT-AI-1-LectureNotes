1d743ac2f83296c4042582142841442030d4d60a
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-03-28 15:24:46 +0900

    Update dice3.java

--- Java/JaekyoungKim/java/src/homework/fifth/diceList/dice3.java
+++ Java/JaekyoungKim/java/src/homework/fifth/diceList/dice3.java
@@ -12,6 +12,7 @@ public class dice3 {
         final int MAX_NUM=3;
         List<diceRolling> diceRollingList=new ArrayList<>();
         for (int i=0; i<MAX_NUM;i++){
+            diceRollingList.add(new diceRolling(DiceEnum.DicecheckPatternIndex(CustomRandom.generateNumber(1,6))));
         }
         System.out.println(diceRollingList);
 
