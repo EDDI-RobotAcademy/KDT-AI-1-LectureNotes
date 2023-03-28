750589580e64e745a727a647a8367053c59a61aa
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-03-28 15:24:40 +0900

    feat: [김재경] 주사위 중간 저장....[HOMEWORK]

---
+++ Java/JaekyoungKim/java/src/homework/fifth/diceList/dice3.java
@@ -0,0 +1,19 @@
+package homework.fifth.diceList;
+
+import homework.third.Player;
+import utilityRandom.CustomRandom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class dice3 {
+    public static void main(String[] args) {
+        final int MAX_NUM=3;
+        List<diceRolling> diceRollingList=new ArrayList<>();
+        for (int i=0; i<MAX_NUM;i++){
+        }
+        System.out.println(diceRollingList);
+
+    }
+}
