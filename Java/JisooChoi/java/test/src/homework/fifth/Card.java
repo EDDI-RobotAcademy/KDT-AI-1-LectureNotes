c7a1498e42664e9791c1dad8e616c341048354d3
zisoo <zisoo_3@naver.com>
2023-03-28 09:20:57 +0900

    update: [최지수] 리스트 기본 문제1 - 피드백 보완 [LISTx]

--- Java/JisooChoi/java/test/src/homework/fifth/Card.java
+++ Java/JisooChoi/java/test/src/homework/fifth/Card.java
@@ -13,4 +13,5 @@ public class Card {
                 "name='" + name + '\'' +
                 '}';
     }
+
 }
--- Java/JisooChoi/java/test/src/homework/fifth/CardListTest.java
+++ Java/JisooChoi/java/test/src/homework/fifth/CardListTest.java
@@ -23,4 +23,5 @@ public class CardListTest {
 
         System.out.println(cards);
     }
+
 }
