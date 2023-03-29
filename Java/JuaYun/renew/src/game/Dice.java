a47bbb8fe43b1316d8f30c357b2967b37e3e1a4d
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-03-29 11:54:20 +0900

    feat: [윤주아] 배열주사위 리스트로 바꾸기 [LISTx]

---
+++ Java/JuaYun/renew/src/game/Dice.java
@@ -0,0 +1,25 @@
+package game;
+
+
+import utility.random.CustomRandom;
+
+public class Dice {
+    final private int MIN = 1;
+    final private int MAX = 6;
+    final private int diceNumber;
+
+    public Dice() {
+        this.diceNumber = CustomRandom.generateNumber(MIN, MAX);
+    }
+
+    public int getDiceNumber() {
+        return diceNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Dice{" +
+                "diceNumber=" + diceNumber +
+                '}';
+    }
+}
