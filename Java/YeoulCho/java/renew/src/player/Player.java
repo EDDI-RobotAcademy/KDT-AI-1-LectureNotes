790fcb2f0814af699fcc7863d0cad9430c561301
YeoulCho <cyu5733@gmail.com>
2023-03-29 11:47:26 +0900

    update: [조여울] 기존 배열 방식의 코드를 List 형태로 리팩토링 [DICE-GAME-4]

--- Java/YeoulCho/java/renew/src/player/Player.java
+++ Java/YeoulCho/java/renew/src/player/Player.java
@@ -3,13 +3,15 @@ package player;
 import game.Dice;
 import game.GameScore;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 // Player(플레이어)에게 필요한게 뭐지 ?
 public class Player {
     final private int MAX_DICE_NUM = 3;
     final private String name;
-    final private Dice[] gameDices = new Dice[MAX_DICE_NUM];
+    final private List<Dice> gameDiceList = new ArrayList<>();
     final private GameScore gameScore;
 
     public Player(String name) {
@@ -27,12 +29,15 @@ public class Player {
         int diceNumberSum = 0;
 
         for (int i = 0; i < MAX_DICE_NUM; i++) {
-            gameDices[i] = new Dice();
+            gameDiceList.add(new Dice());
+            //gameDices[i] = new Dice();
 
-            diceNumberSum += gameDices[i].getDiceNumber();
-
-            if (gameDices[FIRST_DICE_INFO].getDiceNumber() %
+            //diceNumberSum += gameDices[i].getDiceNumber();
+            diceNumberSum += gameDiceList.get(i).getDiceNumber();
+            if(gameDiceList.get(FIRST_DICE_INFO).getDiceNumber() %
                     DECISION_EVEN == ODD) {
+            //if (gameDices[FIRST_DICE_INFO].getDiceNumber() %
+            //        DECISION_EVEN == ODD) {
                 break;
             }
         }
@@ -41,8 +46,12 @@ public class Player {
     }
 
     public Dice getSelectedGameDice(int index) {
-        return gameDices[index];
+        if(gameDiceList.size()> index){
+        return gameDiceList.get(index);
+        //gameDices[index];
+    }return null;
     }
+
 
     public GameScore getGameScore() {
         return gameScore;
@@ -56,7 +65,7 @@ public class Player {
     public String toString() {
         return "Player{" +
                 "name='" + name + '\'' +
-                ", gameDices=" + Arrays.toString(gameDices) +
+                ", gameDiceList=" + gameDiceList +
                 ", gameScore=" + gameScore +
                 '}';
     }
