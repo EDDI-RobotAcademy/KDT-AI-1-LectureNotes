bc4ff6044b300d2a1b837da09db8c67fad2cac2d
jinho1593 <jinho9168@gmail.com>
2023-03-29 13:03:55 +0900

    refactor: [김진호] 상품 문제 Hash를 통한 개선 [PRODUCT-PROB]

---
+++ Java/JinHoKim/java/renew/src/productProblem/product/controller/ProductController.java
@@ -0,0 +1,20 @@
+package productProblem.product.controller;
+import productProblem.product.RefactorManager;
+import utility.generator.SequenceGenerator;
+
+public class ProductController {
+    public static void main(String[] args) {
+        System.out.println(SequenceGenerator.getSpecificEntityId("Entity"));
+        System.out.println(SequenceGenerator.getSpecificEntityId("Entity"));
+
+        RefactorManager manager = new RefactorManager();
+        manager.addRandomMember();
+        manager.printMemberList();
+        manager.addRandomProduct();
+        manager.printProductList();
+        manager.buyRandomProduct();
+        manager.printProductList();
+        manager.addMember();
+        manager.printMemberList();
+    }
+}
