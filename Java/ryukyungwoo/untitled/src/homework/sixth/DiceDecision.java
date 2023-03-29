a1a96f1fd5bda65414272b4311a722cd1e83ec64
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-03-29 09:53:00 +0900

    init: [유경우] 새로운 작업 환경 재구성 [INIT-REPO]

---
+++ Java/ryukyungwoo/untitled/src/homework/sixth/DiceDecision.java
@@ -0,0 +1,4 @@
+package homework.sixth;
+
+public class DiceDecision {
+}
--- Java/ryukyungwoo/untitled/src/homework/sixth/Player.java
+++ Java/ryukyungwoo/untitled/src/homework/sixth/Player.java
@@ -1,16 +1,6 @@
 package homework.sixth;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Player {
-    final private int PLAYER_NUM = 2;
+    final private int HOW_MANY_PLAYERS = 2;
 
-    List<Player> playerList = new ArrayList<Player>();
-
-    public Player (Dice dice) {
-        for (int i = 0; i < PLAYER_NUM; i++) {
-            playerList.add(new Dice(dice.getDiceList()));
-        }
-    }
 }
