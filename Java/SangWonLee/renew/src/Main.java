e46bd8d4ba27cd22d3fb7e4d6218bb173789c4f8
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-03-29 16:38:47 +0900

    feat: [이상원] Map 기본 문제 [Mapx]

--- Java/SangWonLee/renew/src/Main.java
+++ Java/SangWonLee/renew/src/Main.java
@@ -1,9 +1,21 @@
-
-
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        Map<Integer,String> abcd = new HashMap<>();
+
+
+
+
+
     }
+
+
+
+
+
 }
