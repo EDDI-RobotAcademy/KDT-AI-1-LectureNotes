e1e507a48733db973e57384eb8e27ac2ce2fa37f
YeoulCho <cyu5733@gmail.com>
2023-03-29 11:09:20 +0900

    init: [조여울] 과거 방식 가져오기 [INIT-REPO]

---
+++ Java/YeoulCho/java/renew/src/utility/generator/SequenceGenerator.java
@@ -0,0 +1,9 @@
+package utility.generator;
+
+public class SequenceGenerator {
+    private static int currentMemberId = 0;
+
+    public static int getCurrentSequenceMemberId () {
+        return currentMemberId++;
+    }
+}
