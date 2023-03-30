b6f4c934601b0e6761ad60cee801f23845b75dfc
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-03-30 15:20:56 +0900

    feat: [유경우] 만들기 중간 [PRACTICEx]

--- Java/ryukyungwoo/JAVA/renew/src/practice/dice3/Dice.java
+++ Java/ryukyungwoo/JAVA/renew/src/practice/dice3/Dice.java
@@ -2,20 +2,29 @@ package practice.dice3;
 
 import utility.random.CustomRandom;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dice {
 
     final private int MAX_DICE_NUM = 6;
     final private int MIN_DICE_NUM = 1;
-    final private int diceNumber;
+    final private int HOW_MANY_ROLL_DICE= 3;
+    List<Integer> diceList = new ArrayList<>();
     public Dice () {
-        this.diceNumber =
-        CustomRandom.generateNumber(MIN_DICE_NUM, MAX_DICE_NUM);
+        for (int i = 0; i < HOW_MANY_ROLL_DICE; i++) {
+            this.diceList.add(CustomRandom.generateNumber(MIN_DICE_NUM, MAX_DICE_NUM));
+        }
     }
 
-    public int getDiceNumber() {
-        return diceNumber;
+    @Override
+    public String toString() {
+        return "Dice{" +
+                "diceList=" + diceList +
+                '}';
     }
 
     public static void main(String[] args) {
+        System.out.println(new Dice());
     }
 }
--- Java/ryukyungwoo/JAVA/renew/src/practice/dice3/Player.java
+++ Java/ryukyungwoo/JAVA/renew/src/practice/dice3/Player.java
@@ -1,47 +1,12 @@
 package practice.dice3;
 
-import utility.random.CustomRandom;
-
-import java.util.ArrayList;
-import java.util.List;
-class PlayerRollingDice {
-    final private int rollingDice = 3;
-    List<Integer> playerRollingDiceList = new ArrayList<>();
-
-    public PlayerRollingDice () {
-        for (int i = 0; i < rollingDice; i++){
-            Dice dice = new Dice();
-            playerRollingDiceList.add(dice.getDiceNumber());
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "PlayerRollingDice{" +
-                "playerRollingDiceList=" + playerRollingDiceList +
-                '}';
-    }
-}
 
 public class Player {
-    final private int playerNumber = 2;
-    List<List> playerNumberList = new ArrayList<>();
-
+    final private int HOW_MANY_PLAYERS = 2;
+    private Player player;
     public Player () {
-        for (int i = 0; i < playerNumber; i++){
-            PlayerRollingDice playerRollingDice = new PlayerRollingDice();
-            playerNumberList.add(playerRollingDice.playerRollingDiceList);
+        for (int i = 0; i < HOW_MANY_PLAYERS; i++) {
+            this.player = new Dice();
         }
-    }
-
-    @Override
-    public String toString() {
-        return "Player{" +
-                "playerNumberList=" + playerNumberList +
-                '}';
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new Player());
     }
 }
