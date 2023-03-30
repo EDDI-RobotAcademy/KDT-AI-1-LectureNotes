1424117da4b352373a1ce6ccff776365538d7cc6
JiwonChoi <128335544+cjw0824@users.noreply.github.com>
2023-03-30 16:39:32 +0900

    update :[최지원] 주사위3 문제 수정 중

--- Java/JiwonChoi/java/test/src/homework/homework3/homeworkDice3.java
+++ Java/JiwonChoi/java/test/src/homework/homework3/homeworkDice3.java
@@ -4,7 +4,7 @@ package homework.homework3;
 class winnerCheck { //점수 더 높은사람이 win
     public static String winnerName(String _1p, String _2p, int _1pScore, int _2pScore) { //이름1,이름2, 점수1, 점수2 입력
         String winner;
-        if(_1pScore < -998) {
+        if(_1pScore < -998) {               //주사위 4가 나왔을때 큰 숫자를 빼서 지도록..
             winner = _2p + " 승리!(주사위 4)";
         } else if(_2pScore < -998) {
             winner = _1p + " 승리!(주사위 4)";
@@ -19,8 +19,21 @@ class winnerCheck { //점수 더 높은사람이 win
     }
 }
 
-class game { //주사위 총점
+class gameSet { //주사위 총점
+    String _1p;
+    String _2p;
+    int _1pScore;
+    int _2pScore;
+
+    public gameSet(String _1p, String _2p, int _1pScore, int _2pScore) {
+        this._1p=_1p;
+        this._2p=_2p;
+        this._1pScore=_1pScore;
+        this._2pScore=_2pScore;
+    }
+
     public static int[] gamePlay(String _1p, String _2p) {
+
         //주사위 3개의 값을 배열로
         int [] _1pDices  = new int [3];
         for(int i = 0; i < _1pDices.length; i++ ){
@@ -31,7 +44,7 @@ class game { //주사위 총점
             _2pDices[i] = (int)(Math.random() * 6) + 1;
         }
 
-        int _1pTotalScore = 0 ;
+        int _1pTotalScore = 0;
         int _2pTotalScore = 0;
 
         //1p 짝수일땐 못굴림+ 주사위합
@@ -97,7 +110,10 @@ class game { //주사위 총점
         int [] a = {_1pTotalScore, _2pTotalScore};
         return a;
     }
+
 }
+
+
 public class homeworkDice3 {
     public static void main(String[] args) {
         //플레이어
@@ -105,7 +121,7 @@ public class homeworkDice3 {
         String _2p = "이땡땡";
 
         //총점
-        int[] score = game.gamePlay(_1p,_2p);
+        int[] score = gameSet.gamePlay(_1p,_2p);
         int _1pScore = score[0];
         int _2pScore = score[1];
         System.out.println('\n' +_1p + " -> " + _1pScore + "점");
