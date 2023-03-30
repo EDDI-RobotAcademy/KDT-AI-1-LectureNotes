46a0ec2690a7db3387a78e55f27dbceb6706954b
hanhwack <hanhwack93@gmail.com>
2023-03-30 09:09:07 +0900

    update: [한확] hash 기본 문제 추가 [HOMEWORKx]

--- Java/hanhwack/java/renew/src/homework/basicMap.java
+++ Java/hanhwack/java/renew/src/homework/basicMap.java
@@ -2,19 +2,34 @@ package homework;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
+
 
 public class basicMap {
     private static Map<String, Integer> nameAge = new HashMap<>();
+    private static Map<String, Integer> nameNumber = new HashMap<>();
+    private static Map<Integer, Integer> speedNumber = new HashMap<>();
+    private static Map<String, String> nickReal = new HashMap<>();
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        String name = "Han" ;
+        String name2 = "Kim" ;
+        int shortNumber = 01 ;
+        String nickName = "tall" ;
 
 
-        nameAge.put("Han", 12);
+        nameAge.put(name, 12);
         int age = nameAge.get("Han");
+        nameNumber.put(name2, 5905);
+        int Number = nameNumber.get("Kim");
+        speedNumber.put(shortNumber, 12);
+        int longNumber = speedNumber.get(12345678);
+        nickReal.put(nickName, "Son");
+        String realName = nickReal.get("tall");
 
-        System.out.println(age);
+        System.out.println("name : "+ name + " age :" + age);
+        System.out.println("name : "+ name2 + " Number :" + Number);
+        System.out.println("short number : "+ shortNumber + " long number :" + longNumber);
+        System.out.println("nick name : "+ nickName + " real name :" + realName);
 
     }
 }
