f919d1d70c9181deb7a7ac350be6a15ad2df04f8
YeoulCho <cyu5733@gmail.com>
2023-03-30 20:53:56 +0900

    feat: [조여울] 주사위 문제3 다시 풀어보기 [HOMEWORKx]

--- Java/YeoulCho/java/untitled/src/homework/RealPlayer.java
+++ Java/YeoulCho/java/untitled/src/homework/RealPlayer.java
@@ -9,24 +9,13 @@ public class RealPlayer {
     int DICE_NUMBER = 3;
     String PlayerName;
     int totalScore;
-    int specialDice;
+    static int specialDice;
     int[] DiceArray = new int[3];
+    boolean issteal=false; //훔쳤니?
 
     public RealPlayer(String name) {
         this.PlayerName = name;
         totalScore = rollDice();
-
-
-
-    }
-
-    private int specialThirdDice() {
-        specialDice = DiceArray[2];
-        DiceArray[2] = CustomRandom.generateNumber(1, 6);
-        if (specialDice == 4) {
-            totalScore = -777;
-        }
-        return specialDice;
     }
 
 
@@ -36,11 +25,23 @@ public class RealPlayer {
             if (DiceArray[0] % 2 != 0) {
                 break;
             }
-
-
-
             DiceArray[i] = CustomRandom.generateNumber(1, 6);
             Dicesum += DiceArray[i];
+        }
+        specialDice = DiceArray[2];
+
+        switch (specialDice) {
+            case 1:
+                Dicesum += 3;
+                issteal =true; //훔쳤어요
+                break;
+            case 3:
+                Dicesum += 2;
+                break;
+            case 4:
+                Dicesum = -7777;
+                break;
+
         }
         return Dicesum;
     }
@@ -58,13 +59,23 @@ public class RealPlayer {
                 '}';
     }
 
+    public void stolen (boolean issteal) {
+        if (issteal){
+        this.totalScore -= 3;
+    }}
+
+    public boolean issteal() {
+        return issteal;
+    }
 
     public static void main(String[] args) {
         RealPlayer Player1 = new RealPlayer("Player1");
         RealPlayer Player2 = new RealPlayer("Player2");
+        Player2.stolen(Player1.issteal()); //Player1이 훔쳤어 true야 그럼 Player2에서 3점 뺏어와
+        Player1.stolen(Player2.issteal());
+
         System.out.println("Player1: " + Player1);
         System.out.println("Player2: " + Player2);
-
 
 
         if (Player1.getTotalScore() > Player2.getTotalScore()) {
