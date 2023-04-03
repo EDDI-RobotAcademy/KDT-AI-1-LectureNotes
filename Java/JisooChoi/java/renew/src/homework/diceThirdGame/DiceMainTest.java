0853c3eb072269822ff324a14a243ae286211708
zisoo <zisoo_3@naver.com>
2023-04-03 19:07:53 +0900

    update: [최지수] : 주사위 문제 3번 - 배열 사용 완료 [DICE-3]

--- Java/JisooChoi/java/renew/src/homework/diceThirdGame/DiceMainTest.java
+++ Java/JisooChoi/java/renew/src/homework/diceThirdGame/DiceMainTest.java
@@ -10,6 +10,7 @@ public class DiceMainTest {
         // ★ 여전히 두 플레이어 중 누가 공격자가 이고,
         //  공격당하는 사람이 되는지는 모르겠어서 임의로 설정
         game.thirdDiceGame(game.player1, game.player2);
+        game.thirdDiceGame(game.player2, game.player1);
 
         DicePlayer winner = game.win(game.player1, game.player2);
         System.out.println(game);
