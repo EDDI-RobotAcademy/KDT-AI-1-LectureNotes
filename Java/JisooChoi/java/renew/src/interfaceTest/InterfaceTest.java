40854196cb743b709eaa227ddd36ed0f8d533e9d
zisoo <zisoo_3@naver.com>
2023-04-04 12:45:11 +0900

    feat: [최지수] 인터페이스 문법 [INTERFACE-GRAMMAR]

---
+++ Java/JisooChoi/java/renew/src/interfaceTest/InterfaceTest.java
@@ -0,0 +1,10 @@
+package interfaceTest;
+
+
+public class InterfaceTest {
+    public static void main(String[] args) {
+        GameCharacter gameCharacter = new GameCharacter();
+
+        System.out.println("1번 스킬 데미지: "+gameCharacter.firstSkill());
+    }
+}
---
+++ Java/JisooChoi/java/renew/src/interfaceTest/Skill.java
@@ -0,0 +1,9 @@
+package interfaceTest;
+
+// 인터페이스는 아래와 같이 껍데기만 만들게 되어 있다.
+public interface Skill {
+    float firstSkill ();
+    void secondSkill ();
+
+}
+
