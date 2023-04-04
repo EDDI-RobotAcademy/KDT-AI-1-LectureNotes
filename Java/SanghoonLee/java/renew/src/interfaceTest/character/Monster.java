e7dd49dbadaab85fe74deec01c8ae59a4a1c2b35
Sanghoon Lee <gcccompil3r@gmail.com>
2023-04-04 13:54:53 +0900

    feat: [이상훈] RPG 게임 만들기 [RPG]

---
+++ Java/SanghoonLee/java/renew/src/interfaceTest/character/Monster.java
@@ -0,0 +1,46 @@
+package interfaceTest.character;
+
+import interfaceTest.Skill;
+import utility.random.CustomRandom;
+
+public class Monster implements Skill {
+    int strength;       // 완력
+    int intelligent;    // 지능
+    int dexterity;      // 재주
+    int agility;        // 민첩
+    int hp;
+
+    final int START = 20;
+    final int END = 30;
+    final int HP_MIN = 100;
+    final int HP_MAX = 200;
+
+    public Monster () {
+        this.strength = CustomRandom.generateNumber(START, END);
+        this.intelligent = CustomRandom.generateNumber(START, END);
+        this.dexterity = CustomRandom.generateNumber(START, END);
+        this.agility = CustomRandom.generateNumber(START, END);
+        this.hp = CustomRandom.generateNumber(HP_MIN, HP_MAX);
+    }
+
+    @Override
+    public float firstSkill() {
+        return 0;
+    }
+
+    @Override
+    public void secondSkill() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Monster{" +
+                "strength=" + strength +
+                ", intelligent=" + intelligent +
+                ", dexterity=" + dexterity +
+                ", agility=" + agility +
+                ", hp=" + hp +
+                '}' + '\n';
+    }
+}
