97142e2d258f41626071ef708365a51934026262
jinho1593 <jinho9168@gamil.com>
2023-04-04 12:44:44 +0900

    feat: [김진호] 인터페이스 문법 [INTERFACEx]

---
+++ Java/JinHoKim/java/renew/src/zpractice/FirstProb1.java
@@ -0,0 +1,12 @@
+/*package zpractice;
+
+public class FirstProb1 {
+    public static void main(String[] args) {
+        final int START = 1;
+        final int END = 100;
+        final int EVEN_DICISION = 2;
+        final int EVEN = 0;
+
+        for (int i = 0;)
+    }
+}*/
---
+++ Java/JinHoKim/java/renew/src/zpractice/FirstProb3.java
@@ -0,0 +1,4 @@
+package zpractice;
+
+public class FirstProb3 {
+}
