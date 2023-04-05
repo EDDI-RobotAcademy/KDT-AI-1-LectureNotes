73d6409550791b3530987516ddce84ec808bad21
bbbii <hyeongjin1326@gmail.com>
2023-04-05 15:22:12 +0900

    feat: [김형진] RPG 게임 [GAMEx]

---
+++ Java/HyeongJinKim/renew/src/game/rpg/refactorRpgGame/characterModel/RefactorGameCharacter.java
@@ -0,0 +1,27 @@
+package game.rpg.refactorRpgGame.characterModel;
+
+import utility.random.CustomRandom;
+
+public class RefactorGameCharacter extends CharacterModel implements Skill {
+
+    public RefactorGameCharacter(int statMin, int statMax, int hpMin, int hpMax) {
+
+        super(CustomRandom.generateNumber(statMin, statMax),
+                CustomRandom.generateNumber(statMin, statMax),
+                CustomRandom.generateNumber(statMin, statMax),
+                CustomRandom.generateNumber(statMin, statMax),
+                CustomRandom.generateNumber(hpMin, hpMax));
+    }
+
+    @Override
+    public <T> void targetingSkill(T target) {
+        final RefactorMonster monster = (RefactorMonster) target;
+        final int damage = (int) (getStrength() * 1.7f + getDexterity() * 2.5f + getAgility() * 0.6);
+        monster.calculateDamage(damage);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
