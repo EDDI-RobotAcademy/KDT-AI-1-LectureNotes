b700e00b9de9eb0b7b2a3b765160349d4be698f3
hanhwack <hanhwack93@gmail.com>
2023-04-05 14:47:33 +0900

    feat: 기초 문제 종합 1번 리팩터링 [BASICx]

---
+++ Java/hanhwack/java/renew/src/homework/basicProblem/BasicMultiple.java
@@ -0,0 +1,13 @@
+package homework.basicProblem;
+
+public class BasicMultiple {
+    public static void main(String[] args) {
+        MultipleCalculate multipleCalculate = new MultipleCalculate();
+        multipleCalculate.setNum(2);
+        multipleCalculate.getNum();
+        multipleCalculate.setNum(3);
+        multipleCalculate.getNum();
+        multipleCalculate.setNum(4);
+        multipleCalculate.getMultipleSum();
+    }
+}
--- Java/hanhwack/java/renew/src/homework/basicProblem/sellApple.java
+++ Java/hanhwack/java/renew/src/homework/basicProblem/sellApple.java
@@ -1,4 +1,4 @@
-package homework;
+package homework.basicProblem;
 
 class Random {
 
