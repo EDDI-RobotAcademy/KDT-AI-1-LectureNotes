22104318a3850295dc1e778e90487f1e5500081f
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-04-06 12:29:14 +0900

    feat: [김재경] 5번 주사위 문제 풀이[DICE-PROB]

---
+++ Java/JaekyoungKim/java/renew/src/finalDice/manager/FinalDiceGameManager.java
@@ -0,0 +1,123 @@
+package finalDice.manager;
+
+import finalDice.dice.RefactorDice;
+import finalDice.player.RefactorDiceGamePlayer;
+
+import finalDice.score.RefactorScore;
+import jdk.swing.interop.SwingInterOpUtils;
+import utility.random.CustomRandom;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FinalDiceGameManager {
+    final private int PLAYER_NUM = 3;
+    final private List<RefactorDiceGamePlayer> playerList = new ArrayList<>();
+
+    public FinalDiceGameManager() {
+        for (int i = 0; i < PLAYER_NUM; i++) {
+            playerList.add(new RefactorDiceGamePlayer("플레이어" + (i + 1)));
+            System.out.println(playerList.get(i));
+        }
+    }
+
+    private int findTargetPlayerIndex (int currentPlayerIdx) {
+        int targetPlayerIdx = 0;
+
+        do {
+            targetPlayerIdx = CustomRandom.generateNumber(PLAYER_NUM - 1);
+        } while (currentPlayerIdx == targetPlayerIdx);
+
+        return targetPlayerIdx;
+    }
+
+    private int findSpecialDiceNumber (int playerIndex) {
+        final int ARRAY_BIAS = 1;
+        final int SPECIAL_DICE_INDEX = 3 - ARRAY_BIAS;
+
+        RefactorDice currentPlayerSpecialDice =
+                playerList.get(playerIndex).getSelectedGameDice(SPECIAL_DICE_INDEX);
+
+        if (currentPlayerSpecialDice == null) { return 0; }
+
+        int currentPlayerSpecialDiceNumber =
+                currentPlayerSpecialDice.getDiceNumber();
+
+        return currentPlayerSpecialDiceNumber;
+    }
+
+    public void playGame() {
+        for (int currentPlayerIdx = 0; currentPlayerIdx < PLAYER_NUM; currentPlayerIdx++) {
+            final int currentPlayerSpecialDiceNumber = findSpecialDiceNumber(currentPlayerIdx);
+
+            if (currentPlayerSpecialDiceNumber == 0) { continue; }
+
+            final RefactorScore currentPlayerScore =
+                    playerList.get(currentPlayerIdx).getGameScore();
+
+            applyAbilityOfSpecialDice(
+                    currentPlayerIdx,
+                    currentPlayerSpecialDiceNumber,
+                    currentPlayerScore);
+        }
+    }
+
+    private void applyAbilityOfSpecialDice(
+            int currentPlayerIdx,
+            int currentPlayerSpecialDiceNumber,
+            RefactorScore currentPlayerScore
+    ) {
+        final int STEAL = 1;
+        final int BUFF = 3;
+        final int DEATH = 4;
+
+        final int STEAL_SCORE = 3;
+        final int BUFF_SCORE = 2;
+        final int DEATH_SCORE = -1;
+
+        switch (currentPlayerSpecialDiceNumber) {
+            case STEAL:
+                final int targetPlayerIndex = findTargetPlayerIndex(currentPlayerIdx);
+                final RefactorScore targetPlayerScore =
+                        playerList.get(targetPlayerIndex).getGameScore();
+                targetPlayerScore.takeScore(currentPlayerScore, STEAL_SCORE);
+                break;
+
+            case BUFF:
+                currentPlayerScore.addScore(BUFF_SCORE);
+                break;
+
+            case DEATH:
+                currentPlayerScore.loseAll(DEATH_SCORE);
+                break;
+        }
+    }
+
+    public void printResult() {
+        for (int i = 0; i < PLAYER_NUM; i++) {
+            System.out.println(playerList.get(i));
+        }
+    }
+
+    public void checkWinner() {
+        final int WINNER_IDX = 0;
+        final int SECOND_IDX = 1;
+        final int DRAW = 0;
+        //RefactorPlayer가 Comparable<RefactorPalyer>를 가지고 있는데
+        // Comparable<RefactorPalyer>강 ㅓㅄ<.
+
+        Collections.sort(playerList, Collections.reverseOrder());
+        System.out.println(playerList.get(0));
+
+        RefactorDiceGamePlayer winner = playerList.get(WINNER_IDX);
+        //무승부 고려함
+        RefactorDiceGamePlayer second = playerList.get(SECOND_IDX);
+        if (winner.compareTo(second) == DRAW) {
+            System.out.println("무승부!");
+            return;
+        }
+        System.out.println("승리: " + winner.getName());
+        System.out.println("전적 상황: " + playerList);
+        // TODO: 확장성 문제가 존재함 추후 사용자 숫자 증대시 리팩토링 필요
+    }}
