83b034c23cdd7aece6606a77660bef9285a1783f
cluypkoj <cluypkoj68@gmail.com>
2023-04-07 15:06:38 +0900

    feat: [백지우] Java 1차 종합 평가 문제 [Test]

---
+++ Java/JiWooBaek/Exam/evaluation/src/problems/prob22/Problem22.java
@@ -0,0 +1,23 @@
+package problems.prob22;
+
+
+// 22번 문제입니다.
+public class Problem22 {
+    public static void main(String[] args) {
+        // 이론 문제입니다.
+
+        // 배열과 리스트는 Stack와 Heap의 차이점으로 설명할 수 있습니다.
+        // 예로 들어, int[] numberArray = {1,2,3,4,5};와 같이
+        // Stack은 Loop { final int data } 형태가 있다면 data 변수가
+        // 루프마다 초기화 되는 것을 볼 수 있는데, 이러한 지역 변수 특성을
+        // 가지는 것이 Stack의 특성입니다.
+        // 반면, 리스트의 경우, new를 통해 할당하는 경우를 통해 Heap라는 공간에
+        // 할당하며 Heap에 설정되는 정보들은 메모리에 상주하게 되어 데이터에
+        // 접근할 수 있습니다.
+        // 결론적으로 Stack은 중괄호{}내에서 사용되는 것이 특징이며
+        //          Heap는 new 이후에 사용됩니다.
+
+
+        //
+    }
+}
