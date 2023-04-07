47aa4cb761ef7c26d7f93d799d21eab53836046d
iknowca <iknowca@gmail.com>
2023-04-07 15:13:30 +0900

    fix: [김원기] 11번 문제 divider 수정 [TEST-1]

--- Java/WonkiKim/java/ComprehensiveEvaluation/TestFormat/src/problems/prob11/Problem11.java
+++ Java/WonkiKim/java/ComprehensiveEvaluation/TestFormat/src/problems/prob11/Problem11.java
@@ -6,7 +6,7 @@ public class Problem11 {
     public static void main(String[] args) {
         final int START = 50;
         final int END = 100;
-        final int DIVIDER = 7;
+        final int DIVIDER = 2;
         final int modifiedStart = (START%DIVIDER==0)?START/DIVIDER:START/DIVIDER+1;
         SumCalculator sumCalculator = new SumCalculator();
         System.out.println(sumCalculator.summation(modifiedStart, (int) (END/DIVIDER))*DIVIDER);
