aacafa87b2cdf4141b0952aa507a4a6e791544fb
bbbii <hyeongjin1326@gmail.com>
2023-04-07 15:11:37 +0900

    feat: [김형진] JAVA 1차 종합 평가 문제 [EXAMㅌ]

---
+++ Java/HyeongJinKim/exam/evauation/src/problems/prob6/Problem6.java
@@ -0,0 +1,11 @@
+package problems.prob6;
+
+import customLibrary.utility.Multiple;
+
+public class Problem6 {
+    public static void main(String[] args) {
+        System.out.println(Multiple.generateMultipleList(50, 100, 5));
+    }
+}
+
+// 6. 50 ~ 100까지 숫자 중 5의 배수를 출력해보세요.
