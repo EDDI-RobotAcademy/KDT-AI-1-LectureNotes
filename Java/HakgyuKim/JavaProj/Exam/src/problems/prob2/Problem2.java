184c97a54dad403a552152cbf25689a722f4565b
Hakgyu Kim <zage4448@gmail.com>
2023-04-07 15:15:59 +0900

    feat: [김학규] 평가 제출 [EXAMx]

---
+++ Java/HakgyuKim/JavaProj/Exam/src/problems/prob2/Problem2.java
@@ -0,0 +1,10 @@
+package problems.prob2;
+
+public class Problem2 {
+    public static void main(String[] args) {
+
+        Company company = new Company("Eddi주식회사", 200000000,
+                10,6000000);
+        System.out.println(company);
+    }
+}
---
+++ Java/HakgyuKim/JavaProj/Exam/src/problems/prob22/Problem22.java
@@ -0,0 +1,9 @@
+package problems.prob22;
+
+public class Problem22 {
+    public static void main(String[] args) {
+        // 배열과 리스트의 차이
+        // 배열은 정해진 공간이 있고 정해진 식별자가 존재하지만
+        // 리스트는 정해진 식별자가 없고 앞의 요소가 삭제되면 새로 추가되는 요소가 그 공간에 저장된다.
+    }
+}
