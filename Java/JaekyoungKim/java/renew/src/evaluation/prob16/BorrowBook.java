db2b39b31e20cf97bb5d94c0bf18ab49449d7c85
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-04-09 19:57:14 +0900

    feat: [김재경] 16번 24번 문제 주석 [HOMEWORKx]

---
+++ Java/JaekyoungKim/java/renew/src/evaluation/prob16/BorrowBook.java
@@ -0,0 +1,25 @@
+package evaluation.prob16;
+
+import utility.generator.IdGenerator;
+
+public class BorrowBook {
+    private final int id;
+    private final int bookId;
+    private final int accountId;
+
+    public BorrowBook(int bookId, int accountId) {
+        this.id = IdGenerator.getSpecificEntityId("BorrowBook");
+        this.bookId = bookId;
+        this.accountId = accountId;
+    }
+
+    @Override
+    public String toString() {
+        return "BorrowBook{" +
+                "id=" + id +
+                ", bookId=" + bookId +
+                ", accountId=" + accountId +
+                '}' + '\n';
+    }
+}
+
