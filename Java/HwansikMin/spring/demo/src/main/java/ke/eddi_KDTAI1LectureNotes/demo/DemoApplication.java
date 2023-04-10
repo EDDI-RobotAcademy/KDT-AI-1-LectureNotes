bc70037de1b1722a1fbb2bf2f71297cb70468c63
hwansik <hwansik08@gmail.com>
2023-04-10 10:35:09 +0900

    init: [민환식] 초기 설정 [SPRING]

---
+++ Java/HwansikMin/spring/demo/src/main/java/ke/eddi_KDTAI1LectureNotes/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package ke.eddi_KDTAI1LectureNotes.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ Java/HwansikMin/spring/demo/src/test/java/ke/eddi_KDTAI1LectureNotes/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ke.eddi_KDTAI1LectureNotes.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
