727745eb19ca2ae1a18a46f22f4a20ed542c9b4c
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-04-11 01:03:02 +0900

    add: [김진호] 내용 업데이트[update]

--- Java/JinHoKim/java/demo/src/test/java/kr/eddi/demo/DemoApplicationTests.java
+++ Java/JinHoKim/java/demo/src/test/java/kr/eddi/demo/DemoApplicationTests.java
@@ -2,12 +2,9 @@ package kr.eddi.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class DemoApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
