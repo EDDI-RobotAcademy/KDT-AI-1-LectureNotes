8f9d5106f8a2a51b6e58acc432ee60c52b646c3b
Hakgyu Kim <zage4448@gmail.com>
2023-04-10 12:16:42 +0900

    feat: [김학규] 간단한 HTML, JSON Controller 구성 [SPRINGx]

--- Spring/HakgyuKim/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Spring/HakgyuKim/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package kr.eddi.demo;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Spring/HakgyuKim/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Spring/HakgyuKim/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package kr.eddi.demo;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
