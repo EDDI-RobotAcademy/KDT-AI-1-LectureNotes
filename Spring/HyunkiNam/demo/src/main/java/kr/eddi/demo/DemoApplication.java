5544de3bfa1bf9500a72ecd64ad9780d9359cd3a
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-04-10 12:37:38 +0900

    init: [남현기] 간단한 HTML, JSON Controller 구성 [SPRINGx]

--- Spring/HyunkiNam/demo/src/main/java/kr/eddi/demo/DemoApplication.java
+++ Spring/HyunkiNam/demo/src/main/java/kr/eddi/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package kr.eddi.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,6 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 
 }
