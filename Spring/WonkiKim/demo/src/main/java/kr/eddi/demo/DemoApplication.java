75c392e20ce758839b01ea496a5f4e2f8fd4df07
iknowca <iknowca@gmail.com>
2023-04-10 10:37:45 +0900

    init: [김원기] 스프링부트 기본 설정 [SPRING]

---
+++ Spring/WonkiKim/demo/src/main/java/kr/eddi/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package kr.eddi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
