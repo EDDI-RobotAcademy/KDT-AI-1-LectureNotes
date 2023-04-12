c7b6cdf021664fdd4fb4a4c2746cd4e2813f26e8
hanhwack <hanhwack93@gmail.com>
2023-04-12 12:45:04 +0900

    feat: [한확] form 데이터 처리 방법 중간 세이브 [HTML-FORMx]

--- Vue/hanhwack/frontend/src/App.vue
+++ Vue/hanhwack/frontend/src/App.vue
@@ -4,11 +4,11 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <!--
-        router-lint 파트는 UI 엔지잉ㄴ Vue에서 경로를 지정
-        부가적으로 to라는 파라메터는 실제 URL에 입력하는 경로가 됩니다
+        router-link 파트는 UI 엔진인 Vue에서 경로를 지정
+        부가적으로 to라는 파라메터는 실제 URL에 입력하는 경로가 됩니다.
         나타나는 형태는 text 형태고 누르면 자동으로 URL을 입력한 결과를 보여줍니다.
         이 작업을 자동화 하려면 반드시 router/index.js에
-        /htmlTest를 누가 보여줄 것인지 컴폰너트 등록을 해줘야 합니다.
+        /htmlTest를 누가 보여줄 것인지 컴포넌트 등록을 해줘야 합니다.
       -->
       <router-link to="/htmlTest">HtmlTest</router-link>
     </nav>
@@ -17,6 +17,7 @@
 </template>
 
 <style scoped>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -37,4 +38,5 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
+
 </style>
