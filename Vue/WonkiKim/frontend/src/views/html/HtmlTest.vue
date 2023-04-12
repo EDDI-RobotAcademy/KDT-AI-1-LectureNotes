75dd003ceafa61a400ea29e6357650c1c13bfbe4
iknowca <iknowca@gmail.com>
2023-04-12 13:50:19 +0900

    feat: [김원기] VUE, HTML 기본설정 [VUEx]

---
+++ Vue/WonkiKim/frontend/src/views/html/HtmlTest.vue
@@ -0,0 +1,235 @@
+<template lang="">
+    <!-- vuein 입력하면 자동 완성 포맷으로 만들어짐 -->
+    <div>
+        <!-- h 태그 1 ~ 6까지 존재함 -->
+        <h3>실제 코드 형태로 입력해보기</h3>
+        <h6>원래는 코드 형태 출력이 아닌 실제 코드가 동작해야 합니다.</h6>
+        <h4>chatgpt 같은 것을 만든다면 필요할지도 모르겠네요.</h4>
+        <!-- 
+            강제 스타일 먹이기는 아래와 같이 style="css속성"
+            여기서 css 속성은 text-align으로
+            글자를 정렬할 때 사용하며 디폴트로 center 정렬이였으나
+            아래 function saveLocal .... 에 대해서는 left 정렬을 적용하는 작업입니다.
+        -->
+        <pre style="text-align: left;">
+            function saveLocalData () {
+                let second = document.getElementById("second")
+                let value = second.value
+                localStorage.setItem(1, value)
+                getLocalData()
+            }
+        </pre>
+<!-- strong은 강조표현, b도 강조 표현으로 BOLD체에 해당함-->
+        <p><strong>TypeScript</strong> <b>Javascript</b> Python</p>
+        <!--em과 i는 모두 이탤릭체를 표현함-->
+        <p><em>Rust</em>, <i>c</i>, Verilog</p>
+        <!--mark로 형광펜 효과를 낼 수 있음-->
+        <h2> Python <mark> TensorFlow</mark></h2>
+        <!-- span은 줄바꿈이 없습니다 - 특정 스타일을 적용할 때 많이 사용하는 편-->
+        <p>Deep Learning <span style="color: green;">keras </span></p>
+        <!-- ul 은 Unordered List의 약자로 순서가 필요하지 않은 경우 사용
+        li는 느낌 그대로 list에 해당함.-->
+        <ul style="text-align: left;">
+            <li>EDDI</li>
+            <li>Electric Car</li>
+            <li>MSA</li>
+            <li>Deep Learning</li>
+            <li>Distrivution Server</li>
+        </ul>
+
+        <ul style="text-align: left;">
+            <li>Vue
+                <ol type="a" start="3">
+                    <li>Vue Init</li>
+                    <li>Vue Router</li>
+                    <li>Vue Templeate</li>
+                    <li>Vue Script</li>
+                    <li>Vue props</li>
+                    <li>Vue v-model</li>
+                    <li>Vue axios</li>
+                    <li>vue Local Component</li>
+                    <li>Vue Global Component</li>
+                    <li>Vuetify</li>
+                    <li>Image Gallery</li>
+                    <li>File Upload</li>
+                    <li>Login/Logout</li>
+                    <li>V-Dialog</li>
+                    <li>Carousel</li>
+                    <li>Card</li>
+                    <li>Grid</li>
+                </ol>
+            </li>
+            <li>DDD
+                <ol type="i">
+                    <li>Domain Driven Development</li>
+                    <li>How to write Backlog</li>
+                    <li>Behavioral Domain</li>
+                    <li>Value Object</li>
+                    <li>Domain Service</li>
+                    <li>Repository</li>
+                </ol>
+            </li>
+            <li>Hexagonal Architecture
+                <ol>
+                    <li>Hexagonal Architecture</li>
+                    <li>Domain Hexagon</li>
+                </ol>
+            </li>
+            
+        </ul>
+        <!--table은 표를 만들 때 사용
+        가장 기본 패턴으로 table > tr > td 형식을 사용함
+        tr은 table-row에 해당하며 td 는 table-data로 cell혹은 column이라 봐도 무방
+        th는 table-head로 제목이나 메인 정보 같은 것을 배치하는 편-->
+        <table>
+            <caption><strong>Table Example</strong></caption>
+            <tr>
+                <th>1 row 1 column</th>
+                <td>1 row 2 column</td>
+                <td>1 row 3 column</td>
+            </tr>
+            <tr>
+                <th>2 row 1 column</th>
+                <td>2 row 2 column</td>
+                <td>2 row 3 column</td>
+            </tr>
+        </table>
+        <br>
+        <!-- colspan을 통해 합칠 열의 개수를 지정할 수 있음 -->
+        <table style="width: 600px">
+            <!-- caption을 통해서 table(표) 에 제목 느낌을 줄 수 있음-->
+            <caption>
+                <strong>Junior who pursue Modern Development</strong>
+                <p>Pursue agile process based development</p>
+            </caption>
+            <tr>
+                <th>
+                    Name
+                </th>
+                <td>이름</td>
+                <th>Contect</th>
+                <td>010-1234-5678</td>
+            </tr>
+            <tr>
+                <th>Address</th>
+                <td colspan="3"> 주소</td>
+            </tr>
+        </table>
+        <!--
+            Image를 추가하고 싶을 때는 아래와 같이 이미지가 위치하는 경로를 지정해주면 됩니다
+            부가적으로 download로 확보 했는지 아니면
+            실질적으로 서비스 차원에서 사용자가 등록한 이미지인지 같은 것을 디렉토리 이름으로 구별하면 좋습니다.₩
+        -->
+        <h2><strong>Vue Icon</strong></h2>
+        <img src="@/assets/logo.png" width="100">
+        <!-- figure로 image를 감싸고 figcaption 을 달면 image에 대한 설명을 붙일 수 있습니다. -->
+        <figure>
+            <img src="@/assets/logo.png" width="200">
+            <figcaption style="font-size: 32px;"> Vue Icon!</figcaption>
+        </figure>
+
+        <h1>Text Link 생성</h1>
+        <a href="https://golang.org">Golang Homepage</a>
+        <h1>이미지 링크 생성</h1>
+        <a href="https://python.org"><figure><img src="@/assets/logo.png"  alt=""><figcaption> to Python</figcaption></figure></a>
+        <!--
+            form 태그는 말 그대로 format을 지정하새ㅓ 데이터를 전송합니다.
+            method에 POST를 지정할 수 있고 default 값은 GET입니다.
+            input type에는 text, number, submit등등이 올 수 있습니다.
+            text는 말 그대로 문자열, number는 숫자, submit은 등록 개념이라 보면 됩니다.
+            속성값 value를 통해 표기되는 형태를 지정할 수 있습니다
+            추후 이 부분의 submit을 prevent하여 vue가 axios하도록 구성하게 됩니다
+        -->
+        <form action="/">
+            <input type="text" value ="찾을 값">
+            <input type="submit" title="찾기" value="찾기">
+        </form>
+        <!--
+            다중 선택 (checkbox)
+            input type으로 checkbox를 사용하여 value값에 선택한 값을 볼 수 있습니다.
+            javascript에서 어떤 값을 선택했는지 처리할 수 있고
+            이를 토대로 vue족에 axios하는 데이터를 처리할 수 있습니다.
+        -->
+        <h3>다중 선택</h3>
+        <form action="/" style="text-align: left">
+            <li>
+                <input type="checkbox" value="grammer">Grammar
+            </li>
+            <li>
+                <input type="checkbox" value="writing">Writing
+            </li>
+            <li>
+                <input type="checkbox" value="reading">Reading
+            </li>
+
+        </form>
+        <!--
+            단일 선택(radio)
+            input 타입으로 radio를 사용하여 value값에 선택한 값을 볼 수 있습니다
+            단일 선택이 되도록 명확하게 지정하려면 name값을 모두 같은 것으로 지정하여 중복 선택이 불가하게 만듭니다.
+            마찬가지로 javascript에서 어떤 값을 선택했는지 처리할 수 있고
+            이를 토대로 vue쪽에 axios하는 데이터를 처리할 수 있습니다
+        -->
+        <form action="/" style="text-align: left">
+
+        <h4>단일 선택</h4>
+        <li>
+                <input type="radio" name="Lang" value="rust">Rust
+            </li>
+            <li>
+                <input type="radio" name="Lang" value="typescript">TypeScript
+            </li>
+            <li>
+                <input type="radio" name="Lang" value="kotlin">Kotlin
+            </li>
+        </form>
+
+        <!--
+            email및 password입력이 필요한 상황
+        -->
+        <form action="/" style="text-align: left">
+
+            <fieldset>
+                <legend>log in</legend>
+                <ul>
+                    <li>
+                        <label for="email">email: </label>
+                        <input type="text" name="" id="">
+                    </li>
+                    <li>
+                        <label for="pwd">password: </label>
+                        <input type="password" id="pwd">
+                    </li>
+                </ul>
+            </fieldset>
+        </form>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    
+}
+
+</script>
+
+<style scoped>
+    table{
+        border: 2px solid #ccc
+    }
+    th {
+        border: 2px solid #0000FF
+    }
+     td {
+        border: 1px solid #aaa
+    }
+
+    th {
+        padding: 15px; /* 셀 테두리와 내용 사이의 간경(패딩) */
+    }
+    td {
+        padding: 10px;
+    }
+</style>
