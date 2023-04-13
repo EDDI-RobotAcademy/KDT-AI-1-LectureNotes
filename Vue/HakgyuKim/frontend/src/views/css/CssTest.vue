07d95df9d31af7343c6798796a5258e425a96e8d
Hakgyu Kim <zage4448@gmail.com>
2023-04-13 11:26:44 +0900

    feat: [김학규] 그림자 효과 주기 [CSS-SHADOW]

--- Vue/HakgyuKim/frontend/src/views/css/CssTest.vue
+++ Vue/HakgyuKim/frontend/src/views/css/CssTest.vue
@@ -14,6 +14,10 @@
             <p class="ns-font">한글 폰트 테스트 - 밤색이다!</p>
         </div>
         <p><a href="https://threejs.org/"><b>Three.js</b></a></p>
+        <h1 class="shadow-option1">Micro-Frontend</h1>
+        <h1 class="shadow-option2">Micro-Frontend</h1>
+        <h1 class="shadow-option3">Micro-Frontend</h1>
+        <h1 class="shadow-option4">Micro-Frontend</h1>
     </div>
 </template>
 
@@ -59,5 +63,26 @@ export default {
     }
     a {
         text-decoration: none;
+    }
+    .shadow-option1 {
+        color: orange;
+        text-shadow: 1px 1px;
+    }
+    .shadow-option2 {
+        color: orange;
+        text-shadow: 5px 5px 3px black;
+    }
+    .shadow-option3 {
+        color: white;
+        text-shadow: 4px -4px 5px red;
+    }
+    .shadow-option4 {
+        background-color: black;
+        color: #000;
+        text-shadow: 0 0 8px #ccc, 
+                     0 -5px 4px #ff3, 
+                     -2px -10px 4px #fd3, 
+                     -2px -15px 11px #f80,
+                     -2px -18px 8px red;
     }
 </style>
