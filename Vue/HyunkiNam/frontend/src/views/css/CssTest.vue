dd2a38db30b81759c1a65da5dbda54a0543602fb
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-04-13 10:44:40 +0900

    feat: [남현기] id 및 class 관련 CSS 설정 [CSS-ID-CLASS]

--- Vue/HyunkiNam/frontend/src/views/css/CssTest.vue
+++ Vue/HyunkiNam/frontend/src/views/css/CssTest.vue
@@ -3,6 +3,15 @@
         <h1>Css Test</h1>
         <h2>Web Doc</h2>
         <p>Content: HTML, Design: CSS, Data Handle: JavaScript(TypeScript)</p>
+        <section>
+            <h2>Who am I?</h2>
+            <p>Interest at MSA</p>
+        </section>
+        <div id="container">
+            <h1>Test id</h1>
+            <h2 class="accent"> </h2>
+            <p>안녕! <span class="browntext">밤색이닷!</span></p>
+        </div>
     </div>
 </template>
 <script>
@@ -17,5 +26,25 @@ export default {
     }
     p{
         color: blue;
+    }
+    section{
+        width: 96%;
+        padding: 15px;
+        border: 5px solid gray;
+    }
+    /* id 설정의 경우 # */
+    #container{
+        width: 600px;
+        padding: 15px;
+        border: 1px dotted gray;
+    }
+    /* class의 경우엔 아래와 같이 .*/
+    h2.accent {
+        background-color: aquamarine;
+        color: black;
+        padding: 5px;
+    }
+    .browntext {
+        color: brown;
     }
 </style>
