1364f103e4b677930a68d304b8cee7aa5612069c
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-04-13 14:38:55 +0900

    feat: [이상원] Vue Event 처리 [VUE-EVENT]

---
+++ Java/SangWonLee/renew/src/Constructor.java
@@ -0,0 +1,13 @@
+public class Constructor {
+
+//    public Constructor(){}
+
+    public static void main(String[] args) {
+
+        Constructor cs = new Constructor();
+
+
+
+
+    }
+}
---
+++ Vue/SangWonLee/frontend/src/views/basics/event/VueClickEventPage.vue
@@ -0,0 +1,29 @@
+<template lang="">
+    <div>
+        <p>{{ count }} 번 클릭했습니다.</p> 
+                <button @click="clickHandler">카운트하기</button><br>
+        <button @click="alertHandler">경고 메시지 출력하기</button><br>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            count: 0,
+        }
+    },
+    methods: {
+        clickHandler() {
+            this.count++
+        },
+
+        alertHandler() {
+            alert('누르지마!')
+        },
+    }
+
+}
+</script>
+<style lang="">
+    
+</style>
