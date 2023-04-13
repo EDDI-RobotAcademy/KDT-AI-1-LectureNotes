152254f9ce20c461fa4d843fb2e113b6fb4c2167
Hakgyu Kim <zage4448@gmail.com>
2023-04-13 10:08:25 +0900

    feat: [김학규] CssTest 기본설정 [CSS]

---
+++ Vue/HakgyuKim/frontend/src/views/css/CssTest.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>Css Test</h1>
+        <h2>Web Doc</h2>
+        <p>Content: HTML, Design: CSS, Data Handle: JavaScript(TypeScript)</p>
+        <section>
+            <h2>Who am I ?</h2>
+            <p>Interest at MSA</p>
+        </section>
+    </div>
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
+
+<style scoped>
+    h2 {
+        font-size: 20px;
+        color: orange;
+    }
+    p {
+        color: blue;
+    }
+    section {
+        width: 96%;
+        padding: 15px;
+        border: 5px solid gray;
+    }
+    
+</style>
