2e06af52325f9ae10a08cf5d7337894a5b8c9705
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-04-13 15:21:38 +0900

    add: [장현민]Vue 추가[VUEx]

--- Vue/Changhyunmin/frontend/src/router/index.js
+++ Vue/Changhyunmin/frontend/src/router/index.js
@@ -3,6 +3,10 @@ import VueRouter from 'vue-router'
 // @는 현재 src 폴더를 의미함
 import HomeView from '@/views/HomeView.vue'
 import HtmlTest from '@/views/html/HtmlTest.vue'
+import CssTest from '@/views/css/CssTest'
+import VueDataBindingPage from '@/views/basics/binding/VueDataBindingPage'
+import VueClickEventPage from '@/views/basics/event/VueClickEventPage'
+import VueInputEventPage from '@/views/basics/event/VueInputEventPage'
 Vue.use(VueRouter)
 
 const routes = [
@@ -23,7 +27,29 @@ const routes = [
     path: '/htmlTest',
     name: 'HtmlTest',
     component: HtmlTest
+  },
+  {
+    path: '/CssTest',
+    name: 'CssTest',
+    component: CssTest
+
+  },
+  {
+    path: '/vue-data-binding-page',
+    name: 'VueDataBindingPage',
+    component: VueDataBindingPage
+  },
+  {
+    path: '/vue-click-binding-page',
+    name: 'VueClickEventPage',
+    component: VueClickEventPage
+  },
+  {
+    path: '/vue-input-binding-page',
+    name: 'VueInputEventPage',
+    component: VueInputEventPage
   }
+  
 ]
 
 const router = new VueRouter({
--- Vue/Changhyunmin/frontend/src/views/html/HtmlTest.vue
+++ Vue/Changhyunmin/frontend/src/views/html/HtmlTest.vue
@@ -85,7 +85,7 @@
             tr은 table-row에 해당하며 td는 table-date로 cell혹은 column이라 봐도 무방
             th는 table-heat로 재목이나 메인 정보 같은 것을 배치하는 편
          -->
-         
+        
          <table>
             <tr>
                 <th>1 row 1 column</th>
@@ -125,62 +125,62 @@
          <h2>Image</h2>
          <img src="@/assets/logo.png">
          <!-- figure image를 감싸고 figcaption을 달면 image에 대한 설명을 붙일 수 있습니다.-->
-        <figure>
+         <figure>
             <img src="@/assets/logo.png">
-            <figcaption style = "font-size: 32px;">마리오가 달리다!</figcaption>
+            <figcaption style="font-size: 32px;">마리오가 달린다!</figcaption>
         </figure>
-        <h1>Text Link</h1>
-        <a href ="https://www.nintendo.co.kr/character/mario/">Golang Homepage</a>
+        <h1>Text Link 생성</h1>
+        <a href="https://golang.org">Golang Homepage</a>
         <h1>이미지 링크 생성</h1>
-        <a href ="https://www.nintendo.co.kr/character/mario/">
+        <a href="https://mario.nintendo.com/">
             <img src="@/assets/logo.png">
         </a>
-        <!-- 
-            form 태그는 말 그대로 format(양식)을 저장해서 데이터를 전송합니다.
-            method에 post 지정할 수 있고 default 값은 get입니다.
-
-            input tupe에는 text, number, submit 등등이 올 수 있습니다.
+        <!--
+            form 태그는 말 그대로 format(양식)을 지정해서 데이터를 전송합니다
+            method에 post를 지정할 수 있고 default 값은 get입니다.
+            input type에는 text, number, submit 등등이 올 수 있습니다.
             text는 말 그대로 문자열, number는 숫자, submit은 등록 개념이라 보면 됩니다.
-            추후 이 부분의 submit을 prevent 하여 vue가 axios하도록 구성하게 됩니다.
-    -->
+            속성값 value를 통해 표기되는 형태를 지정할 수 있습니다.
+            추후 이 부분의 submit을 prevent 하여 vue가 axios 하도록 구성하게 됩니다.
+        -->
         <form action="/">
             <input type="text" value="찾을 값">
             <input type="submit" value="찾기">
         </form>
         <!--
-            다중선택 "checkbox"
+            다중 선택(checkbox)
             input type으로 checkbox를 사용하여 value 값에 선택한 값을 볼 수 있습니다.
             javascript에서 어떤 값을 선택 했는지 처리할 수 있고
-            이를토대로 vue쪽에 axios 하는 데이터를 처리할 수 있습니다.
+            이를 토대로 vue쪽에 axios 하는 데이터를 처리할 수 있습니다.
         -->
-
-        <form action="/" style="text-align: left">
+        <h3>다중 선택</h3>
+        <form action="/" style="text-align: left;">
             <li><input type="checkbox" value="grammar">Grammar</li>
-            <li><input type="checkbox" value="writng">Writng</li>
+            <li><input type="checkbox" value="writing">Writing</li>
             <li><input type="checkbox" value="reading">Reading</li>
         </form>
         <!--
-            단일선택(radio)
-            input type으로 radio 사용하여 value 값에 선택한 값을 볼 수 있습니다.
+            단일 선택(radio)
+            input type으로 radio를 사용하여 value 값에 선택한 값을 볼 수 있습니다.
             단일 선택이 되도록 명확하게 지정하려면
-            name값을 모두 같은것으로 지정하여 중복선택이 불가하게 만듭니다
+            name값을 모두 같은 것으로 지정하여 중복 선택이 불가하게 만듭니다.
             마찬가지로 javascript에서 어떤 값을 선택 했는지 처리할 수 있고
             이를 토대로 vue쪽에 axios 하는 데이터를 처리할 수 있습니다.
         -->
         <h4>단일 선택</h4>
-            <form action="/" style="text-align: left">
+        <form action="/" style="text-align: left;">
             <li>
-                <label><input type="radio" Name="subject" value="rust">Rust</label>
+                <label><input type="radio" name="subject" value="rust">Rust</label>
             </li>
             <li>
-            <label><input type="radio" Name="subject" value="TypeScropt">TypeScropt</label>
+                <label><input type="radio" name="subject" value="typescript">TypeScript</label>
             </li>
             <li>
-            <label><input type="radio" Name="subject" value="Kotlin">Kotlin</label>
+                <label><input type="radio" name="subject" value="Kotlin">Kotlin</label>
             </li>
         </form>
-        <!-- email 및 password 입력이 필요한 상황-->
-        <form>
+        <!-- email 및 password(비밀번호) 입력이 필요한 상황 -->
+        <form style="text-align: left;">
             <fieldset>
                 <legend>로그인</legend>
                 <ul>
@@ -195,8 +195,110 @@
                 </ul>
             </fieldset>
         </form>
+        <form style="text-align: left;">
+            <fieldset>
+                <legend>범주 테스트</legend>
+                <ul>
+                    <li>
+                        <label for="member"></label>
+                        <!--
+                            input type="number"를 통해 숫자를 인식
+                            value에 초기값, min은 최소값, max는 최대값,
+                            화살표 UI가 제공되는데 누를 때마다 이동하는 숫자의 크기는 step
+                        -->
+                        <input type="number" id="member" value="1" min="1" max="10" step="1">
+                    </li>
+                    <li>
+                        <label for="satisfy"></label>
+                        <!--
+                            range를 사용하면 막대 바가 생성됨
+                            최소값, 최대값, 초기값, step 설정은 위의 number와 완벽하게 동일함
+                        -->
+                        <input type="range" id="satisfy" value="1" min="1" max="5" step="1">
+                    </li>
+                </ul>
+            </fieldset>
+        </form>
+        <!-- email 및 password(비밀번호) 기반 회원 가입 -->
+        <form style="text-align: left;">
+            <fieldset>
+                <legend>회원 가입</legend>
+                <ul>
+                    <li>
+                        <label for="email">이메일: </label>
+                        <input type="text" id="email">
+                    </li>
+                    <li>
+                        <label for="pwd">비밀번호: </label>
+                        <input type="password" id="pwd">
+                    </li>
+                </ul>
+            </fieldset>
+
+            <input type="submit" value="회원 가입">
+            <!-- 입력을 잘못 했을 경우 정보를 초기화 합니다 -->
+            <input type="reset" value="초기화">
+        </form>
+        <!-- checkbox, radio로 표현하기에 너무 많은 경우 -->
+        <form style="text-align: left;">
+            <fieldset>
+                <legend>과목 선택</legend>
+                <ul>
+                    <li>
+                        <label for="subject">과목: </label>
+                        <!--
+                            select의 경우엔 드롭 다운 형태의 목록을 만들 수 있습니다.
+                            multiple을 선택하여 여러 옵션들을 복수 선택 할 수 있습니다.
+                            size는 눈에 보이는 리스트의 개수입니다.
+                        -->
+                        <select size="5" id="subject" multiple>
+                            <!-- option에는 실제 배치하고 싶은 옵션 리스트를 작성합니다. -->
+                            <option value="architecture">소프트웨어 아키텍처</option>
+                            <option value="ddd">DDD 설계</option>
+                            <option value="clean">Clean 아키텍처</option>
+                            <option value="hexagonal">Hexagonal 아키텍처</option>
+                            <option value="msa">MSA 설계</option>
+                            <option value="rxjava">Reactive Java Programming</option>
+                            <option value="r2dbc">R2DBC Reactive DB System</option>
+                            <option value="jpa">Reactive에서는 사용할 수 없는 JPA</option>
+                            <option value="mfe">Micro Frontend Service</option>
+                            <option value="edm">Event Driven MSA</option>
+                            <option value="kafka">Kafka 기반 메시지 브로커 시스템</option>
+                        </select>
+                    </li>
+                </ul>
+            </fieldset>
+        </form>
+        <!-- progress바 만들기 -->
+        <ul>
+            <li>
+                <label>10초 남았음</label>
+                <progress value="50" max="60"></progress>
+
+            </li>
+            <li>
+                <label>30% 채웠음</label>
+                <progress value="30" max="100"></progress>
+            </li>
+        </ul>
+        <!-- meter 만들기: 값의 크기를 표시-->
+        <ul>
+            <li>
+                <label> 투명도 0.8</label>
+                <meter value="0.8"></meter>
+            </li>
+            <li>
+                <label> 사용률 64%</label>
+                <meter min="0" max="100" value="64"></meter>
+            </li>
+            <li>
+                <label> 트래픽 초과까지</label>
+                <meter min="1024" max="16384" value="9277"></meter>
+            </li>
+        </ul>
     </div>
 </template>
+
 <script>
 export default {
     
@@ -211,5 +313,10 @@ export default {
     }
     td{
         padding: 7px;
+    }
+    ul li {
+        list-style: none;
+        margin: 15px 0;
+        font-size: 16px;
     }
 </style>
