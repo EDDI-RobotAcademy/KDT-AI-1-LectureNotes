12215bb692a913779f9e1d00af16c5dc06004407
hanhwack <hanhwack93@gmail.com>
2023-04-13 14:57:50 +0900

    feat: [한확] Vue 마우스 클릭 이벤트 처리 [MOUSE-EVENT]

--- Vue/hanhwack/frontend/src/views/basics/event/VueClickEventPage.vue
+++ Vue/hanhwack/frontend/src/views/basics/event/VueClickEventPage.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
         <p>{{ count }} 번 클릭했습니다!</p>
-        <button @click="clickHandler">카운트 하기</button>
+        <button @click="clickHandler">카운트 하기</button><br>
+        <button @click="alertHandler">경고 메시지 출력하기</button><br>
+        <button @click.right="rightClickHandler">오른쪽 클릭</button>
+        <button @click.left="leftClickHandler">왼쪽 클릭</button>
+
     </div>
 </template>
 
@@ -11,11 +15,21 @@ export default {
     data () {
         return {
             count : 0,
+            
         }
     },
     methods: {
         clickHandler () {
             this.count++
+        },
+        alertHandler () {
+            alert('누르지마!')
+        },
+        rightClickHandler (comment) {
+            console.log('(' + comment.clientX + ', ' + comment.clientY + ')')
+        },
+        leftClickHandler (comment) {
+            console.log('(' + comment.clientX + ', ' + comment.clientY + ')')
         }
     },
     
---
+++ Vue/hanhwack/frontend/src/views/basics/event/VueInputEventPage.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <p><input :value="message" @change="handleInput"></p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data () {
+        return {
+            message : '안녕하세요'
+            
+        }
+    },
+    methods: {
+        
+        handleInput (event) {
+            console.log('event: ' + event)
+            this.message = event.target.value
+            console.log('event.target.value: ' + event.target.value)
+        }
+    },
+    
+}
+</script>
+<style>
+    
+</style>
