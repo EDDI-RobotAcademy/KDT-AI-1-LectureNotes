ad31be47aaa90481b4295feba3e5c2c3dda85b41
bbbii <hyeongjin1326@gmail.com>
2023-04-13 14:29:21 +0900

    feat: [김형진] Vue Event 처리 [VUE-EVENT]

--- Vue/HyeongJinKim/frontend/src/views/basics/event/VueClickEventPage.vue
+++ Vue/HyeongJinKim/frontend/src/views/basics/event/VueClickEventPage.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <p>{{ count }} 번 클릭했습니다!</p>
-    <button @click="clickHandler">카운트하기</button>
+    <button @click="clickHandler">카운트하기</button><br />
+    <button @click="alertHandler">경고 메시지 출력하기</button><br />
   </div>
 </template>
 
@@ -16,7 +17,11 @@ export default {
     clickHandler() {
       this.count++;
     },
+    alertHandler() {
+      alert("누르지마!");
+    },
   },
 };
 </script>
+
 <style></style>
