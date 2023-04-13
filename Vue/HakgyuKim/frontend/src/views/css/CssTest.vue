a4191c09d8670e569e560014b4e512e55cc0e8c3
Hakgyu Kim <zage4448@gmail.com>
2023-04-13 11:12:31 +0900

    feat: [김학규] a 태그에 밑줄 없애기 [CSS-A-NODECOR]

--- Vue/HakgyuKim/frontend/src/views/css/CssTest.vue
+++ Vue/HakgyuKim/frontend/src/views/css/CssTest.vue
@@ -13,6 +13,7 @@
             <p>안녕! <span class="browntext">밤색이다!</span></p>
             <p class="ns-font">한글 폰트 테스트 - 밤색이다!</p>
         </div>
+        <p><a href="https://threejs.org/"><b>Three.js</b></a></p>
     </div>
 </template>
 
@@ -56,6 +57,7 @@ export default {
         font-family: 'Noto Sans KR', sans-serif;
         font-size: 30px;
     }
-
-    
+    a {
+        text-decoration: none;
+    }
 </style>
