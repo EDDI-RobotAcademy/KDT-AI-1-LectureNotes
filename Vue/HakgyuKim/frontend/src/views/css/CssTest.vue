56e41dd0334fc717b261bf6709a0a48499c4ff6b
Hakgyu Kim <zage4448@gmail.com>
2023-04-13 10:32:07 +0900

    feat: [김학규] id 및 class 관련 CSS 설정 [CSS-CLASS-ID]

--- Vue/HakgyuKim/frontend/src/views/css/CssTest.vue
+++ Vue/HakgyuKim/frontend/src/views/css/CssTest.vue
@@ -7,6 +7,11 @@
             <h2>Who am I ?</h2>
             <p>Interest at MSA</p>
         </section>
+        <div id="container">
+            <h1>Test id</h1>
+            <h2 class="accent">Accent Test</h2>
+            <p>안녕! <span class="browntext">밤색이다!</span></p>
+        </div>
     </div>
 </template>
 
@@ -29,6 +34,21 @@ export default {
         width: 96%;
         padding: 15px;
         border: 5px solid gray;
+    }
+    /* id 설정의 경우 # */
+    #container {
+        width: 600px;
+        padding: 15px;
+        border: 1px dotted gray;
+    }
+    /* class의 경우엔 아래와 같이 . */
+    h2.accent {
+        background-color: aquamarine;
+        color: black;
+        padding: 5px;
+    }
+    .browntext {
+        color: brown;
     }
     
 </style>
