e94ef99d638c25ecadadc1481fba07507c39eaed
Sanghoon Lee <gcccompil3r@gmail.com>
2023-04-13 11:35:13 +0900

    feat: [이상훈] justify(양쪽 정렬) [CSS-JUSTIFY]

--- Vue/SanghoonLee/frontend/src/views/css/CssTest.vue
+++ Vue/SanghoonLee/frontend/src/views/css/CssTest.vue
@@ -18,6 +18,10 @@
         <h1 class="shadow-option2">Micro-Frontend</h1>
         <h1 class="shadow-option3">Micro-Frontend</h1>
         <h1 class="shadow-option4">Micro-Frontend</h1>
+        <p class="align-right">오른쪽 정렬</p>
+        <p class="align-left">오른쪽 정렬</p>
+        <p class="align-center">가운데 정렬: 으아나ㅓㄹ아너 인오링ㅁ홈ㅇㄹ허밍림ㄴ 얾닝럼ㄴㅇ리멍 린ㅁㅇ러ㅣㅁ어밇 ㅁ얼힌ㅇㄹ 혼이ㅏㅓ ㄹ화ㅣㅇㄴㅀㄴ 일힌ㅇㄹ혼 잃노일혼 이랗ㄴ 힌ㅇㄹ회</p>
+        <p class="align-justify">양쪽 정렬: 으아나ㅓㄹ아너 인오링ㅁ홈ㅇㄹ허밍림ㄴ 얾닝럼ㄴㅇ리멍 린ㅁㅇ러ㅣㅁ어밇 ㅁ얼힌ㅇㄹ 혼이ㅏㅓ ㄹ화ㅣㅇㄴㅀㄴ 일힌ㅇㄹ혼 잃노일혼 이랗ㄴ 힌ㅇㄹ회</p>
     </div>
 </template>
 
@@ -86,5 +90,17 @@ export default {
             2px -10px 6px #fd3,
             -2px -15px 11px #f80,
             2px -19px 18px #f20;
+    }
+    .align-left {
+        text-align: left;
+    }
+    .align-right {
+        text-align: right;
+    }
+    .align-center {
+        text-align: center;
+    }
+    .align-justify {
+        text-align: justify;
     }
 </style>
