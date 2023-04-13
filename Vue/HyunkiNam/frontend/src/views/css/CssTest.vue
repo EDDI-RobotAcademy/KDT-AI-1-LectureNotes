db84d07bd115f5cf4a9660e32a0f95cda83b1607
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-04-13 09:37:52 +0900

    feat: [남현기] 기본적인 CSS 사용 방법 [CSS-BASIC]

---
+++ Vue/HyunkiNam/frontend/src/views/css/CssTest.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Css Test</h1>
+        <h2>Web Doc</h2>
+        <p>Content: HTML, Design: CSS, Data Handle: JavaScript(TypeScript)</p>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    h2{
+        font-size: 20px;
+        color: orange;
+    }
+    p{
+        color: blue;
+    }
+</style>
