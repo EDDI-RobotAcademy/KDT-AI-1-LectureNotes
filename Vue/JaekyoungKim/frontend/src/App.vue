c4a9b6b73b6a12eeae93fb42b8f813fad879700d
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-04-13 11:35:33 +0900

    feat: [김재경] justify 양쪽 정렬[CSS]

--- Vue/JaekyoungKim/frontend/src/App.vue
+++ Vue/JaekyoungKim/frontend/src/App.vue
@@ -26,9 +26,9 @@ nav {
 }
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #364a5e;
 }
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #641822;
 }
 </style>
--- Vue/JaekyoungKim/frontend/src/views/css/CssTest.vue
+++ Vue/JaekyoungKim/frontend/src/views/css/CssTest.vue
@@ -14,6 +14,14 @@
             <p class="ns-font">한글 폰트 테스트 - 밤색이닷!</p>
         </div>
         <p><a href="https://threejs.org">ThreeJs</a></p>
+        <h1 class="shadow-option1">Micro-Frontend</h1>
+        <h1 class="shadow-option2">Micro-Frontend</h1>
+        <h1 class="shadow-option3">Micro-Frontend</h1>
+        <h1 class="shadow-option4">Micro-Frontend</h1>
+        <p class="align-rignt">오른쪽 정렬</p>
+        <p class="align-left">왼쪽 정렬</p>
+        <p class="align-center">가운데 정렬: ㄴㅁㄹ어ㅏㅣㅣ ㅣㅣㅣ ㅣㅣㅣㅣ ㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣ; 험ㄴ;ㅣㅏ허ㅣ남 엏;ㄴㅁ어ㅏㅎ;ㅣ</p>
+        <p class="align-justify">양쪽 정렬:  ㄴㅁㄹ어ㅏㅣㅣ ㅣㅣㅣ ㅣㅣㅣㅣ ㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣ; 험ㄴ;ㅣㅏ허ㅣ남 엏;ㄴㅁ어ㅏㅎ;ㅣ</p>
     </div>
 </template>
 <script>
@@ -27,7 +35,7 @@ export default {
         color: orange;
     }
     p {
-        color: blue;
+        color: rgb(72, 102, 84);
     }
     section {
         width: 96%;
@@ -59,4 +67,38 @@ export default {
     a{
         text-decoration:none;
     }
+    .shadow-option1 {
+        color : orangered;
+        text-shadow: 1px 1px
+
+    }
+    .shadow-option2{
+    
+        text-shadow: 3px 3px;
+    }
+    .shadow-option3{
+        color: rgb(211, 7, 7);
+    text-shadow: 7px -7px 2px #813059;
+}
+.shadow-option4{
+    background-color: #4e4b4b;
+    color: #4e3030;
+    text-shadow: 0 0 4px #ccc,
+     0 -5px 4px rgb(194, 130, 70),
+     2px -10px 6px rgb(211, 165, 38),
+     -2px -15px 7px #f80,
+     2px -19px 18px rgb(204, 47, 22);
+}
+.align-left{
+    text-align: left;
+}
+.align-right{
+    text-align: right;
+}
+.align-center{
+    text-align: center;
+}
+.align-justify{
+    text-align: justify;
+}
     </style>
