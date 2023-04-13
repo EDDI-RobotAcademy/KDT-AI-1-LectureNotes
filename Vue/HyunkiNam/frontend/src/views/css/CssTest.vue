c8010ec9db07f9ea8548cf3f7b9bb1ec2f2ffb8c
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-04-13 11:00:38 +0900

    feat: [남현기] CSS 폰트 적용하기 [CSS-FONT]

--- Vue/HyunkiNam/frontend/src/views/css/CssTest.vue
+++ Vue/HyunkiNam/frontend/src/views/css/CssTest.vue
@@ -11,6 +11,7 @@
             <h1>Test id</h1>
             <h2 class="accent"> </h2>
             <p>안녕! <span class="browntext">밤색이닷!</span></p>
+            <p class="ns-font">한글 폰트 테스트 - 밤색이닷!</p>
         </div>
     </div>
 </template>
@@ -46,5 +47,12 @@ export default {
     }
     .browntext {
         color: brown;
+    }
+    /* 폰트 적용
+       https://fonts.google.com/noto/specimen/Noto+Sans+KR */
+       @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@900&display=swap');
+    p.ns-font {
+        font-family: 'Noto Sans KR', sans-serif;
+        font-size: 30px;
     }
 </style>
