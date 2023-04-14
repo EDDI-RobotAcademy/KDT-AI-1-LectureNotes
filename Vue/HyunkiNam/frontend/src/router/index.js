8a4f972b3aebac538ed1c1bb9281b139ff5c72db
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-04-14 09:19:09 +0900

    feat: [남현기] v-model을 통한 양방향 데이터 맵핑 [VUE-MODEL]

--- Vue/HyunkiNam/frontend/src/router/index.js
+++ Vue/HyunkiNam/frontend/src/router/index.js
@@ -7,6 +7,7 @@ import CssTest from '@/views/css/CssTest.vue'
 import VueDataBindingPage from '@/views/basics/binding/VueDataBindingPage.vue'
 import VueClickEventPage from '@/views/basics/event/VueClickEventPage.vue'
 import VueInputEventPage from '@/views/basics/event/VueInputEventPage.vue'
+import DuplexWithModelPage from '@/views/basics/vModel/DuplexWithModelPage'
 
 Vue.use(VueRouter)
 
@@ -48,6 +49,11 @@ const routes = [
     path: '/vue-input-event-page',
     name: 'VueInputEventPage',
     component: VueInputEventPage
+  },
+  {
+    path: '/duplex-model-test-page',
+    name: 'DuplexWithModelPage',
+    component: DuplexWithModelPage
   },
 ]
 
