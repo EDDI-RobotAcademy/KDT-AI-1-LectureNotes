85a53bf51040490699fb680ab092c0ddd27755e6
Hakgyu Kim <zage4448@gmail.com>
2023-04-14 09:02:50 +0900

    feat: [김학규] Vue Basics 추가 [BASICS]

--- Vue/HakgyuKim/frontend/src/App.vue
+++ Vue/HakgyuKim/frontend/src/App.vue
@@ -11,7 +11,10 @@
         /htmlTest를 누가 보여줄 것인지 컴포넌트 등록을 해줘야 합니다.
       -->
       <router-link to="/htmlTest">HtmlTest</router-link> |
-      <router-link to="/cssTest">CssTest</router-link> 
+      <router-link to="/cssTest">CssTest</router-link> |
+      <router-link to="/vue-data-binding-page">VueDataBindingPage</router-link> |
+      <router-link to="/vue-click-event-page">VueClickEventPage</router-link> |
+      <router-link to="/vue-input-event-page">VueInputEventPage</router-link> |
     </nav>
     <router-view/>
   </div>
