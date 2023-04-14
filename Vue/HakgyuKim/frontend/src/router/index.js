a7d676c276d7dfcee8ceb4751103872213700475
Hakgyu Kim <zage4448@gmail.com>
2023-04-14 11:11:52 +0900

    init: [김학규] vuetify 설치 완료 [VUETIFY-INIT]

--- Vue/HakgyuKim/frontend/src/router/index.js
+++ Vue/HakgyuKim/frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // @는 현재 src 폴더를 의미함
 import HomeView from '@/views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
 import HtmlTest from '@/views/html/HtmlTest.vue'
 import CssTest from '@/views/css/CssTest.vue'
 import VueDataBindingPage from '@/views/basics/binding/VueDataBindingPage.vue'
@@ -26,7 +27,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutView
   },
   {
     path: '/htmlTest',
