5d678d78013cfbf799d1476cc8aa650782ae6772
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-04-14 13:52:41 +0900

    feat: [윤주아] axios 테스트 페이지 생성 [AXIOS-TEST]

--- Vue/JuaYun/frontend/src/views/basics/componentTest/ComponentTestPage.vue
+++ Vue/JuaYun/frontend/src/views/basics/componentTest/ComponentTestPage.vue
@@ -7,19 +7,46 @@
             public void globalComponent(int counter) {
                 intial-test = counter;
             }
+
             아래코드는 위 코드와 동의어 입니다.
             initial-test는 v-bind로 맵핑했음
         -->
         <global-component :initial-test="counter"/>
         <p>:initial-test는 v-bind:initial-test와 같습니다만 축약 표현입니다.</p>
+        <!-- 
+            public void localComponent(int number1, int number2) {
+                System.out.prinln(number1 * number2)
+            }
+        -->
+        <!-- 파마메터를 두개 전달 -->
+        <local-component :initial-value="number1" :initial-value2="number2"/><br>
+        <!-- 이후 값이 증가하던 말던 상관없음 -->
+        <button @click="plusNumber1">number1 증가</button>{{ number1 }}<br>
+        <button @click="plusNumber2">number2 증가</button>{{ number2 }}<br>
     </v-container>
 </template>
 
 <script>
+
+import LocalComponent from '@/components/componentTest/LocalComponent.vue'
+
 export default {
+    components: {
+        'local-component': LocalComponent
+    },
     data () {
         return {
-            counter: 0
+            counter: 0,
+            number1: 3,
+            number2: 7,
+        }
+    },
+    methods: {
+        plusNumber1 () {
+            this.number1++
+        },
+        plusNumber2 () {
+            this.number2++
         }
     }
 }
