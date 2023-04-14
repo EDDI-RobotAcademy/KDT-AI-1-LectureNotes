5df8cae72c1020cd450be4b0f84062e0ae5b3558
hanhwack <hanhwack93@gmail.com>
2023-04-14 14:22:42 +0900

    feat: [한확] Vue Test Controller [VUE-CONTROL]

--- Vue/hanhwack/frontend/src/views/HomeView.vue
+++ Vue/hanhwack/frontend/src/views/HomeView.vue
@@ -19,9 +19,9 @@
         <router-link to="/duplex-model-test-page">textarea 테스트 페이지</router-link> |
         <router-link to="/checkbox-test-page">checkbox 테스트 페이지</router-link> |
         <router-link to="/multi-checkbox-test-page">다중 checkbox 테스트 페이지</router-link> |
-        <router-link to="/component-test-page">component 테스트 페이지</router-link> 
+        <router-link to="/component-test-page">component 테스트 페이지</router-link> |
         <router-link to="/axios-test-page">axios 통신 테스트</router-link> |
-        
+
       </nav>
     </div>
   </v-container>
