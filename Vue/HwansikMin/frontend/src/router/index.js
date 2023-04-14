eda04f33fc19616c984df00810f8b01d27fbf5c0
hwansik <hwansik08@gmail.com>
2023-04-14 14:07:52 +0900

    feat: [민환식] axios 테스트 페이지 생성 [AXIOS-TEST]

--- Vue/HwansikMin/frontend/src/router/index.js
+++ Vue/HwansikMin/frontend/src/router/index.js
@@ -17,6 +17,8 @@ import VueMultiCheckboxPage from '@/views/basics/vModel/VueMultiCheckboxPage.vue
 
 import ComponentTestPage from '@/views/basics/componentTest/ComponentTestPage.vue'
 
+import DataSendTestPage from '@/views/basics/axios/DataSendTestPage.vue'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -77,6 +79,11 @@ const routes = [
     path: '/global-component-test-page',
     name: 'ComponentTestPage',
     component: ComponentTestPage
+  },
+  {
+    path: '/axios-test-page',
+    name: 'DataSendTestPage',
+    component: DataSendTestPage
   }
 ]
 
