3c9b98826334f5b24b1c12bbce96ed07f38e85f6
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-04-16 20:45:31 +0900

    feat: [유경우] vue 문제은행 [VUEx]

---
+++ Vue/ryukyungwoo/frontend/src/homework/componetHomework/DoubleLocalComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p>
+            <input type="text" v-model="A">
+        </p>
+        <button @click="double">2곱하기</button>
+        {{ result }}
+    </div>
+</template>
+<script>
+export default {
+    name: "DoubleLocalComponent",
+    props:{
+        A:Number
+    },
+    data() {
+        return {
+            A:0
+        }
+    },
+    methods: {
+        double() {
+            result = A * 2
+        }
+    },
+}
+</script>
+<style>
+    
+</style>
