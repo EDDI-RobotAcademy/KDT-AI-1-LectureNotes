554ef7458d591c1212164b138e72e9f8e9824393
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-04-17 14:10:51 +0900

    feat: [남현기] Navigation Bar 추가 [VUE-NAV-BAR]

---
+++ Vue/HyunkiNam/frontend/src/views/basics/layout/navigation/NavigationMenuPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav>
+        <v-app-bar color="dark" app dark>
+            <v-app-bar-nav-icon @click="navigation_drawer = !navigation_drawer" />
+            <v-img class="mx-2" src="@/assets/logo.png" max-height="40" max-width="40" contain />
+            <v-toolbar-title class="text-uppercase text--darken-4">
+                <span>EDDI</span>
+            </v-toolbar-title>
+        </v-app-bar>
+    </nav>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style></style>
