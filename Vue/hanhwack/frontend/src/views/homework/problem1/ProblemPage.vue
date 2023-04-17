c454df408186c6e1d55faef7712f5cba5f133a97
hanhwack <hanhwack93@gmail.com>
2023-04-17 10:18:38 +0900

    feat: [한확] VUE 연습 문제 [ 1 ]의 2번 풀이

--- Vue/hanhwack/frontend/src/views/homework/problem1/ProblemPage.vue
+++ Vue/hanhwack/frontend/src/views/homework/problem1/ProblemPage.vue
@@ -4,14 +4,20 @@
         <global-add-component :first-value="3" :second-value="7"/>
         <!-- 2. 뺄셈을 수행할 수 있는 Local Component를 만들어보세요. -->
         <local-sub-component :first-value="7" :second-value="2"/>
+        <!-- 3. 어떤 수를 입력하면 해당 숫자 곱하기 2의 결과를 출력하는 Local Component를 만들어보세요. -->
+        <local-input-two-times-component :receivedNumber="3"/>
     </div>
 </template>
 <script>
 import GlobalAddComponent from '@/components/homework/problem1/GlobalAddComponent.vue';
 import LocalSubComponent from '@/components/homework/problem1/LocalSubComponent.vue'
+import LocalInputTwoTimesComponent from '@/components/homework/problem1/LocalInputTwoTimesComponent.vue'
+
+
 export default {
     components: { GlobalAddComponent,
                   LocalSubComponent, 
+                  LocalInputTwoTimesComponent, 
  
     }
 }
