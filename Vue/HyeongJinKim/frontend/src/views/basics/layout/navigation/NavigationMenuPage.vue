4717bc24f6746491ecafce87b24ca20b4dcc2c9d
bbbii <hyeongjin1326@gmail.com>
2023-04-17 14:13:10 +0900

    update: [김형진] Navigation Bar 주석 추가 [VUE-NAV-BAR]

--- Vue/HyeongJinKim/frontend/src/views/basics/layout/navigation/NavigationMenuPage.vue
+++ Vue/HyeongJinKim/frontend/src/views/basics/layout/navigation/NavigationMenuPage.vue
@@ -1,5 +1,9 @@
 <template>
   <nav>
+    <!-- 
+        아래 링크를 참고하여 본인  취향의 Navigation Bar를 만들 수 있습니다.
+        https://vuetifyjs.com/en/components/app-bars/
+     -->
     <v-app-bar color="dark" app dark>
       <v-app-bar-nav-icon @click="navigation_drawer = !navigation_drawer" />
       <v-img
