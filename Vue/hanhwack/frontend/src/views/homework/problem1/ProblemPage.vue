a92ebe943e4db0f0f0692a0161565ddc430ea2a5
hanhwack <hanhwack93@gmail.com>
2023-04-17 10:16:20 +0900

    feat: [한확] VUE 연습 문제 [ 1 ]의 2번 풀이

--- Vue/hanhwack/frontend/src/views/homework/problem1/ProblemPage.vue
+++ Vue/hanhwack/frontend/src/views/homework/problem1/ProblemPage.vue
@@ -2,12 +2,18 @@
     <div>
         <!-- 1. 덧셈을 수행할 수 있는 Global Component를 만들어보세요. -->
         <global-add-component :first-value="3" :second-value="7"/>
+        <!-- 2. 뺄셈을 수행할 수 있는 Local Component를 만들어보세요. -->
+        <local-sub-component :first-value="7" :second-value="2"/>
     </div>
 </template>
 <script>
 import GlobalAddComponent from '@/components/homework/problem1/GlobalAddComponent.vue';
+import LocalSubComponent from '@/components/homework/problem1/LocalSubComponent.vue'
 export default {
-    components: { GlobalAddComponent }
+    components: { GlobalAddComponent,
+                  LocalSubComponent, 
+ 
+    }
 }
 </script>
 <style lang="">
