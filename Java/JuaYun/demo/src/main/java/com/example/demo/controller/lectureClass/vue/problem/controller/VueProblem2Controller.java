d96b024b664e6ce723d5c9a8cfb07ba664cf0f58
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-04-18 15:14:28 +0900

    feat: [윤주아] 주사위게임최종 [VUE-RANDOM-DICE]

--- Java/JuaYun/demo/src/main/java/com/example/demo/controller/lectureClass/vue/problem/controller/VueProblem2Controller.java
+++ Java/JuaYun/demo/src/main/java/com/example/demo/controller/lectureClass/vue/problem/controller/VueProblem2Controller.java
@@ -1,11 +1,15 @@
 package com.example.demo.controller.lectureClass.vue.problem.controller;
 
+import com.example.demo.controller.lectureClass.vue.problem.controller.form.MultiPlayerWinnerResponseForm;
 import com.example.demo.controller.lectureClass.vue.problem.controller.form.WinnerResponseForm;
+import com.example.demo.controller.lectureClass.vue.problem.dice.Dice;
 import com.example.demo.controller.lectureClass.vue.problem.dice.DiceGameManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.plaf.MenuBarUI;
 
 @Slf4j
 @RestController
@@ -32,6 +36,24 @@ public class VueProblem2Controller {
 
         log.info("responseForm: " + responseForm);
         return responseForm;
+    }
+
+    @GetMapping("/secondGame")
+    public MultiPlayerWinnerResponseForm doSecondGame () {
+        log.info("secondGame()");
+
+        final int DICE_NUM = 3;
+        final int PLAYER_NUM = 2;
+
+        DiceGameManager diceGameManager = new DiceGameManager(PLAYER_NUM, DICE_NUM);
+        log.info("diceGameManager: " + diceGameManager);
+
+        diceGameManager.applyCondition();
+        log.info("after condition process - diceGameManager: " + diceGameManager);
+
+        MultiPlayerWinnerResponseForm multiPlayerWinnerResponseForm = new MultiPlayerWinnerResponseForm(diceGameManager.checkWinner(), diceGameManager.getPlayerList());
+
+        return multiPlayerWinnerResponseForm;
     }
 }
 // 오류가 발생하면 f12를 누르고 소스로 들어가 확인해본다.
