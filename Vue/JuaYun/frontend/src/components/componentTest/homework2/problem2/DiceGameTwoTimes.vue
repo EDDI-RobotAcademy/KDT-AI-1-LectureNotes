e5fb16c30f36a28f79e34357043c0d58de31eac3
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-04-18 10:54:24 +0900

    feat: [윤주아] 주사위 vue[VUE]

---
+++ Vue/JuaYun/frontend/src/components/componentTest/homework2/problem2/DiceGameTwoTimes.vue
@@ -0,0 +1,29 @@
+<template lang="">
+    <div>
+        <h3>주사위 2개를 굴려 합이 짝수면 승리 홀수면 패배!</h3>
+        <v-btn color="primary" @click="startTowTimesDiceGame">주사위게임시작!</v-btn>
+        <p>승자 판정: {{result}}</p>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+    data () {
+        return{
+            result: '알 수 없음',
+        }
+    },
+    methods: {
+        startTowTimesDiceGame() {
+            axios.get('hppt://localhost:7777/dece-game/firstGame')
+                .then((res) => {
+                    this.result = res.data
+                })
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
