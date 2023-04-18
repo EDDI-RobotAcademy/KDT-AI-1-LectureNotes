d5d291b3b943904d8293d9ccf874c61ca8e6c77a
dusdn943 <dusdn943@gmail.com>
2023-04-18 12:09:16 +0900

    feat: [최연우] Vue 문제 2번 1:1 주사위 게임 [VUE-DICE-GAME]

--- Spring/yeonwoochoi/demo/src/main/java/com/example/demo/lectureClass/vue/controller/vue/basics/controller/form/VueProblem2Controller.java
+++ Spring/yeonwoochoi/demo/src/main/java/com/example/demo/lectureClass/vue/controller/vue/basics/controller/form/VueProblem2Controller.java
@@ -1,6 +1,7 @@
 package com.example.demo.lectureClass.vue.controller.vue.basics.controller.form;
 
 import com.example.demo.lectureClass.vue.controller.vue.dice.DiceGameManager;
+import com.example.demo.lectureClass.vue.controller.vue.problem.controller.form.MultiPlayerWinnerResponseForm;
 import com.example.demo.lectureClass.vue.controller.vue.problem.controller.form.WinnerResponseForm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,25 +12,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/dice-game")
 public class VueProblem2Controller {
-     /*
-    랜덤한 주사위 2개를 돌립니다.
-    돌린 주사위의 값의 합이 짝수라면 승리를 반환하고 홀수라면 패배를 반환합니다.
-     */
+    /*
+   랜덤한 주사위 2개를 돌립니다.
+   돌린 주사위의 값의 합이 짝수라면 승리를 반환하고 홀수라면 패배를 반환합니다.
+    */
     @GetMapping("/firstGame")
     public WinnerResponseForm doFirstDiceGame () {
         log.info("firstGame()");
-
         final int DICE_NUM = 2;
-
         DiceGameManager diceGameManager = new DiceGameManager(DICE_NUM);
         log.info("diceGameManager: " + diceGameManager);
-
         int tmpSum = diceGameManager.calculatePlayerDiceSum();
         WinnerResponseForm responseForm = new WinnerResponseForm(
                 tmpSum,
                 diceGameManager.checkWinIfSumEven(tmpSum));
-
         log.info("responseForm: " + responseForm);
         return responseForm;
+    }
+
+    @GetMapping("/secondGame")
+    public MultiPlayerWinnerResponseForm doSecondGame () {
+        log.info("secondGame()");
+
+        final int DICE_NUM = 3;
+        final int PLAYER_NUM = 2;
+
+        DiceGameManager diceGameManager = new DiceGameManager(PLAYER_NUM, DICE_NUM);
+        log.info("diceGameManager: " + diceGameManager);
+
+        diceGameManager.applyCondition();
+        log.info("after condition process - diceGameManager: " + diceGameManager);
+
+        MultiPlayerWinnerResponseForm multiPlayerWinnerResponseForm = new MultiPlayerWinnerResponseForm(
+                diceGameManager.checkWinner(),
+                diceGameManager.getPlayerList()
+        );
+
+        return multiPlayerWinnerResponseForm;
     }
 }
