8f0eea5653aabdfe47927e32990aab8d68787eca
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-04-19 18:10:26 +0900

    update: [남현기] HomeworkPage import 수정

--- Vue/HyunkiNam/frontend/src/views/homework/HomeworkPage.vue
+++ Vue/HyunkiNam/frontend/src/views/homework/HomeworkPage.vue
@@ -16,17 +16,17 @@
 </template>
 <script>
 
-import MinusComponent from '@/components/homeworkComponent/problem1/MinusComponent.vue.js';
-import MultipleTwoComponent from '@/components/homeworkComponent/problem1/MultipleTwoComponent.vue.js';
-import CalculateComponent from '@/components/homeworkComponent/problem1/CalculateComponent.vue.js';
-import CheckboxComponent from '@/components/homeworkComponent/problem1/CheckboxComponent.vue.js';
-import WinConditionIsEven from '@/components/homeworkComponent/problem2/WinConditionIsEven.vue.js';
-import DiceGameComponent from '@/components/homeworkComponent/problem2/DiceGameComponent.vue.js';
-import IsCheckboxComponent from '@/components/homeworkComponent/problem2/IsCheckboxComponent.vue.js';
-import CheckEvenOddComponent from '@/components/homeworkComponent/problem2/CheckEvenOddComponent.vue.js';
-import SendHelloComponent from '@/components/homeworkComponent/problem2/SendHelloComponent.vue.js';
+import MinusComponent from '@/components/homeworkComponent/problem1/MinusComponent.vue';
+import MultipleTwoComponent from '@/components/homeworkComponent/problem1/MultipleTwoComponent.vue';
+import CalculateComponent from '@/components/homeworkComponent/problem1/CalculateComponent.vue';
+import CheckboxComponent from '@/components/homeworkComponent/problem1/CheckboxComponent.vue';
+import WinConditionIsEven from '@/components/homeworkComponent/problem2/WinConditionIsEven.vue';
+import DiceGameComponent from '@/components/homeworkComponent/problem2/DiceGameComponent.vue';
+import IsCheckboxComponent from '@/components/homeworkComponent/problem2/IsCheckboxComponent.vue';
+import CheckEvenOddComponent from '@/components/homeworkComponent/problem2/CheckEvenOddComponent.vue';
+import SendHelloComponent from '@/components/homeworkComponent/problem2/SendHelloComponent.vue';
 import DiceGameTwoTimes from '@/components/HomeworkSolutionComponent/DiceGameTwoTimes.vue';
-import BuyFruitComponent from '@/components/homeworkComponent/problem3/BuyFruitComponent.vue.js';
+import BuyFruitComponent from '@/components/homeworkComponent/problem3/BuyFruitComponent.vue';
 
 export default {
     components: {
