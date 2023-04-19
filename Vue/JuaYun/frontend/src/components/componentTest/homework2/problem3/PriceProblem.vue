e2113e6e0383e48720f902148f77cef6826f0864
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-04-19 11:55:10 +0900

    feat: [윤주아] 과일계산하기완성 [HOMEWORK]

--- Vue/JuaYun/frontend/src/components/componentTest/homework2/problem3/PriceProblem.vue
+++ Vue/JuaYun/frontend/src/components/componentTest/homework2/problem3/PriceProblem.vue
@@ -3,7 +3,7 @@
     <h3>2000원짜리 사과 3개, 10000원짜리 수박 5개를 구매해봅시다.</h3>
     <input type="number" v-model="apple">사과<br>
     <input type="number" v-model="watermelon">수박<br>
-    <v-btn color="primary" @click="AllPrice">총합</v-btn>
+    <v-btn color="primary" @click="AllPrice">계산</v-btn>
     <p>사과의 값: {{ applePrice }}, 수박의 값: {{ watermelonPrice }}</p>
     <p>총합: {{ result }}</p>
   </div>
@@ -24,12 +24,11 @@ export default {
         AllPrice () {
             axios.get('http://localhost:7777/apple-watermelon/result')
             .then((res) => {
-                this.result = res.data.PriceSum
                 this.applePrice = res.data.applePrice
                 this.watermelonPrice = res.data.watermelonPrice
+                this.result = res.data.priceSum
             })
-        }
-        
+        }        
     }
 }
 </script>
