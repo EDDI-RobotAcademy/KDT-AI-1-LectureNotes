8bafa466453434a0f6ea6e17040428a1ea540ed3
Sanghoon Lee <gcccompil3r@gmail.com>
2023-04-20 14:04:57 +0900

    update: [이상훈] 일단 계정 생성 및 캐릭터 정보값 확보 완료 [VUE-PROB-4]

--- Spring/SanghoonLee/demo/src/main/java/kr/eddi/demo/lectureClass/vue/problem/entity/CharacterStatus.java
+++ Spring/SanghoonLee/demo/src/main/java/kr/eddi/demo/lectureClass/vue/problem/entity/CharacterStatus.java
@@ -1,5 +1,6 @@
 package kr.eddi.demo.lectureClass.vue.problem.entity;
 
+import kr.eddi.demo.lectureClass.utility.random.CustomRandom;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -12,5 +13,17 @@ public class CharacterStatus {
     private int vital;
     private int intelligence;
 
-    private int whosAccountId;
+    private Long whosAccountId;
+
+    public CharacterStatus(Long id) {
+        final int MIN = 10;
+        final int MAX = 20;
+
+        this.strength = CustomRandom.generateNumber(MIN, MAX);
+        this.dexterity = CustomRandom.generateNumber(MIN, MAX);
+        this.vital = CustomRandom.generateNumber(MIN, MAX);
+        this.intelligence = CustomRandom.generateNumber(MIN, MAX);
+
+        this.whosAccountId = id;
+    }
 }
