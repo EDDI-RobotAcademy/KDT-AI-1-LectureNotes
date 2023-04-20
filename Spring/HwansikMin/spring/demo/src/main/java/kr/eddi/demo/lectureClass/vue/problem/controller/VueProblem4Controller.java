0b9d5ca6b5e3acbb4526c6984e09cc6126563c51
hwansik <hwansik08@gmail.com>
2023-04-20 14:09:41 +0900

    update: [민환식] 일단 계정 생성 및 캐릭터 정보값 확보 완료 [VUE-PROB-4]

--- Spring/HwansikMin/spring/demo/src/main/java/kr/eddi/demo/lectureClass/vue/problem/controller/VueProblem4Controller.java
+++ Spring/HwansikMin/spring/demo/src/main/java/kr/eddi/demo/lectureClass/vue/problem/controller/VueProblem4Controller.java
@@ -16,15 +16,20 @@ public class VueProblem4Controller {
 
     private static Long accountNumber = 1L;
     private static List<Account> accountList = new ArrayList<>();
+    private static List<CharacterStatus> characterStatusList = new ArrayList<>();
 
     @PostMapping("/create")
     public Boolean createAccount (@RequestBody AccountCreationForm accountCreationForm) {
         log.info("createAccount(): " + accountCreationForm);
 
+        // 리스트는 트리 기반의 검색을 합니다.
+        // 반면 해쉬는 키를 던지면 값을 즉각 반환합니다.
         final Account account = accountCreationForm.toAccount(accountNumber);
         accountNumber++;
-
         accountList.add(account);
+
+        final CharacterStatus characterStatus = new CharacterStatus(account.getId());
+        characterStatusList.add(characterStatus);
 
         return true;
     }
@@ -33,7 +38,7 @@ public class VueProblem4Controller {
     public CharacterStatus getCharacterStatus () {
         log.info("getCharacterStatus()");
 
-        return null;
+        return characterStatusList.get(0);
     }
 }
 
