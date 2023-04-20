69cd993306c0bde2c016e2d37e29df4458154b63
Sanghoon Lee <gcccompil3r@gmail.com>
2023-04-20 10:12:28 +0900

    feat: [이상훈] 기본 윤곽 제공 [VUE-PROB-4]

---
+++ Vue/SanghoonLee/frontend/src/components/homework/problem4/GameAccountStatusLoad.vue
@@ -0,0 +1,17 @@
+<template lang="">
+    <div>
+        <v-btn color="teal" @click="playGame">플레이</v-btn>
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        playGame () {
+            alert('플레이!')
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
