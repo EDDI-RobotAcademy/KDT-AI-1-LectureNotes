136cd7021c720a8770e0a7f5e4c0f0564d18f885
김학규 <zage4448@gmail.com>
2023-04-20 22:01:00 +0900

    feat: [김학규] Vue 문제 은행 5 중간 세이브2 [HOMEWORK5]

--- Spring/HakgyuKim/demo/src/main/java/com/example/demo/lectureClass/homework/bank4/Bank4Controller.java
+++ Spring/HakgyuKim/demo/src/main/java/com/example/demo/lectureClass/homework/bank4/Bank4Controller.java
@@ -1,6 +1,7 @@
 package com.example.demo.lectureClass.homework.bank4;
 
 import com.example.demo.lectureClass.homework.bank4.account.Account;
+import com.example.demo.lectureClass.homework.bank4.character.Character;
 import com.example.demo.lectureClass.homework.bank4.enemy.Enemy;
 import com.example.demo.lectureClass.homework.bank4.form.AccountCreationForm;
 import com.example.demo.lectureClass.homework.bank4.form.CharacterStatusForm;
@@ -8,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Slf4j
@@ -15,27 +17,31 @@ import java.util.List;
 @RequestMapping("/character-problem")
 public class Bank4Controller {
     private static Long accountNumber = 1L;
-    private static List<Account> accountList = new ArrayList<>();
+    HashMap<Long, Account> accountMap = new HashMap<>();
     GameManager gameManager;
 
     @PostMapping("/create")
     public void createAccount(@RequestBody AccountCreationForm accountCreationForm) {
         final Account account = accountCreationForm.toAccount(accountNumber);
+        accountMap.put(accountNumber, account);
         accountNumber++;
-        accountList.add(account);
-
     }
 
     @GetMapping("/getCharacterStatus")
-    public CharacterStatusForm getCharacterStatus() {
+    public CharacterStatusForm getCharacterStatus(@RequestParam ("accountId")int id) {
+        Account currentAccount = accountMap.get(id);
+        int characterNum = currentAccount.getCharacterList().size() + 1;
+        currentAccount.getCharacterList().add(new Character("캐릭터" + characterNum, characterNum));
+
         CharacterStatusForm characterStatusForm = new CharacterStatusForm(
-                gameManager.getCharacter().getEmail(),
-                gameManager.getCharacter().getHealth(),
-                gameManager.getCharacter().getStrength(),
-                gameManager.getCharacter().getDexterity(),
-                gameManager.getCharacter().getIntelligence(),
-                gameManager.getCharacter().getSkill()
+                currentAccount.getCharacterList().get(characterNum).getName(),
+                currentAccount.getCharacterList().get(characterNum).getHealth(),
+                currentAccount.getCharacterList().get(characterNum).getStrength(),
+                currentAccount.getCharacterList().get(characterNum).getDexterity(),
+                currentAccount.getCharacterList().get(characterNum).getIntelligence(),
+                currentAccount.getCharacterList().get(characterNum).getSkill()
         );
+
         log.info("info: " + characterStatusForm);
         return characterStatusForm;
     }
--- Spring/HakgyuKim/demo/src/main/java/com/example/demo/lectureClass/homework/bank4/account/Account.java
+++ Spring/HakgyuKim/demo/src/main/java/com/example/demo/lectureClass/homework/bank4/account/Account.java
@@ -1,8 +1,12 @@
 package com.example.demo.lectureClass.homework.bank4.account;
 
+import com.example.demo.lectureClass.homework.bank4.character.Character;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @ToString
@@ -12,5 +16,7 @@ public class Account {
     final private Long id;
     final private String email;
     final private String password;
+
+    List<Character> characterList = new ArrayList<>();
 
 }
