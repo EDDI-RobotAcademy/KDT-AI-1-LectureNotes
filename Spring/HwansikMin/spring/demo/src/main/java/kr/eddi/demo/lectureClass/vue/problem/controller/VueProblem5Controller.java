1471eee3d7c89b615762d1f0ad87ac48e72a5226
hwansik <hwansik08@gmail.com>
2023-04-21 12:05:22 +0900

    feat: [민환식] 로그인 처리 완료 [BMP-2]

--- Spring/HwansikMin/spring/demo/src/main/java/kr/eddi/demo/lectureClass/vue/problem/controller/VueProblem5Controller.java
+++ Spring/HwansikMin/spring/demo/src/main/java/kr/eddi/demo/lectureClass/vue/problem/controller/VueProblem5Controller.java
@@ -1,6 +1,7 @@
 package kr.eddi.demo.lectureClass.vue.problem.controller;
 
 import kr.eddi.demo.lectureClass.vue.problem.controller.form.GameAccountForm;
+import kr.eddi.demo.lectureClass.vue.problem.controller.form.LoginResponseForm;
 import kr.eddi.demo.lectureClass.vue.problem.entity.GameAccount;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,6 +33,27 @@ public class VueProblem5Controller {
         gameAccountList.add(gameAccount);
 
         return true;
+    }
+
+    @PostMapping("/login")
+    public LoginResponseForm gameAccountLogin (@RequestBody GameAccountForm gameAccountForm) {
+        log.info("gameAccountLogin()");
+
+        final Long LOGIN_FAILED_ACCOUND_ID = 0L;
+
+        for (int i = 0; i < gameAccountList.size(); i++) {
+            final GameAccount searchedGameAccount = gameAccountList.get(i);
+            final String searchedGameAccountEmail = searchedGameAccount.getEmail();
+
+            if (searchedGameAccountEmail.equals(gameAccountForm.getEmail())) {
+                if (searchedGameAccount.getPassword().equals(gameAccountForm.getPassword())) {
+                    return new LoginResponseForm(true, searchedGameAccount.getId());
+                }
+                return new LoginResponseForm(false, LOGIN_FAILED_ACCOUND_ID);
+            }
+        }
+
+        return new LoginResponseForm(false, LOGIN_FAILED_ACCOUND_ID);
     }
 
     private boolean checkDuplicatedEmail(GameAccount gameAccount) {
