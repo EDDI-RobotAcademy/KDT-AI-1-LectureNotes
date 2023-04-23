444dad223fb4dfced2bc3da8577c740f24f6e35f
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-04-23 22:01:30 +0900

    update: [이상원] Spring에서 Vue로 과일 값 가져오기 (수정중) [HOMEWORKx]

--- Spring/SangWonLee/demo/src/main/java/kr/eddi/demo/lectureClass/vue/problem/entity/FruitSale.java
+++ Spring/SangWonLee/demo/src/main/java/kr/eddi/demo/lectureClass/vue/problem/entity/FruitSale.java
@@ -1,12 +1,12 @@
 package kr.eddi.demo.lectureClass.vue.problem.entity;
 
-import kr.eddi.demo.lectureClass.vue.problem.controller.form.FruitNumber;
+import kr.eddi.demo.lectureClass.vue.problem.controller.form.FruitAppleWatermelonForm;
 
 public class FruitSale {
     private int sale;
 
-    public FruitSale(FruitNumber fruitNumber) {
-        this.sale = (int)(Math.random() * (1000-500) + 1);
+    public FruitSale(FruitAppleWatermelonForm fruitAppleWatermelonForm) {
+
 
     }
 }
