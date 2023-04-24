fed55e4dedaf97d22c16d55b489b10af39f7d981
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-04-24 21:48:42 +0900

    update: [유경우] 주사위 재설계 [DOMAIN-HOMEWORK]

--- Java/ryukyungwoo/JAVA/selfStudy/src/jdp1/PlayerList.java
+++ Java/ryukyungwoo/JAVA/selfStudy/src/jdp1/PlayerList.java
@@ -3,32 +3,61 @@ package jdp1;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PlayerList {
-    List<Player> playerList;
+class Player {
+    List<Dice> diceList;
+    final private int HOW_MANY_DICES = 3;
+    private int score;
     private String name;
-    final private int HOW_MANY_PLAYERS = 2;
-    public PlayerList () {
-        playerList = new ArrayList<>();
-        for (int i = 0; i < HOW_MANY_PLAYERS; i++) {
-            playerList.add(new Player());
-            name = "플레이어" + (i + 1);
+
+    public Player () {
+        diceList = new ArrayList<>();
+        for (int i = 0; i < HOW_MANY_DICES; i++) {
+            diceList.add(new Dice());
+            score += diceList.get(i).getDiceNumber();
         }
     }
 
-    public List<Player> getPlayerList() {
-
-        return playerList;
+    public List<Dice> getDiceList() {
+        return diceList;
     }
 
-    public String getName() {
-        return name;
+    public int getScore() {
+        return score;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
     public String toString() {
-        return "PlayerList{" +
-                "playerList=" + playerList +
+        return "Player{" +
+                "diceList=" + diceList +
+                ", score=" + score +
                 ", name='" + name + '\'' +
+                '}' + '\n';
+    }
+}
+public class PlayerList {
+    List<Player> playerList;
+    final private int HOW_MANY_PLAYERS = 2;
+
+    public PlayerList () {
+        playerList = new ArrayList<>();
+        for (int i = 0; i < HOW_MANY_PLAYERS; i++) {
+            playerList.add(new Player());
+            playerList.get(i).setName("user: " + (i + 1));
+        }
+    }
+
+    public List<Player> getPlayerList() {
+        return playerList;
+    }
+
+    @Override
+    public String toString() {
+        return '\n' + "PlayerList{" +
+                "playerList=" + '\n' + playerList +
                 '}';
     }
 }
--- Java/ryukyungwoo/JAVA/selfStudy/src/jdp1/controller/DiceController.java
+++ Java/ryukyungwoo/JAVA/selfStudy/src/jdp1/controller/DiceController.java
@@ -1,11 +1,9 @@
 package jdp1.controller;
 
-import jdp1.DecisionDice;
 import jdp1.PlayerList;
 
 public class DiceController {
     public static void main(String[] args) {
-        DecisionDice decisionDice = new DecisionDice();
-        System.out.println(decisionDice);
+        System.out.println(new PlayerList());
     }
 }
