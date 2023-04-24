93a545c1d0e602d13b24bb3b9836ff9686a7f531
cluypkoj <cluypkoj68@gmail.com>
2023-04-24 14:32:00 +0900

    feat: [백지우] 2023/04/24 누락분 모두 추가

--- Spring/JiWooBaek/demo/demo/src/main/java/com/example/demo/config/CorsConfig.java
+++ Spring/JiWooBaek/demo/demo/src/main/java/com/example/demo/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:8080") // vue는 허용하겠다는 소리
+                .allowedOrigins("http://127.0.0.1:8080")    // vue는 허용하겠다는 소리
                 .allowedOrigins("http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
