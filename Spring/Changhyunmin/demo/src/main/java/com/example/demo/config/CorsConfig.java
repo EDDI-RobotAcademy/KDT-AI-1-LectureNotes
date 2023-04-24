c4606ea17d4c8fac5645239a4468b528586d5281
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-04-24 14:39:40 +0900

    feat: [장현민]JPA 환경 구축 및 Spring[JPAx]

--- Spring/Changhyunmin/demo/src/main/java/com/example/demo/config/CorsConfig.java
+++ Spring/Changhyunmin/demo/src/main/java/com/example/demo/config/CorsConfig.java
@@ -1,5 +1,4 @@
-package kr.eddi.demo.config;
-
+package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,12 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.20.20:8080")    // vue는 허용하겠다는 소리
+                .allowedOrigins("http://127.0.0.1:8080")    // vue는 허용하겠다는 소리
                 .allowedOrigins("http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
+
 }
