6cc471ec3602ea17086f6dcbad71b7749b954024
hwansik <hwansik08@gmail.com>
2023-04-25 16:24:19 +0900

    fix: [민환식] Demo Application 실행 이슈 해결 [BOARDx]

--- Spring/HwansikMin/spring/demo/src/main/java/kr/eddi/demo/lectureClass/jpa/board/entity/JpaBoard.java
+++ Spring/HwansikMin/spring/demo/src/main/java/kr/eddi/demo/lectureClass/jpa/board/entity/JpaBoard.java
@@ -1,20 +1,16 @@
 package kr.eddi.demo.lectureClass.jpa.board.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Entity
 @Getter
@@ -36,11 +32,9 @@ public class JpaBoard {
         this.content = content;
     }
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
     @CreationTimestamp
     private LocalDateTime createDate;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
     @UpdateTimestamp
     private LocalDateTime updateDate;
 }
--- Spring/HwansikMin/spring/demo/src/main/java/kr/eddi/demo/lectureClass/jpa/board/service/JpaBoardServiceImpl.java
+++ Spring/HwansikMin/spring/demo/src/main/java/kr/eddi/demo/lectureClass/jpa/board/service/JpaBoardServiceImpl.java
@@ -8,12 +8,11 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class JpaBoardServiceImpl {
+public class JpaBoardServiceImpl implements JpaBoardService {
 
     final private JpaBoardRepository boardRepository;
 
@@ -25,23 +24,6 @@ public class JpaBoardServiceImpl {
     @Override
     public JpaBoard register(JpaBoard jpaBoard) {
         return boardRepository.save(jpaBoard);
-    }
-
-    @Override
-    public JpaBoard read(Long boardId) {
-        Optional<JpaBoard> maybeJpaBoard = boardRepository.findById(boardId);
-
-        if (maybeJpaBoard.isEmpty()) {
-            log.info("정보가 없습니다!");
-            return null;
-        }
-
-        return maybeJpaBoard.get();
-    }
-
-    @Override
-    public void delete(Long boardId) {
-        boardRepository.deleteById(boardId);
     }
 
 }
