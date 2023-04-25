dbf038c38229a59945805e41bee1e13dd3136677
hwansik <hwansik08@gmail.com>
2023-04-25 09:38:27 +0900

    feat: [민환식] 게시물 조회하기 기능 완료 [BOARDx]

---
+++ Spring/HwansikMin/spring/demo/src/main/java/kr/eddi/demo/lectureClass/jpa/board/service/JpaBoardService.java
@@ -0,0 +1,9 @@
+package kr.eddi.demo.lectureClass.jpa.board.service;
+
+import kr.eddi.demo.lectureClass.jpa.board.entity.JpaBoard;
+
+import java.util.List;
+
+public class JpaBoardService {
+    List<JpaBoard> list();
+}
---
+++ Spring/HwansikMin/spring/demo/src/main/java/kr/eddi/demo/lectureClass/jpa/board/service/JpaBoardServiceImpl.java
@@ -0,0 +1,22 @@
+package kr.eddi.demo.lectureClass.jpa.board.service;
+
+import kr.eddi.demo.lectureClass.jpa.board.entity.JpaBoard;
+import kr.eddi.demo.lectureClass.jpa.board.repository.JpaBoardRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class JpaBoardServiceImpl {
+
+    final private JpaBoardRepository boardRepository;
+
+    @Override
+    public List<JpaBoard> list() {
+        return null;
+    }
+}
