1b39ff4c78eca7ca930c8d002e16cdbdc68bcfd3
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-04-25 09:35:29 +0900

    feat: [남현기] 게시물 조회하기 기능 완료 [BOARDx]

---
+++ Spring/HyunkiNam/demo/src/main/java/kr/eddi/demo/lectureClass/jpa/board/controller/JpaBoardController.java
@@ -0,0 +1,27 @@
+package kr.eddi.demo.lectureClass.jpa.board.controller;
+
+import kr.eddi.demo.lectureClass.jpa.board.entity.JpaBoard;
+import kr.eddi.demo.lectureClass.jpa.board.service.JpaBoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/jpa-board")
+public class JpaBoardController {
+
+    final private JpaBoardService boardService;
+
+    @GetMapping("/list")
+    public List<JpaBoard> boardList (){
+        log.info("boardList()");
+
+        return boardService.list();
+    }
+}
---
+++ Spring/HyunkiNam/demo/src/main/java/kr/eddi/demo/lectureClass/jpa/board/service/JpaBoardService.java
@@ -0,0 +1,10 @@
+package kr.eddi.demo.lectureClass.jpa.board.service;
+
+import kr.eddi.demo.lectureClass.jpa.board.entity.JpaBoard;
+
+import java.util.List;
+
+public interface JpaBoardService {
+
+    List<JpaBoard> list();
+}
