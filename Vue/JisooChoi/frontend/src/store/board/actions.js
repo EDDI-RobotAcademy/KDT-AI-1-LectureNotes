e0da80cb2951afbf6f85ab409b2b05efe68d62f7
zisoo <zisoo_3@naver.com>
2023-04-25 21:43:31 +0900

    feat: [최지수] 게시판 리스트 UI 주석 달기

--- Vue/JisooChoi/frontend/src/store/board/actions.js
+++ Vue/JisooChoi/frontend/src/store/board/actions.js
@@ -1,43 +1,46 @@
 import {
-    REQUEST_BOARD_LIST_TO_SPRING,
-    REQUEST_BOARD_TO_SPRING,
-} from './mutation-types'
+  REQUEST_BOARD_LIST_TO_SPRING,
+  REQUEST_BOARD_TO_SPRING,
+} from "./mutation-types";
 
-import axiosInst from '@/utility/axiosInst'
+import axiosInst from "@/utility/axiosInst";
 
 export default {
-    requestBoardToSpring ({ commit }, boardId) {
-        return axiosInst.get(`/jpa-board/${boardId}`)
-            .then((res) => {
-                commit(REQUEST_BOARD_TO_SPRING, res.data)
-            })
-    },
-    requestBoardListToSpring ({ commit }) {
-        return axiosInst.get('/jpa-board/list')
-            .then((res) => {
-                commit(REQUEST_BOARD_LIST_TO_SPRING, res.data)
-            })
-    },
-    requestCreateBoardToSpring ({}, payload) {
+  // 기본적으로 Vuex를 사용하면 mutation을 이용한 action.js에서 commit을 한다.
+  // 즉, API를 이용해 받은 response 값을 commit으로 저장하여 getter를 호출해 가져오는 형태로 사용한다.
+  requestBoardToSpring({ commit }, boardId) {
+    return axiosInst.get(`/jpa-board/${boardId}`).then((res) => {
+      commit(REQUEST_BOARD_TO_SPRING, res.data);
+    });
+  },
+  requestBoardListToSpring({ commit }) {
+    return axiosInst.get("/jpa-board/list").then((res) => {
+      commit(REQUEST_BOARD_LIST_TO_SPRING, res.data);
+    });
+  },
+  requestCreateBoardToSpring({}, payload) {
+    const { title, content, writer } = payload;
 
-        const { title, content, writer } = payload
-
-        return axiosInst.post('/jpa-board/register', { title, content, writer })
-            .then((res) => {
-                alert('게시물 등록 성공!')
-                return res
-            })
-            .catch(() => {
-                alert('문제 발생!')
-            })
-        },
-        requestDeleteBoardToSpring ({}, boardId) {
-            return axiosInst.delete(`/jpa-board/${boardId}`)
-                .then((res) => {
-                    alert('삭제 성공!')
-                })
-                .catch(() => {
-                    alert('문제 발생!')
-                })
-        }
-}
+    // 게시물 등록
+    return axiosInst
+      .post("/jpa-board/register", { title, content, writer })
+      .then((res) => {
+        alert("게시물 등록 성공!");
+        return res;
+      })
+      .catch(() => {
+        alert("문제 발생!");
+      });
+  },
+  // 게시물 삭제
+  requestDeleteBoardToSpring({}, boardId) {
+    return axiosInst
+      .delete(`/jpa-board/${boardId}`)
+      .then((res) => {
+        alert("삭제 성공!");
+      })
+      .catch(() => {
+        alert("문제 발생!");
+      });
+  },
+};
--- Vue/JisooChoi/frontend/src/views/basics/board/BoardListPage.vue
+++ Vue/JisooChoi/frontend/src/views/basics/board/BoardListPage.vue
@@ -1,40 +1,42 @@
 <template lang="">
-    <div>
-        <h2>Vue + Spring + JPA 게시판</h2>
-        <div style="text-align: left; margin: 15px;">
-            <router-link :to="{ name: 'BoardRegisterPage' }">
-                게시물 작성
-            </router-link>
-        </div>
-        <board-list-form :boards="boards"/>
+  <div>
+    <h2>Vue + Spring + JPA 게시판</h2>
+    <div style="text-align: left; margin: 15px">
+      <!-- 페이지 링크를 라우터로 구현하여 누르면 register 페이지로 이동 -->
+      <router-link :to="{ name: 'BoardRegisterPage' }">
+        게시물 작성
+      </router-link>
     </div>
+    <!-- 게시물 목록을 컴포넌트 형태로 만들어서 가지고 왔다. -->
+    <board-list-form :boards="boards" />
+  </div>
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex';
-import BoardListForm from '@/components/board/BoardListForm.vue'
+// 우리가 만들어둔 store의 actions과 states를 매핑하여 연결해 준 것으로 추정 !
+import { mapActions, mapState } from "vuex";
+import BoardListForm from "@/components/board/BoardListForm.vue";
 
-const boardModule = 'boardModule'
+const boardModule = "boardModule";
 
 export default {
-    name: 'BoardListPage',
-    components: { BoardListForm },
-    // state 관리자인 vuex에 state값(boards)를 모니터링
-    computed: {
-        ...mapState(boardModule, ['boards']),
-    },
-    mounted () {
-        // vuex의 action 호출
-        this.requestBoardListToSpring()
-    },
-    methods: {
-        // vuex의 action을 method에 맵핑
-        ...mapActions(
-            boardModule, ['requestBoardListToSpring']
-        )
-    }
-}
+  name: "BoardListPage",
+  components: { BoardListForm },
+  // state 관리자인 vuex에 state값(boards)를 모니터링
+  computed: {
+    // computed는 템플릿의 데이터 표현을 더 직관적이고 간결하게 도와주는 속성이다.
+    ...mapState(boardModule, ["boards"]),
+  },
+  mounted() {
+    // mounted()는 컴포넌트, 템플릿, 렌더링 된 DOM에 접근이 가능하다. (DOM 조작)
+    // vuex의 action 호출
+    this.requestBoardListToSpring();
+  },
+  methods: {
+    // vuex의 action을 method에 맵핑
+    ...mapActions(boardModule, ["requestBoardListToSpring"]),
+  },
+};
 </script>
 
-<style lang="">
-</style>
+<style lang=""></style>
