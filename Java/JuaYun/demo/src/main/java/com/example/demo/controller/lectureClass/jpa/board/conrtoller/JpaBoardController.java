cccef4a4904d1d3bb8e9e1dc751810cf2c528a13
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-04-25 10:23:22 +0900

    feat: [윤주아] 게시물 조회하기 기능 완료 [BOARDx]

--- Java/JuaYun/demo/src/main/java/com/example/demo/controller/lectureClass/jpa/board/conrtoller/JpaBoardController.java
+++ Java/JuaYun/demo/src/main/java/com/example/demo/controller/lectureClass/jpa/board/conrtoller/JpaBoardController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller.lectureClass.jpa.board.conrtoller;
 
 import com.example.demo.controller.lectureClass.jpa.board.entity.JpaBoard;
+import com.example.demo.controller.lectureClass.jpa.board.service.JpaBoardService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/jpa-board")
 public class JpaBoardController {
 
---
+++ Java/JuaYun/demo/src/main/java/com/example/demo/controller/lectureClass/jpa/board/service/JpaBoardService.java
@@ -0,0 +1,9 @@
+package com.example.demo.controller.lectureClass.jpa.board.service;
+
+import com.example.demo.controller.lectureClass.jpa.board.entity.JpaBoard;
+
+import java.util.List;
+
+public interface JpaBoardService {
+    List<JpaBoard> list();
+}
