2835bc8dc0fb95bd097f6371df8e2f159871ab6c
zisoo <zisoo_3@naver.com>
2023-04-25 09:41:39 +0900

    feat: [최지수] 게시물 조회하기 기능 완료 [BOARDx]

--- Spring/JisooChoi/demo/src/main/java/kr/eddi/demo/lectureClass/jpa/board/entity/JpaBoard.java
+++ Spring/JisooChoi/demo/src/main/java/kr/eddi/demo/lectureClass/jpa/board/entity/JpaBoard.java
@@ -7,14 +7,15 @@ import jakarta.persistence.Id;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Entity
 public class JpaBoard {
 
+    // CustomSequence 역할을 한다.
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long boardId;
+    private Long boardId; // 게시판 아이디 (유일성)
 
     private String title; // 제목
     private String writer; // 작성자
@@ -26,10 +27,11 @@ public class JpaBoard {
         this.content = content;
     }
 
+    // 아래는 디지털 포렌식 때문에 쓴다.
     @CreatedDate
-    private Date createdDate;
+    private LocalDateTime createdDate;
 
     @UpdateTimestamp
-    private Date updateDate;
+    private LocalDateTime updateDate;
 
 }
---
+++ Spring/JisooChoi/demo/src/main/java/kr/eddi/demo/lectureClass/jpa/board/service/JpaBoardServiceImpl.java
@@ -0,0 +1,23 @@
+package kr.eddi.demo.lectureClass.jpa.board.service;
+
+import kr.eddi.demo.lectureClass.jpa.board.entity.JpaBoard;
+import kr.eddi.demo.lectureClass.jpa.board.repository.JpaBoardRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class JpaBoardServiceImpl implements JpaBoardService{
+
+    final private JpaBoardRepository boardRepository;
+
+    @Override
+    public List<JpaBoard> list(){
+        return boardRepository.findAll(Sort.by(Sort.Direction.DESC, "boardId"));
+    }
+}
