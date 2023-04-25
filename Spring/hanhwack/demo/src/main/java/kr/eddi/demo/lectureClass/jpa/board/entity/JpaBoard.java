c634c1ba76ae44b045dfc1062ef3fc46b53d48c7
hanhwack <hanhwack93@gmail.com>
2023-04-25 09:20:11 +0900

    feat: [한확] Board Entity 생성 [BOARDx]

---
+++ Spring/hanhwack/demo/src/main/java/kr/eddi/demo/lectureClass/jpa/board/entity/JpaBoard.java
@@ -0,0 +1,37 @@
+package kr.eddi.demo.lectureClass.jpa.board.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+
+import java.time.LocalDateTime;
+
+@Entity
+public class JpaBoard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardId;
+
+    private String title;
+    private String writer;
+    private String content;
+
+
+
+    public JpaBoard(String title, String writer, String content) {
+        this.title = title;
+        this.writer = writer;
+        this.content = content;
+    }
+
+    @CreatedDate
+    private LocalDateTime createDate;
+    @UpdateTimestamp
+    private LocalDateTime updateDate;
+}
