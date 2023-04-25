b84e0e788763c000496234790545e79ffac6afda
bbbii <hyeongjin1326@gmail.com>
2023-04-25 09:39:35 +0900

    feat: [김형진] 게시물 조회하기 기능 완료 [BOARDx]

--- Spring/HyeongJinKim/demo/src/main/java/kr/eddi/demo/lectureClass/jpa/board/controller/JpaBoardController.java
+++ Spring/HyeongJinKim/demo/src/main/java/kr/eddi/demo/lectureClass/jpa/board/controller/JpaBoardController.java
@@ -1,6 +1,8 @@
 package kr.eddi.demo.lectureClass.jpa.board.controller;
 
 import kr.eddi.demo.lectureClass.jpa.board.entity.JpaBoard;
+import kr.eddi.demo.lectureClass.jpa.board.service.JpaBoardService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/jpa-board")
 public class JpaBoardController {
 
