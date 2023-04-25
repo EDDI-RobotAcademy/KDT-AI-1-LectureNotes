052cbff9a7b16f56ff5ef4c46ce1a5b6273c19d4
wlsdbwls <wlsdbwls614@gmail.com>
2023-04-25 09:29:34 +0900

    feat: [진유진] BoardList Controller [BOARDx]

---
+++ Java/Jinyujin/demo/src/main/java/com/example/demo/lectureClass/jpa/board/controller/JpaBoardController.java
@@ -0,0 +1,26 @@
+package com.example.demo.lectureClass.jpa.board.controller;
+
+import com.example.demo.lectureClass.jpa.board.entity.JpaBoard;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/jpa-board")
+public class JpaBoardController {
+
+    final private JpaBoardService boardService;
+
+    @GetMapping("/list")
+    public List<JpaBoard> boardList() {
+        log.info("boardList()");
+
+        return boardService.list();
+    }
+}
--- Java/Jinyujin/demo/src/main/java/com/example/demo/lectureClass/jpa/board/entity/JpaBoard.java
+++ Java/Jinyujin/demo/src/main/java/com/example/demo/lectureClass/jpa/board/entity/JpaBoard.java
@@ -7,18 +7,25 @@ import jakarta.persistence.Id;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
 public class JpaBoard {
+    // 얘들의 각각의 목적이 뭘까?
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long boardId;
+    // boardId의 존재 이유는?
+    // 유일성 + 구분해주기 위해서
+    // 예전에 Custom Sequence로 하나씩 올라가게, map으로 해줬는데 그 역할을 위 3줄이 하게 되는 것
+    // (기억 안나니 한 번 더 보자!)
 
     private String title;
     private String writer;
     private String content;
+    // 결국 위 3개가 사용자가 관리하는 주 목적
 
     public JpaBoard(String title, String writer, String content) {
         this.title = title;
@@ -27,8 +34,9 @@ public class JpaBoard {
     }
 
     @CreatedDate
-    private Date createdDate;
+    private LocalDateTime createDate;
 
     @UpdateTimestamp
-    private Date updateDate;
+    private LocalDateTime updateDate;
+    // 날짜 두 줄은 자동으로 생성된다고 함
 }
