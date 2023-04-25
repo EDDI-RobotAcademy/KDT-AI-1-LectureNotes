441ebdfb86b7a1f6736b34fa192a0651e5fe2a88
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-04-25 09:17:49 +0900

    feat: [윤주아]  Board Entity 생성[BOARDx]

---
+++ Java/JuaYun/demo/src/main/java/com/example/demo/controller/lectureClass/jpa/board/entity/JpaBoard.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller.lectureClass.jpa.board.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.util.Date;
+
+
+@Entity
+
+public class JpaBoard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardId;
+
+    private String title;
+    private String writer;
+    private String content;
+
+    // 생성자 생성
+    public JpaBoard(String title, String writer, String content) {
+        this.title = title;
+        this.writer = writer;
+        this.content = content;
+    }
+
+    @CreatedDate
+    private Date createdDate;
+    @UpdateTimestamp
+    private Date updateDate;
+}
