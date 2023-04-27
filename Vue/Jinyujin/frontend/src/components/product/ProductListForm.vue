e4325d8654e1a170f30462277bc1a431bef0b1c2
wlsdbwls <wlsdbwls614@gmail.com>
2023-04-27 20:46:30 +0900

    feat: [진유진] 상품게시판 목록 UI 초기구성 [PRODUCTx}

---
+++ Vue/Jinyujin/frontend/src/components/product/ProductListForm.vue
@@ -0,0 +1,48 @@
+<template lang="">
+    <div>
+        <h3>상품 목록</h3>
+        <table>
+            <tr>
+                <td>상품명</td>
+                <td>상품가격</td>
+                <td>제조사</td>
+                <td>유통기한</td>
+                <td>제조일</td>
+            </tr>
+            <tr v-if="!products || (Arrays.isArray(products) && products.length === 0)">
+                <td>
+                    현재 등록된 상품이 없습니다!
+                </td>
+            </tr>
+            <tr v-else v-for="product in products" :key="product.productId">
+                <td>
+                    {{ product.productId }}
+                </td>
+                <td>
+                    <router-link :to="{
+                        name: 'ProductReadPage'
+                        params: { productId: product.productId.toString() }}">
+                        {{ product.name }}
+                    </router-link>
+                </td>
+                <td>
+                    {{ product.price }}
+                </td>
+                <td>
+                    {{ product.expDate }}
+                </td>
+                <td>
+                    {{ product.manufactureDate }}
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
