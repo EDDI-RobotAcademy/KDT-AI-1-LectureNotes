d3c7edfdf04ae7dcda7298e64b0ec92c42f48ab5
youngjaeKim <hkm2379@gmail.com>
2023-04-28 16:22:52 +0900

    상품

---
+++ Spring/YoungjaeKim/demo/src/main/java/kr/eddi/demo/lectureClass/jpa/product/repository/JpaProductRepository.java
@@ -0,0 +1,7 @@
+package kr.eddi.demo.lectureClass.jpa.product.repository;
+
+import kr.eddi.demo.lectureClass.jpa.product.entity.JpaProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaProductRepository extends JpaRepository<JpaProduct, Long>{
+}
