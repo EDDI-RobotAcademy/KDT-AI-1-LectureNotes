9579db78f0d2d9d5dd0813fb6fc921a7a3d96710
wlsdbwls <128335435+wlsdbwls@users.noreply.github.com>
2023-04-28 11:50:32 +0900

    feat: [진유진] JPA 환경 구축 [JPAx]

--- Vue/Jinyujin/frontend/src/App.vue
+++ Vue/Jinyujin/frontend/src/App.vue
@@ -1,6 +1,42 @@
 <template>
   <v-app>
-    <navigation-menu-page/>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
     <v-main>
       <router-view/>
     </v-main>
@@ -8,34 +44,12 @@
 </template>
 
 <script>
-import NavigationMenuPage from './views/basics/layout/navigation/NavigationMenuPage.vue';
 
 export default {
-  components: {
-    NavigationMenuPage
-  }
-}
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
