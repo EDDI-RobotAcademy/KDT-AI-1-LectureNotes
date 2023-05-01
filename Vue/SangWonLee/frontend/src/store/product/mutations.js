0323f4be965661506ce6a242a5e78142fd4fb51b
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-05-01 23:11:45 +0900

    add: [이상원] product 문제 주석 추가 (수정중2)

--- Vue/SangWonLee/frontend/src/store/product/mutations.js
+++ Vue/SangWonLee/frontend/src/store/product/mutations.js
@@ -2,6 +2,8 @@ import {
   REQUEST_PRODUCT_LIST_TO_SPRING,
   REQUEST_PRODUCT_TO_SPRING,
 } from "./mutation-types";
+// mutation-types에서 REQUEST_PRODUCT_LIST_TO_SPRING, REQUEST_PRODUCT_TO_SPRING 두 개의 상수를 가져와서 사용한다.
+
 
 export default {
   [REQUEST_PRODUCT_LIST_TO_SPRING](state, receivedData) {
--- Vue/SangWonLee/frontend/src/store/product/states.js
+++ Vue/SangWonLee/frontend/src/store/product/states.js
@@ -2,3 +2,6 @@ export default {
   products: [],
   product: {},
 };
+
+// products 는 빈 배열로 초기화 되어있고,
+// product 는 빈 객체로 초기화 되어있다.
