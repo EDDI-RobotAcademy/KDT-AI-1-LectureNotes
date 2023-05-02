54605aeb41ef9b39000e152358a49e24a1b09be0
hwansik <hwansik08@gmail.com>
2023-05-02 10:17:34 +0900

    fix: [민환식] defaultRouterSet, defaultSet은 인식이 안됨 [VRRx]

--- Vue/HwansikMin/frontend/src/router/index.js
+++ Vue/HwansikMin/frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+// @는 현재 src 폴더를 의미함
+import HomeView from '@/views/HomeView.vue'
 import vuetifyTestRoutes from './uiVuetify'
 import productRoutes from './product'
 import boardRoutes from './board'
@@ -10,11 +11,16 @@ import axiosTestRoutes from './axiosTest'
 import componentTestRoutes from './componentTest'
 import vueBasicRoutes from './vueBasic'
 import htmlCssRoutes from './htmlCss'
-import defaultSetRoutes from './defaultSet'
 
 Vue.use(VueRouter)
 
 const routes = [
+
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
   {
     path: '/about',
     name: 'about',
@@ -23,7 +29,6 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
-  ...defaultSetRoutes,
   ...htmlCssRoutes,
   ...vueBasicRoutes,
   ...componentTestRoutes,
