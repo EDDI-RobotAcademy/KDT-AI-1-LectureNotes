a2bf3fd6ed824229605ce3cf3f2c5281db0eeabe
YeoulCho <cyu5733@gmail.com>
2023-05-02 10:17:25 +0900

    feat: [조여울] defaultRouterSet 추가 [VRRx]

--- Vue/YeoulCho/frontend/src/router/index.js
+++ Vue/YeoulCho/frontend/src/router/index.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 //@는 현재 src 폴더를 의미함
-import HomeView from '@/views/HomeView.vue'
 
 
 
-
+import defaultSetRoutes from './defaultRouterSet'
 import vueBasicRoutes from './vueBaxic'
 import componentTestRoutes from './componentTest'
 import axiosTestRoutes from './axiosTest'
@@ -17,21 +16,9 @@ import vuetifyTestRoutes from './uiVuetify/index,'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
   
   
+  ...defaultSetRoutes,
   ...vueBasicRoutes,
   ...componentTestRoutes,
   ...axiosTestRoutes,
