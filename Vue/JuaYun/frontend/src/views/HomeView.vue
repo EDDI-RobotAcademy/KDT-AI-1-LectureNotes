bcb83fbe56f6dd83969763c465d0bc5acddfe422
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-05-02 11:28:22 +0900

    feat: [윤주아] vuetify 이미지 갤러리 [VIGx]

--- Vue/JuaYun/frontend/src/views/HomeView.vue
+++ Vue/JuaYun/frontend/src/views/HomeView.vue
@@ -34,6 +34,8 @@
        <router-link to="/board-list-page">Vue + Spring + JPA 게시판</router-link> |
        <router-link to="/vuetify-table-test">Vuetify Table 테스트</router-link> |
         <router-link to="/vuetify-table-advanced">Vuetify Table 추가 테스트</router-link> |
+        <router-link to="/vuetify-image-gallery">Vuetify Image 갤러리</router-link> |
+        
       </nav>
     </div>
   </v-container>
