4593c59aef1e06263efc3a798734f7c941336737
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-05-02 10:33:24 +0900

    feat: [남현기] v-data-table에 데이터 맵핑하기 [VDTx]

--- Vue/HyunkiNam/frontend/src/views/HomeView.vue
+++ Vue/HyunkiNam/frontend/src/views/HomeView.vue
@@ -31,6 +31,7 @@
         <router-link to="/product-image-page">상품</router-link> |
         <router-link to="/board-list-page">Vue + SPRING + JPA 게시판</router-link> |
         <router-link to="/vuetify-table-test">Vuetify Table 테스트</router-link> |
+        <router-link to="/vuetify-table-advanced">Vuetify Table 추가 테스트</router-link> |
 
 
       </nav>
