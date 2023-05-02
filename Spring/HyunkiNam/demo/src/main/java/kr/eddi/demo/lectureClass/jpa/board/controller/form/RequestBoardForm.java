7f0acbd948d29d4448d31cf9bfbd97f7068492f8
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-05-02 10:07:36 +0900

    update: [남현기] RequestBoardForm 수정

--- Spring/HyunkiNam/demo/src/main/java/kr/eddi/demo/lectureClass/jpa/board/controller/form/RequestBoardForm.java
+++ Spring/HyunkiNam/demo/src/main/java/kr/eddi/demo/lectureClass/jpa/board/controller/form/RequestBoardForm.java
@@ -2,17 +2,18 @@ package kr.eddi.demo.lectureClass.jpa.board.controller.form;
 
 import kr.eddi.demo.lectureClass.jpa.board.entity.JpaBoard;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @ToString
-@RequiredArgsConstructor
+@NoArgsConstructor
 // 전달 받을 데이터 Form
 public class RequestBoardForm {
-    final private String title;
-    final private String content;
-    final private String writer;
+    private String title;
+    private String content;
+    private String writer;
     
     // 전달 받은 값들을 가지고 JpaBoard 형식 만들어주기
     public JpaBoard toJpaBoard() {
