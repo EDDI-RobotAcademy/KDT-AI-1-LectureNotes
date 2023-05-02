d2b3e1343a9c557bad23c4957bd9281eac9d89cb
hwansik <hwansik08@gmail.com>
2023-05-02 10:46:47 +0900

    feat: [민환식] 상품 리스트 요청 처리 [IBPP-1] 230501

---
+++ Spring/HwansikMin/spring/demo/src/main/java/kr/eddi/demo/lectureClass/jpa/product/entity/jpaProduct.java
@@ -0,0 +1,27 @@
+package kr.eddi.demo.lectureClass.jpa.product.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor
+public class JpaProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+    private String productName;
+    private Integer price;
+
+    public JpaProduct(Long productId, String productName, Integer price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.price = price;
+    }
+}
