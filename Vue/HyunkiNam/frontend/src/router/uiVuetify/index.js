c7568d75ac0363a74b0c62188049263b506be0fa
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-05-02 11:30:03 +0900

    feat: [남현기] vuetify 이미지 갤러리 [VIGx]

--- Vue/HyunkiNam/frontend/src/router/uiVuetify/index.js
+++ Vue/HyunkiNam/frontend/src/router/uiVuetify/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 // @는 현재 src 폴더를 의미함
 import TableTestPage from '@/views/uiVuetify/TableTestPage.vue'
 import TableAdvanced from '@/views/uiVuetify/TableAdvanced.vue'
+import ImageGalleryTest from '@/views/uiVuetify/ImageGalleryTest.vue'
 
 Vue.use(VueRouter)
 
@@ -16,6 +17,11 @@ const vuetifyTestRoutes = [
         path: '/vuetify-table-advanced',
         name: 'TableAdvanced',
         component: TableAdvanced
+    },
+    {
+        path: '/vuetify-image-gallery',
+        name: 'ImageGalleryTest',
+        component: ImageGalleryTest
     },
 ]
 
