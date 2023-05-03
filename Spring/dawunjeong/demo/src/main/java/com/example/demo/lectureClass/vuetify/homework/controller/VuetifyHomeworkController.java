538ce99cf1684086880b65edf086115409a205a7
jeongdawun <jeongdawun33@gmail.com>
2023-05-03 18:26:49 +0900

    feat: [정다운] Vue 문제 은행 [ 6 ] 2가지 방식으로 풀어보기 [HOMEWORK]

--- Spring/dawunjeong/demo/src/main/java/com/example/demo/lectureClass/vuetify/homework/controller/VuetifyHomeworkController.java
+++ Spring/dawunjeong/demo/src/main/java/com/example/demo/lectureClass/vuetify/homework/controller/VuetifyHomeworkController.java
@@ -2,12 +2,25 @@ package com.example.demo.lectureClass.vuetify.homework.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @RestController
 @RequestMapping("/vuetify")
 public class VuetifyHomeworkController {
     @GetMapping("/img")
     public String getImageName () {
-        return "naruto.png";
+
+        List<String> imageNameList = new ArrayList<>();
+        imageNameList.add("dragonquest.png");
+        imageNameList.add("ff7.png");
+        imageNameList.add("mario.png");
+        imageNameList.add("naruto.png");
+        imageNameList.add("zeida.png");
+
+        int selectNum = (int) (Math.random() * 4);
+
+        return imageNameList.get(selectNum);
     }
 }
--- Vue/dawunjeong/frontend/src/store/uiVuetify/actions.js
+++ Vue/dawunjeong/frontend/src/store/uiVuetify/actions.js
@@ -1,5 +1,6 @@
 import {
     REQUEST_TEST_IMAGE_PATH_TO_SPRING,
+    REQUEST_TEST_IMAGE_NAME_TO_SPRING
 } from './mutation-types'
 
 import axiosInst from '@/utility/axiosInst'
@@ -9,6 +10,13 @@ export default {
         return axiosInst.get('/give-me/image-path')
             .then((res) => {
                 commit(REQUEST_TEST_IMAGE_PATH_TO_SPRING, res.data)
+                return res.data
+            })
+    },
+    requestImageNameToSpring ({ commit }) {
+        return axiosInst.get('/vuetify/img')
+            .then((res) => {
+                commit(REQUEST_TEST_IMAGE_NAME_TO_SPRING, res.data)
                 return res.data
             })
     },
