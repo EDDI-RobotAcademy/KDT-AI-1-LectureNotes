37adf4202e2ba8dc19bd9ccf58db51c4021587ec
Sanghoon Lee <gcccompil3r@gmail.com>
2023-05-03 11:51:20 +0900

    update: [이상훈] d3 관련 일부 주석 추가 [D3COMMENT]

--- Vue/SanghoonLee/frontend/src/views/plot/D3PlotTestPage.vue
+++ Vue/SanghoonLee/frontend/src/views/plot/D3PlotTestPage.vue
@@ -30,7 +30,7 @@ export default {
             ],
             width: 800,
             height: 500,
-            padding: 0,
+            padding: 40,
         }
     },
     computed: {
@@ -38,11 +38,20 @@ export default {
             return this.line(this.data)
         },
         line () {
+            // x, y값을 표현할 때 d와 i가 존재하는 것을 볼 수 있음
+            // 그리고 실제로 x는 xScale을 통해서 실행이됨
+            console.log('path()에서 불려짐: 넌 언제 그려지니 ?')
             return d3.line()
+                    // d는 실질적인 data를 의미함
+                    // i는 해당 data의 index를 의미함
                     .x((d, i) => this.xScale(i))
                     .y((d) => this.yScale(d))
         },
         xScale () {
+            // xScale은 쉽게 생각하자면 아래와 같습니다.
+            // 데이터를 표현하는 폭이 40, 760의 범주를 가지고 있음
+            // domain을 보면 this.data가 들어가서 d, i를 나누고 있음
+            // 여기서 실제로 가지고 있는 데이터를 표현하는 화면의 비율에 맞게 스케일 작업을 진행
             return d3.scaleLinear()
                     .range([this.padding, this.width - this.padding])
                     .domain(d3.extent(this.data, (d, i) => i))
@@ -89,12 +98,16 @@ export default {
             // translate(0, height)와 같으므로
             // 사실상 맨 처음 시작점을 왼쪽 하단으로 잡았다 보면 됨
             // height 값은 꼭대기가 0점이고 숫자가 커지면 아래로 내려감
-            .attr("transform", "translate(0," +  height + ")")
+            // 전체 화면에서 오른쪽으로 40만큼 수평이동 시켜서 왼쪽에 숫자를 출력할 구간을 확보함
+            .attr("transform", "translate(40," +  height + ")")
             .call(d3.axisBottom(x))
 
         // https://observablehq.com/@d3/d3-line
         g.append("g")
             .call(d3.axisLeft(y))
+            // 위에서도 40만큼 공간을 확보하므로 전체 구성이 깨지지 않으려면
+            // 여기서도 40만큼 수평이동 시켜서 이상하지 않게 만들어야함
+            .attr('transform', `translate(40, 0)`)
             .append("text")
             .attr("fill", "#000")
             // 여기서는 transform에 회전 변환을 적용함
@@ -109,6 +122,9 @@ export default {
 
         g.append("path")
             .datum(this.reg_data)
+            // 위에서도 40만큼 공간을 확보하므로 전체 구성이 깨지지 않으려면
+            // 여기서도 40만큼 수평이동 시켜서 그래프 전체의 윤곽이 흐트러지지 않게 구성함
+            .attr('transform', `translate(40, 0)`)
             .attr("fill", "none")
             .attr("stroke", "steelblue")
             .attr("stroke-width", 1.5)
