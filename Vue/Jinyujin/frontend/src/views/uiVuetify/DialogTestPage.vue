09b2c06fbdf434b21c5d0fae0c883b1b0d015fed
wlsdbwls <wlsdbwls614@gmail.com>
2023-05-03 10:06:57 +0900

    upadate: [진유진] Dialog 주석 추가 [DIALOGx]

--- Vue/Jinyujin/frontend/src/views/uiVuetify/DialogTestPage.vue
+++ Vue/Jinyujin/frontend/src/views/uiVuetify/DialogTestPage.vue
@@ -1,14 +1,20 @@
 <template lang="">
     <div>
-        <p class="blue lighten-4 red--text">Test</p>
+        <p class="pink lighten-4 pink--text">Test</p>
         <p class="green white--text">Test</p>
-        <p class="yellow darken-3">Test</p>
-        <p class="teal lighten-1 text-center">Test</p>
+        <p class="yellow lighten-2">Test</p>
+        <p class="teal lighten-3 text-center">Test</p>
+        <!-- 
+            가운데 정렬했을 때 글씨에 색깔 입히는 법 궁금 
+            text에 색을 적용할 때는 단순히 간단한 색깔만 가능할까?
+        -->
+        <!-- vuetify 색깔 알려주는 곳: https://vuetifyjs.com/en/styles/colors/ -->
         <!-- 
             지금 App.vue에서 #app에 vue 전체 text-align이 center에 쓰이도록 나와있음
             그것을 지웠을 경우 text-center를 쓰면 가운데 정렬이 됨 
          -->
-        <v-btn class="teal lighten-3" rounded @click="btnClick">버튼</v-btn>
+        <v-btn class="purple lighten-4" rounded @click="btnClick">버튼</v-btn>
+        <p></p>
         <!-- rounded를 붙이면 둥글게 생성됨 -->
         
         <v-layout justify-center>
@@ -17,7 +23,7 @@
             <v-dialog v-model="isDialogOpened" fullscreen persistent max-width="400">
             <!-- 최대 폭 400으로 지정 -->
             <!-- 
-                fullscreen을 빼면 팝업처럼 작게 띄울 수 있음 
+                persistent 앞에 fullscreen을 빼면 팝업처럼 작게 띄울 수 있음 
                 전체 화면으로 만들고 싶은 경우에 쓰면 됨
             -->
                 <template v-slot:activator="{on}">
@@ -28,6 +34,7 @@
                     v-card가 나타나게 만들어주는 트리거
                 -->
                 <v-card>
+                    <!-- 결제 버튼 누르면 어떤 창이 뜨도록 보여주는게 v-card -->
                     <v-card-title class="headline">
                         {{ name }}
                     </v-card-title>
@@ -36,21 +43,23 @@
                         <h3>{{ aggroSupport }}</h3>
                         <p>{{ aggroFinish }}</p>
                     </v-card-text>      
+                    <!-- 문구를 직접 넣는 것보다 아래 return 값에 넣어주면 더 깔끔하게 코드 작성 가능 -->
                     <v-card-actions>
                         <v-spacer></v-spacer>
                         <v-btn color="teal darken-1" text @click="btnCancel">
                             취소
                         </v-btn>
+                        <!-- 
+                            actions로 구성하고 sapacer로 공간 나누기 그리고 v-card 안에서 취소 버튼을 만들어줌
+                            spacer를 빼면 왼쪽에 버튼이 위치하고 넣으면 오른쪽에 위치하게 됨
+                            br이 아니라 왼쪽 공간을 띄우는 태그
+                        -->
+                        <!-- 그리고 취소 버튼 누르면 v-card 창이 꺼지도록 해주는게 isDalogopened -->
                         <v-btn color="teal darken-1" text @click="btnPay">
                             결제
                         </v-btn>
                     </v-card-actions>              
                 </v-card>
-                <!-- 이 안에 만들어 놓은 버튼 누르면 같이 어떤 창이 뜨게 만들어 주는게 v-card -->
-                <!-- actions로 구성하고 sapacer로 공간 나누기 그리고 v-card 안에서 취소 버튼을 만들어줌-->
-                <!-- 그리고 취소 버튼 누르면 v-card 창이 꺼지도록 해주는게 isDalogopened -->
-                <!-- 결제 버튼도 만들어주기 -->
-                <!-- 문구를 직접 넣는 것보다 아래 return 값에 넣어주면 더 깔끔하게 코드 작성 가능 -->
                 <!-- dialog를 작성할 때는 v-card와 같이 사용하는 것이 좋음 -->
             </v-dialog>
         </v-layout>     
