7107edd749e210f3d3aaa6c0d29000c916983ce0
jeongdawun <jeongdawun33@gmail.com>
2023-05-03 18:25:14 +0900

    update: [정다운] 크로셀 사용해보기 오타 수정 [CAROUSELx]

--- Vue/dawunjeong/frontend/src/views/uiVuetify/CarouselTestPage.vue
+++ Vue/dawunjeong/frontend/src/views/uiVuetify/CarouselTestPage.vue
@@ -1,7 +1,7 @@
 <template lang="">
 <v-container>
     <div>
-        <v-carousel :cycle="cycle" :interval="interval" :height="height" v-model="model">
+        <v-carousel :cycle="cycle" :interval="interval" :height="height" v-model="carouselModel">
             <v-carousel-item v-for="(picture, idx) in bannerPictures" :key="idx">
                 <v-sheet :height="height" tile>
                     <v-row class="fill-height" align="center" justify="center">
