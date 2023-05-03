b64748766e44377063f1a25d391ccd3d133cf814
wlsdbwls <wlsdbwls614@gmail.com>
2023-05-03 12:08:15 +0900

    feat: [진유진] d3을 활용하여 그래프 그려보기 [D3PLOTx]

---
+++ Vue/Jinyujin/frontend/src/router/d3plot/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import D3PlotTestPage from '@/views/plot/D3PlotTestPage.vue'
+
+Vue.use(VueRouter)
+
+const d3plotTestRoutes = [
+    {
+        path: '/d3-plot-test',
+        name: 'D3PlotTestPage',
+        component: D3PlotTestPage
+    },
+
+]
+
+export default d3plotTestRoutes
--- Vue/Jinyujin/frontend/src/router/index.js
+++ Vue/Jinyujin/frontend/src/router/index.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // @는 현재 src 폴더를 의미함
 
-
-
-
 import productRoutes from './product'
 import boardRoutes from './board'
 import vuetifyTestRoutes from './uiVuetify'
@@ -15,6 +12,8 @@ import componentTestRoutes from './componentTest'
 import vueBasicRoutes from './vueBasic'
 import htmlCssRoutes from './htmlCss'
 import defaultSetRoutes from './defaultRouterSet'
+import solvedRoutes from './solved'
+import d3plotTestRoutes from './d3plot'
 
 // 지저분한 궁극적인 이유?
 // 분류가 안되어서 - 즉 도메인 정리가 안됨
@@ -23,12 +22,6 @@ import defaultSetRoutes from './defaultRouterSet'
 Vue.use(VueRouter)
 
 const routes = [
- 
-  
-  
-  
-  
- 
   ...vuetifyTestRoutes,
   // 이렇게 하면 분할한 라우터가 여기에 자동으로 붙음 - 뷰티파이에 대한 라우터구나 알 수 있음
   ...productRoutes,
@@ -40,6 +33,8 @@ const routes = [
   ...vueBasicRoutes,
   ...htmlCssRoutes,
   ...defaultSetRoutes,
+  ...solvedRoutes,
+  ...d3plotTestRoutes,
 ]
 
 const router = new VueRouter({
