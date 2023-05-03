171e1b59dc7db5dbe2fad50d0ff367d225634f91
wlsdbwls <wlsdbwls614@gmail.com>
2023-05-03 09:43:15 +0900

    feat: [진유진] Dialog 사용해보기 [DIALOGx]

--- Vue/Jinyujin/frontend/src/router/uiVuetify/index.js
+++ Vue/Jinyujin/frontend/src/router/uiVuetify/index.js
@@ -5,6 +5,7 @@ import TableTestPage from '@/views/uiVuetify/TableTestPage.vue'
 import TableAdvanced from '@/views/uiVuetify/TableAdvanced.vue'
 import ImageGalleryTest from '@/views/uiVuetify/ImageGalleryTest.vue'
 import PrintImageTest from '@/views/uiVuetify/PrintImageTest.vue'
+import DialogTestPage from '@/views/uiVuetify/DialogTestPage.vue'
 
 Vue.use(VueRouter)
 
@@ -28,6 +29,11 @@ const vuetifyTestRoutes = [
         path: '/print-image-test',
         name: 'PrintImageTest',
         component: PrintImageTest
+    },
+    {
+        path: '/dialog-test-page',
+        name: 'DialogTestPage',
+        component: DialogTestPage
     },
 ]
 
--- Vue/Jinyujin/frontend/src/views/HomeView.vue
+++ Vue/Jinyujin/frontend/src/views/HomeView.vue
@@ -40,6 +40,7 @@
         <router-link to="/vuetify-table-advanced">Vuetify Table 추가 테스트</router-link> |
         <router-link to="/vuetify-image-gallery">Vuetify Image 갤러리</router-link> |
         <router-link to="/print-image-test">이미지 출력 테스트</router-link> |
+        <router-link to="/dialog-test-page">Dialog 테스트</router-link> |
       </nav>
     </div>
   </v-container>
