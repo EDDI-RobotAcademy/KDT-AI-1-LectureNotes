c491f5c8af8a45f1eabc03c30247d40d95d4ed02
zisoo <zisoo_3@naver.com>
2023-05-04 12:09:27 +0900

    chore: [최지수] 헬스 문제, 프론트 주석 완료 [D3-HEALTHx]

--- Spring/JisooChoi/demo/src/main/java/kr/eddi/demo/lectureClass/vue/d3/controller/form/HealthDataResponse.java
+++ Spring/JisooChoi/demo/src/main/java/kr/eddi/demo/lectureClass/vue/d3/controller/form/HealthDataResponse.java
@@ -7,8 +7,7 @@ public class HealthDataResponse {
     private Integer healthAmount;
 
     /*
-        인자 값으로 랜덤 값을 받아와서 값을 넣어주고
-        HealthDataResponse 타입으로 반환한다.
+        인자 값으로 랜덤 값을 받아와서 값을 넣고, 생성해준다. (생성자)
     */
     public HealthDataResponse(Integer healthAmount) {
         this.healthAmount = healthAmount;
--- Vue/JisooChoi/frontend/src/views/plot/D3HealthPrintPage.vue
+++ Vue/JisooChoi/frontend/src/views/plot/D3HealthPrintPage.vue
@@ -10,14 +10,17 @@
 </template>
 
 <script>
+// npm install d3 --save-dev
 import * as d3 from "d3"
 import { mapActions, mapState } from "vuex"
 
 const d3PlotModule = 'd3PlotModule'
 
 export default {
+    // data : 사용하는 지역 변수 
     data () {
         return {
+            // data : 그래프 상에서 표현하려고 하는 data 
             width: 800,
             height: 500,
             padding: 40, // 패딩 값을 없애면 화면이 거의 사라진다.. 왜지?
@@ -26,17 +29,28 @@ export default {
     },
     computed: {
         drawPlot () {
+            // ▲ 2번 째 -> ... -> 다시 7번 째
+            console.log('drawPlot()')
+
             return this.line(this.data)
         },
         line () {
+            // ▲ 3번 째
+            console.log('line()')
+
             return d3.line()
                     /*
                         i는 인덱스, d는 데이터
+
+                        이 부분에서 x, y축 눈금 선들이 먼저 그려지도록 mounted로 날아간다 !
                     */
                     .x((d, i) => this.xScale(i))
                     .y((d) => this.yScale(d.healthAmount))
         },
         xScale () {
+            // ▲ 5번 째
+            console.log('xScale()')
+
             return d3.scaleLinear()
                     /*
                         range은 잘 모르겠으나, 없애면 왼쪽에 일자 형식으로 압축 되어버림
@@ -46,6 +60,9 @@ export default {
                     .domain([0, 6])
         },
         yScale () {
+            // ▲ 6번 째
+            console.log('yScale()')
+
             return d3.scaleLinear()
                     /*
                         range은 잘 모르겠으나, 없애면 위쪽에 일자 형식으로 압축 되어버림
@@ -56,14 +73,22 @@ export default {
         },
         ...mapState(d3PlotModule, ['healthDataList']),
     },
+    // ▲ 1번 째
     async created () {
+        console.log('created()')
+
         this.data = await this.requestHealthDataToSpring()
         console.log("healthDataList: " + JSON.stringify(this.data))
     },
-    methods: {
+    methods: { // 메서드에는 콘솔 안 찍힘
         ...mapActions(d3PlotModule, ['requestHealthDataToSpring']),
     },
+
+    // path가 먼저 그려지기 전에 아래 mounted를 통해 x, y 축 들이 먼저 생긴다.
     mounted () {
+        // ▲ 4번 째
+        console.log('mounted()')
+
         const width = 800
         const height = 500
         const svg = d3.select("svg")
