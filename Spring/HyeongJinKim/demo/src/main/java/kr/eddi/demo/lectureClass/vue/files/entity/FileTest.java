98b91167e5332bfcf54596f5587969e3c8687572
bbbii <hyeongjin1326@gmail.com>
2023-05-04 10:40:03 +0900

    refactor: [김형진] Spring 리팩토링

---
+++ Spring/HyeongJinKim/demo/src/main/java/kr/eddi/demo/lectureClass/vue/files/entity/FileTest.java
@@ -0,0 +1,24 @@
+package kr.eddi.demo.lectureClass.vue.files.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class FileTest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer price;
+    private String productName;
+    private String imagePath;
+
+    public FileTest(Integer price, String productName, String imagePath) {
+        this.price = price;
+        this.productName = productName;
+        this.imagePath = imagePath;
+    }
+}
---
+++ Spring/HyeongJinKim/demo/src/main/java/kr/eddi/demo/lectureClass/vue/files/service/FileService.java
@@ -0,0 +1,4 @@
+package kr.eddi.demo.lectureClass.vue.files.service;
+
+public interface FileService {
+}
