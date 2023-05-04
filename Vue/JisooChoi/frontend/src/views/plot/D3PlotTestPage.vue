c59e9dda98e5aa2bbe6190cd7e0d5c346d69c5e7
zisoo <zisoo_3@naver.com>
2023-05-04 12:10:21 +0900

    feat: [최지수] D3 헬스 정보 출력 [D3-HEALTHx]

--- Vue/JisooChoi/frontend/src/views/plot/D3PlotTestPage.vue
+++ Vue/JisooChoi/frontend/src/views/plot/D3PlotTestPage.vue
@@ -12,8 +12,10 @@
 // npm install d3 --save-dev
 import * as d3 from "d3"
 export default {
+    // data : 사용하는 지역 변수 
     data () {
         return {
+            // data : 그래프 상에서 표현하려고 하는 data 
             data: [90.9, 70, 90.2, 25, 10.2, 92],
             reg_data: [
                 { date: "24-Apr-23", amount: 93.24 },
@@ -37,20 +39,26 @@ export default {
             return this.line(this.data)
         },
         line () {
-            // x, y값을 표현할 때 d와 i가 존재하는 것을 볼 수 있음
-            // 그리고 실제로 x는 xScale을 통해서 실행이됨
+            /*
+                x, y값을 표현할 때 d와 i가 존재하는 것을 볼 수 있음
+                그리고 실제로 x는 xScale을 통해서 실행이됨
+            */
             console.log('path()에서 불려짐: 넌 언제 그려지니?')
             return d3.line()
-                    // d는 실질적인 data를 의미함
-                    // i는 해당 data의 index를 의미함
-                    .x((d, i) => this.xScale(i)) // i는 인덱스 
-                    .y((d) => this.yScale(d)) // d는 데이터
+                    /*
+                        d는 실질적인 data를 의미함
+                        i는 해당 data의 index를 의미함
+                    */
+                    .x((d, i) => this.xScale(i))
+                    .y((d) => this.yScale(d))
         },
         xScale () {
-            // xScale은 쉽게 생각하자면 아래와 같습니다.
-            // 데이터를 표현하는 폭이 40, 760의 범주를 가지고 있음
-            // domain을 보면 this.data가 들어가서 d, i를 나누고 있음
-            // 여기서 실제로 가지고 있는 데이터를 표현하는 화면의 비율에 맞게 스케일 작업을 진행
+            /*
+                xScale은 쉽게 생각하자면 아래와 같습니다.
+                데이터를 표현하는 폭이 40, 760의 범주를 가지고 있음
+                domain을 보면 this.data가 들어가서 d, i를 나누고 있음
+                여기서 실제로 가지고 있는 데이터를 표현하는 화면의 비율에 맞게 스케일 작업을 진행
+            */
             return d3.scaleLinear()
                     .range([this.padding, this.width - this.padding])
                     .domain(d3.extent(this.data, (d, i) => i))
@@ -95,26 +103,34 @@ export default {
                         })
 
         g.append("g")
-            // transform은 평행 이동, 회전 변환 등등을 수행할 수 있음
-            // translate(0, height)와 같으므로
-            // 사실상 맨 처음 시작점을 왼쪽 하단으로 잡았다 보면 됨
-            // height 값은 꼭대기가 0점이고 숫자가 커지면 아래로 내려감
-            // 전체 화면에서 오른쪽으로 40만큼 수평이동 시켜서 왼쪽에 숫자를 출력할 구간을 확보함
+            /*
+                transform은 평행 이동, 회전 변환 등등을 수행할 수 있음
+                translate(0, height)와 같으므로
+                사실상 맨 처음 시작점을 왼쪽 하단으로 잡았다 보면 됨
+                height 값은 꼭대기가 0점이고 숫자가 커지면 아래로 내려감
+                전체 화면에서 오른쪽으로 40만큼 수평이동 시켜서 왼쪽에 숫자를 출력할 구간을 확보함
+            */
             .attr("transform", "translate(40," +  height + ")")
             .call(d3.axisBottom(x))
 
         g.append("g")
             .call(d3.axisLeft(y))
-            // 위에서도 40만큼 공간을 확보하므로 전체 구성이 깨지지 않으려면
-            // 여기서도 40만큼 수평이동 시켜서 이상하지 않게 만들어야 함
+            /*
+                위에서도 40만큼 공간을 확보하므로 전체 구성이 깨지지 않으려면
+                여기서도 40만큼 수평이동 시켜서 이상하지 않게 만들어야 함
+            */
             .attr('transform', `translate(40, 0)`)
             .append("text")
             .attr("fill", "#000")
-            // 여기서는 transform에 회전 변환을 적용함
-            // -90도 회전시켜서 y축에 Price를 표현하였음 (기본값은 x축임)
+            /*
+                여기서는 transform에 회전 변환을 적용함
+                -90도 회전시켜서 y축에 Price를 표현하였음 (기본값은 x축임)
+            */
             .attr("transform", "rotate(-90)")
-            // y에 대해 16이기 때문에 회전 변환이 끝난 이후에 y축에 대해 16이 아니라
-            // 회전하기 이전에 16에 대해 위의 회전 변환을 적용함
+            /*
+                y에 대해 16이기 때문에 회전 변환이 끝난 이후에 y축에 대해 16이 아니라
+                회전하기 이전에 16에 대해 위의 회전 변환을 적용함
+            */
             .attr("y", 16)
             .attr("dy", "0.71em")
             .attr("text-anchor", "end")
@@ -122,8 +138,10 @@ export default {
 
         g.append("path")
             .datum(this.reg_data)
-            // 위에서도 40만큼 공간을 확보하므로 전체 구성이 깨지지 않으려면
-            // 여기서도 40만큼 수평이동 시켜서 그래프 전체의 윤곽이 흐트러지지 않게 구성함
+            /*
+                위에서도 40만큼 공간을 확보하므로 전체 구성이 깨지지 않으려면
+                여기서도 40만큼 수평이동 시켜서 그래프 전체의 윤곽이 흐트러지지 않게 구성함
+            */
             .attr("fill", "none")
             .attr("stroke", "steelblue")
             .attr("stroke-width", 1.5)
