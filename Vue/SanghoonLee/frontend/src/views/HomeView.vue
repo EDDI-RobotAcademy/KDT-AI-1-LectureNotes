c0be4e3edd89f792499292ec8e780fd6b8078aac
Sanghoon Lee <gcccompil3r@gmail.com>
2023-05-04 13:48:29 +0900

    fix: [이상훈] Code에 대한 문제점 개선 [CODE-REVIEWx]

--- Vue/SanghoonLee/frontend/src/views/HomeView.vue
+++ Vue/SanghoonLee/frontend/src/views/HomeView.vue
@@ -40,6 +40,9 @@
         <router-link to="/d3-health-print-page">d3 health 문제</router-link> |
         <router-link to="/file-handler-test-page">File 처리 테스트</router-link> |
         <router-link to="/file-list-show-page">Spring에 요청해서 파일 리스트 보기</router-link> |
+        <router-link to="/d3-review1-page">D3 Review 1</router-link> |
+
+        
       </nav>
     </div>
   </v-container>
