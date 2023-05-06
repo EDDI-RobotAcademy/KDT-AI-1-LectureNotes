21f6edded7f333971bde57ba7c7fbb517f03f369
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-05-06 12:50:07 +0900

    update: [이상원] d3 기능 추가

--- Vue/SangWonLee/frontend/src/store/product/mutations.js
+++ Vue/SangWonLee/frontend/src/store/product/mutations.js
@@ -8,8 +8,8 @@ import {
 export default {
   [REQUEST_PRODUCT_LIST_TO_SPRING](state, receivedData) {
     state.products = receivedData;
-  },
+  }, // 메소드 이름이 REQUEST_PRODUCT_LIST_TO_SPRING으로 첫번째 매개변수는 state, 두번째 매개변수는 receivedData
   [REQUEST_PRODUCT_TO_SPRING](state, receivedData) {
     state.product = receivedData;
-  },
+  }, // 메소드 이름이 REQUEST_PRODUCT_TO_SPRING으로 첫번째 매개변수는 state, 두번째 매개변수는 receivedData
 };
