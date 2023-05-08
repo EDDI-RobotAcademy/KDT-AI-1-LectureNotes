ab72fc96719cd2a48d0fc0ce635aa69e7415015c
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-05-08 21:36:08 +0900

    feat: [윤주아] 기초문제 진행중 [JAVAx]

--- Java/JuaYun/exam/evaluation/src/problems/prob12/StudentClass.java
+++ Java/JuaYun/exam/evaluation/src/problems/prob12/StudentClass.java
@@ -2,29 +2,37 @@ package problems.prob12;
 
 import problems.CustomRandom;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-// 평균 구하는 방법 총점 나누기 학생수
+// 12. 랜덤으로 20명의 학생에게 점수를 생성하고 평균을 계산
+// 13. 학생들의 점수를 정렬하여 순위를 출력
 public class StudentClass {
-    private int Student = 20;
-    private int MAX_SCORE = 100;
-    private int MIN_SCORE = 0;
-    List<Integer> StudentScoreList = new ArrayList<>();
-    int AverageScore = 0;
-    int ScoreSum = 0;
+
+    List<Integer> studentList = new ArrayList<>();
 
     public StudentClass() {
-        for (int i = 0; i < Student; i++) {
-            StudentScoreList.add(CustomRandom.generateNumber(MIN_SCORE,MAX_SCORE));
-            ScoreSum += StudentScoreList.get(i);
-            AverageScore = ScoreSum / Student;
-
+        int sum = 0;
+        for (int i = 0; i < 20; i++) {
+            studentList.add(CustomRandom.generateNumber(0,100));
+            sum += studentList.get(i);
+            // 원래 작성한 코드는
+            // sum += i;
+            // studentList 에 넣어줬던 값을 합해야하기 때문에
+            // studentList.get(i) 로 작성해줘야함
         }
+        float avg = (float) sum/studentList.size();
+        // 처음작성은 sum/20으로 표현했었음
+        // 이후 작성한 지금의 형태가 확실히 뭘 하고자하는지 보임
 
-        StudentScoreList.sort(Comparator.reverseOrder());
-        System.out.println("학생들의 점수" + StudentScoreList);
-        System.out.println("학생들의 총점" + ScoreSum);
-        System.out.println("평균점수" + AverageScore);
+        System.out.println("12번 문제. 학생들의 점수는: " + studentList);
+        System.out.println("12번 문제. 평균점수는: " + avg);
+
+        Collections.sort(studentList, Collections.reverseOrder());
+        // 내림차순으로 정렬하는 방법 꼭 기억하기
+        // 오름차순은 Collections.sort(studentList)
+        System.out.println("13번 문제. 순위: " + studentList);
     }
-
 }
--- Java/JuaYun/exam/evaluation/src/problems/prob3/Problem3.java
+++ Java/JuaYun/exam/evaluation/src/problems/prob3/Problem3.java
@@ -1,30 +1,8 @@
 package problems.prob3;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
+// 문자열 10개를 리스트에 넣고 랜덤으로 출력해봅시다.
 public class Problem3 {
     public static void main(String[] args) {
-
-        List<String> letterList = new ArrayList<>();
-
-        letterList.add("A");
-        letterList.add("B");
-        letterList.add("C");
-        letterList.add("D");
-        letterList.add("E");
-        letterList.add("F");
-        letterList.add("G");
-        letterList.add("H");
-        letterList.add("I");
-        letterList.add("J");
-
-        Collections.shuffle(letterList);
-
-        for(int i = 0; i < letterList.size(); i++) {
-            System.out.println(letterList.get(i));
-        }
-
+        RandomList randomList = new RandomList();
     }
 }
