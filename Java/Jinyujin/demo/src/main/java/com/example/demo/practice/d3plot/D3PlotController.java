8c17593047c336597c6643b2baa31e2422e6a5ab
wlsdbwls <wlsdbwls614@gmail.com>
2023-05-08 18:40:36 +0900

    update: [진유진] 헬스 정보 + 날짜 출력 날짜 코드 변경 [UPDATE]

--- Java/Jinyujin/demo/src/main/java/com/example/demo/practice/d3plot/D3PlotController.java
+++ Java/Jinyujin/demo/src/main/java/com/example/demo/practice/d3plot/D3PlotController.java
@@ -3,6 +3,7 @@ package com.example.demo.practice.d3plot;
 import com.example.demo.lectureClass.utility.random.CustomRandom;
 import com.example.demo.lectureClass.vue.d3.controller.form.HealthDataResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cglib.core.Local;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,8 @@ public class D3PlotController {
     public List<HealthDataResponse> giveMeHealthData() {
         log.info("giveMeHealthData()");
 
-        LocalDate showDate = LocalDate.now();
+//        LocalDate showDate = LocalDate.now();
+        LocalDate showDate = LocalDate.of(2023, 04, 27);
         healthDataList.clear();
 
         for (int i = 0; i < 7; i++) {
