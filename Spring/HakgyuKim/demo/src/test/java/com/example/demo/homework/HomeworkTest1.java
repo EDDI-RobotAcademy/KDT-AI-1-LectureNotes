dc54411d02c2ba07ec8326f6f262c8d203b54628
Hakgyu Kim <zage4448@gmail.com>
2023-05-09 14:13:34 +0900

    feat: [김학규] 회원 가입 테스트 해보기 [SAP-1]

---
+++ Spring/HakgyuKim/demo/src/test/java/com/example/demo/homework/HomeworkTest1.java
@@ -0,0 +1,31 @@
+package com.example.demo.homework;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class HomeworkTest1 {
+
+    @Test
+    @DisplayName("아이디와 비밀번호로 회원가입")
+    void accountRegister () {
+        final String expectedEmail = "test@test.com";
+        final String expectedPassword = "test123";
+        HomeworkAccount homeworkTestAccount = new HomeworkAccount(expectedEmail, expectedPassword);
+        homeworkAccountService.register(homeworkTestAccount);
+
+        if (homeworkTestAccount == null) {
+            System.out.println("중복된 닉네임이 있습니다");
+            return;
+        }
+
+        final String actualEmail = homeworkTestAccount.getEmail();
+        final String actualPassword = homeworkTestAccount.getPassword();
+
+        assertEquals(expectedEmail, actualEmail);
+        assertEquals(expectedPassword, actualPassword);
+    }
+}
