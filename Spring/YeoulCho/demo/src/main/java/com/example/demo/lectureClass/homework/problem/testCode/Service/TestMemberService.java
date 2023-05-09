404252fc1c2f6643d57f16c62c6555f6be6f6d45
YeoulCho <cyu5733@gmail.com>
2023-05-09 16:20:00 +0900

    feat: [조여울]리퀘스트폼을 통해 받은 회원 정보로 계정을 생성합니다. [BCAP-1]

---
+++ Spring/YeoulCho/demo/src/main/java/com/example/demo/lectureClass/homework/problem/testCode/Service/TestMemberService.java
@@ -0,0 +1,7 @@
+package com.example.demo.lectureClass.homework.problem.testCode.Service;
+
+import com.example.demo.lectureClass.homework.problem.testCode.entity.TestMember;
+
+public interface TestMemberService {
+    TestMember register(TestMember testMember);
+}
