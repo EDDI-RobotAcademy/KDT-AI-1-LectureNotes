f2b48c8ba6fb4759181592a5887781b5b3d3701e
dusdn943 <dusdn943@gmail.com>
2023-05-09 15:20:21 +0900

    update: [최연우] 람다 주석 추가 [D3COMMENT]

--- Spring/yeonwoochoi/demo/src/main/java/com/example/demo/lectureClass/testCode/student/entity/TestStudent.java
+++ Spring/yeonwoochoi/demo/src/main/java/com/example/demo/lectureClass/testCode/student/entity/TestStudent.java
@@ -19,7 +19,9 @@ public class TestStudent {
     private String name;
 
     @Setter
+    // 데이터베이스와 객체를 맵핑하기 위해 사용
     @ManyToOne
+    // 왜래키를 맵핑할 때 사용
     @JoinColumn(name = "lecture_id")
     private TestLecture testLecture;
 
