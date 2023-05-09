85af5fbfdf6c1d9cb8d219267cba52331260787e
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-05-09 17:59:21 +0900

    fix: [남현기] 변수명 수정 [SMP-1]

--- Spring/HyunkiNam/demo/src/test/java/kr/eddi/demo/homeworkTest/HomeworkTest.java
+++ Spring/HyunkiNam/demo/src/test/java/kr/eddi/demo/homeworkTest/HomeworkTest.java
@@ -1,5 +1,6 @@
 package kr.eddi.demo.homeworkTest;
 
+import kr.eddi.demo.lectureClass.testCode.member.entity.TestMember;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,20 +18,20 @@ public class HomeworkTest {
     @Test
     @DisplayName("Spring JUnit Test")
     void MemberTest(){
-        final String expected_Id = "testing";
+        final String expected_Name = "testing";
         final String expected_Password = "test";
 
-        TestMember testMember = new TestMember(expected_Id, expected_Password);
+        TestMember testMember = new TestMember(expected_Name, expected_Password);
         testMemberService.register(testMember);
 
-        Optional<TestMember> maybeTestMember= testMemberRepository.findByMemberId(expected_Id);
+        Optional<TestMember> maybeTestMember= testMemberRepository.findByMemberId(expected_Name);
 
         if(maybeTestMember.isEmpty()){
             System.out.println("회원 정보 없음");
         }
 
         testMember = maybeTestMember.get();
-        assertEquals(expected_Id, testMember.getMemberId());
+        assertEquals(expected_Name, testMember.getMemberName());
         assertEquals(expected_Password, testMember.getMemberPassword());
     }
 }
