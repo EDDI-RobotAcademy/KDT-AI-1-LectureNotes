3a3de0e9b24c2ae00b15efcf3af11a815b683481
Hakgyu Kim <zage4448@gmail.com>
2023-05-09 14:44:06 +0900

    feat: [김학규] 회원 정보 저장하기 구현 [SAP-4]

--- Spring/HakgyuKim/demo/src/test/java/com/example/demo/homework/HomeworkTest1.java
+++ Spring/HakgyuKim/demo/src/test/java/com/example/demo/homework/HomeworkTest1.java
@@ -4,6 +4,7 @@ import com.example.demo.lectureClass.testCode.homework.homework1.entity.Homework
 import com.example.demo.lectureClass.testCode.homework.homework1.service.HomeworkAccountService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 public class HomeworkTest1 {
 
+    @Autowired
     private HomeworkAccountService homeworkAccountService;
 
     @Test
