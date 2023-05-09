046b9078070c2fe2d0ad5232cd12f4b7e8be82c4
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-05-09 12:16:23 +0900

    feat: [윤주아] 학생이 수강 신청하기 테스트 [TESTx]

---
+++ Spring/JuaYun/demo/src/main/java/com/example/demo/lectureClass/student/service/TestStudentService.java
@@ -0,0 +1,7 @@
+package com.example.demo.lectureClass.student.service;
+
+import com.example.demo.lectureClass.student.entity.TestStudent;
+
+public interface TestStudentService {
+    TestStudent register(TestStudent testStudent);
+}
