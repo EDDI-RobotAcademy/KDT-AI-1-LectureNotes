b527a2af66508530b601716e3ed5bd9bf7b68b32
dusdn943 <dusdn943@gmail.com>
2023-05-10 10:21:30 +0900

    feat: [최연우] 회원 가입 로직 구현 완료 [ATP-1]

---
+++ Spring/yeonwoochoi/demo/src/main/java/com/example/demo/lectureClass/testCode/account/service/TestAccountService.java
@@ -0,0 +1,8 @@
+package com.example.demo.lectureClass.testCode.account.service;
+
+import com.example.demo.lectureClass.testCode.account.controller.form.TestAccountRequestForm;
+import com.example.demo.lectureClass.testCode.account.entity.TestAccount;
+
+public interface TestAccountService {
+    TestAccount register(TestAccountRequestForm requestForm);
+}
