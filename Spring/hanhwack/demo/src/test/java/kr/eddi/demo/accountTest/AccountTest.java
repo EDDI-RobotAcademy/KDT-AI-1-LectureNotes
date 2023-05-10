d94820ae1be411e81517e0d043e3bbccd1c76fde
hanhwack <hanhwack93@gmail.com>
2023-05-10 12:51:24 +0900

    update: [한확] 중복된 이메일 주소 회원 가입 방지 로직 구현 추가 [ATP-1]

--- Spring/hanhwack/demo/src/test/java/kr/eddi/demo/accountTest/AccountTest.java
+++ Spring/hanhwack/demo/src/test/java/kr/eddi/demo/accountTest/AccountTest.java
@@ -84,6 +84,17 @@ public class AccountTest {
         assertEquals(email, account.getEmail());
         assertEquals(password, account.getPassword());
     }
+    @Test
+    @DisplayName("똑같은 사용자는 회원 가입 할 수 없음")
+    void 이미_존재하는_이메일로_회원_가입시도 () {
+        final String email = "test@test.com";
+        final String password = "test";
+
+        TestAccountRequestForm requestForm = new TestAccountRequestForm(email, password);
+        TestAccount account = testAccountService.register(requestForm);
+
+        assertTrue(account == null);
+    }
 
     @Test
     @DisplayName("잘못된 비밀번호 정보를 토대로 로그인")
