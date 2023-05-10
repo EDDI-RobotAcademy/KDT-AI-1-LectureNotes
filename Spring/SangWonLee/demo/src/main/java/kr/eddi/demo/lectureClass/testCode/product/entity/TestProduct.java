f0e7a9d706a723c32d0e322ba39ba6aa177fe362
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-05-10 16:31:07 +0900

    feat: [이상원] 회원이 상품 주문하기 [ORDERx]

--- Spring/SangWonLee/demo/src/main/java/kr/eddi/demo/lectureClass/testCode/product/entity/TestProduct.java
+++ Spring/SangWonLee/demo/src/main/java/kr/eddi/demo/lectureClass/testCode/product/entity/TestProduct.java
@@ -1,4 +1,4 @@
-package kr.eddi.demo.lectureClass.testCode.account.entity;
+package kr.eddi.demo.lectureClass.testCode.product.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,20 +7,20 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Getter
+@Entity
 @NoArgsConstructor
-public class TestAccount {
+public class TestProduct {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String email;
-    private String password;
+    private String productName;
+    private Integer price;
 
-    public TestAccount(String email, String password) {
-        this.email = email;
-        this.password = password;
+    public TestProduct(String productName, Integer price) {
+        this.productName = productName;
+        this.price = price;
     }
 }
