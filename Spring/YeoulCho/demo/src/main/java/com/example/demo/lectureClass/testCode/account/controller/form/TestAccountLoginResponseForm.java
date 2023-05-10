219adc246734bbf26672d591a99c97776a9265aa
YeoulCho <cyu5733@gmail.com>
2023-05-10 10:50:56 +0900

    feat: [조여울] 로그인 로직 완료 [ATP-2]

---
+++ Spring/YeoulCho/demo/src/main/java/com/example/demo/lectureClass/testCode/account/controller/form/TestAccountLoginResponseForm.java
@@ -0,0 +1,11 @@
+package com.example.demo.lectureClass.testCode.account.controller.form;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+@Getter
+@RequiredArgsConstructor
+public class TestAccountLoginResponseForm {
+    final private UUID userToken;
+}
--- Spring/YeoulCho/demo/src/test/java/com/example/demo/accountTest/AccountTest.java
+++ Spring/YeoulCho/demo/src/test/java/com/example/demo/accountTest/AccountTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.accountTest;
 
+import com.example.demo.lectureClass.testCode.account.controller.form.TestAccountLoginResponseForm;
 import com.example.demo.lectureClass.testCode.account.controller.form.TestAccountRequestForm;
 import com.example.demo.lectureClass.testCode.account.entity.TestAccount;
 import com.example.demo.lectureClass.testCode.account.repository.TestAccountRepository;
@@ -9,7 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
@@ -74,4 +75,40 @@ public class AccountTest {
         // 넌뭐하는엔티티.나도몰라(object.이것도한다());
 
     }
+    @Test
+    @DisplayName("잘못된 비밀번호 정보를 토대로 로그인")
+    public void 이메일만_맞게_입력한_상태에서_로그인(){
+        final String email = "test@test.com";
+        final String password ="응 틀렸어";
+        TestAccountRequestForm requestForm = new TestAccountRequestForm(email,password);
+        TestAccountLoginResponseForm responseForm = testAccountService.login(requestForm);
+
+        //assertEquals(false, responseForm.isLogin());
+        assertTrue(responseForm.getUserToken() ==null);
+    }
+
+    @Test
+    @DisplayName("이메일을 잘못 입력한 상태로 로그인")
+    public void 이메일을_틀리게_입력한_상태에서_로그인(){
+        final String email = "gogo@gogo.com";
+        final String password ="응 틀렸어";
+        TestAccountRequestForm requestForm = new TestAccountRequestForm(email,password);
+        TestAccountLoginResponseForm responseForm = testAccountService.login(requestForm);
+
+        //assertEquals(false, responseForm.isLogin());
+        assertTrue(responseForm.getUserToken() ==null);
+    }
+
+    @Test
+    @DisplayName("올바른 입력한 정보를 토대로 로그인")
+    public void 올바른_정보로_로그인(){
+        final String email = "test@test.com";
+        final String password ="test";
+        TestAccountRequestForm requestForm = new TestAccountRequestForm(email,password);
+        TestAccountLoginResponseForm responseForm = testAccountService.login(requestForm);
+
+        //assertEquals(false, responseForm.isLogin());
+        assertTrue(responseForm.getUserToken() !=null);
+    }
+
 }
