fd9733335b6711a22af6f5929200b7e17cee0f0a
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-05-10 10:51:57 +0900

    feat: [윤주아] 로그인 로직 완료 [ACCOUNTx-2]

--- Spring/JuaYun/demo/src/main/java/com/example/demo/lectureClass/account/service/TestAccountServiceImpl.java
+++ Spring/JuaYun/demo/src/main/java/com/example/demo/lectureClass/account/service/TestAccountServiceImpl.java
@@ -1,11 +1,16 @@
 package com.example.demo.lectureClass.account.service;
 
+import com.example.demo.lectureClass.account.controller.form.TestAccountLoginResponseForm;
 import com.example.demo.lectureClass.account.controller.form.TestAccountRequestForm;
 import com.example.demo.lectureClass.account.entity.TestAccount;
 import com.example.demo.lectureClass.account.repository.TestAccountRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -16,5 +21,23 @@ public class TestAccountServiceImpl implements TestAccountService {
     public TestAccount register(TestAccountRequestForm requestForm) {
 
         return testAccountRepository.save(requestForm.toTestAccount());
+    }
+
+    @Override
+    public TestAccountLoginResponseForm login(TestAccountRequestForm requestForm) {
+        final Optional<TestAccount> maybeAccount =
+                testAccountRepository.findByEmail(requestForm.getEmail());
+
+        if (maybeAccount.isEmpty()) {
+            log.debug("로그인 실패!");
+            return new TestAccountLoginResponseForm(null);
+        }
+        TestAccount account = maybeAccount.get();
+
+        if (account.getPassword().equals(requestForm.getPassword())) {
+            log.debug("로그인 성공!");
+            return new TestAccountLoginResponseForm(UUID.randomUUID());
+        }
+        return new TestAccountLoginResponseForm(null);
     }
 }
--- Spring/JuaYun/demo/src/test/java/com/example/demo/accountTest/AccountTest.java
+++ Spring/JuaYun/demo/src/test/java/com/example/demo/accountTest/AccountTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.accountTest;
 
+import com.example.demo.lectureClass.account.controller.form.TestAccountLoginResponseForm;
 import com.example.demo.lectureClass.account.controller.form.TestAccountRequestForm;
 import com.example.demo.lectureClass.account.entity.TestAccount;
 import com.example.demo.lectureClass.account.repository.TestAccountRepository;
@@ -9,7 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class AccountTest {
@@ -31,5 +32,40 @@ public class AccountTest {
 
         assertEquals(email, account.getEmail());
         assertEquals(password, account.getPassword());
+    }
+
+    @Test
+    @DisplayName("잘못된 비밀번호 정보를 토대로 로그인")
+    void 이메일만_맞게_입력한_상태에서_로그인 () {
+        final String email = "test@test.com";
+        final String password = "응틀렸어";
+        TestAccountRequestForm requestForm = new TestAccountRequestForm(email, password);
+        TestAccountLoginResponseForm responseForm = testAccountService.login(requestForm);
+
+        assertTrue(responseForm.getUserToken() == null);
+    }
+
+    @Test
+    @DisplayName("이메일을 잘못 입력한 상태로 로그인")
+    void 이메일을_틀리게_입력한_상태에서_로그인 () {
+        final String email = "gogo@gogo.com";
+        final String password = "응틀렸어";
+
+        TestAccountRequestForm requestForm = new TestAccountRequestForm(email, password);
+        TestAccountLoginResponseForm responseForm = testAccountService.login(requestForm);
+
+        assertTrue(responseForm.getUserToken() == null);
+    }
+
+    @Test
+    @DisplayName("올바른 입력한 정보를 토대로 로그인")
+    void 올바른_정보로_로그인 () {
+        final String email = "test@test.com";
+        final String password = "test";
+
+        TestAccountRequestForm requestForm = new TestAccountRequestForm(email, password);
+        TestAccountLoginResponseForm responseForm = testAccountService.login(requestForm);
+
+        assertTrue(responseForm.getUserToken() != null);
     }
 }
