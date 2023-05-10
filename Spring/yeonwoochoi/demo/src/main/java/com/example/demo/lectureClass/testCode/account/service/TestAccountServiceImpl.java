6f4f49b763324a2152f979e7f7c659dd2450d567
dusdn943 <dusdn943@gmail.com>
2023-05-10 10:53:30 +0900

    feat: [최연우] 로그인 로직 완료 [ATP-2]

--- Spring/yeonwoochoi/demo/src/main/java/com/example/demo/lectureClass/testCode/account/service/TestAccountServiceImpl.java
+++ Spring/yeonwoochoi/demo/src/main/java/com/example/demo/lectureClass/testCode/account/service/TestAccountServiceImpl.java
@@ -1,22 +1,44 @@
 package com.example.demo.lectureClass.testCode.account.service;
 
+import com.example.demo.lectureClass.testCode.account.controller.form.TestAccountLoginResponseForm;
 import com.example.demo.lectureClass.testCode.account.controller.form.TestAccountRequestForm;
 import com.example.demo.lectureClass.testCode.account.entity.TestAccount;
 import com.example.demo.lectureClass.testCode.account.repository.TestAccountRepository;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class TestAccountServiceImpl implements TestAccountService {
-
     final private TestAccountRepository testAccountRepository;
     @Override
     public TestAccount register(TestAccountRequestForm requestForm) {
 
         return testAccountRepository.save(requestForm.toTestAccount());
+    }
+
+    @Override
+    public TestAccountLoginResponseForm login(TestAccountRequestForm requestForm) {
+        final Optional<TestAccount> maybeAccount =
+                testAccountRepository.findByEmail(requestForm.getEmail());
+
+        if (maybeAccount.isEmpty()) {
+            log.debug("로그인 실패!");
+            return new TestAccountLoginResponseForm(null);
+        }
+
+        TestAccount account = maybeAccount.get();
+
+        if (account.getPassword().equals(requestForm.getPassword())) {
+            log.debug("로그인 성공!");
+            return new TestAccountLoginResponseForm(UUID.randomUUID());
+        }
+
+        return new TestAccountLoginResponseForm(null);
     }
 }
