5b9ad2415a274d3b4e5633e7a52d327e4281a2a2
hwansik <hwansik08@gmail.com>
2023-05-11 09:45:38 +0900

    feat: [민환식] 사용자의 주문 내역 조회하기 [ORDERx]

--- Spring/HwansikMin/spring/demo/src/main/java/kr/eddi/demo/lectureClass/testCode/order/service/TestOrderService.java
+++ Spring/HwansikMin/spring/demo/src/main/java/kr/eddi/demo/lectureClass/testCode/order/service/TestOrderService.java
@@ -1,8 +1,13 @@
 package kr.eddi.demo.lectureClass.testCode.order.service;
 
+import kr.eddi.demo.lectureClass.testCode.order.controller.form.TestOrderListRequestForm;
 import kr.eddi.demo.lectureClass.testCode.order.controller.form.TestOrderRequestForm;
 import kr.eddi.demo.lectureClass.testCode.order.entity.TestOrder;
 
+import java.util.List;
+
 public interface TestOrderService {
     TestOrder order (TestOrderRequestForm requestForm);
+
+    List<TestOrder> orderListForAccount(TestOrderListRequestForm orderListRequestForm);
 }
