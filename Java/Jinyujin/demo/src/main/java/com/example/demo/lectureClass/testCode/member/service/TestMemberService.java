600ba9f286d74539ee9e2f503debe3d0e3eef82c
wlsdbwls <wlsdbwls614@gmail.com>
2023-05-11 17:04:07 +0900

    chore: [진유진] 삭제한 파일 [DELETE]

--- Java/Jinyujin/demo/src/main/java/com/example/demo/lectureClass/testCode/member/service/TestMemberService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.lectureClass.testCode.member.service;
-
-import com.example.demo.lectureClass.testCode.member.entity.TestMember;
-
-public interface TestMemberService {
-    TestMember register(TestMember testMember);
-}
