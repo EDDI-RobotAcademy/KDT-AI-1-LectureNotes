9a247a3dd87ecbdeeb8cbdf7d13ec9ef5d1a0911
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-05-11 17:49:12 +0900

    update: [이상원] order 주석 추가 (수정 중)

--- Spring/SangWonLee/demo/src/main/java/kr/eddi/demo/lectureClass/testCode/order/service/TestOrderServiceImpl.java
+++ Spring/SangWonLee/demo/src/main/java/kr/eddi/demo/lectureClass/testCode/order/service/TestOrderServiceImpl.java
@@ -2,6 +2,8 @@ package kr.eddi.demo.lectureClass.testCode.order.service;
 
 import kr.eddi.demo.lectureClass.testCode.account.entity.TestAccount;
 import kr.eddi.demo.lectureClass.testCode.account.repository.TestAccountRepository;
+import kr.eddi.demo.lectureClass.testCode.order.controller.form.TestAccountResponseForm;
+import kr.eddi.demo.lectureClass.testCode.order.controller.form.TestOrderAccountRequestForm;
 import kr.eddi.demo.lectureClass.testCode.order.controller.form.TestOrderListRequestForm;
 import kr.eddi.demo.lectureClass.testCode.order.controller.form.TestOrderRequestForm;
 import kr.eddi.demo.lectureClass.testCode.order.entity.TestOrder;
@@ -12,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,26 +25,36 @@ public class TestOrderServiceImpl implements TestOrderService {
     final private TestAccountRepository accountRepository;
     final private TestProductRepository productRepository;
     final private TestOrderRepository orderRepository;
-
-    // 지금 redis를 쓸 수 없으므로 임시 방편용
+    
     private Long alwaysReturnFirst (String userToken) {
         return 1L;
     }
-
+    // userToken을 인자로 받아 항상 1L로 반환.
+    // Redis를 사용할 수 없을 때 임시로 사용하는 방법이다.
+    // 원래는 Redis를 사용하여 userToken을 검증하려고 할거임
+    
     @Override
     public TestOrder order(TestOrderRequestForm requestForm) {
         final TestAccount account = isValidateAccount(alwaysReturnFirst(requestForm.getUserToken()));
+        // userToken을 받아서 계정이 있는지 없는지 확인한다.
+        // 여기서는 Redis로 확인하려고 하지만 아직 배우지 않은 상태이므로,
+        //  1로 반환시켜서 진행
+
         if (account == null) return null;
+        // 없으면 null
 
         final Optional<TestProduct> maybeProduct = productRepository.findById(requestForm.getProductId());
+        // productId 가 있는지 없는지 확인
 
         if (maybeProduct.isEmpty()) {
             log.debug("주문을 진행할 수 없습니다!");
             return null;
         }
         final TestProduct product = maybeProduct.get();
+        // 있으면 TestProduct로 값 가져와서
 
         return orderRepository.save(new TestOrder(account, product));
+        // account, product를 인자로 jpa로 save한다.
     }
 
     @Override
@@ -54,13 +67,41 @@ public class TestOrderServiceImpl implements TestOrderService {
         return orderRepository.findAllByAccountId(account.getId());
     }
 
+    @Override
+    public List<TestAccountResponseForm> findAllAccountWhoBuyProduct(
+            TestOrderAccountRequestForm requestForm) {
+
+        List<TestOrder> orderList =
+                orderRepository.findAllAccountWhoBuyProduct(requestForm.getProductId());
+        // accountId만 따로 추출이 필요
+        List<TestAccountResponseForm> responseFormList = new ArrayList<>();
+
+        for (TestOrder order: orderList) {
+            Optional<TestAccount> maybeAccount =
+                    accountRepository.findById(order.getTestAccount().getId());
+
+            if (maybeAccount.isPresent()) {
+                final TestAccount testAccount = maybeAccount.get();
+                final TestAccountResponseForm responseForm = new TestAccountResponseForm(
+                        testAccount.getId(), testAccount.getEmail());
+
+                responseFormList.add(responseForm);
+            }
+        }
+
+        return responseFormList;
+    }
+
     private TestAccount isValidateAccount(Long accountId) {
         final Optional<TestAccount> maybeAccount = accountRepository.findById(accountId);
+        // 입력받은 accountId 로 accountRepository.findById 메소드를 호출하여 TextAccount 객체를 찾는다.
+        // 없으면 빈 Optional이 반환되고, 있으면 Optional<TestAccount> 객체를 maybeAccount 변수에 저장한다.
 
         if (maybeAccount.isEmpty()) {
             log.debug("주문을 진행할 수 없습니다!");
             return null;
         }
         return maybeAccount.get();
+        // 있으면 TestAccount 객체 값 반환.
     }
 }
--- Spring/SangWonLee/demo/src/test/java/kr/eddi/demo/orderTest/OrderTest.java
+++ Spring/SangWonLee/demo/src/test/java/kr/eddi/demo/orderTest/OrderTest.java
@@ -3,6 +3,8 @@ package kr.eddi.demo.orderTest;
 import kr.eddi.demo.lectureClass.testCode.account.controller.form.TestAccountLoginResponseForm;
 import kr.eddi.demo.lectureClass.testCode.account.controller.form.TestAccountRequestForm;
 import kr.eddi.demo.lectureClass.testCode.account.service.TestAccountService;
+import kr.eddi.demo.lectureClass.testCode.order.controller.form.TestAccountResponseForm;
+import kr.eddi.demo.lectureClass.testCode.order.controller.form.TestOrderAccountRequestForm;
 import kr.eddi.demo.lectureClass.testCode.order.controller.form.TestOrderListRequestForm;
 import kr.eddi.demo.lectureClass.testCode.order.controller.form.TestOrderRequestForm;
 import kr.eddi.demo.lectureClass.testCode.order.entity.TestOrder;
@@ -14,11 +16,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class OrderTest {
-    @Autowired
+    @Autowired // 객체화 한다고 생각하면 된다.
     private TestAccountService testAccountService;
 
     @Autowired
@@ -37,14 +39,22 @@ public class OrderTest {
 
         TestAccountRequestForm requestForm = new TestAccountRequestForm(email, password);
         TestAccountLoginResponseForm responseForm = testAccountService.login(requestForm);
+        // 받은 email과 password로 로그인한다. (아이디 비번이 다르면 테스트 실패임)
 
         String userToken = responseForm.getUserToken().toString();
+        // userToken을 받아온다.
+        System.out.println(userToken);
+        // 궁금해서 확인
+
+
 
         TestOrderRequestForm orderRequestForm = new TestOrderRequestForm(userToken, productId);
         TestOrder order = testOrderService.order(orderRequestForm);
+        // userTocken과 productId를 가지고 주문 저장
 
         assertEquals(productId, order.getTestProduct().getId());
         assertEquals(accountId, order.getTestAccount().getId());
+
     }
 
     @Test
@@ -65,6 +75,23 @@ public class OrderTest {
 
         for (TestOrder order: orderListForAccount) {
             assertEquals(accountId, order.getTestAccount().getId());
+        }
+    }
+
+    @Test
+    @DisplayName("특정 물품을 구매한 회원 리스트를 조회합니다")
+    void 특정_물품을_구매한_회원_정보_조회 () {
+        final Long productId = 2L;
+
+        TestOrderAccountRequestForm requestForm = new TestOrderAccountRequestForm(productId);
+        List<TestAccountResponseForm> accountResponseFormList =
+                testOrderService.findAllAccountWhoBuyProduct(requestForm);
+
+        System.out.println("accountList size: " + accountResponseFormList.size());
+
+        for (TestAccountResponseForm responseForm: accountResponseFormList) {
+            System.out.println("account email: " + responseForm.getEmail());
+            assertTrue(responseForm.getAccountId() != null);
         }
     }
 
