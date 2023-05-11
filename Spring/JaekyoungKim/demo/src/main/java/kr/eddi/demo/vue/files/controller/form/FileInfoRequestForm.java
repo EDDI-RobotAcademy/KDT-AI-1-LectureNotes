30061d3e15c0cffbd89db2693972df123e6c334c
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-05-11 11:35:51 +0900

    feat: [김재경] vue와 스프링 파일 전송[SPRINGx]

---
+++ Spring/JaekyoungKim/demo/src/main/java/kr/eddi/demo/vue/files/controller/form/FileInfoRequestForm.java
@@ -0,0 +1,20 @@
+package kr.eddi.demo.vue.files.controller.form;
+
+
+import kr.eddi.demo.vue.files.entity.FileTest;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@RequiredArgsConstructor
+public class FileInfoRequestForm {
+
+    final private Integer price;
+    final private String productName;
+
+    public FileTest toFileTest(String originalFileName) {
+        return new FileTest(price, productName, originalFileName);
+    }
+}
