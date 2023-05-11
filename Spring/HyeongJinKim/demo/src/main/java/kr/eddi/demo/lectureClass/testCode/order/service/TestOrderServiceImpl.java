8e5f2953f49818976cb095bb5f8fcf54446c5802
bbbii <hyeongjin1326@gmail.com>
2023-05-11 11:38:01 +0900

    feat: [김형진] 특정 물품을 구매한 회원 조회하기 [ORDERx]

--- Spring/HyeongJinKim/demo/src/main/java/kr/eddi/demo/lectureClass/testCode/order/service/TestOrderServiceImpl.java
+++ Spring/HyeongJinKim/demo/src/main/java/kr/eddi/demo/lectureClass/testCode/order/service/TestOrderServiceImpl.java
@@ -1,7 +1,9 @@
 package kr.eddi.demo.lectureClass.testCode.order.service;
 
+import kr.eddi.demo.lectureClass.testCode.order.controller.form.TestAccountResponseForm;
 import kr.eddi.demo.lectureClass.testCode.account.entity.TestAccount;
 import kr.eddi.demo.lectureClass.testCode.account.repository.TestAccountRepository;
+import kr.eddi.demo.lectureClass.testCode.order.controller.form.TestOrderAccountRequestForm;
 import kr.eddi.demo.lectureClass.testCode.order.controller.form.TestOrderListRequestForm;
 import kr.eddi.demo.lectureClass.testCode.order.controller.form.TestOrderRequestForm;
 import kr.eddi.demo.lectureClass.testCode.order.entity.TestOrder;
@@ -12,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -62,5 +65,30 @@ public class TestOrderServiceImpl implements TestOrderService {
             return null;
         }
         return maybeAccount.get();
+    }
+
+    @Override
+    public List<TestAccountResponseForm> findAllAccountWhoBuyProduct(
+            TestOrderAccountRequestForm requestForm) {
+
+        List<TestOrder> orderList =
+                orderRepository.findAllAccountWhoBuyProduct(requestForm.getProductId());
+        // accountId만 따로 추출이 필요
+        List<TestAccountResponseForm> responseFormList = new ArrayList<>();
+
+        for (TestOrder order: orderList) {
+            Optional<TestAccount> maybeAccount =
+                    accountRepository.findById(order.getTestAccount().getId());
+
+            if (maybeAccount.isPresent()) {
+                final TestAccount testAccount = maybeAccount.get();
+                final TestAccountResponseForm responseForm = new TestAccountResponseForm(
+                        testAccount.getId(), testAccount.getEmail());
+
+                responseFormList.add(responseForm);
+            }
+        }
+
+        return responseFormList;
     }
 }
--- Spring/HyeongJinKim/demo/src/test/java/kr/eddi/demo/orderTest/OrderTest.java
+++ Spring/HyeongJinKim/demo/src/test/java/kr/eddi/demo/orderTest/OrderTest.java
@@ -1,7 +1,10 @@
 package kr.eddi.demo.orderTest;
+
 import kr.eddi.demo.lectureClass.testCode.account.controller.form.TestAccountLoginResponseForm;
 import kr.eddi.demo.lectureClass.testCode.account.controller.form.TestAccountRequestForm;
 import kr.eddi.demo.lectureClass.testCode.account.service.TestAccountService;
+import kr.eddi.demo.lectureClass.testCode.order.controller.form.TestAccountResponseForm;
+import kr.eddi.demo.lectureClass.testCode.order.controller.form.TestOrderAccountRequestForm;
 import kr.eddi.demo.lectureClass.testCode.order.controller.form.TestOrderListRequestForm;
 import kr.eddi.demo.lectureClass.testCode.order.controller.form.TestOrderRequestForm;
 import kr.eddi.demo.lectureClass.testCode.order.entity.TestOrder;
@@ -10,10 +13,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class OrderTest {
@@ -58,6 +61,23 @@ public class OrderTest {
 
         for (TestOrder order: orderListForAccount) {
             assertEquals(accountId, order.getTestAccount().getId());
+        }
+    }
+
+    @Test
+    @DisplayName("특정 물품을 구매한 회원 리스트를 조회합니다")
+    void 특정_물품을_구매한_회원_정보_조회 () {
+        final Long productId = 2L;
+
+        TestOrderAccountRequestForm requestForm = new TestOrderAccountRequestForm(productId);
+        List<TestAccountResponseForm> accountResponseFormList =
+                testOrderService.findAllAccountWhoBuyProduct(requestForm);
+
+        System.out.println("accountList size: " + accountResponseFormList.size());
+
+        for (TestAccountResponseForm responseForm: accountResponseFormList) {
+            System.out.println("account email: " + responseForm.getEmail());
+            assertTrue(responseForm.getAccountId() != null);
         }
     }
 }
