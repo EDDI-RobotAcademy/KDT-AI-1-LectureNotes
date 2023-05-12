e793bf0f92972872e84f8492623a3ee2d07bd64d
YeoulCho <cyu5733@gmail.com>
2023-05-12 20:42:17 +0900

    feat: [조여울] FetchType Lazy 상황에서 join fetch 사용하기 [LAZY-MEMBER-LISTx]

--- Spring/YeoulCho/demo/src/main/java/com/example/demo/lectureClass/fetchType/account/entity/JpaAccount.java
+++ Spring/YeoulCho/demo/src/main/java/com/example/demo/lectureClass/fetchType/account/entity/JpaAccount.java
@@ -8,16 +8,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor
 public class JpaAccount {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Getter
     private String email;
 
-    @Getter
     private String password;
 
     public JpaAccount(String email, String password) {
--- Spring/YeoulCho/demo/src/main/java/com/example/demo/lectureClass/fetchType/account/service/JpaAccountServiceImpl.java
+++ Spring/YeoulCho/demo/src/main/java/com/example/demo/lectureClass/fetchType/account/service/JpaAccountServiceImpl.java
@@ -1,14 +1,19 @@
 package com.example.demo.lectureClass.fetchType.account.service;
 
 import com.example.demo.lectureClass.fetchType.account.entity.JpaAccount;
+import com.example.demo.lectureClass.fetchType.account.entity.JpaAccountRole;
+import com.example.demo.lectureClass.fetchType.account.form.JpaAccountResponseForm;
 import com.example.demo.lectureClass.fetchType.account.form.JpaAccountWithRoleRequestForm;
 import com.example.demo.lectureClass.fetchType.account.rpository.JpaAccountRepository;
 import com.example.demo.lectureClass.fetchType.account.rpository.JpaAccountRoleRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 @Slf4j
 @Service
@@ -35,5 +40,26 @@ public class JpaAccountServiceImpl implements JpaAccountService {
         accountRoleRepository.save(requestForm.toJpaAccountRole(account));
 
         return account;
+    }
+
+    @Override
+    //@Transactional 영....속...성 컨텐츠가...transactional을 끝날때까지 section를 종료하지마라
+    //결론 Lazy로 로딩되지 않은 account 정보가 section이 유지되었기 때문에 account를 로딩 할 수 있다.
+    public List<JpaAccountResponseForm> accountListWithRole(String role) {
+        final List<JpaAccountRole> matchedAccountRoleList=
+                accountRoleRepository.findAllByRole(role);
+        log.info("matchedAccountRoleList: "+ matchedAccountRoleList);
+        //JpaAccountRole 정보 id, role, account
+
+        List<JpaAccountResponseForm> responseFormList = new ArrayList<>();
+
+        for(JpaAccountRole accountRole : matchedAccountRoleList){
+            final Optional<JpaAccount> maybeAccount = accountRepository.findById(accountRole.getId());
+            if(maybeAccount.isEmpty()){return null;}
+
+            final JpaAccount account = maybeAccount.get();
+            responseFormList.add(new JpaAccountResponseForm(account.getId(), account.getEmail()));
+        }
+        return responseFormList;
     }
 }
