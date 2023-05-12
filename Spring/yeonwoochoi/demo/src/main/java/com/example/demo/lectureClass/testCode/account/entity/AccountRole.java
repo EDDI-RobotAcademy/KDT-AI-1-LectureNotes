1727a8d73a81bc2b9117adaf552990d55ed22637
dusdn943 <dusdn943@gmail.com>
2023-05-12 10:26:23 +0900

    feat: [최연우] Account Role (권한) 추가 [MEMBER-ROLEx]

---
+++ Spring/yeonwoochoi/demo/src/main/java/com/example/demo/lectureClass/testCode/account/entity/AccountRole.java
@@ -0,0 +1,20 @@
+package com.example.demo.lectureClass.testCode.account.entity;
+
+import jakarta.persistence.*;
+@Entity
+public class AccountRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String role;
+
+    @ManyToOne
+    private TestAccount account;
+
+    public AccountRole(String role, TestAccount account) {
+        this.role = role;
+        this.account = account;
+    }
+}
--- Spring/yeonwoochoi/demo/src/main/java/com/example/demo/lectureClass/testCode/account/service/TestAccountServiceImpl.java
+++ Spring/yeonwoochoi/demo/src/main/java/com/example/demo/lectureClass/testCode/account/service/TestAccountServiceImpl.java
@@ -2,23 +2,54 @@ package com.example.demo.lectureClass.testCode.account.service;
 
 import com.example.demo.lectureClass.testCode.account.controller.form.TestAccountLoginResponseForm;
 import com.example.demo.lectureClass.testCode.account.controller.form.TestAccountRequestForm;
+import com.example.demo.lectureClass.testCode.account.controller.form.TestAccountWithRoleRequestForm;
 import com.example.demo.lectureClass.testCode.account.entity.TestAccount;
 import com.example.demo.lectureClass.testCode.account.repository.TestAccountRepository;
+import com.example.demo.lectureClass.testCode.account.repository.TestAccountRoleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+
 import java.util.Optional;
 import java.util.UUID;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class TestAccountServiceImpl implements TestAccountService {
 
     final private TestAccountRepository testAccountRepository;
+    final private TestAccountRoleRepository testAccountRoleRepository;
+
     @Override
     public TestAccount register(TestAccountRequestForm requestForm) {
+        final Optional<TestAccount> maybeAccount =
+                testAccountRepository.findByEmail(requestForm.getEmail());
+        if (maybeAccount.isPresent()) {
+            log.debug("이미 가입된 회원입니다!");
+            return null;
+        }
+        return testAccountRepository.save(requestForm.toTestAccount());
+    }
+    @Override
+    public TestAccountLoginResponseForm login(TestAccountRequestForm requestForm) {
+        final Optional<TestAccount> maybeAccount =
+                testAccountRepository.findByEmail(requestForm.getEmail());
+        if (maybeAccount.isEmpty()) {
+            log.debug("로그인 실패!");
+            return new TestAccountLoginResponseForm(null);
+        }
+        TestAccount account = maybeAccount.get();
+        if (account.getPassword().equals(requestForm.getPassword())) {
+            log.debug("로그인 성공!");
+            return new TestAccountLoginResponseForm(UUID.randomUUID());
+        }
+
+        return new TestAccountLoginResponseForm(null);
+    }
+
+    @Override
+    public TestAccount registerWithRole(TestAccountWithRoleRequestForm requestForm) {
         final Optional<TestAccount> maybeAccount =
                 testAccountRepository.findByEmail(requestForm.getEmail());
 
@@ -27,26 +58,21 @@ public class TestAccountServiceImpl implements TestAccountService {
             return null;
         }
 
-        return testAccountRepository.save(requestForm.toTestAccount());
-    }
+        final TestAccount account = testAccountRepository.save(requestForm.toTestAccount());
+        testAccountRoleRepository.save(requestForm.toTestAccountRole(account));
 
-    @Override
-    public TestAccountLoginResponseForm login(TestAccountRequestForm requestForm) {
-        final Optional<TestAccount> maybeAccount =
-                testAccountRepository.findByEmail(requestForm.getEmail());
-
-        if (maybeAccount.isEmpty()) {
-            log.debug("로그인 실패!");
-            return new TestAccountLoginResponseForm(null);
-        }
-
-        TestAccount account = maybeAccount.get();
-
-        if (account.getPassword().equals(requestForm.getPassword())) {
-            log.debug("로그인 성공!");
-            return new TestAccountLoginResponseForm(UUID.randomUUID());
-        }
-
-        return new TestAccountLoginResponseForm(null);
+        return account;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
