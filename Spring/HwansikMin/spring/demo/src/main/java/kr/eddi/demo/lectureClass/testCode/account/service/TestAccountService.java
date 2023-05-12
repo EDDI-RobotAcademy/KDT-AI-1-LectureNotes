e3e06e5ccf8beddb46743379138d14b5d555df5c
hwansik <hwansik08@gmail.com>
2023-05-12 11:43:17 +0900

    feat: [민환식] 회원 Role 값을 바탕으로 회원 조회하기 [MEMBER-LISTx]

--- Spring/HwansikMin/spring/demo/src/main/java/kr/eddi/demo/lectureClass/testCode/account/service/TestAccountService.java
+++ Spring/HwansikMin/spring/demo/src/main/java/kr/eddi/demo/lectureClass/testCode/account/service/TestAccountService.java
@@ -4,6 +4,9 @@ import kr.eddi.demo.lectureClass.testCode.account.controller.form.TestAccountLog
 import kr.eddi.demo.lectureClass.testCode.account.controller.form.TestAccountRequestForm;
 import kr.eddi.demo.lectureClass.testCode.account.controller.form.TestAccountWithRoleRequestForm;
 import kr.eddi.demo.lectureClass.testCode.account.entity.TestAccount;
+import kr.eddi.demo.lectureClass.testCode.order.controller.form.TestAccountResponseForm;
+
+import java.util.List;
 
 public interface TestAccountService {
     TestAccount register(TestAccountRequestForm requestForm);
@@ -11,4 +14,6 @@ public interface TestAccountService {
     TestAccountLoginResponseForm login(TestAccountRequestForm requestForm);
 
     TestAccount registerWithRole(TestAccountWithRoleRequestForm requestForm);
+
+    List<TestAccountResponseForm> accountListWithRole(String role);
 }
