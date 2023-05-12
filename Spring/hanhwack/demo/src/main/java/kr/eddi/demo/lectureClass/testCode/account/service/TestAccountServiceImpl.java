2262b8407d9942eeb8862feb966b2dda1e200639
hanhwack <hanhwack93@gmail.com>
2023-05-12 12:30:37 +0900

    feat: [한확] Account Role (권한) 추가 [MEMBER-ROLEx]

--- Spring/hanhwack/demo/src/main/java/kr/eddi/demo/lectureClass/testCode/account/service/TestAccountServiceImpl.java
+++ Spring/hanhwack/demo/src/main/java/kr/eddi/demo/lectureClass/testCode/account/service/TestAccountServiceImpl.java
@@ -2,8 +2,10 @@ package kr.eddi.demo.lectureClass.testCode.account.service;
 
 import kr.eddi.demo.lectureClass.testCode.account.controller.form.TestAccountLoginResponseForm;
 import kr.eddi.demo.lectureClass.testCode.account.controller.form.TestAccountRequestForm;
+import kr.eddi.demo.lectureClass.testCode.account.controller.form.TestAccountWithRoleRequestForm;
 import kr.eddi.demo.lectureClass.testCode.account.entity.TestAccount;
 import kr.eddi.demo.lectureClass.testCode.account.repository.TestAccountRepository;
+import kr.eddi.demo.lectureClass.testCode.account.repository.TestAccountRoleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,7 @@ import java.util.UUID;
 public class TestAccountServiceImpl implements TestAccountService {
 
     final private TestAccountRepository testAccountRepository;
+    final private TestAccountRoleRepository testAccountRoleRepository;
     @Override
     public TestAccount register(TestAccountRequestForm requestForm) {
 
@@ -50,4 +53,20 @@ public class TestAccountServiceImpl implements TestAccountService {
 
         return new TestAccountLoginResponseForm(null);
     }
+
+    @Override
+    public TestAccount registerWithRole(TestAccountWithRoleRequestForm requestForm) {
+        final Optional<TestAccount> maybeAccount = testAccountRepository.findByEmail(requestForm.getEmail());
+        if(maybeAccount.isPresent()) {
+            log.debug("이미 가입된 회원입니다.");
+            return null;
+        }
+
+        final TestAccount account = testAccountRepository.save(requestForm.toTestAccount());
+        testAccountRoleRepository.save(requestForm.toTestAccountRole(account));
+
+        return account;
+
+    }
+
 }
