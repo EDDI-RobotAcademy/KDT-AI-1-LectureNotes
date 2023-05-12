a00d0114c4e37b1ff982eeb536d1b37238291c5c
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-05-12 15:06:30 +0900

    feat: [이상원] FetchType Lazy 상황에서 join fetch 사용하기 [LAZY-MEMBER-LISTx]

--- Spring/SangWonLee/demo/src/main/java/kr/eddi/demo/lectureClass/fetchType/account/entity/JpaAccountRole.java
+++ Spring/SangWonLee/demo/src/main/java/kr/eddi/demo/lectureClass/fetchType/account/entity/JpaAccountRole.java
@@ -1,24 +1,27 @@
-package kr.eddi.demo.lectureClass.testCode.account.entity;
+package kr.eddi.demo.lectureClass.fetchType.account.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@ToString(exclude = {"account"})
 @NoArgsConstructor
-public class AccountRole {
+public class JpaAccountRole {
 
     @Id
+    @Getter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String role;
 
     @Getter
-    @ManyToOne
-    private TestAccount account;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private JpaAccount account;
 
-    public AccountRole(String role, TestAccount account) {
+    public JpaAccountRole(String role, JpaAccount account) {
         this.role = role;
         this.account = account;
     }
