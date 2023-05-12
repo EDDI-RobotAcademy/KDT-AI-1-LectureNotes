f293aeb928bae8efeb672cce92f105af9f471a29
zisoo <zisoo_3@naver.com>
2023-05-12 10:04:56 +0900

    remove: [최지수] 불필요한 폴더 정리

--- Spring/JisooChoi/demo/src/main/java/kr/eddi/demo/lectureClass/testCode/account/service/TestTypeAccountService.java
+++
@@ -1,8 +0,0 @@
-package kr.eddi.demo.lectureClass.testCode.account.service;
-
-import kr.eddi.demo.lectureClass.testCode.account.controller.form.TestTypeAccountRequestForm;
-import kr.eddi.demo.lectureClass.testCode.account.entity.TestAccountType;
-
-public interface TestTypeAccountService {
-    TestAccountType register(TestTypeAccountRequestForm requestForm);
-}
