d23e9e1378bfcd3e69b7f8e4a9b24a21644bd98c
wlsdbwls <wlsdbwls614@gmail.com>
2023-05-15 20:26:24 +0900

    feat: [진유진] 문제은행 Spring JUnit Test [3] 완료

--- Java/Jinyujin/demo/src/main/java/com/example/demo/lectureClass/fetchType/account/controller/form/JpaAccountRoleRequestForm.java
+++ Java/Jinyujin/demo/src/main/java/com/example/demo/lectureClass/fetchType/account/controller/form/JpaAccountRoleRequestForm.java
@@ -2,11 +2,12 @@ package com.example.demo.lectureClass.fetchType.account.controller.form;
 
 import com.example.demo.lectureClass.fetchType.account.entity.JpaAccount;
 import com.example.demo.lectureClass.fetchType.account.entity.JpaAccountRole;
+import com.example.demo.lectureClass.fetchType.account.entity.Role;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public class JpaAccountRoleRequestForm {
-    final private String role;
+    final private Role role;
 }
--- Java/Jinyujin/demo/src/main/java/com/example/demo/lectureClass/fetchType/account/controller/form/JpaAccountWithRoleRequestForm.java
+++ Java/Jinyujin/demo/src/main/java/com/example/demo/lectureClass/fetchType/account/controller/form/JpaAccountWithRoleRequestForm.java
@@ -2,7 +2,11 @@ package com.example.demo.lectureClass.fetchType.account.controller.form;
 
 import com.example.demo.lectureClass.fetchType.account.entity.JpaAccount;
 import com.example.demo.lectureClass.fetchType.account.entity.JpaAccountRole;
+import com.example.demo.lectureClass.fetchType.account.entity.Role;
+import com.example.demo.lectureClass.fetchType.account.entity.RoleType;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Getter
@@ -11,13 +15,14 @@ public class JpaAccountWithRoleRequestForm {
 
     final private String email;
     final private String password;
-    final private String role;
+    final private RoleType roleType;
 
     public JpaAccount toJpaAccount() {
         return new JpaAccount(email, password);
     }
 
-    public JpaAccountRole toJpaAccountRole(JpaAccount account) {
-        return new JpaAccountRole(role, account);
-    }
+//    public JpaAccountRole toJpaAccountRole(JpaAccount account) {
+//        return new JpaAccountRole(role, account);
+//    }
+
 }
