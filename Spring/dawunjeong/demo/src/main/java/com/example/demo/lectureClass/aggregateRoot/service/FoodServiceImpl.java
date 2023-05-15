486808c393a033328aaf677bf2a74a51f174acb0
jeongdawun <jeongdawun33@gmail.com>
2023-05-15 12:35:16 +0900

    feat: [정다운] 식재료 Aggregate Root 구성하기 완료 [AGGREGATEx]

---
+++ Spring/dawunjeong/demo/src/main/java/com/example/demo/lectureClass/aggregateRoot/service/FoodServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.demo.lectureClass.aggregateRoot.service;
+
+import com.example.demo.lectureClass.aggregateRoot.entity.*;
+import com.example.demo.lectureClass.aggregateRoot.repository.*;
+import com.example.demo.lectureClass.aggregateRoot.service.request.FoodRegisterRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class FoodServiceImpl implements FoodService{
+
+    final FoodRepository foodRepository;
+    final FoodCategoryRepository foodCategoryRepository;
+    final FoodAmountRepository foodAmountRepository;
+    final CategoryRepository categoryRepository;
+    final AmountRepository amountRepository;
+
+    @Override
+    public void register(FoodRegisterRequest request) {
+        // 1. 식재료 Entity 및 Image 처리
+        final Food food = request.toFood();
+        foodRepository.save(food);
+        // 2. 수량 처리
+        final Amount amount = amountRepository.findByAmountType(
+                request.getAmountType()).get();
+        final FoodAmount foodAmount =
+                new FoodAmount(
+                        food, amount,
+                        request.getFoodPrice(),
+                        request.getFoodCalorie(),
+                        request.getUnit(),
+                        request.getMax(),
+                        request.getMin());
+
+        foodAmountRepository.save(foodAmount);
+        // 3. 카테고리 처리
+        final Category category = categoryRepository.findByCategoryType(
+                request.getCategoryType());
+        final FoodCategory foodCategory =
+                new FoodCategory(food, category);
+
+        foodCategoryRepository.save(foodCategory);
+    }
+}
