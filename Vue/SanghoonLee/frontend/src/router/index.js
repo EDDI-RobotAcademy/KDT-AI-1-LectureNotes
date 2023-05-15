843c1fb6ca768ada1f18400f84e53adb6aacd0e9
Sanghoon Lee <gcccompil3r@gmail.com>
2023-05-15 10:13:37 +0900

    feat: [이상훈] 음식으로 애그리게이트 루트 구성하기 [AGGREGATEx]

--- Vue/SanghoonLee/frontend/src/router/index.js
+++ Vue/SanghoonLee/frontend/src/router/index.js
@@ -13,6 +13,7 @@ import htmlCssRoutes from './htmlCss'
 import defaultSetRoutes from './defaultRouterSet'
 import d3PlotTestRoutes from './d3plot'
 import filesTestRoutes from './files'
+import foodSetRoutes from './food'
 
 Vue.use(VueRouter)
 
@@ -29,6 +30,7 @@ const routes = [
   ...vuetifyTestRoutes,
   ...d3PlotTestRoutes,
   ...filesTestRoutes,
+  ...foodSetRoutes,
 ]
 
 const router = new VueRouter({
