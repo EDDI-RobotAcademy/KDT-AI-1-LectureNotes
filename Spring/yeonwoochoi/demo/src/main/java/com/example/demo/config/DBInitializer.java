b378510e1d5e0bec9558c7e568a2fcf98655501a
dusdn943 <dusdn943@gmail.com>
2023-05-16 11:28:35 +0900

    feat: [최연우] Test 문제은행 3 풀이 완료 [TEST-EXERCISE-3]

--- Spring/yeonwoochoi/demo/src/main/java/com/example/demo/config/DBInitializer.java
+++ Spring/yeonwoochoi/demo/src/main/java/com/example/demo/config/DBInitializer.java
@@ -6,6 +6,9 @@ import com.example.demo.lectureClass.aggregateRoot.food.entity.Category;
 import com.example.demo.lectureClass.aggregateRoot.food.entity.CategoryType;
 import com.example.demo.lectureClass.aggregateRoot.food.repository.AmountRepository;
 import com.example.demo.lectureClass.aggregateRoot.food.repository.CategoryRepository;
+import com.example.demo.lectureClass.refactorAccount.entity.Role;
+import com.example.demo.lectureClass.refactorAccount.entity.RoleType;
+import com.example.demo.lectureClass.refactorAccount.repository.RoleRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,15 +24,35 @@ public class DBInitializer {
 
     private final CategoryRepository categoryRepository;
     private final AmountRepository amountRepository;
+    private final RoleRepository roleRepository;
 
     @PostConstruct
     private void init () {
         log.debug("initializer 시작!");
-
         initCategoryTypes();
         initAmountTypes();
 
+       initAccountRoleTypes();
+
         log.debug("initializer 종료!");
+    }
+
+    private void initAccountRoleTypes() {
+        try {
+            final Set<RoleType> roles =
+                    roleRepository.findAll().stream()
+                            .map(Role::getRoleType)
+                            .collect(Collectors.toSet());
+
+            for (RoleType type: RoleType.values()) {
+                if (!roles.contains(type)) {
+                    final Role role = new Role(type);
+                    roleRepository.save(role);
+                }
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
     }
 
     private void initAmountTypes () {
@@ -38,7 +61,6 @@ public class DBInitializer {
                     amountRepository.findAll().stream()
                             .map(Amount::getAmountType)
                             .collect(Collectors.toSet());
-
             for (AmountType type: AmountType.values()) {
                 if (!amounts.contains(type)) {
                     final Amount amount = new Amount(type);
@@ -49,14 +71,12 @@ public class DBInitializer {
             log.error(e.getMessage(), e);
         }
     }
-
     private void initCategoryTypes () {
         try {
             final Set<CategoryType> categories =
                     categoryRepository.findAll().stream()
                             .map(Category::getCategoryType)
                             .collect(Collectors.toSet());
-
             for (CategoryType type: CategoryType.values()) {
                 if (!categories.contains(type)) {
                     final Category category = new Category(type);
