805e8c69d10452f2d589bd0b03f02a4eed824e04
hwansik <hwansik08@gmail.com>
2023-05-17 17:50:35 +0900

    feat: [민환식] Vue+Spring+JPA 2차 종합 평가 문제 [EXAMx]

---
+++ exam/second/HwansikMin/frontend/src/components/problem3/problem3.vue
@@ -0,0 +1,23 @@
+<!--vue에서 select option을 사용해서 A, B 두 개의 옵션을 넣고 
+    옵션을 선택하면 옵션 정보를 출력하도록 만들어봅시다.-->
+<template lang="">
+    <div v-if="selectOption">
+        <select v-model="selectOption">
+            <option vlaue="A">A</option>
+            <option vlaue="B">B</option>
+        </select>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            selectOption: null
+        }
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
