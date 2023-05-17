172c3ad7c19e438f7ed85bd155ad7db479093b53
cluypkoj <cluypkoj68@gmail.com>
2023-05-17 17:50:59 +0900

    2차 종합 평가

---
+++ exam/second/JiwooBaek/frontend/src/components/homework/problem2/DiceGameTwoTimes.vue
@@ -0,0 +1,33 @@
+<template lang="">
+    <div>
+        <h3>주사위 굴리기 게임</h3>
+        <v-btn color="primary" @click="startTwoTimesDiceGame">주사위 게임 시작!</v-btn>
+        <p>얻은 결과: {{ playingNumber }}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data () {
+        return {
+            result: '알 수 없음',
+            playingNumber : 0,
+        }
+    },
+    methods: {
+        startTwoTimesDiceGame () {
+            axios.get('http://localhost:7777/dice-game/firstGame')
+                .then((res) => {
+                    this.result = res.data.result
+                    this.playingNumber = res.data.winnerNumber
+                })
+        }
+    }
+}
+
+</script>
+
+<style lang="">
+
+</style>
