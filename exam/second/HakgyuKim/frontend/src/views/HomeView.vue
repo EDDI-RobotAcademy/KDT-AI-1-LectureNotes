21a0bc04edf9d5e115f756e1221f88fa22d3f759
Hakgyu Kim <zage4448@gmail.com>
2023-05-17 09:28:31 +0900

    init: [김학규] 2차 평가 저장소 생성

---
+++ exam/second/HakgyuKim/frontend/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template lang="">
+  <div>
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style lang=""></style>
