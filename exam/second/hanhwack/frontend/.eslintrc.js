11b2b34f4638f7f7f2b384252d6b702a40c03dc4
hanhwack <hanhwack93@gmail.com>
2023-05-17 17:49:12 +0900

    feat: [한확] Vue + Spring + JPA 2차 종합 평가 문제 [EXAM-2]

--- exam/second/hanhwack/frontend/.eslintrc.js
+++ exam/second/hanhwack/frontend/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:vue/essential", "standard"],
+  extends: ["plugin:vue/essential"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
--- exam/second/hanhwack/frontend/src/views/HomeView.vue
+++ exam/second/hanhwack/frontend/src/views/HomeView.vue
@@ -1,15 +1,31 @@
 <template>
-  <hello-world />
+  <div id="app">
+    <nav>
+      <router-link :to="{ name: 'Test1Page' }"> 1번문제 </router-link>|
+      <router-link :to="{ name: 'Test2Page' }"> 2번문제 </router-link>|
+      <router-link :to="{ name: 'Test3Page' }"> 3번문제 </router-link>|
+      <router-link :to="{ name: 'Test4Page' }"> 4번문제 </router-link>|
+      <router-link :to="{ name: 'Test7Page' }"> 7번문제 </router-link>|
+      <router-link :to="{ name: 'Test8Page' }"> 8번문제 </router-link>|
+
+
+    </nav>
+    <router-view/>
+  </div>
+
 </template>
-
-<script>
-import HelloWorld from "../components/HelloWorld";
-
-export default {
-  name: "Home",
-
-  components: {
-    HelloWorld,
-  },
-};
-</script>
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+nav {
+  padding: 30px;
+}
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
