ea67f686d55c0ee4627e2d2c6be7616038ed8975
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-05-17 14:12:27 +0900

    feat: [이상원] 1번 문제 완료

--- exam/second/SangWonLee/frontend/src/router/index.js
+++ exam/second/SangWonLee/frontend/src/router/index.js
@@ -1,29 +1,55 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
-Vue.use(VueRouter)
+import CheckboxPage from "@/views/problem1/CheckboxPage";
+import SpringGetHi from "@/views/problem2/SpringGetHi";
+import OptionPrint from "@/views/problem3/OptionPrint";
+import DicePrintPage from "@/views/problem4-6/DicePrintPage";
+
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/about",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/checkbox-page",
+    name: "CheckboxPage",
+    component: CheckboxPage,
+  },
+  {
+    path: "/spring-get-hi",
+    name: "SpringGetHi",
+    component: SpringGetHi,
+  },
+  {
+    path: "/option-print",
+    name: "OptionPrint",
+    component: OptionPrint,
+  },
+  {
+    path: "/dice-print",
+    name: "DicePrintPage",
+    component: DicePrintPage,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
