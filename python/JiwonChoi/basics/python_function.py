40a060961a22e36874073b388c081e0c37a4fd8c
JiwonChoi <128335544+cjw0824@users.noreply.github.com>
2023-05-23 15:44:35 +0900

    feat: [최지원] python 기본 [PYTHON-BASIC]

---
+++ python/JiwonChoi/basics/python_function.py
@@ -0,0 +1,30 @@
+def bigger_than_20(num):
+    return  num > 20
+
+def add_10(num):
+    return num + 10
+
+def times(num1, num2):
+    return num1 * num2
+
+def function_test():
+    numList = [res for res in range(10, 31, 5)]
+    filteredNumList = filter(bigger_than_20, numList)
+
+    for num in filteredNumList:
+        print("element: {0}".format(num))
+
+    print(filteredNumList)
+    print(type(filteredNumList))
+
+    print(list(filter(bigger_than_20, numList))) #같은
+    print(list(filter(lambda i: i> 20, numList))) #내용
+
+    print(numList)
+    for i in map(add_10, numList):
+        print("num: {0}".format(i))
+
+
+    print(list(map((lambda i: i+10), numList)))
+    print(times)
+    print(times(10,10))
