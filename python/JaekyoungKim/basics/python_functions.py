bf031a069d32d11f25f386f981ee3e65b31ac980
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-05-23 15:44:24 +0900

    feat: [김재경] python  기본 문법 [PYTHONx]

---
+++ python/JaekyoungKim/basics/python_functions.py
@@ -0,0 +1,28 @@
+def bigger_than_20(num):
+    return num>20
+
+def add_10(num):
+    return num+10
+def times(num1,num2):
+    return num1*num2
+def functions_test():
+    numList=[res for res in range(10,31,5)]
+    filteredNumList= filter(bigger_than_20,numList)
+
+    for num in filteredNumList:
+        print("element: {0}".format(num))
+
+    print(filteredNumList)
+    print(type(filteredNumList))
+
+    print(list(filter(bigger_than_20,numList)))
+    print(list(filter(lambda i:i>20,numList)))
+
+    for i in map(add_10,numList):
+        print("num: {0}".format(i))
+
+    print(list(map((lambda i:i+10),numList)))
+    print(times)
+    # 람다는 잠깐쓰고 버린다
+    print(times(10,10))
+    
---
+++ python/JaekyoungKim/main.py
@@ -0,0 +1,27 @@
+# This is a sample Python script.
+from basics.python_functions import functions_test
+from basics.basic_grammar import python_basics
+
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+    print("hello python!")
+
+    python_basics()
+
+    functions_test()
+
+
+
