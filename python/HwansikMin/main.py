023f876d060ea0cc43af06c4ee94d38453065db1
hwansik <hwansik08@gmail.com>
2023-05-23 15:50:59 +0900

    feat: [민환식] python 기본 [PYTHON-BASIC]

---
+++ python/HwansikMin/main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from basics.basic_grammar import python_basics
+from basics.python_functions import functions_test
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    python_basics()
+    functions_test()
