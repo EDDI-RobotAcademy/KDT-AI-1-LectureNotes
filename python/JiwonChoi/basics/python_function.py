5bc41582564607d65471c47b4586e83eaa9ca22e
JiwonChoi <128335544+cjw0824@users.noreply.github.com>
2023-05-23 16:36:17 +0900

    feat: [최지원] python 기초 [PYTHON-BASICx]

--- python/JiwonChoi/basics/python_function.py
+++ python/JiwonChoi/basics/python_function.py
@@ -7,6 +7,30 @@ def add_10(num):
 def times(num1, num2):
     return num1 * num2
 
+def intersect(preList, postList):
+    retList = []
+    for x in preList:
+        if x in postList:
+            retList.append(x)
+    return retList
+
+def change(x):
+    x = x[:] #원본을 복사했기 때문에 원본을 변경하지 않음
+    x[0] = 'H'
+    print("in function =", x)
+
+def change2(x):
+    x[0] = 'H'
+
+#전역 변수
+glob = 1
+def xChgScope(num):
+    global glob #global이 붙었기 때문에 아예 7로 바뀜
+    glob = 7
+    return glob + num
+
+import random
+import math
 def function_test():
     numList = [res for res in range(10, 31, 5)]
     filteredNumList = filter(bigger_than_20, numList)
@@ -28,3 +52,34 @@ def function_test():
     print(list(map((lambda i: i+10), numList)))
     print(times)
     print(times(10,10))
+
+    function_address = times
+    print(function_address(10, 10))
+
+    list1 = "apple"
+    list2 = "grape"
+    list3 = "banana"
+
+    print(intersect(list1, list2))
+    print(intersect(list1, list3))
+
+    wordList = ['J', 'A', 'M']
+    print(wordList)
+
+    change(wordList) #원본을 변경하지 않음
+    print(wordList)
+
+    change2(wordList)  # 원본을 변경함
+    print(wordList)
+
+    print(glob)
+    print(xChgScope(3))
+    print(glob)
+
+    for _ in range(5):
+        print(random.randrange(3, 7)) # 3, 4, 5, 6까지
+
+    print(math.pow(3, 4))
+    print(3 ** 4)
+
+    print(math.sqrt(9))
