d0f2c50b730d7e2a3f9591a39b4d7fcffdb3dde5
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-05-23 16:36:53 +0900

    feat: [윤주아] 파이썬 기초 [PYTHON-BASICx]

---
+++ python/JuaYun/main.py
@@ -0,0 +1,13 @@
+from basic.basic_grammar import python_basics
+from basic.python_functions import functions_test
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    python_basics()
+    functions_test()
