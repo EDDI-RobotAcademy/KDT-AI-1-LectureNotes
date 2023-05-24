afb747d9b837b7352f0fd796db6e3bdfb2b8be55
hanhwack <hanhwack93@gmail.com>
2023-05-24 16:31:32 +0900

    feat: [한확] 클래스 기본 [PY-CLASSx]

--- python/hanhwack/main.py
+++ python/hanhwack/main.py
@@ -1,6 +1,8 @@
 from basics.basic_grammar import python_basics
 from basics.python_functions import functions_test
 from problem.gas_station_prob import gas_station_problem_solve
+from basics.python_class import class_test_function
+
 
 
 
@@ -14,3 +16,4 @@ if __name__ == '__main__':
     python_basics()
     functions_test()
     gas_station_problem_solve()
+    class_test_function()
