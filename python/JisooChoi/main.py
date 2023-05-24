fea89a9e23c8830c3c5717d84c2f3cee46d123ff
zisoo <zisoo_3@naver.com>
2023-05-25 08:33:59 +0900

    feat: [최지수] 날자 정보 계산하기 [PY-DATEx]

--- python/JisooChoi/main.py
+++ python/JisooChoi/main.py
@@ -1,7 +1,8 @@
 from basics.basic_grammar import python_basics
+from basics.python_class import class_test_function
 from basics.python_functions import functions_test
 from basics.thread_test import thread_test_sequence
-from problem.gas_station_prob import gas_prob
+from problem.gas_station_prob import gas_prob, gas_station_problem_solve
 
 
 def print_hi(name):
@@ -11,7 +12,8 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # python_basics()
-    # functions_test()
-    # thread_test_sequence()
-    gas_prob()
+    python_basics()
+    functions_test()
+    class_test_function()
+    gas_station_problem_solve()
+    thread_test_sequence()
