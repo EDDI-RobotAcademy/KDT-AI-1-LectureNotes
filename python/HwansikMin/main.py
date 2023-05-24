3ed966a0f2a4f66ab72cec5819aa8a7ec1ce8111
hwansik <hwansik08@gmail.com>
2023-05-24 11:25:25 +0900

    feat: [민환식] thread 기본 예제 [PY-THREADx]

--- python/HwansikMin/main.py
+++ python/HwansikMin/main.py
@@ -6,6 +6,7 @@
 from basics.basic_grammar import python_basics
 from basics.python_class import class_test_function
 from basics.python_functions import functions_test
+from basics.thread_test import thread_test_sequence
 from problem.gas_station_prob import gas_station_problem_solve
 
 def print_hi(name):
@@ -18,4 +19,6 @@ if __name__ == '__main__':
     python_basics()
     functions_test()
     gas_station_problem_solve()
+    
     class_test_function()
+    thread_test_sequence()
