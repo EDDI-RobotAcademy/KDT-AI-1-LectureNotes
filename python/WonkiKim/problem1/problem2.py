1a82114f490fc122e07d6405eb957421d564e9ec
wonki kim <iknowca@gmail.com>
2023-05-24 21:51:14 +0900

    fix: [김원기]  전역변수 오타 수정 [PYTHONx]

--- python/WonkiKim/problem1/problem2.py
+++ python/WonkiKim/problem1/problem2.py
@@ -11,18 +11,18 @@ class Adder(threading.Thread):
 
     def __init__(self, process_time):
         super().__init__()
-        self.tik = time.time()
+        Adder.tik = time.time()
         self.process_time = process_time
 
     def run(self):
 
         while True:
-            self.lock.acquire()
+            Adder.lock.acquire()
             Adder.number += self.process_time
-            self.lock.release()
             tok = time.time()
             print("{0}sec".format(tok - self.tik))
             print("adder{0}: {1}->{2}".format(self.process_time, Adder.number - self.process_time, Adder.number))
+            Adder.lock.release()
             time.sleep(self.process_time)
 
             tok = time.time()
