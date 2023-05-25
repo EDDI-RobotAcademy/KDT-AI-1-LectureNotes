a5593cf500cb4fdfeb9f981b4b2286bce6be7abb
Sanghoon Lee <gcccompil3r@gmail.com>
2023-05-25 14:19:16 +0900

    feat: [이상훈] Vue <-> Spring <-> FastAPI [VSFx]

--- Vue/SanghoonLee/frontend/src/router/index.js
+++ Vue/SanghoonLee/frontend/src/router/index.js
@@ -14,6 +14,7 @@ import defaultSetRoutes from './defaultRouterSet'
 import d3PlotTestRoutes from './d3plot'
 import filesTestRoutes from './files'
 import foodSetRoutes from './food'
+import fastApiTestRoutes from './fastapi'
 
 Vue.use(VueRouter)
 
@@ -31,6 +32,7 @@ const routes = [
   ...d3PlotTestRoutes,
   ...filesTestRoutes,
   ...foodSetRoutes,
+  ...fastApiTestRoutes,
 ]
 
 const router = new VueRouter({
