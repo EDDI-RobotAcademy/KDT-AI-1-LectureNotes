4580dcb622fd06709b64ed001dad506e3a6e92d6
hanhwack <hanhwack93@gmail.com>
2023-05-25 10:18:43 +0900

    feat: [한확] 병렬처리하기 + Lock 없이 병렬처리하기 [PY-PROB2]

--- python/hanhwack/main.py
+++ python/hanhwack/main.py
@@ -3,6 +3,10 @@ from basics.python_functions import functions_test
 from problem.gas_station_prob import gas_station_problem_solve
 from basics.python_class import class_test_function
 from basics.thread_test import thread_test_sequence
+from problem.advanced_multi_process import adv_parallel_process_problem
+from problem.parallel_process import parallel_process_problem
+
+
 
 
 
@@ -18,4 +22,8 @@ if __name__ == '__main__':
     functions_test()
     gas_station_problem_solve()
     class_test_function()
-    thread_test_sequence()
+    # thread_test_sequence()
+    # parallel_process_problem()
+    adv_parallel_process_problem()
+
+    print(list(range(6, 1, -6)))
