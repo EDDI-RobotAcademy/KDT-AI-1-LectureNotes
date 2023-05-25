061a756e9641e86a0ebeb38364c1894bbccca41c
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-05-25 09:51:43 +0900

    feat: [김재경] python 0525 thread , lock() [PYTHON]

--- python/JaekyoungKim/main.py
+++ python/JaekyoungKim/main.py
@@ -1,9 +1,11 @@
 # This is a sample Python script.
 from Homework.Homework1 import Homework1
+from Homework.advanced_multi_process import adv_parallel_process_problem
+from Homework.parallel_process import parallel_process_problem
 from basics.python_class import class_test_function
 from basics.python_functions import functions_test
 from basics.basic_grammar import python_basics
-
+from basics.thread_test import thread_test_sequence
 
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -21,12 +23,14 @@ if __name__ == '__main__':
 
     print("hello python!")
 
-    python_basics()
+    #python_basics()
 
-    functions_test()
+    #functions_test()
 
-    Homework1()
-    class_test_function()
-
+    #Homework1()
+    #class_test_function()
+    #thread_test_sequence()
+    adv_parallel_process_problem()
+    #parallel_process_problem()
 
 
