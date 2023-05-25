bd8be6c07a5f1abc736c7345d6efb9507b6346ef
dusdn943 <dusdn943@gmail.com>
2023-05-25 14:58:00 +0900

    feat: [최연우] Vue <-> Spring <-> FastAPI [VSFx]

--- Vue/yeonwoochoi/frontend/src/store/index.js
+++ Vue/yeonwoochoi/frontend/src/store/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
 import boardModule from '@/store/board/BoardModule.js'
 import productModule from '@/store/Product/ProductModule.js'
 import uiVuetifyModule from '@/store/uiVuetify/UiVuetifyModule.js'
 import d3PlotModule from './d3Plot/D3PlotModule'
 import filesModule from './files/FilesModule'
 import foodModule from './food/FoodModule'
+import fastApiModule from './fastapi/FastApiModule'
 
 Vue.use(Vuex)
 
@@ -17,7 +17,8 @@ const store = new Vuex.Store({
     uiVuetifyModule: uiVuetifyModule,
     d3PlotModule: d3PlotModule,
     filesModule: filesModule,
-    foodModule: foodModule
+    foodModule: foodModule,
+    fastApiModule: fastApiModule
   },
 })
 
