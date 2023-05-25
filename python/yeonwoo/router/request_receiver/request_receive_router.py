4d65286599a7f8936b9c6bc6b77185ec8e1622c7
dusdn943 <dusdn943@gmail.com>
2023-05-25 12:23:25 +0900

    feat: [최연우] Integer data Spring <-> Python [SPRING-2-PYTHON]

--- python/yeonwoo/router/request_receiver/request_receive_router.py
+++ python/yeonwoo/router/request_receiver/request_receive_router.py
@@ -7,3 +7,7 @@ async def receive_spring_data():
     print("Receive_Spring_Data!")
     ready_asset = { 'name': 'Hi', 'major': 'C' }
     return ready_asset
+
+@request_receiver.get("/request-in-data")
+async def request_integer_process():
+    return 3
