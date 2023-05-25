12c9d04a39ad97a7210c6e56801bfd0b5d16d5e0
YeoulCho <cyu5733@gmail.com>
2023-05-25 12:20:15 +0900

    feat: [조여울] Integer data Spring <-> Python [SPRING-2-PYTHON]

--- python/YeoulCho/router/request_receiver/request_receiver_router.py
+++ python/YeoulCho/router/request_receiver/request_receiver_router.py
@@ -7,3 +7,7 @@ async def receive_spring_data():
     print("Receive Spring Data!")
     ready_asset = { 'name':'hi', 'major': 'c'}
     return ready_asset
+
+@request_receiver.get("/request-int-data")
+async def receive_integer_process():
+    return 3
