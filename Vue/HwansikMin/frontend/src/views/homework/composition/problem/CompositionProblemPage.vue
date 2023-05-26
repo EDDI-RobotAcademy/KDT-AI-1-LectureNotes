b04dd3e02018a94153ffc2249b9cc1c5988332aa
hwansik <hwansik08@gmail.com>
2023-05-26 11:08:43 +0900

    update: [이상훈] 추가 팁 [COMPOSITION-PROBx]

--- Vue/HwansikMin/frontend/src/views/homework/composition/problem/CompositionProblemPage.vue
+++ Vue/HwansikMin/frontend/src/views/homework/composition/problem/CompositionProblemPage.vue
@@ -5,6 +5,7 @@
         <p>{{ receivedFastApiInteger }}</p>
         <p>{{ receivedSpringInteger }}</p>
         <p>세 정보의 합: {{ compositionInteger }}</p>
+        <p>추가 팁: {{ printLikeItToo }}</p>
     </div>
 </template>
 
@@ -17,15 +18,23 @@ const springModule = 'springModule'
 export default {
     data () {
         return {
+            fastApiReceivedData: this.receivedFastApiInteger
         }
     },
     computed: {
         ...mapState(fastApiModule, ['receivedFastApiInteger', 'receivedSpringFromFastApiInteger']),
         ...mapState(springModule, ['receivedSpringInteger']),
+        // 과거 방식
         compositionInteger () {
             return this.$store.state.fastApiModule.receivedFastApiInteger +
                 this.$store.state.fastApiModule.receivedSpringFromFastApiInteger +
                 this.$store.state.springModule.receivedSpringInteger
+        },
+        // 추가 팁 (최신 방식)
+        printLikeItToo () {
+            return this.receivedFastApiInteger +
+                this.receivedSpringFromFastApiInteger +
+                this.receivedSpringInteger
         }
     },
     methods: {
