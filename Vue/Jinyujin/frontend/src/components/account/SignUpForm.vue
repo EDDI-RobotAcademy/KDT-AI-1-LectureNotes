78023c4890c969f63e06ffb85d1557d90be50788
wlsdbwls <128335435+wlsdbwls@users.noreply.github.com>
2023-05-28 19:59:49 +0900

    featL [진유진] 로그인 페이지 [SIGN-IN]

--- Vue/Jinyujin/frontend/src/components/account/SignUpForm.vue
+++ Vue/Jinyujin/frontend/src/components/account/SignUpForm.vue
@@ -24,14 +24,14 @@
                                     <v-btn text large outlined style="font-size: 13px"
                                             class="mt-3 ml-5" color="teal lighten-1"
                                             @click="checkDuplicateEmail"
-                                            :disabled="email == ''">
+                                            :disabled="false">
                                         이메일 <br/>중복 확인
                                     </v-btn>
                                 </div>
 
                                 <v-btn type="submit" block x-large rounded
                                         color="orange lighten-1" class="mt-6"
-                                        :disabled="emailPass == false">회원 신청하기</v-btn>
+                                        :disabled="!isFormValid()">회원 신청하기</v-btn>
                             </v-form>
                         </v-card-text>
                     </v-card>
@@ -42,11 +42,13 @@
 </template>
 
 <script>
-
+import axiosInst from '@/utility/axiosInst'
 import { mapActions } from 'vuex'
 
+const accountModule = 'accountModule'
+
 export default {
-    data() {
+    data () {
         return {
             email: "",
             emailPass: false,
@@ -62,29 +64,36 @@ export default {
     },
     methods: {
         ...mapActions('accountModule', ['requestSpringToCheckEmailDuplication']),
-        onSubmit() {
+        onSubmit () {
             if (this.$refs.form.validate()) {
                 const { email } = this
                 this.$emit("submit", { email })
             } else {
                 alert('올바른 정보를 입력하세요!')
             }
+
             if (!this.emailPass) {
                 alert("이메일 중복 확인을 해주세요!")
             }
         },
-        async checkDuplicateEmail() {
+        async checkDuplicateEmail () {
             const emailValid = this.email.match(
                 /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@]+)*)|(.+))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
             )
+            this.emailPass = false
+
             if (emailValid) {
                 const { email } = this
                 console.log('before actions - email: ' + email)
                 this.emailPass = await this.requestSpringToCheckEmailDuplication({ email })
             }
+        },
+        isFormValid () {
+            return this.emailPass && this.email_rule[1](this.email) === true
         }
-    }
+    },
 }
+
 </script>
 
 <style lang="">
--- Vue/Jinyujin/frontend/src/views/account/SignInPage.vue
+++ Vue/Jinyujin/frontend/src/views/account/SignInPage.vue
@@ -11,7 +11,31 @@ export default {
     methods: {
     }
 }
+</script><template lang="">
+    <div>
+        <sign-in-form @submit="onSubmit"></sign-in-form>
+    </div>
+</template>
+
+<script>
+import SignInForm from "@/components/account/SignInForm.vue"
+
+export default {
+    components: {
+        SignInForm,
+    },
+    methods: {
+        onSubmit () {
+
+        }
+    }
+}
+
 </script>
+
+<style lang="">
+    
+</style>
 
 <style lang="">
     
