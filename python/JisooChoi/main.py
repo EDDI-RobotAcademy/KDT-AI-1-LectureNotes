602e48d8625925239641c1e546c550136e4880bf
zisoo <zisoo_3@naver.com>
2023-05-29 23:41:29 +0900

    update: [최지수] 파이썬 주유소 문제 풀이 완료 [PY-STATIONx]

--- python/JisooChoi/main.py
+++ python/JisooChoi/main.py
@@ -3,7 +3,7 @@ from basics.python_class import class_test_function
 from basics.python_functions import functions_test
 from basics.thread_test import thread_test_sequence
 from problem.advanced_multi_process import adv_parallel_process_problem
-from problem.gas_station_prob import gas_prob, gas_station_problem_solve
+from problem.gas_station_prob import gas_station_problem_solve, gas_prob
 
 from fastapi import Depends, FastAPI
 
@@ -16,32 +16,33 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
+if __name__ == '__main__':
     # python_basics()
     # functions_test()
     # class_test_function()
-    # gas_station_problem_solve()
+    gas_station_problem_solve()
+    gas_prob()
     # thread_test_sequence()
     # parallel_process_problem()
     # adv_parallel_process_problem()
 
-app = FastAPI()
-
-origins = ["http://localhost:8080"]
-from fastapi.middleware.cors import CORSMiddleware
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-@app.get("/")
-async def root_index():
-    return { "message": "Hello from FastAPI" }
-
-
-# 파이썬 도메인 분리를 위한 코드
-app.include_router(request_receiver)
+# app = FastAPI()
+#
+# origins = ["http://localhost:8080"]
+# from fastapi.middleware.cors import CORSMiddleware
+#
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+#
+# @app.get("/")
+# async def root_index():
+#     return { "message": "Hello from FastAPI" }
+#
+#
+# # 파이썬 도메인 분리를 위한 코드
+# app.include_router(request_receiver)
