cff5c27bad54f6b916655e8ab632f7c68fcc1325
dusdn943 <dusdn943@gmail.com>
2023-05-31 14:19:38 +0900

    feat: [최연우] ML 구동하는 현재 컴퓨터 사양 살펴보기 [ML-SPECx]

--- python/yeonwoo/main.py
+++ python/yeonwoo/main.py
@@ -2,42 +2,40 @@ from basics.basic_grammar import python_basics
 from basics.python_class import class_test_function
 from basics.python_functions import functions_test
 from basics.thread_test import thread_test_sequence
-from problem.advanced_multi_process import adv_parallel_process_problem
 from problem.gas_station_prob import gas_station_problem_solve
-from problem.parallel_process import parallel_process_problem
-from router.request_receiver.request_receive_router import request_receiver
 
 from fastapi import Depends, FastAPI
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from router.macchine_learning.ml_router import ml_router
+from router.request_receiver.request_receive_router import request_receiver
+
+from fastapi.middleware.cors import CORSMiddleware
+
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+# # Press the green button in the gutter to run the script.
 
 # if __name__ == '__main__':
-    # python_basics()
-    # functions_test()
-    # gas_station_problem_solve()
-    # class_test_function()
-    # thread_test_sequence()
-    # parallel_process_problem()
-    # adv_parallel_process_problem()
+#     python_basics()
+#     functions_test()
+#     gas_station_problem_solve()
+#     class_test_function()
+#     thread_test_sequence()
 
 app = FastAPI()
-
-# 데이터를 외부에서 가져옴 (파이썬, 스프링)
-# 뷰에서 버튼을 누를때 웹으로 전송이 되는데
-# 스프링에서는 콘피그를 만들어서 방어해줌?
-#
-origins = {"http://localhost:8080"}
-from fastapi.middleware.cors import CORSMiddleware
+origins = ["http://localhost:8080"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins
-
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-
 @app.get("/")
 async def root_index():
     return { "message": "Hello from FastAPI" }
 
 app.include_router(request_receiver)
+app.include_router(ml_router)
---
+++ python/yeonwoo/router/macchine_learning/ml_router.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter
+
+from tensorflow.python.client import device_lib
+import tensorflow as tf
+
+ml_router = APIRouter()
+
+
+@ml_router.get("/ml-device-info")
+async def print_device_info():
+    print("device info:", device_lib.list_local_devices())
+    devices = device_lib.list_local_devices()
+    device_info = [{'name': device.name, 'device_type': device.device_type} for device in devices]
+    return device_info
+
+
+@ml_router.get('/get-all-physical-cpu')
+async def print_all_physical_cpu():
+    physical_cpus = tf.config.list_physical_devices('CPU')
+    for cpu in physical_cpus:
+        print(cpu)
+
+    logical_cpus = tf.config.list_logical_devices('CPU')
+    for logical_cpu in logical_cpus:
+        print(logical_cpu)
+
+
+@ml_router.get('/get-device-details')
+async def get_device_details():
+    cpus = tf.config.list_physical_devices('CPU')
+
+    for cpu in cpus:
+        cpu_details = tf.config.experimental.get_device_details(cpu)
+        print(cpu_details)
+
+        #print("Device name:", cpu_details['name'])
+        #print("Device index:", cpu_details['device_index'])
+        #print("Device physical device desc:", cpu_details['physical_device_desc'])
