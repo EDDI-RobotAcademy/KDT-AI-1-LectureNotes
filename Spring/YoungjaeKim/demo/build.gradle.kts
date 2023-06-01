6d03524ad273409f751019f1a40dd458bda8035f
youngjaeKim <hkm2379@gmail.com>
2023-06-01 18:05:43 +0900

    CICD

--- Spring/YoungjaeKim/demo/build.gradle.kts
+++ Spring/YoungjaeKim/demo/build.gradle.kts
@@ -18,6 +18,10 @@ repositories {
 	mavenCentral()
 }
 
+tasks.named("jar") {
+	enabled = false
+}
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
