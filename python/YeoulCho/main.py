4866c11f835b4bbba8455269cf18784eed9685d6
YeoulCho <cyu5733@gmail.com>
2023-06-07 11:37:22 +0900

    feat: [조여울] AWS 배포를 위한 파일 작성

--- python/YeoulCho/main.py
+++ python/YeoulCho/main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+from typing import Optional
+
 from fastapi import FastAPI
 
 from basics.basic_grammer import python_basics
@@ -8,6 +10,7 @@ from basics.thread_test import thread_test_sequence
 from problem.Q2_thread import perform_add_process, perform_add_process_lock
 from problem.Q3_vueSpringFastAPI import prb3
 from problem.gas_station_prob import gas_station_problem_solve
+from router.machine_learning.ml_router import ml_router
 from router.request_receiver.request_receiver_router import request_receiver
 
 
@@ -38,18 +41,22 @@ app = FastAPI()
 
 @app.get("/")
 async def root_index():
-    return {"message": "Hello from FASTAPI"}
+    return {"message": "Hello from FASTAPI AWS"}
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
 
 app.include_router(request_receiver)
-
 app.include_router(prb3)
+app.include_router(ml_router)
 
 origins = ["http://localhost:8080"]
 from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=origins, # *별 찍으면 안됨! 전부다 허용하겠다는 이야기라
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["*"], # *은 전부다
     allow_headers=["*"],
 )
+
