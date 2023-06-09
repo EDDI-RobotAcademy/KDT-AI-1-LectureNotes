f1d53048c868bc535c3cf9b835e7139eec34cda2
zisoo <zisoo_3@naver.com>
2023-06-09 10:18:05 +0900

    update: [최지수] 자동 대출 (멸망) [PY-KERASx]

--- python/JisooChoi/utility/deep_learning/samples/keras_loan_inference_exmaple.py
+++ python/JisooChoi/utility/deep_learning/samples/keras_loan_inference_exmaple.py
@@ -6,7 +6,7 @@ max_loan_amount = 45000000
 step_size = 1000000
 loan_amounts_range = np.arange(min_loan_amount, max_loan_amount + step_size, step_size)
 
-loaded_model = load_model("load_model.h5")
+loaded_model = load_model("loan_model.h5")
 
 미상환금액 = 2000000000000
 미상환건수 = 1000
@@ -18,4 +18,4 @@ prediction = loaded_model.predict(new_data)
 
 predicted_class = np.argmax(prediction)
 loan_amount = loan_amounts_range[predicted_class]
-print("Predicted loan amount: ", loan_amount)
+print("Predicted loan amount:", loan_amount)
