b65db7f0d2377128e6449edd2e386cfb9cffe9df
zisoo <zisoo_3@naver.com>
2023-06-09 11:37:49 +0900

    feat: [최지수] 미상환 금액에 따른 변동 대출 자동화 [KERAS-LOANx]

--- python/JisooChoi/utility/deep_learning/samples/keras_loan_inference_exmaple.py
+++ python/JisooChoi/utility/deep_learning/samples/keras_loan_inference_exmaple.py
@@ -8,14 +8,13 @@ loan_amounts_range = np.arange(min_loan_amount, max_loan_amount + step_size, ste
 
 loaded_model = load_model("loan_model.h5")
 
-미상환금액 = 2000000000000
-미상환건수 = 1000
-연체율 = 0.5
+미상환금액 = 1000000000
+미상환건수 = 1
+연체율 = 0.1
 
 new_data = np.array([[미상환금액, 미상환건수, 연체율]])
 
 prediction = loaded_model.predict(new_data)
-
 predicted_class = np.argmax(prediction)
 loan_amount = loan_amounts_range[predicted_class]
 print("Predicted loan amount:", loan_amount)
