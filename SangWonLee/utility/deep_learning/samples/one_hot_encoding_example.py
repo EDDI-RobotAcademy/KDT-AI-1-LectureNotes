2b356e8d501478c8b38b32e31b6a6830dd9492d8
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-06-12 00:23:06 +0900

    update: [이상원] 주석 추가 및 AI 학습을 하는데 정리한 것들

--- SangWonLee/utility/deep_learning/samples/one_hot_encoding_example.py
+++ SangWonLee/utility/deep_learning/samples/one_hot_encoding_example.py
@@ -1,25 +1,45 @@
 import numpy as np
 
+# 수치 계산을 위한 핵심 라이브러리.
+
 traffic_light = ["빨간색", "노란색", "초록색"]
 
 
 def one_hot_encode(signal):
     encoded_signal = np.zeros(len(traffic_light))
+    # 지정된 크기의 배열을 생성하고 모든 요소를 0으로 초기화 한다. [0, 0, 0]
     encoded_signal[traffic_light.index(signal)] = 1
+    # traffic_light 리스트에서 signal이 위치한 인덱스를 찾고,
+    # 그 인덱스를 이용하여 encoded_signal 배열의 해당 위치에 1을 할당한다.
     return encoded_signal
+
+
+def inverse_data(signal):
+    index = np.argmax(signal)
+    # 가장 큰 값의 index의 번호를 반환한다. ex) [0, 1, 0] 이면 1이 제일 크니까 '1' 을 반환함(index의 1임)
+    original_data = traffic_light[index]
+    # traffic_light 의 index를 가져옴
+
+    return original_data
 
 
 current_traffic_light = '초록색'
 encoded_signal = one_hot_encode(current_traffic_light)
 print('현재 신호등의 상태:', current_traffic_light)
 print('엔코딩 결과:', encoded_signal)
+original_signal = inverse_data(encoded_signal)
+print(original_signal)
 
 current_traffic_light = '빨간색'
 encoded_signal = one_hot_encode(current_traffic_light)
 print('현재 신호등의 상태:', current_traffic_light)
 print('엔코딩 결과:', encoded_signal)
+original_signal = inverse_data(encoded_signal)
+print(original_signal)
 
 current_traffic_light = '노란색'
 encoded_signal = one_hot_encode(current_traffic_light)
 print('현재 신호등의 상태:', current_traffic_light)
 print('엔코딩 결과:', encoded_signal)
+original_signal = inverse_data(encoded_signal)
+print(original_signal)
--- SangWonLee/utility/deep_learning/samples/traffic_light_example.py
+++ SangWonLee/utility/deep_learning/samples/traffic_light_example.py
@@ -7,7 +7,7 @@ signals = ["빨간색", "노란색", "초록색"]  # x
 actions = ["전진", "정지"]  # y
 # 차량의 행위 = a * 신호등 색상
 # y = ax + b
-# y = ax
+# y
 
 # 대출 가능한 금액 = a1 * 미상환금액 + a2 * 미상환건수 + a3 * 연체율
 # 사용자 점수 = a1 * 미상환금액 + a2 * 미상환건수 + a3 * 연체율
@@ -19,24 +19,42 @@ data = []
 labels = []
 
 for _ in range(100):
-    signal = np.random.choice(signals)
+    signal = np.random.choice(signals)  # signals 리스트에서 임의의 값을 선택
     if signal == "노란색":
         action = "정지"
     else:
-        action = "정지" if signal == "빨간색" else "전진"
+        action = "정지" \
+            if signal == "빨간색" else "전진"
 
-    data.append(signal)
-    labels.append(action)
+    data.append(signal)  # data 리스트에 추가
+    labels.append(action)  # labels 리스트에 추가
+
+# --
 
 signal_mapping = {signal: i for i, signal in enumerate(signals)}
+# signals 리스트의 각 요소에 대해 인덱스를 매핑
+print(signal_mapping)  # {'빨간색': 0, '노란색': 1, '초록색': 2}
+print(data)
+# ['노란색', '빨간색', '빨간색', '노란색', '노란색', '초록색', '빨간색', '노란색', '노란색', '노란색', '초록색', ...
+# '초록색', '노란색', '초록색']
+
 mapped_data = [signal_mapping[signal] for signal in data]
+# data 리스트의 각 요소들을 signal_mapping 딕셔너리를 이용해서 매핑된 값을 변환.
+# (위에 있는 signal 이랑 여기있는 signal 이랑 다르다.)
+
 print(mapped_data)
 
 vehicle_action_mapping = {action: i for i, action in enumerate(actions)}
+# actios 리스트의 각 요소에 대해 익덱스를 매핑
+print(vehicle_action_mapping)  # {'전진': 0, '정지': 1}
+print(labels)
+
 mapped_labels = [vehicle_action_mapping[action] for action in labels]
+# labes의 각 요소들에 대해서 vehicle_action_mapping 딕셔너리를 이용해 매핑된 값을 변환
 print(mapped_labels)
 
 encoded_traffic_light = keras.utils.to_categorical(mapped_data, num_classes=len(signals))
+# len(signals)만큼의
 encoded_action = keras.utils.to_categorical(mapped_labels, num_classes=len(actions))
 print(encoded_traffic_light)
 print(encoded_action)
