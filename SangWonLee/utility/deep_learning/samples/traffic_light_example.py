6cd1cec6dd318f807412f596762dd3de3d8f86b0
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-06-13 01:30:43 +0900

    update: [이상원] 신호등 문제 주석 추가

--- SangWonLee/utility/deep_learning/samples/traffic_light_example.py
+++ SangWonLee/utility/deep_learning/samples/traffic_light_example.py
@@ -54,26 +54,63 @@ mapped_labels = [vehicle_action_mapping[action] for action in labels]
 print(mapped_labels)
 
 encoded_traffic_light = keras.utils.to_categorical(mapped_data, num_classes=len(signals))
-# len(signals)만큼의
+# mapped_data의 각 요소를 len(signals)만큼의 이진 벡터로 변환한다. 0 = [1,0,0], 1 = [0,1,0], 2 = [0,0,1]
 encoded_action = keras.utils.to_categorical(mapped_labels, num_classes=len(actions))
+# mapped_lables의 각 요소를 len(actions)만큼의 이진 벡터로 변환한다.
 print(encoded_traffic_light)
 print(encoded_action)
 
 model = Sequential()
+# Sequential 클래스의 인스턴스 생성
 model.add(Dense(32, input_dim=len(signals), activation="relu"))
+# Dense는 완전 연결 레이어로, 각 입력 노드가 모든 출력 노드와 연결되어 있다.
+# 유닛 수는 32, 입력 데이터는 signals의 길이 만큼의 값
+# relu는 입력값이 0보다 작으면 0 출력하고, 0보다 크면 그대로 출력
+model.summary()
+# Param 은 레이어가 가지는 파라미터의 수를 말함 (입력 차원 + 1) x 유닛수 임
+# ex) 위에는 (len(signals)+ 1) x 32 = 128
+
 model.add(Dense(len(actions), activation="softmax"))
+# 이전 레이어의 Output Shape가 32 이므로, 입력 차원은 32임.
+# len(actions) 만큼의 유닛을 가지고 있음
+# softmax는 입력값을 확률 분포로 변환하는 함수임.
+# 입력 값의 지수 값을 계산한 후, 전체 합을 나누어 정규화한다.
+model.summary()
 
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+# .compile 메소드는 손실 함수, 최적화 알고리즘 ,평가 지표 등을 설정한다.
+# loss는 손실 함수를 지정한다. categorical_crossentropy 예측값과 실제값 사이의 교차 엔트로피를 계산한다.
+# optimizer는 모델의 최적화 알고리즘을 지정한다.
+# adam은 경사 하강법(stochastic gradient descent) 알고리즘의 변형인데, 학습률을 동적으로 조절하여 학습 속도를 개선한 알고리즘..
+# metrics는 모델의 성능을 평가하는 지표를 지정함.
+# accuracy는 정확도를 평가함
+model.summary()
 
 model.fit(encoded_traffic_light, encoded_action, epochs=5000, batch_size=32)
+# .fit은 학습하는 메소드
+# encoded_traffic_light은 입력 데이터, encoded_action은 목표값
+# epochs는 5000번 사용하여 학습한다.
+# batch_size는 배치 크기를 지정, 한번에 처리할 데이터 수를 나타냄
+# 배치 크기가 32로 지정되어 있으므로, 한번에 32개의 데이터 처리하여 학습.
+model.summary()
+
+# 테스트 시작
 
 test_traffic_light = "초록색"
 mapped_test_signal = signal_mapping[test_traffic_light]
+# 초록색을 인덱스로 매핑함 ex) 초록색 = 2
 encoded_test_signal = keras.utils.to_categorical([mapped_test_signal], num_classes=len(signals))
+# mapped_test_signal를 len(signals)만큼의 이진 벡터로 변환한다. 2 = [0,0,1]
 
 predictions = model.predict(encoded_test_signal)
+# model을 사용해서 encoded_test_signal로 모델의 예측값 계산
+print(predictions)
 predicted_action_index = np.argmax(predictions)
+# 예측값의 인덱스를 찾는다. 초록색이니까 전진이 나왔겠고, 전진은 0
+print(predicted_action_index)
+
 predicted_action = actions[predicted_action_index]
+# actions의 0은 전진
 
 print("테스트 신호:", test_traffic_light)
 print("예측된 차량 상태:", predicted_action)
