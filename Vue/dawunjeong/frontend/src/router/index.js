13eea2eb59b6b55386e09cc2ec38e0e10aad18d7
jeongdawun <jeongdawun33@gmail.com>
2023-06-13 14:15:41 +0900

    feat: [정다운] 결제하면 메일 전송하기 [PAYMENTx]

--- Vue/dawunjeong/frontend/src/router/index.js
+++ Vue/dawunjeong/frontend/src/router/index.js
@@ -21,6 +21,7 @@ import fastApiHomeworkRoutes from './fastapiHomework'
 import accountTestRoutes from './account'
 import authenticationTestRoutes from './authentication'
 import afterLoginTestRoutes from './afterLogin'
+import paymentRoutes from './payment'
 
 
 Vue.use(VueRouter)
@@ -45,6 +46,7 @@ const routes = [
   ...accountTestRoutes,
   ...authenticationTestRoutes,
   ...afterLoginTestRoutes,
+  ...paymentRoutes,
 ]
 
 const router = new VueRouter({
