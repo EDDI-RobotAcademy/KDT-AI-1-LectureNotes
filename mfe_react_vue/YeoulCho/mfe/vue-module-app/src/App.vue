3cfcffa8604fd079b835c0146040543b2ba5c217
YeoulCho <cyu5733@gmail.com>
2023-06-14 16:55:02 +0900

    init: [조여울] MFE vue remotes 초기화

---
+++ mfe_react_vue/YeoulCho/mfe/vue-module-app/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container">
+    <div>Name: vue-module-app</div>
+    <div>Framework: vue3</div>
+    <div>Language: JavaScript</div>
+    <div>CSS: Empty CSS</div>
+  </div>
+</template>
