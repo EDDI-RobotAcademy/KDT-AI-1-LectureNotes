970be3226624964038b22c62a501000fcfe970fa
JiwonChoi <128335544+cjw0824@users.noreply.github.com>
2023-06-15 17:41:52 +0900

    comment: [최지원] python 대출 데이터분석 주석 추가

--- python/JiwonChoi/except_extract_customer_data.py
+++ python/JiwonChoi/except_extract_customer_data.py
@@ -1,13 +1,16 @@
+# 비실명화된 고객 대출 상품 데이터 분석
 import pandas as pd
 
-df = pd.read_excel('customer_data.xlsx')
+df = pd.read_excel('customer_datas/customer_data.xlsx')
 total_rows = df.shape[0]
 
 # ~ 표시를 통해 NOT 연산이 가능함
 #filtered_df = df[~df['GOODS_CD'].isin(['760036', '760054', '760143', '760010'])]
+# DF.isin([values]) DF의 요소가 values와 일치하는지 bool형식으로 반환
+# ~DF.isin([values])values가 포함되지 않으면..
 #filtered_df.to_excel('except_extract_data.xlsx')
 
-unique_goods_values = df['GOODS_CD'].unique()
+unique_goods_values = df['GOODS_CD'].unique() #'GOODS_CD' 열의 고유값들
 
 for value in unique_goods_values:
     print(value)
--- python/JiwonChoi/goods_cd_separator.py
+++ python/JiwonChoi/goods_cd_separator.py
@@ -1,9 +1,15 @@
 import pandas as pd
 
-df = pd.read_excel('customer_data.xlsx')
+df = pd.read_excel('customer_datas/customer_data.xlsx')
 
 #total_rows = df.shape[0]
 #filtered_df = df[df['GOODS_CD'].astype(str) == '760036']
+# 'GOODS_CD'가 '760036'일때
+#DF.astype(dtype, copy=True, errors='raies')
+#열의 요소의 데이터 타입을 변경하는 메서드
+#dtype=>변경할 type,
+# 생략 가능 { copy=>사본을 생성할지 여부, errors => 변경불가시 오류를 발생시킴'raies', 안시킴'ignore'}
+
 #filtered_df.to_excel('IBK사잇돌2.xlsx')
 
 # 시너지 론(760044)의 경우 대출 한도 및 자세한 약정 파악이 불가하므로 폐기처분
@@ -17,4 +23,5 @@ print(range(len(goods_code)))
 for i in range(len(goods_code)):
     filtered_df = df[df['GOODS_CD'].astype(str) == goods_code[i]]
     filtered_df.to_excel(goods_name[i] + '.xlsx')
+    print(goods_name[i], ".xlsx end")
 
