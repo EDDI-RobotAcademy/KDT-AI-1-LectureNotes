50bb66a41ab631d7fe137747080dd58e67dbe247
zisoo <zisoo_3@naver.com>
2023-06-16 11:40:58 +0900

    update: [최지수] add MFE CorsConfig [MFE-CORSx]

--- Spring/JisooChoi/demo/src/main/java/kr/eddi/demo/config/CorsConfig.java
+++ Spring/JisooChoi/demo/src/main/java/kr/eddi/demo/config/CorsConfig.java
@@ -12,7 +12,8 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://127.0.0.1:8080/", // vue는 허용하겠다는 소리
-                        "http://localhost:8080/"
+                        "http://localhost:8080/",
+                        "http://localhost:3001"
 //                        "http://192.168.20.3:8080/"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
