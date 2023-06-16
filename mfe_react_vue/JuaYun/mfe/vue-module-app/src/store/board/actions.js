341cf23f5787932b98dc71128bf669db7f1f06a6
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-06-16 15:59:32 +0900

    feat: [윤주아] 게시물 등록, 읽기, 쓰기 [MFE-BOARDx]

--- mfe_react_vue/JuaYun/mfe/vue-module-app/src/store/board/actions.js
+++ mfe_react_vue/JuaYun/mfe/vue-module-app/src/store/board/actions.js
@@ -3,56 +3,55 @@ import {
     REQUEST_BOARD_TO_SPRING,
 } from './mutation-types'
 
-import axiosInst from '@/utility/axiosInst'
+import axiosInst from '../../utility/axiosInst'
 
 export default {
     requestBoardToSpring ({ commit }, boardId) {
-        console.log(", boardId: " + boardId)
-
-        return axiosInst.get(`/jpa-board/${boardId}`)
+        return axiosInst.springAxiosInst.get(`/jpa-board/${boardId}`)
             .then((res) => {
                 commit(REQUEST_BOARD_TO_SPRING, res.data)
             })
     },
-    requestBoardListToSpring  ({ commit }) {
-        return axiosInst.get('/jpa-board/list')
+    requestBoardListToSpring ({ commit }) {
+        axiosInst.springAxiosInst.get('/jpa-board/list')
             .then((res) => {
                 commit(REQUEST_BOARD_LIST_TO_SPRING, res.data)
             })
     },
     requestCreateBoardToSpring ({}, payload) {
+
         const { title, content, writer } = payload
 
-        return axiosInst.post('/jpa-board/register', { title, content, writer })
-        .then((res) => {
-            alert('게시물 등록 성공!')
-            return res
-        })
-        .catch(() => {
-            alert('문제 발생!')
-        })
+        return axiosInst.springAxiosInst.post('/jpa-board/register', { title, content, writer })
+            .then((res) => {
+                alert('게시물 등록 성공!')
+                return res
+            })
+            .catch(() => {
+                alert('문제 발생!')
+            })
     },
     requestDeleteBoardToSpring ({}, boardId) {
-        return axiosInst.delete(`/jpa-board/${boardId}`)
+        return axiosInst.springAxiosInst.delete(`/jpa-board/${boardId}`)
             .then((res) => {
                 alert('삭제 성공!')
             })
             .catch(() => {
                 alert('문제 발생!')
             })
-     },
-     requestBoardModifyToSpring ({}, payload) {
+    },
+    requestBoardModifyToSpring ({}, payload) {
         const { title, content, boardId, writer } = payload
 
         console.log("title: " + title + ", content: " + content + 
                     ", writer: " + writer + ", boardId: " + boardId)
 
-        return axiosInst.put(`/jpa-board/${boardId}`, { title, content, writer })
+        return axiosInst.springAxiosInst.put(`/jpa-board/${boardId}`, { title, content, writer })
             .then((res) => {
                 alert("수정 성공!")
             })
             .catch(() => {
                 alert('문제 발생!')
             })
-    },
-} 
+    }
+}
