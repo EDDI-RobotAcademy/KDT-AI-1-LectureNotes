ea6097db17d5bcd9a724c38e3b31b36382e38f60
YeoulCho <cyu5733@gmail.com>
2023-06-19 14:46:14 +0900

    init: [조여울] 웹 소켓 설정 초기화

---
+++ Spring/YeoulCho/demo/src/main/java/com/example/demo/config/WebSocketIOConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.config;
+
+import com.corundumstudio.socketio.SocketIOServer;
+import jakarta.persistence.Basic;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebSocketIOConfig {
+
+    private String host = "localhost";
+    private Integer port = 9999;
+
+    @Bean
+    public SocketIOServer scckerIOServer(){
+        com.corundumstudio.socketio.Configuration config =
+                new com.corundumstudio.socketio.Configuration();
+        config.setHostname(host);
+        config.setPort(port);
+
+        return new SocketIOServer(config);
+    }
+}
