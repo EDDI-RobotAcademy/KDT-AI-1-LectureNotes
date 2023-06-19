530a12bbc5d0993bc3e1edbe61e2c66c959b3c23
jeongdawun <jeongdawun33@gmail.com>
2023-06-19 13:37:37 +0900

    chore: [정다운] git ignore 추가 및 오타 수정

--- mfe_react_vue/dawunjeong/mfe/vue-module-app/webpack.config.js
+++ mfe_react_vue/dawunjeong/mfe/vue-module-app/webpack.config.js
@@ -79,7 +79,7 @@ module.exports = (_, argv) => ({
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authroization',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
     }
   },
 });
