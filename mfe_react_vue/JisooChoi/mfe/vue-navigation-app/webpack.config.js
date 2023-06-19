c528f16bbafceb4e5722547c5ce240cb04a5eabf
zisoo <zisoo_3@naver.com>
2023-06-19 09:59:17 +0900

    init: [최지수] MFE Vue Navigation App

---
+++ mfe_react_vue/JisooChoi/mfe/vue-navigation-app/webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const { VueLoaderPlugin } = require("vue-loader");
+
+module.exports = (_, argv) => ({
+  output: {
+    publicPath: "http://localhost:8080/",
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".vue", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 8080,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          "babel-loader",
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+              appendTsSuffixTo: ["\\.vue$"],
+              happyPackMode: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+
+  plugins: [
+    new VueLoaderPlugin(),
+    new ModuleFederationPlugin({
+      name: "vue_navigation_app",
+      filename: "remoteEntry.js",
+      remotes: {},
+      exposes: {},
+      shared: require("./package.json").dependencies,
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+});
