a6ac8e1e3a8f47ee96fad07a13803e809133da22
Sanghoon Lee <gcccompil3r@gmail.com>
2023-06-19 09:45:32 +0900

    init: [이상훈] MFE Vue Navigation App

---
+++ mfe_react_vue/SanghoonLee/mfe/vue-navigation-app/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="mt-10 text-3xl mx-auto max-w-6xl">
+    <div>Name: vue-navigation-app</div>
+    <div>Framework: vue3</div>
+    <div>Language: JavaScript</div>
+    <div>CSS: Tailwind</div>
+  </div>
+</template>
