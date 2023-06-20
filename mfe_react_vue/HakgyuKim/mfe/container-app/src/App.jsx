b1bfb652322c8f07d3378991df8893cff5cc3265
Hakgyu Kim <zage4448@gmail.com>
2023-06-20 10:04:07 +0900

    init: [김학규] MFE React Module App

--- mfe_react_vue/HakgyuKim/mfe/container-app/src/App.jsx
+++ mfe_react_vue/HakgyuKim/mfe/container-app/src/App.jsx
@@ -1,34 +1,25 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { vueMount } from 'vueModuleApp/Sample';
 import { navigationMount } from 'vueNavigationApp/VueNavigation';
 
 const App = () => {
   const vueRef = useRef(null)
-  const [count, setCount ] = useState(0)
   const vuetifyNavigationRef = useRef(null)
 
   useEffect(() => {
-    vueMount(vueRef.current, {
-      count,
-      incrementCount: () => setCount(currentCount => currentCount + 1)
-    });   
+    vueMount(vueRef.current)
     navigationMount(vuetifyNavigationRef.current)
-  }, [count]);
-
-
-
+  }, []);
 
   return (
     <div>
-      <div ref={vuetifyNavigationRef}/>
-      {/* <div style=
-          {{ margin: '10px', padding: '10px', textAlign: 'center', backgroundColor: 'cyan' }}> */}
+      <div style={{ zIndex: 999 }} ref={vuetifyNavigationRef}/>
+      <div style=
+          {{ margin: '10px', padding: '10px', textAlign: 'center',
+              backgroundColor: 'cyan', position: 'relative', zIndex: 1 }}>
         <h1>나는 React로 만들어진 Container!</h1>
         <div ref={vueRef}/>
-        <button onClick={() => setCount(currentCount => currentCount + 1)}>
-          { count }
-        </button>
-      {/* </div> */}
+      </div>
     </div>
   )
 };
