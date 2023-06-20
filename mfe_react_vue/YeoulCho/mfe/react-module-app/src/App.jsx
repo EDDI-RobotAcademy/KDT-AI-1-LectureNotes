777b5badb105b5d988b1ecd43701bc808587e979
YeoulCho <cyu5733@gmail.com>
2023-06-20 10:36:01 +0900

    feat: [조여울] MFE React Counter [MFE-COUNTERx]

--- mfe_react_vue/YeoulCho/mfe/react-module-app/src/App.jsx
+++ mfe_react_vue/YeoulCho/mfe/react-module-app/src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import Counter from "./components/Counter";
 import "./index.css";
 
 const App = () => (
   <div className="container">
-    <p>Hi</p>
-    {/* <Counter/> */}
+    <Counter/>
   </div>
 );
 
