e96cb291b7015de049b117c3e9f5706634e80a75
zisoo <zisoo_3@naver.com>
2023-06-20 09:22:54 +0900

    fix: [최지수] MFE React z-index overlay [MFE-NAVIGATIONx]

--- Spring/JisooChoi/demo/src/main/java/kr/eddi/demo/config/CorsConfig.java
+++ Spring/JisooChoi/demo/src/main/java/kr/eddi/demo/config/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig implements WebMvcConfigurer {
                         "http://127.0.0.1:8080/", // vue는 허용하겠다는 소리
                         "http://localhost:8080/",
                         "http://localhost:3000",
-                        "http://localhost:3001"
+                        "http://localhost:3001",
+                        "http://localhost:3002"
 //                        "http://192.168.20.3:8080/"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
--- mfe_react_vue/JisooChoi/mfe/container-app/webpack.config.js
+++ mfe_react_vue/JisooChoi/mfe/container-app/webpack.config.js
@@ -3,12 +3,18 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const ExternalTemplateRemotePlugin = require('external-remotes-plugin')
 
+const deps = require("./package.json").dependencies
+
 module.exports = (_, argv) => ({
   mode: "development",
   entry: "./src/index",
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"]
+  },
   devServer: {
-    static: path.join(__dirname, 'dist'),
+    //static: path.join(__dirname, 'dist'),
     port: 3000,
+    historyApiFallback: true,
   },
   output: {
     publicPath: 'auto',
@@ -16,13 +22,31 @@ module.exports = (_, argv) => ({
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        options: {
-          presets: ['@babel/preset-react']
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
         },
       },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }
+      },
+      // {
+      //   test: /\.jsx?$/,
+      //   loader: "babel-loader",
+      //   exclude: /node_modules/,
+      //   options: {
+      //     presets: ['@babel/preset-react']
+      //   },
+      // },
     ],
   },
   plugins: [
@@ -33,8 +57,20 @@ module.exports = (_, argv) => ({
     new ModuleFederationPlugin({
       name: "containerApp",
       remotes: {
-        vueModuleApp: 'vueModuleApp@http://localhost:3001/remoteEntry.js'
+        vueModuleApp: 'vueModuleApp@http://localhost:3001/remoteEntry.js',
+        vueNavigationApp: 'vueNavigationApp@http://localhost:3002/remoteEntry.js'
       },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        }
+      }
     }),
   ],
 });
