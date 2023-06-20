45a6b0ed1ad648a5125618959386cdafe030420e
wlsdbwls <wlsdbwls614@gmail.com>
2023-06-20 09:50:47 +0900

    init: [진유진] MFE React Module App

---
+++ mfe_react_vue/Jinyujin/mfe/react-module-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => (
+  <div className="container">
+    <p>Hi</p>
+    {/* <Counter/> */}
+  </div>
+);
+
+export { App }
---
+++ mfe_react_vue/Jinyujin/mfe/react-module-app/webpack.config.js
@@ -0,0 +1,77 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const ExternalTemplateRemotesPlugin = require('external-remotes-plugin')
+
+const deps = require("./package.json").dependencies;
+module.exports = (_, argv) => ({
+  // mode 지정이 필요
+  mode: 'development',
+  entry: './src/index',
+  output: {
+    publicPath: "auto",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 3003,
+    historyApiFallback: true,
+    hot: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authroization',
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "reactModuleApp",
+      filename: "remoteEntry.js",
+      exposes: {
+        './ReactSample': './src/bootstrap.js',
+        // './Counter': './src/components/Counter.js'
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      chunks: ['main'],
+    }),
+    new ExternalTemplateRemotesPlugin(), // 외부에서 배치할꺼라고 하면서 만드심
+  ],
+});
