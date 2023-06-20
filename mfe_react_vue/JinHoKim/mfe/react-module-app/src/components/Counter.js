ac1ac0a398e863fa24a521b5f53171ad20deed92
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-06-20 10:36:32 +0900

    feat: [김진호] MFE React Counter [MFE-COUNTERx]

---
+++ mfe_react_vue/JinHoKim/mfe/react-module-app/src/components/Counter.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+// rafce
+const Counter = () => {
+  // count는 vue의 state
+  // setCount는 Vue의 action
+  //useState를 통해 count를 초기값 0으로 설정
+  const [count, setCount] = useState(0)
+
+  const onIncrement = () => setCount(count + 1)
+  const onDecrement = () => setCount(count - 1)
+  
+
+  return (
+    <div>
+      <h1>나는 React Counter!</h1>
+      <p>현재 count: <strong>{ count }</strong></p>
+      <button onClick={onIncrement}>플러스</button>
+      <button onClick={onDecrement}>마이너스</button>
+    </div>
+  )
+}
+
+export default Counter
