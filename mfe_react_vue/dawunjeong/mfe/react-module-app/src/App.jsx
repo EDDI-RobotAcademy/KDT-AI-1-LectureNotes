962af7b9cc95ddb9d4c2ab48309655827352b556
jeongdawun <jeongdawun33@gmail.com>
2023-06-20 09:54:44 +0900

    init: [정다운] MFE React Module App

---
+++ mfe_react_vue/dawunjeong/mfe/react-module-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Counter } from "./components/Counter";
+
+import "./index.css";
+
+const App = () => (
+  <div className="container">
+    <p>Hi</p>
+  {/* <Counter/> */}
+  </div>
+);
+
+export { App }
