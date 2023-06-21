35a9bcfe1649eb4ec916bff42540829dca6fdcde
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-06-21 11:36:42 +0900

    feat: [김진호] React 기반의 게시물 등록 중간 세이브 [REACT-BOARD-REGx]

--- mfe_react_vue/JinHoKim/mfe/react-board-app/src/App.jsx
+++ mfe_react_vue/JinHoKim/mfe/react-board-app/src/App.jsx
@@ -6,12 +6,14 @@ import "./index.css";
 import { Routes, Route } from "react-router-dom";
 
 import BoardListPage from "./page/BoardListPage";
+import BoardRegisterPage from "./page/BoardRegisterPage"
 
 const App = () => {
   return (
     <>
     <Routes>
-      <Route element={<BoardListPage/>} path="/" exact></Route>
+        <Route element={<BoardListPage />} path="/" exact></Route>
+        <Route element={<BoardRegisterPage />} path="/register" exact></Route>
     </Routes>
     </>
   )
