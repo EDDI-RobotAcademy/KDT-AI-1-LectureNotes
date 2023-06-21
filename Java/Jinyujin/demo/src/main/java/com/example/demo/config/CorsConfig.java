a26d23e16f83925807d9655af346a107b5da15c5
wlsdbwls <wlsdbwls614@gmail.com>
2023-06-21 11:36:44 +0900

    init: [진유진] React Board App 구성하기 위한 초기화 작업

--- Java/Jinyujin/demo/src/main/java/com/example/demo/config/CorsConfig.java
+++ Java/Jinyujin/demo/src/main/java/com/example/demo/config/CorsConfig.java
@@ -13,7 +13,9 @@ public class CorsConfig implements WebMvcConfigurer {
                                 "http://localhost:8080",
 //                              "http://localhost:8081",
                                 "http://localhost:3000",
-                                "http://localhost:3001")
+                                "http://localhost:3001",
+                                "http://localhost:3004",
+                                "http://localhost:3010")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
