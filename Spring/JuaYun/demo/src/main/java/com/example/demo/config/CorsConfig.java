5cb0dfbaf1de4d5e705edbc8c96495244eb50688
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-06-21 12:50:28 +0900

    feat: [윤주아] React기반 게시물 리스트, 등록, 읽기 윤곽잡기 [REACT-READx]

--- Spring/JuaYun/demo/src/main/java/com/example/demo/config/CorsConfig.java
+++ Spring/JuaYun/demo/src/main/java/com/example/demo/config/CorsConfig.java
@@ -15,6 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
                         "http://localhost:8080",
                         "http://localhost:3000",
                         "http://localhost:3001",
+                        "http://localhost:3004",
                         "http://localhost:3010")
                 .allowedOrigins("GET", "POST", "PUT", "DELETE");
 
--- mfe_react_vue/JuaYun/mfe/react-board-app/src/App.jsx
+++ mfe_react_vue/JuaYun/mfe/react-board-app/src/App.jsx
@@ -5,12 +5,16 @@ import "./index.css";
 import { Routes, Route } from "react-router-dom";
 
 import BoardListPage from "./page/BoardListPage"
+import BoardRegisterPage from "./page/BoardRegisterPage"
+import BoardReadPage from "./page/BoardReadPage"
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route element={<BoardListPage/>} path="/" exact></Route>
+        <Route element={<BoardRegisterPage/>} path="/register" exact></Route>
+        <Route element={<BoardReadPage/>} path="/read/:boardId" exact></Route>
       </Routes>
     </>
   )
