59aef1b41de1ab34e8f4c38a5ef6fecff1f960b5
Hakgyu Kim <zage4448@gmail.com>
2023-06-21 09:50:56 +0900

    init: [김학규] React Board App 구성하기 위한 초기화 작업

---
+++ mfe_react_vue/HakgyuKim/mfe/react-board-app/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import { Route, Routes } from "react-router-dom";
+
+import BoardListPage from "./page/BoardListPage";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route element={<BoardListPage/>} path="/" exact></Route>
+      </Routes>
+    </>
+  )
+};
+
+export { App }
