bc684b4b7f5ca9749057422f131028bf243131a7
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-06-21 14:36:02 +0900

    feat: [김진호] React 기반 게시물 수정 윤곽 잡기 [REACT-BOARD-MODIFYx]

--- mfe_react_vue/JinHoKim/mfe/react-board-app/src/App.jsx
+++ mfe_react_vue/JinHoKim/mfe/react-board-app/src/App.jsx
@@ -5,9 +5,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { Routes, Route } from "react-router-dom";
 
-import BoardListPage from "./page/BoardListPage";
+import BoardListPage from "./page/BoardListPage"
 import BoardRegisterPage from "./page/BoardRegisterPage"
-import BoardReadPage from "./page/BoardReadPage";
+import BoardReadPage from "./page/BoardReadPage"
+import BoardModifyPage from "./page/BoardModifyPage"
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Route element={<BoardListPage />} path="/" exact></Route>
         <Route element={<BoardRegisterPage />} path="/register" exact></Route>
         <Route element={<BoardReadPage />} path="/read/:boardId" exact></Route>
+        <Route element={<BoardModifyPage />} path="/modify/:boardId" exact></Route>
     </Routes>
     </>
   )
