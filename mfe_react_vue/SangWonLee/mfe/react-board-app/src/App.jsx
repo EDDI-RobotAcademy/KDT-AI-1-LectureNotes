54616841eb8a2104008e37e457fbf54d13a8a044
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-06-21 15:40:43 +0900

    feat: [이상원] React 기반 게시물 등록,읽기,수정 추가 [REACT-BOARD]

--- mfe_react_vue/SangWonLee/mfe/react-board-app/src/App.jsx
+++ mfe_react_vue/SangWonLee/mfe/react-board-app/src/App.jsx
@@ -5,12 +5,18 @@ import "./index.css";
 import { Routes, Route } from "react-router-dom";
 
 import BoardListPage from "./page/BoardListPage";
+import BoardRegisterPage from "./page/BoardRegisterPage";
+import BoardReadPage from "./page/BoardReadPage";
+import BoardModifyPage from "./page/BoardModifyPage";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route element={<BoardListPage />} path="/" exact></Route>
+        <Route element={<BoardRegisterPage />} path="/register"></Route>
+        <Route element={<BoardReadPage />} path="/read/:boardId"></Route>
+        <Route element={<BoardModifyPage />} path="/modify/:boardId"></Route>
       </Routes>
     </>
   );
