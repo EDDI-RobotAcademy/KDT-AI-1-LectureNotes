882650134205a8bad5538d85fbb42177869bfb2c
bbbii <hyeongjin1326@gmail.com>
2023-06-21 12:06:35 +0900

    feat: [김형진] React 기반 게시물 등록 [REACT-BOARD-REGx]

--- mfe_react_vue/HyeongJinKim/mfe/react-board-app/src/App.jsx
+++ mfe_react_vue/HyeongJinKim/mfe/react-board-app/src/App.jsx
@@ -5,12 +5,14 @@ import "./index.css";
 import { Routes, Route } from "react-router-dom";
 
 import BoardListPage from "./page/BoardListPage";
+import BoardRegisterPage from "./page/BoardRegisterPage";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route element={<BoardListPage />} path="/" exact></Route>
+        <Route element={<BoardRegisterPage />} path="/register" exact></Route>
       </Routes>
     </>
   );
