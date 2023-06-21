22497d1c985add7d6bfb4705b6383ffe5cffcb91
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-06-21 16:45:48 +0900

    update: [김진호] React에서 게시물 수정하기 [REACT-BOARD-READx]

--- mfe_react_vue/JinHoKim/mfe/react-board-app/src/App.jsx
+++ mfe_react_vue/JinHoKim/mfe/react-board-app/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+/* 루트 컴포넌트를 정의하고 렌더링하는 역할을 합니다. */
 
 import "./index.css";
 import { Routes, Route } from "react-router-dom";
@@ -15,7 +16,7 @@ const App = () => {
     <>
     <Routes>
         <Route element={<BoardListPage />} path="/" exact></Route>
-        <Route element={<BoardRegisterPage />} path="/register" exact></Route>
+        <Route element={<BoardRegisterPage />} path="/register"></Route>
         <Route element={<BoardReadPage />} path="/read/:boardId" exact></Route>
         <Route element={<BoardModifyPage />} path="/modify/:boardId" exact></Route>
     </Routes>
