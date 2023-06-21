460c3591ba6b0820d6218114a0472d236b41dd65
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-06-21 11:14:56 +0900

    fix: [김진호] React 게시판 리스트 버그 수정 [REACT-BOARDx]

--- mfe_react_vue/JinHoKim/mfe/react-board-app/src/components/BoardListForm.js
+++ mfe_react_vue/JinHoKim/mfe/react-board-app/src/components/BoardListForm.js
@@ -1,56 +1,54 @@
 import React from 'react'
 import { Link } from 'react-router-dom' // imrr
 
-
-const BoardListPage = ({ boards, isLoding }) => {
+const BoardListForm = ({ boards, isLoading }) => {
   return (
     <div>
       <h2>React로 만든 게시판</h2>
-      { isLoding && "로딩중 ........."}
-      {!isLoding && boards && (
+      { isLoading && "로딩중 ..........." }
+      { !isLoading && boards && (
         <>
-         <Link to="/register">글쓰기</Link>
+          <Link to="/register">글쓰기</Link>
           <table border="1">
             <thead>
               <tr>
-                <th align="center" width="80">
+                <th align='center' width="80">
                   번호
                 </th>
-                <th align="center" width="320">
+                <th align='center' width="320">
                   제목
                 </th>
-                <th align="center" width="100">
+                <th align='center' width="100">
                   작성자
                 </th>
-                <th align="center" width="180">
+                <th align='center' width="180">
                   등록일자
                 </th>
               </tr>
             </thead>
             <tbody>
-              {!boards.length && (
+              { !boards.length ? (
                 <tr>
                   <td colSpan={4}>
                     등록된 게시물이 존재하지 않습니다!
                   </td>
                 </tr>
-              )}
-              {!!boards.length && boards.map((board) => {
+              ) : ( boards.map((board) => (
                 <tr key={board.boardId}>
                   <td align="center">{ board.boardId }</td>
                   <td align="left">
                     { board.title }
                   </td>
-                  <td align='right'>{ board.writer }</td>
-                  <td align='center'>{ board.createDate }</td>
+                  <td align="right">{ board.writer }</td>
+                  <td align="center">{ board.createDate }</td>
                 </tr>
-              })}
+              )))}
             </tbody>
-         </table>
+          </table>
         </>
       )}
     </div>
   )
 }
 
-export default BoardListPage
+export default BoardListForm
