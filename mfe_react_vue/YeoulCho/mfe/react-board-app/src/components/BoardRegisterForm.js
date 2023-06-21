da5dc81e6e55520bdb4e6cefed58ce71778ca0d5
YeoulCho <cyu5733@gmail.com>
2023-06-21 11:59:16 +0900

    update: [조여울] React 기반 게시물 등록 [REACT-BOARD-REGx]

--- mfe_react_vue/YeoulCho/mfe/react-board-app/src/components/BoardRegisterForm.js
+++ mfe_react_vue/YeoulCho/mfe/react-board-app/src/components/BoardRegisterForm.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { Link } from 'react-router-dom'
 
-const BoardRegisterForm = () => {
+const BoardRegisterForm = ({ onRegister }) => {
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+  const [writer, setWriter] = useState("")
+
+  // React는 v-model이 없습니다.
+  // 다 이렇게 만드세요.
+  const handleChangeTitle = useCallback((e) => {
+    setTitle(e.target.value)
+  }, [])
+
+  const handleChangeContent = useCallback((e) => {
+    setContent(e.target.value)
+  }, [])
+
+  const handleChangeWriter = useCallback((e) => {
+    setWriter(e.target.value)
+  }, [])
+
+  const handleSubmit = useCallback(
+    (e) => {
+        e.preventDefault();
+        
+        onRegister(title, content, writer)
+    },
+    [title, content, writer, onRegister]
+  )
+
   return (
-    <div>BoardRegisterForm</div>
+    <div align="center">
+      <h2>게시물 등록</h2>
+      <form onSubmit={handleSubmit}>
+        <table>
+          <tbody>
+            <tr>
+              <td>제목</td>
+              <td>
+                {/* React는 v-model이 없어서 onChange랑 위의 useCallback을 묶어서 사용 */}
+                <input type="text" value={title} onChange={handleChangeTitle}/>
+              </td>
+            </tr>
+            <tr>
+              <td>작성자</td>
+              <td>
+                <input type="text" value={writer} onChange={handleChangeWriter}/>
+              </td>
+            </tr>
+            <tr>
+              <td>내용</td>
+              <td>
+                <textarea rows="5" value={content} onChange={handleChangeContent}/>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div>
+          <button type="submit">등록</button>
+          <Link to="/">취소</Link>
+        </div>
+      </form>
+    </div>
   )
 }
 
