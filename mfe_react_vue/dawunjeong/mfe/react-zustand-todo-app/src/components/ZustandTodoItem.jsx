b214a8eec7c3c25e088d1ecaef87d7fe842adc2e
jeongdawun <jeongdawun33@gmail.com>
2023-06-22 16:07:23 +0900

    update: [정다운] Zustand 사용 형태 주석 추가 [ZUSTAND-TODOx]

--- mfe_react_vue/dawunjeong/mfe/react-zustand-todo-app/src/components/ZustandTodoItem.jsx
+++ mfe_react_vue/dawunjeong/mfe/react-zustand-todo-app/src/components/ZustandTodoItem.jsx
@@ -7,6 +7,12 @@ import { FaTrash } from 'react-icons/fa'
 const ZustandTodoItem = ({ props }) => {
   const [editing, setEditing] = useState(false)
 
+  // 사실상 Vue의 ...mapMutations() 혹은 ...mapActions()와 동일하다 볼 수 있음
+  // 보면 actions 요청인지, state 가져오는지, mutation 작업인지는 결구 우리가 잘 판단해야함
+  // 그러므로 Vue 스타일로 이것을 바라본다면 아래와 같이 매서드 명에 구분을 할 수 있도록 만들어주는 수 밖에 없음
+  // 네이밍을 할 때 handleChangeForMutation = useTodoStore((state) => state.handleChangeForMutation)
+  // 이 스타일을 따라간다면 todosForState = useTodoStore((state) => state.todosForState)
+
   const handleChange = useTodoStore((state) => state.handleChange)
   const deleteTodo = useTodoStore((state) => state.deleteTodo)
   const setUpdate = useTodoStore((state) => state.setUpdate)
