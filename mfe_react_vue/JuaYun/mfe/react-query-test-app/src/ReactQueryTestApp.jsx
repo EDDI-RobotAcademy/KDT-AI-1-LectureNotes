70fcf4e9377de11a1b791461f13f36da484e5b3c
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-06-23 10:31:56 +0900

    init: [윤주아] react-query 테스팅 서비스 초기화 [MFE-REACT-QUERYx]

---
+++ mfe_react_vue/JuaYun/mfe/react-query-test-app/src/ReactQueryTestApp.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// npm install react-query --save-dev
+
+import ReactQueryBasicStarter from "./components/ReactQueryBasicStarter";
+
+import "./index.css";
+
+export const ReactQueryTestApp = () => (
+  <div className="container">
+    <div>
+      <ReactQueryBasicStarter/>
+    </div>
+  </div>
+);
+export default ReactQueryTestApp
---
+++ mfe_react_vue/JuaYun/mfe/react-query-test-app/src/components/ReactQueryBasicStarter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReactQueryBasicStarter = () => {
+  return (
+    <div>ReactQueryBasicStarter</div>
+  )
+}
+
+export default ReactQueryBasicStarter
