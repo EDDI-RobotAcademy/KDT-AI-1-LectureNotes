5f83c6e11e965d0ea27abfadc595f95e2ef48421
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-06-23 11:33:50 +0900

    feat: [윤주아] React Query 테스팅 서비스 [REACT-QUERY-TESTx]

--- mfe_react_vue/JuaYun/mfe/react-query-test-app/src/ReactQueryTestApp.jsx
+++ mfe_react_vue/JuaYun/mfe/react-query-test-app/src/ReactQueryTestApp.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // npm install react-query --save-dev
+// npm install axios --save-dev
 
-import ReactQueryBasicStarter from "./components/ReactQueryBasicStarter";
+
+import { ReactQueryBasicStarter } from "./components/ReactQueryBasicStarter";
 
 import "./index.css";
 
