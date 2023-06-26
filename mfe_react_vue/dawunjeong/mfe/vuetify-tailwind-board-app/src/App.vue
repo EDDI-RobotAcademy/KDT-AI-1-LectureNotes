15f2349d810d9370b6bf76d2f838c7be915fef00
jeongdawun <jeongdawun33@gmail.com>
2023-06-26 10:39:43 +0900

    init: [정다운] MFE 기반의 router 구성

--- mfe_react_vue/dawunjeong/mfe/vuetify-tailwind-board-app/src/App.vue
+++ mfe_react_vue/dawunjeong/mfe/vuetify-tailwind-board-app/src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/dist/vuetify.min.css'
@@ -18,7 +17,6 @@ export default defineComponent({
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data () {
---
+++ mfe_react_vue/dawunjeong/mfe/vuetify-tailwind-board-app/src/page/VuetifyBoardList.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>
+        <p>hi</p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="">
+    
+</style>
