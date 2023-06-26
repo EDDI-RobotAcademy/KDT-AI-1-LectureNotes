22a925924e6fd0da70317bf04e28408f4cef72ab
jeongdawun <jeongdawun33@gmail.com>
2023-06-26 10:22:52 +0900

    init: [정다운] MFE 환경에서 TypeScript 기반 Vuetify 초기화

--- mfe_react_vue/dawunjeong/mfe/vuetify-tailwind-board-app/src/index.ts
+++ mfe_react_vue/dawunjeong/mfe/vuetify-tailwind-board-app/src/index.ts
@@ -1,1 +1,8 @@
-import("./bootloader");
+import("./bootstrap");
+
+// 1. vuetify (vue add vuetify)
+// 2. svg-loader
+// 3. vuex (vue add vuex)
+// 4. axios
+// 5. vue-router
+// 6. @types/vue-router
