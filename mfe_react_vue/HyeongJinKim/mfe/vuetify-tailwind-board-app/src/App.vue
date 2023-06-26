3c0dffdeeefa01f8bb29f637e1d7b65947a261e8
bbbii <hyeongjin1326@gmail.com>
2023-06-26 10:24:48 +0900

    init: [김형진] MFE 환경에서 TypeScript 기반 Vuetify 초기화

--- mfe_react_vue/HyeongJinKim/mfe/vuetify-tailwind-board-app/src/App.vue
+++ mfe_react_vue/HyeongJinKim/mfe/vuetify-tailwind-board-app/src/App.vue
@@ -1,26 +1,30 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <HelloWorld />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/dist/vuetify.min.css";
+import "vuetify/styles";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
 
   components: {
     HelloWorld,
   },
 
-  data () {
+  data() {
     return {
       //
-    }
+    };
   },
-})
+});
 </script>
--- mfe_react_vue/HyeongJinKim/mfe/vuetify-tailwind-board-app/src/main.js
+++ mfe_react_vue/HyeongJinKim/mfe/vuetify-tailwind-board-app/src/main.js
@@ -1,10 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
+import { createApp } from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
+import store from "./store";
 
-loadFonts()
+loadFonts();
 
-createApp(App)
-  .use(vuetify)
-  .mount('#app')
+createApp(App).use(store).use(vuetify).mount("#app");
