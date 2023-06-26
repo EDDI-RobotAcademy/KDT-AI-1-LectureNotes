983b653862e45ca1ab08ea5c7927043ded62d8d0
wlsdbwls <wlsdbwls614@gmail.com>
2023-06-26 10:22:19 +0900

    init: [진유진] MFE 환경에서 TypeScript 기반 Vuetify 초기화

--- mfe_react_vue/Jinyujin/mfe/vuetify-tailwind-board-app/src/App.vue
+++ mfe_react_vue/Jinyujin/mfe/vuetify-tailwind-board-app/src/App.vue
@@ -10,6 +10,12 @@
 import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 
+// 여기서 import 해줘야 ui 잘 나올 것
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/dist/vuetify.min.css'
+// 이 둘을 import 해줬더니 가운데 정렬이 됨
+// 아래꺼가 가운데 정렬 역할 해주는 것 같음
+
 export default defineComponent({
   name: 'App',
 
