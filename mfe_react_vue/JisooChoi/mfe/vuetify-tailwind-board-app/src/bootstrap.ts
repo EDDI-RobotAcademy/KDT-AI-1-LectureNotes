7d5c5501745fb70f8963c8073f7537c47c4f5cf6
zisoo <zisoo_3@naver.com>
2023-06-26 12:29:07 +0900

    update: [최지수] loadFonts 누락분 추가 [MFE-TS-BOARD-LISTx]

--- mfe_react_vue/JisooChoi/mfe/vuetify-tailwind-board-app/src/bootstrap.ts
+++ mfe_react_vue/JisooChoi/mfe/vuetify-tailwind-board-app/src/bootstrap.ts
@@ -18,18 +18,19 @@ import boardModule from './store/index'
 import router from './router'
 
 const vuetifyTaildBoardAppMount = (el: string | Element) => {
-    const vuetify = createVuetify({
-        components: {
-            ...components,
-            ...labsCompoenents,
-        },
-        directives: {
-            ...directives,
-        }
+    loadFonts().then(() => {
+        const vuetify = createVuetify({
+            components: {
+                ...components,
+                ...labsCompoenents,
+            },
+            directives: {
+                ...directives,
+            }
+        })
+        const app = createApp(App).use(vuetify).use(boardModule).use(router)
+        app.mount(el)
     })
-
-    const app = createApp(App).use(vuetify).use(boardModule).use(router)
-    app.mount(el)
 };
 
 const root = document.querySelector('#vuetify-tailwind-board-app')
