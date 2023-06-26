39824374f2b6e345d21a8421eef917db134b0fb7
Hakgyu Kim <zage4448@gmail.com>
2023-06-26 12:29:56 +0900

    feat: [김학규] MFE 기반 TS Board List [MFE-TS-BOARD-LISTx]

--- Spring/HakgyuKim/demo/src/main/java/com/example/demo/config/CorsConfig.java
+++ Spring/HakgyuKim/demo/src/main/java/com/example/demo/config/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig implements WebMvcConfigurer {
                                 "http://localhost:8080",
                                 "http://localhost:3000",
                                 "http://localhost:3001",
-                                "http://localhost:3004")
+                                "http://localhost:3004",
+                                "http://localhost:3007")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
