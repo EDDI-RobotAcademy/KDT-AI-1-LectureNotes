2766baeeacb1627153479a8c6940e35814ed2bcb
Sanghoon Lee <gcccompil3r@gmail.com>
2023-06-26 15:39:48 +0900

    feat: [이상훈] MFE 기반 게시물 수정 [MFE-TS-BOARD-MODIFYx]

--- mfe_react_vue/SanghoonLee/mfe/vuetify-tailwind-board-app/src/store/board/actions.ts
+++ mfe_react_vue/SanghoonLee/mfe/vuetify-tailwind-board-app/src/store/board/actions.ts
@@ -9,12 +9,15 @@ import { AxiosResponse } from 'axios'
 import { async } from '../../../../vue-navigation-app/src/plugin/webfontloader';
 
 export type BoardActions = {
-    requestBoardToSpring(context: ActionContext<BoardState, any>, boardId: number): void
-    requestBoardListToSpring(context: ActionContext<BoardState, any>): void
+    requestBoardToSpring(context: ActionContext<BoardState, any>, boardId: number): Promise<void>
+    requestBoardListToSpring(context: ActionContext<BoardState, any>): Promise<void>
     requestCreateBoardToSpring(context: ActionContext<BoardState, unknown>, payload: {
         title: string, content: string, writer: string
     }): Promise<AxiosResponse>
     requestDeleteBoardToSpring(context: ActionContext<BoardState, any>, boardId: number): Promise<void>
+    requestModifyBoardToSpring(context: ActionContext<BoardState, any>, payload: {
+        title: string, content: string, writer: string, boardId: number
+    }): Promise<void>
 }
 
 const actions: BoardActions = {
@@ -57,6 +60,20 @@ const actions: BoardActions = {
             alert('삭제 성공!')
         } catch (error) {
             alert('requestDeleteBoardToSpring() 문제 발생')
+            throw error
+        }
+    },
+    async requestModifyBoardToSpring(context: ActionContext<BoardState, any>, payload: {
+        title: string, content: string, writer: string, boardId: number
+    }): Promise<void> {
+
+        const { title, content, writer, boardId } = payload
+
+        try {
+            await axiosInst.springAxiosInst.put(`/jpa-board/${boardId}`, { title, content, writer })
+            alert('수정 성공!')
+        } catch (error) {
+            alert('requestModifyBoardToSpring() 문제 발생')
             throw error
         }
     }
