faea5a55220e68c1ddad49edb2c917962b6f0e96
jeongdawun <jeongdawun33@gmail.com>
2023-06-26 12:11:02 +0900

    feat: [정다운] TypeScript Vuetify 기반 Board List [MFE-TS-BOARD-LISTx]

--- Spring/dawunjeong/demo/src/main/java/com/example/demo/config/CorsConfig.java
+++ Spring/dawunjeong/demo/src/main/java/com/example/demo/config/CorsConfig.java
@@ -15,8 +15,11 @@ public class CorsConfig implements WebMvcConfigurer {
                         "http://localhost:8080",
                         "http://localhost:3000",
                         "http://localhost:3001",
+                        "http://localhost:3002",
+                        "http://localhost:3003",
                         "http://localhost:3004",
-                        "http://localhost:3005")
+                        "http://localhost:3005",
+                        "http://localhost:3007")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
