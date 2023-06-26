6b6ec044a80f5ed8962bf7aae554c78331d7eec9
YeoulCho <cyu5733@gmail.com>
2023-06-26 15:41:32 +0900

    feat: [조여울] MFE 기반 게시물 수정 [MFE-TS-BOARD-MODIFYx]

--- mfe_react_vue/YeoulCho/mfe/vuetify-tailwind-board-app/src/page/VuetifyBoardModify.vue
+++ mfe_react_vue/YeoulCho/mfe/vuetify-tailwind-board-app/src/page/VuetifyBoardModify.vue
@@ -1,65 +1,94 @@
 <template lang="">
     <v-container>
-        <v-row>
-            <v-col cols="12">
-                <v-text-field
-                    v-model="title" label="제목"/>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col cols="12">
-                <v-text-field
-                    v-model="writer" label="작성자"/>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col cols="12">
-                <v-textarea
-                    v-model="content" label="내용" auto-grow/>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col cols="12" class="text-right">
-                <v-btn class="ml-2" color="primary" @click="onSubmit">수정완료</v-btn>
-                <v-btn class="ml-2" color="error" @click="onCancle">취소</v-btn>
-            </v-col>
-        </v-row>
+      <h2>Vue3 + TypeScript + Vuetify3 + Spring + JPA</h2>
+      <v-card v-if="board">
+        <v-card-title>게시물 수정</v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field v-model="title" label="제목"/>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field v-model="board.writer" readonly label="작성자"/>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field v-model="board.createDate" readonly label="등록일자"/>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <v-textarea v-model="content" label="내용"/>
+              </v-col>
+            </v-row>
+            <v-row justify="end">
+              <v-col cols="auto">
+                <v-btn color="primary" @click="onModify">수정완료</v-btn>
+              </v-col>
+              <v-col cols="auto">
+                <router-link :to="{ name: 'VuetifyBoardList' }">
+                  <v-btn color="secondary">돌아가기</v-btn>
+                </router-link>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+      </v-card>
     </v-container>
-</template>
-<script>
-const boardModule = 'boardModule'
-import {mapActions} from 'vuex'
-
-export default {
-    data(){
-        return{
-            title: '',
-            writer: '',
-            content: ''
-        }
-    },
-    methods:{
-        ...mapActions(boardModule, ['requestCreateBoardToSpring']),
-        async onSubmit(){
-            const payload={
-                title: this.title,
-                writer: this.writer,
-                content: this.content
-            }
-
-            const board = await this.requestCreateBoardToSpring(payload)
-            await this.$router.push({
-                name: 'VuetifyBoardRead',
-                params: {boardId: board.boardId.toString()}
-            })
-        },
-        onCancle(){
-            // 입력한 숫자에 따라 뒤로가기 2번, 3번 등등의 역할을 수행함
-            this.$router.go(-1)
-        }
-    }
-}
-</script>
-<style lang="">
-    
-</style>
+  </template>
+  
+  <script>
+  import { mapActions, mapState } from 'vuex';
+  
+  const boardModule = 'boardModule'
+  
+  export default {
+      props: {
+          boardId: {
+              type: String,
+              required: true,
+          },
+      },
+      data () {
+          return {
+              title: '',
+              content: '',
+              writer: '',
+          }
+      },
+      computed: {
+          ...mapState(boardModule, ['board'])
+      },
+      methods: {
+          ...mapActions(boardModule, ['requestBoardToSpring', 'requestModifyBoardToSpring']),
+          async onModify () {
+              const payload = {
+                  title: this.title,
+                  writer: this.writer,
+                  content: this.content,
+                  boardId: this.boardId
+              }
+  
+              await this.requestModifyBoardToSpring(payload)
+              await this.$router.push({
+                  name: 'VuetifyBoardRead',
+                  params: { boardId: this.boardId }
+              })
+          }
+      },
+      created () {
+          this.requestBoardToSpring(this.boardId).then(() => {
+              this.title = this.board.title
+              this.writer = this.board.writer
+              this.content = this.board.content
+          })
+      }
+  }
+  </script>
+  <style lang="">
+      
+  </style>
