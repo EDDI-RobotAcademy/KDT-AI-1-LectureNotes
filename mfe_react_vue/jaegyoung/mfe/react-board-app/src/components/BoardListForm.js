bbb96fede1ec7dfd00a71c35ed4872ae9fda4f9e
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-06-28 17:03:52 +0900

    feat: [김재경] mfe react-board-app [MFE]

---
+++ mfe_react_vue/jaegyoung/mfe/react-board-app/src/components/BoardListForm.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Link } from 'react-router-dom' // imrr
+
+const BoardListForm = ({ boards, isLoading }) => {
+  return (
+    <div>
+      <h2>React로 만든 게시판</h2>
+      { isLoading && "로딩중 ..........." }
+      { !isLoading && boards && (
+        <>
+          <Link to="/register">글쓰기</Link>
+          <table border="1">
+            <thead>
+              <tr>
+                <th align='center' width="80">
+                  번호
+                </th>
+                <th align='center' width="320">
+                  제목
+                </th>
+                <th align='center' width="100">
+                  작성자
+                </th>
+                <th align='center' width="180">
+                  등록일자
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              { !boards.length ? (
+                <tr>
+                  <td colSpan={4}>
+                    등록된 게시물이 존재하지 않습니다!
+                  </td>
+                </tr>
+              ) : ( boards.map((board) => (
+                <tr key={board.boardId}>
+                  <td align="center">{ board.boardId }</td>
+                  <td align="left">
+                    <Link to={`/react-board-app/read/${board.boardId}`}>{ board.title }</Link>
+                  </td>
+                  <td align="right">{ board.writer }</td>
+                  <td align="center">{ board.createDate }</td>
+                </tr>
+              )))}
+            </tbody>
+          </table>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default BoardListForm
