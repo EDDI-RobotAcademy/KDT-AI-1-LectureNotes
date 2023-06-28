589b4d26d276948c525e56eb2c31584ffd4c6d9c
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-06-28 17:08:30 +0900

    feat: [김재경] CorsConfig 설정 변경 [SPRING-CORS]

--- Spring/JaekyoungKim/demo/src/main/java/kr/eddi/demo/config/CorsConfig.java
+++ Spring/JaekyoungKim/demo/src/main/java/kr/eddi/demo/config/CorsConfig.java
@@ -10,8 +10,18 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:8080")    // vue는 허용하겠다는 소리
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins(
+                        "http://127.0.0.1:8080",
+                        "http://localhost:8080",
+                        "http://192.168.20.3:8080",
+                        "http://localhost:3000",
+                        "http://localhost:3001",
+                        "http://localhost:3004",
+                        "http://localhost:3007",
+                        "http://localhost:3010",
+                        "http://localhost:3012")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
+
+
     }
 }
