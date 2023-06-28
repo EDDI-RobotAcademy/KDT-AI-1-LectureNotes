c5292a6d2c6c38d2bea088e57c7a022f135c733b
YeoulCho <cyu5733@gmail.com>
2023-06-28 12:29:23 +0900

    feat: [조여울] MFE에 React Counter 붙이기 [MFE-REACT-COUNTERx]

--- mfe_react_vue/YeoulCho/mfe/container-app/src/router/MainRouter.jsx
+++ mfe_react_vue/YeoulCho/mfe/container-app/src/router/MainRouter.jsx
@@ -7,6 +7,7 @@ import { navigationMount } from 'vueNavigationApp/VueNavigation';
 import VuetifyTypeScriptBoardAppRouter from './vuetifyTypeScriptBoardApp/vuetifyTypeScriptBoardAppRouter';
 import VueModuleAppRouter from './vueModuleApp/VueModuleAppRouter';
 import Home from './containerApp/Home';
+import ReactCounterAppRouter from './reactModuleApp/ReactCounterAppRouter';
 
 const MainRouter = () => {
   const vueModuleRef = useRef(null)
@@ -62,6 +63,9 @@ const MainRouter = () => {
                 <Button component={Link} to="/vue-module-app" variant="contained">
                   Vue 게시판
                 </Button>
+                <Button component={Link} to="/react-counter-app" variant="contained">
+                  React 카운터
+                </Button>
               </div>
             </div>
             <Routes>
@@ -83,6 +87,12 @@ const MainRouter = () => {
                     vueModuleRef={vueModuleRef}
                   />
                 }/>
+              <Route
+                exact path="/react-counter-app"
+                element={
+                  <ReactCounterAppRouter/>
+                }/>
+
             </Routes>
           </BrowserRouter>
         </Suspense>
