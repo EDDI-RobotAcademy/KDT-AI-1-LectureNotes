1384714a57ae8e28ce17db70de4fe86ca3d8649d
Sanghoon Lee <gcccompil3r@gmail.com>
2023-06-28 10:34:33 +0900

    refactor: [이상훈] Main Router 구성을 위한 대규모 리팩토링 [REFACTOR-CONTAINER-ROUTERx]

---
+++ mfe_react_vue/SanghoonLee/mfe/container-app/src/router/MainRouter.jsx
@@ -0,0 +1,60 @@
+import React, { Suspense, useEffect, useRef, useState } from 'react'
+import { BrowserRouter, Link } from 'react-router-dom';
+
+import { Button } from '@mui/material'
+
+import { navigationMount } from 'vueNavigationApp/VueNavigation';
+
+const MainRouter = () => {
+  const vuetifyNavigationRef = useRef(null)
+
+  const [naviHeight, setNaviHeight] = useState(0)
+  const buttonRef = useRef(null)
+
+  useEffect(() => {
+    navigationMount(vuetifyNavigationRef.current)
+
+    const updateNaviHeight = () => {
+        const height = vuetifyNavigationRef.current.offsetHeight
+        setNaviHeight(height)
+    }
+
+    const observer = new MutationObserver(updateNaviHeight)
+    observer.observe(vuetifyNavigationRef.current, { attributes: true, childList: true, subtree: true })
+
+    return () => {
+        observer.disconnect()
+    }
+  }, [])
+
+  useEffect(() => {
+    console.log('vuetifyNavigationRef의 높이값: ' + naviHeight)
+  }, [naviHeight])
+
+  useEffect(() => {
+    if (buttonRef.current) {
+        const buttonElement = buttonRef.current
+        const computedStyle = getComputedStyle(buttonElement)
+        const height = computedStyle.height
+
+        console.log('버튼의 높이값: ' + height)
+    }
+  }, [])
+
+  return (
+    <div>
+        <div ref={vuetifyNavigationRef} />
+        <Suspense fallback={<div>로딩중 .........</div>}>
+          <BrowserRouter>
+            <div>
+              <div style={{ position: 'relative' }}>
+                <Button ref={buttonRef} component={Link} to="/" variant="contained">홈</Button>
+              </div>
+            </div>
+          </BrowserRouter>
+        </Suspense>
+    </div>
+  )
+}
+
+export default MainRouter
