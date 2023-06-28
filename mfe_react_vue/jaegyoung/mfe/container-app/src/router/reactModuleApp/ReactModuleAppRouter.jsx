7bd04469374529cbdd14902fee58d7efab41e459
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-06-28 17:02:52 +0900

    feat: [김재경] mfe container-app [MFE]

---
+++ mfe_react_vue/jaegyoung/mfe/container-app/src/router/reactModuleApp/ReactModuleAppRouter.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Counter from 'reactModuleApp/Counter'
+
+const ReactModuleAppRouter = () => {
+  return (
+    <div><Counter/></div>
+  )
+}
+
+export default ReactModuleAppRouter
