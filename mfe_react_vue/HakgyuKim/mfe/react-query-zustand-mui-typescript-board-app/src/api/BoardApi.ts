e6cd831713064db2c8e445bcdecddaf00d37c686
Hakgyu Kim <zage4448@gmail.com>
2023-06-29 10:51:17 +0900

    feat: [김학규] Zustand + React-Query 붙이기 [REACT-QUERY-ZUSTANDx]

---
+++ mfe_react_vue/HakgyuKim/mfe/react-query-zustand-mui-typescript-board-app/src/api/BoardApi.ts
@@ -0,0 +1,21 @@
+import { UseQueryResult, useQuery } from 'react-query'
+import axiosInst from '../utility/axiosInstance'
+import { Board } from '../entity/Board'
+import useBoardStore from '../store/BoardStore'
+
+export const fetchBoardList = async (): Promise<Board[]> => {
+  const response = await axiosInst.springAxiosInst.get<Board[]>('/jpa-board/list')
+  return response.data
+}
+
+export const useBoardListQuery = (): UseQueryResult<Board[], unknown> => {
+  const setBoards = useBoardStore((state) => state.setBoards)
+
+  const queryResult: UseQueryResult<Board[], unknown> = useQuery('boardList', fetchBoardList, {
+    onSuccess: (data) => {
+      setBoards(data)
+    }
+  })
+
+  return queryResult
+}
---
+++ mfe_react_vue/HakgyuKim/mfe/react-query-zustand-mui-typescript-board-app/src/entity/Board.ts
@@ -0,0 +1,8 @@
+export interface Board {
+    baordId: number;
+    title: string;
+    writer: string;
+    content: string;
+    createDate: string;
+    updateDate: string;
+}
