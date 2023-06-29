b181797519ac67908010c56aa94712788f0acdf5
bbbii <hyeongjin1326@gmail.com>
2023-06-29 12:39:18 +0900

    feat: [김형진] React 복합 게시판에서 읽기 구현 [REACT-TSC-READx]

--- mfe_react_vue/HyeongJinKim/mfe/react-query-zustand-mui-typescript-board-app/src/api/BoardApi.ts
+++ mfe_react_vue/HyeongJinKim/mfe/react-query-zustand-mui-typescript-board-app/src/api/BoardApi.ts
@@ -10,13 +10,11 @@ export const fetchBoardList = async (): Promise<Board[]> => {
 
 export const useBoardListQuery = (): UseQueryResult<Board[], unknown> => {
   const setBoards = useBoardStore((state) => state.setBoards);
-
   const queryResult: UseQueryResult<Board[], unknown> = useQuery("boardList", fetchBoardList, {
     onSuccess: (data) => {
       setBoards(data);
     },
   });
-
   return queryResult;
 };
 
@@ -26,6 +24,14 @@ export const registerBoard = async (data: {
   content: string;
 }): Promise<Board> => {
   const response = await axiosInst.springAxiosInst.post<Board>("/jpa-board/register", data);
-
   return response.data;
+};
+
+export const fetchBoard = async (boardId: string): Promise<Board | null> => {
+  const response = await axiosInst.springAxiosInst.get<Board>(`/jpa-board/${boardId}`);
+  return response.data;
+};
+
+export const useBoardQuery = (boardId: string): UseQueryResult<Board | null, unknown> => {
+  return useQuery(["board", boardId], () => fetchBoard(boardId));
 };
