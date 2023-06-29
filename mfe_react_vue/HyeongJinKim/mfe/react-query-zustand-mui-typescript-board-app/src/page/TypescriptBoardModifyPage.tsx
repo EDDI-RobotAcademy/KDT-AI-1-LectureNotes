0e8ee85e297bd07940c4fbc6f24ce6605601e291
bbbii <hyeongjin1326@gmail.com>
2023-06-29 14:16:59 +0900

    feat: [김형진] React 복합 게시판 수정 UI 구현 [REACT-TSC-MODIFY-UIx]

--- mfe_react_vue/HyeongJinKim/mfe/react-query-zustand-mui-typescript-board-app/src/page/TypescriptBoardModifyPage.tsx
+++ mfe_react_vue/HyeongJinKim/mfe/react-query-zustand-mui-typescript-board-app/src/page/TypescriptBoardModifyPage.tsx
@@ -1,41 +1,32 @@
+import React, { useState } from "react";
+
 import { Box, Button, Container, TextField } from "@mui/material";
-import React, { useEffect } from "react";
-import { useQueryClient } from "react-query";
 import { useNavigate, useParams } from "react-router-dom";
-import { fetchBoard, useBoardQuery } from "../api/BoardApi";
+import { useQueryClient } from "react-query";
+import { useBoardQuery } from "../api/BoardApi";
 
 interface RouteParams {
   boardId: string;
   [key: string]: string;
 }
 
-const TypescriptBoardReadPage = () => {
+const TypescriptBoardModifyPage = () => {
   const navigate = useNavigate();
   const { boardId } = useParams<RouteParams>();
   const queryClient = useQueryClient();
 
   const { data: board, isLoading, isError } = useBoardQuery(boardId || "");
+  //   const mutation = useBoardUpdateMutation();
 
-  useEffect(() => {
-    const fetchBoardData = async () => {
-      const data = await fetchBoard(boardId || "");
-      console.log(data);
-    };
+  const [title, setTitle] = useState(board?.title || "");
+  const [content, setContent] = useState(board?.content || "");
 
-    fetchBoardData();
-  });
-
-  const handleEditClick = () => {
-    navigate(`/react-query-zustand-mui-typescript-board-app/modify/${boardId}`);
-  };
-
-  const handleDeleteClick = () => {
-    console.log("삭제");
+  const handleEditFinishClick = () => {
+    // navigate(`/react-query-zustand-mui-typescript-board-app/modify/${boardId}`);
   };
 
   const handleCancelClick = () => {
-    queryClient.invalidateQueries("boardList");
-    navigate("/react-query-zustand-mui-typescript-board-app");
+    navigate(`/react-query-zustand-mui-typescript-board-app/read/${boardId}`);
   };
 
   return (
@@ -44,7 +35,6 @@ const TypescriptBoardReadPage = () => {
         <TextField
           label="제목"
           name="title"
-          disabled
           value={board?.title || ""}
           sx={{ borderRadius: "4px" }}
         />
@@ -56,27 +46,30 @@ const TypescriptBoardReadPage = () => {
           sx={{ borderRadius: "4px" }}
         />
         <TextField
+          label="작성일자"
+          name="createDate"
+          disabled
+          value={board?.createDate || ""}
+          sx={{ borderRadius: "4px" }}
+        />
+        <TextField
           label="내용"
           name="content"
           multiline
           value={board?.content || ""}
-          disabled
           minRows={20}
           maxRows={20}
           sx={{ borderRadius: "4px" }}
         />
-        <Button variant="outlined" onClick={handleEditClick}>
-          수정
-        </Button>
-        <Button variant="outlined" onClick={handleDeleteClick}>
-          삭제
+        <Button variant="outlined" onClick={handleEditFinishClick}>
+          수정 완료
         </Button>
         <Button variant="outlined" onClick={handleCancelClick}>
-          돌아가기
+          취소
         </Button>
       </Box>
     </Container>
   );
 };
 
-export default TypescriptBoardReadPage;
+export default TypescriptBoardModifyPage;
