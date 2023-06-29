68c37dbd2299f8edbee9aee3d895fe2f49a3d250
YeoulCho <cyu5733@gmail.com>
2023-06-29 09:55:23 +0900

    init: [조여울] React TS Complex Composition Board App

---
+++ mfe_react_vue/YeoulCho/mfe/react-query-zustand-mui-typescript-board-app/src/ReactQueryZustandMuiTypescriptBoardApp.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MainRouter from "./router/MainRouter";
+import "./index.scss";
+
+const ReactQueryZustandMuiTypescriptBoardApp: React.FC = () => {
+  return (
+    <MainRouter/>
+  )
+}
+
+export default ReactQueryZustandMuiTypescriptBoardApp
---
+++ mfe_react_vue/YeoulCho/mfe/react-query-zustand-mui-typescript-board-app/src/bootstrap.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import ReactQueryZustandMuiTypescriptBoardApp from './ReactQueryZustandMuiTypescriptBoardApp'
+import { BrowserRouter } from 'react-router-dom'
+import {QueryClient, QueryClientProvider} from "react-query"
+
+const queryClient = new QueryClient({
+    defaultOptions:{
+        queries: {
+            refetchOnWindowFocus: false,
+            retry: false,
+            staleTime: 30000,
+        }
+    }
+})
+
+ReactDOM.render(
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <ReactQueryZustandMuiTypescriptBoardApp/>
+            </BrowserRouter>,
+        </QueryClientProvider>
+    </React.StrictMode>,    
+    document.getElementById("react-query-zustand-mui-typescript-board-app")
+);
+
