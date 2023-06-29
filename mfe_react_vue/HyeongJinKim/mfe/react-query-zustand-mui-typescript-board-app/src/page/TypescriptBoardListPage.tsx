3a20f3c77f12ab8b848b7204a47dc7acbc8d1bc3
bbbii <hyeongjin1326@gmail.com>
2023-06-29 10:27:41 +0900

    feat: [김형진] React 복합 구성 게시판 리스트 UI [REACT-TSC-LISTx]

--- mfe_react_vue/HyeongJinKim/mfe/react-query-zustand-mui-typescript-board-app/src/page/TypescriptBoardListPage.tsx
+++ mfe_react_vue/HyeongJinKim/mfe/react-query-zustand-mui-typescript-board-app/src/page/TypescriptBoardListPage.tsx
@@ -1,7 +1,56 @@
 import React from "react";
+import {
+  Button,
+  Container,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { Link } from "react-router-dom";
 
 const TypescriptBoardListPage = () => {
-  return <div>TypescriptBoardListPage</div>;
+  const boards = [];
+
+  return (
+    <Container maxWidth="lg">
+      <h2>React-Query + Zustand + Typescript Board App</h2>
+      <Button
+        component={Link}
+        to="/react-query-zustand-mui-typescript-board-app/register"
+        variant="contained"
+        color="primary"
+        style={{ marginTop: "20px" }}
+      >
+        글쓰기
+      </Button>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="board table">
+          <TableHead>
+            <TableRow>
+              <TableCell style={{ width: "50%" }}>제목</TableCell>
+              <TableCell align="right">작성자</TableCell>
+              <TableCell align="right">작성일자</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {boards.length === 0 ? (
+              <TableRow>
+                <TableCell colSpan={3} align="center">
+                  등록된 게시물이 없습니다
+                </TableCell>
+              </TableRow>
+            ) : (
+              console.log("데이터 있음")
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
 };
 
 export default TypescriptBoardListPage;
