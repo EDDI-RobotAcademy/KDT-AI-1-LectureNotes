da416e546ad0cbec034e92c6ca7808d9cd8b9421
wlsdbwls <wlsdbwls614@gmail.com>
2023-06-30 09:19:54 +0900

    feat: [진유진] React 복합 구성 게시판 등록하기 [REACT-TSC-REGISTERx]

--- mfe_react_vue/Jinyujin/mfe/react-query-zustand-mui-typescript-board-app/src/router/MainRouter.tsx
+++ mfe_react_vue/Jinyujin/mfe/react-query-zustand-mui-typescript-board-app/src/router/MainRouter.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import TypescriptBoardListPage from '../page/TypescriptBoardListPage'
+import TypescriptBoardRegisterPage from '../page/TypescriptBoardRegisterPage'
 
 const MainRouter = () => {
     return (
@@ -9,6 +10,7 @@ const MainRouter = () => {
                 <Routes>
                     <Route path="/" element={<Navigate to="/react-query-zustand-mui-typescript-board-app" replace />} />
                     <Route path="/react-query-zustand-mui-typescript-board-app" element={<TypescriptBoardListPage />} />
+                    <Route path="/react-query-zustand-mui-typescript-board-app/register" element={<TypescriptBoardRegisterPage />} />
                 </Routes>
             </Suspense>
         </>
