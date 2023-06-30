a39739a496314edcc38660e30829a03fbfd18399
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-06-30 11:11:50 +0900

    feat: [김진호] MFE React Container에 Vue remotes Router List 붙이기[MFE-VUE-REGISTERx]

--- mfe_react_vue/JinHoKim/mfe/container-app/src/router/MainRouter.jsx
+++ mfe_react_vue/JinHoKim/mfe/container-app/src/router/MainRouter.jsx
@@ -9,6 +9,8 @@ import VueModuleAppRouter from './vueModuleApp/VueModuleAppRouter';
 import Home from './containerApp/Home';
 import ReactCounterAppRouter from './reactModuleApp/ReactCounterAppRouter';
 import ReactBoardRoutes from './reactBoardApp/ReactBoardRoutes';
+import ReactTypescriptMuiBoardRoutes from './muiTypescriptBoardApp/ReactTypescriptMuiBoardRoutes';
+import VueModuleAppRegisterRouter from './vueModuleApp/VueModuleAppRegisterRouter';
 
 const MainRouter = () => {
   const vueModuleRef = useRef(null)
@@ -70,6 +72,9 @@ const MainRouter = () => {
                 <Button component={Link} to="/react-board-app" variant="contained">
                   React 게시판
                 </Button>
+                <Button component={Link} to="/react-query-zustand-mui-typescript-board-app" variant="contained">
+                  Mui Typescript 게시판
+                </Button>
               </div>
             </div>
             <Routes>
@@ -98,6 +103,14 @@ const MainRouter = () => {
                 }/>
             </Routes>
             <ReactBoardRoutes/>
+            <ReactTypescriptMuiBoardRoutes/>
+            <Routes>
+              <Route
+                exact path="/vue-module-app/board-register-page"
+                element={
+                  <VueModuleAppRegisterRouter/>
+                }/>
+            </Routes>
           </BrowserRouter>
         </Suspense>
     </div>
