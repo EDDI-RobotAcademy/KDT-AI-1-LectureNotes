db45138beb6baaef0092b86f03c99fc07c125554
Sanghoon Lee <gcccompil3r@gmail.com>
2023-07-04 17:36:21 +0900

    feat: MFE에 인증앱 추가

--- mfe_react_vue/SanghoonLee/mfe-test/vue-auth-app/src/App.vue
+++ mfe_react_vue/SanghoonLee/mfe-test/vue-auth-app/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <v-app>
     <v-main>
-      <GithubSignInPage/>
+      <!-- <GithubSignInPage/> -->
+      <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/dist/vuetify.min.css'
+import 'vuetify/styles'
+
 import GithubSignInPage from './page/github/GithubSignInPage.vue';
 
 export default {
--- mfe_react_vue/SanghoonLee/mfe-test/vue-auth-app/vue.config.js
+++ mfe_react_vue/SanghoonLee/mfe-test/vue-auth-app/vue.config.js
@@ -4,5 +4,20 @@ module.exports = defineConfig({
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
-  }
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule('svg')
+      .exclude.add(path.resolve(__dirname, 'src/assets'))
+      .end();
+
+    config.module
+      .rule('raw-svg')
+      .test(/\.svg$/)
+      .include.add(path.resolve(__dirname, 'src/assets'))
+      .end()
+      .use('raw-loader')
+      .loader('raw-loader')
+      .end();
+  },
 })
