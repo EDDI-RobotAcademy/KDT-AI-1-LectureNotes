a90bbadd6a5baeb09a39eead57e70ae47fe04b22
Sanghoon Lee <gcccompil3r@gmail.com>
2023-07-05 11:09:32 +0900

    feat: Navigation App에 Event Bus 등록

--- mfe_react_vue/SanghoonLee/mfe-test/vue-navigation-app/src/bootstrap.js
+++ mfe_react_vue/SanghoonLee/mfe-test/vue-navigation-app/src/bootstrap.js
@@ -1,4 +1,4 @@
-import { createApp } from "vue";
+import { createApp, h } from "vue";
 
 import "../public/index.scss";
 
@@ -10,7 +10,9 @@ import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import { createVuetify } from "vuetify";
 
-const navigationMount = (el) => {
+let app = null
+
+const navigationMount = (el, eventBus) => {
 
     loadFonts()
     
@@ -18,12 +20,47 @@ const navigationMount = (el) => {
         components, directives
     })
 
-    const app = createApp(App).use(vuetify)
+    app = createApp({
+        render: () => h(App, { eventBus })
+    })
+
+    app.use(vuetify)
+    app.provide('eventBus', eventBus);
     app.mount(el)
 };
 
 const root = document.querySelector('#vue-navigation')
 
-if (root) { navigationMount(root) }
+const eventBus = {
+    listeners: {},
+
+    on(eventName, callback) {
+        if (!this.listeners[eventName]) {
+            this.listeners[eventName] = [];
+        }
+        this.listeners[eventName].push(callback);
+    },
+
+    off(eventName, callback) {
+        if (!this.listeners[eventName]) {
+            return;
+        }
+        const index = this.listeners[eventName].indexOf(callback);
+        if (index !== -1) {
+            this.listeners[eventName].splice(index, 1);
+        }
+    },
+
+    emit(eventName, data) {
+        if (!this.listeners[eventName]) {
+            return;
+        }
+        this.listeners[eventName].forEach((callback) => {
+            callback(data);
+        });
+    },
+};
+
+if (root) { navigationMount(root, eventBus) }
 
 export { navigationMount }
