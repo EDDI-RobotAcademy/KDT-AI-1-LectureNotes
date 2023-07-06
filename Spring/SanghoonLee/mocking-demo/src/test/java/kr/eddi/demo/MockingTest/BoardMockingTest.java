134c3c93bf31359c619e7f453a1d3419964f5bfa
Sanghoon Lee <gcccompil3r@gmail.com>
2023-07-06 15:28:05 +0900

    feat: [이상훈] 게시물 리스트 보기 Mocking 완료 [CI-MOCK-TESTx]

--- Spring/SanghoonLee/mocking-demo/src/test/java/kr/eddi/demo/MockingTest/BoardMockingTest.java
+++ Spring/SanghoonLee/mocking-demo/src/test/java/kr/eddi/demo/MockingTest/BoardMockingTest.java
@@ -12,7 +12,11 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -38,19 +42,35 @@ public class BoardMockingTest {
     @Test
     @DisplayName("Mocking: 게시물 작성 테스트")
     public void 게시물을_작성합니다 () {
+        // 실제 요청될 데이터
         final RequestBoardForm boardForm = new RequestBoardForm(
                 "제목","내용", "작성자"
         );
         final JpaBoard board = boardForm.toJpaBoard();
 
+        // 가상의 예측되는 결과
         when(mockBoardRepository.save(board))
                 .thenReturn(new JpaBoard("제목", "작성자", "내용"));
 
+        // 실제 구동 테스트
         final JpaBoardServiceImpl sut = new JpaBoardServiceImpl(mockBoardRepository);
         final JpaBoard actual = sut.register(board);
 
+        // 예측 결과와 실제 데이터의 비교
         assertEquals(actual.getTitle(), board.getTitle());
         assertEquals(actual.getWriter(), board.getWriter());
         assertEquals(actual.getContent(), board.getContent());
+    }
+
+    @Test
+    @DisplayName("Mocking: 게시물 리스트 보기 테스트")
+    public void 게시물을_리스트_보기 () {
+        when(mockBoardRepository.findAll())
+                .thenReturn(Collections.emptyList());
+
+        final JpaBoardServiceImpl sut = new JpaBoardServiceImpl(mockBoardRepository);
+        final List<JpaBoard> actual = sut.list();
+
+        assertTrue(actual.isEmpty());
     }
 }
